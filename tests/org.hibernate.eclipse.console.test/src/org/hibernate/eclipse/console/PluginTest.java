eee4e98da6859424bb250c3c764a37ffeedaa878
Max Rydahl Andersen <max@hibernate.org>
2004-12-09 14:01:23 +0000

    skeleton for PDE testing
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@5 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ tests/org.hibernate.eclipse.console.test/src/org/hibernate/eclipse/console/PluginTest.java
@@ -0,0 +1,18 @@
+/*
+ * Created on 03-Nov-2004 by max
+ * 
+ */
+package org.hibernate.eclipse.console;
+
+import junit.framework.TestCase;
+
+/**
+ * @author max
+ *
+ */
+public class PluginTest extends TestCase {
+
+	public void testNothing() {
+		System.out.println("funky");
+	}
+}
