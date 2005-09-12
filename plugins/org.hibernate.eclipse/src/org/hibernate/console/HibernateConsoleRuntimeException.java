b92181e202556c6593b76f56674284621317f5bb
Max Rydahl Andersen <max@hibernate.org>
2005-09-12 17:08:30 +0000

    updated deps + fixed build bug
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@466 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.hibernate.eclipse/src/org/hibernate/console/HibernateConsoleRuntimeException.java
+++ plugins/org.hibernate.eclipse/src/org/hibernate/console/HibernateConsoleRuntimeException.java
@@ -11,33 +11,15 @@ package org.hibernate.console;
 public class HibernateConsoleRuntimeException extends RuntimeException {
 
 	
-	/**
-	 * 
-	 */
-	public HibernateConsoleRuntimeException() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	/**
-	 * @param message
-	 */
 	public HibernateConsoleRuntimeException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
-	/**
-	 * @param message
-	 * @param cause
-	 */
+
 	public HibernateConsoleRuntimeException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
-	/**
-	 * @param cause
-	 */
+
 	public HibernateConsoleRuntimeException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 }
