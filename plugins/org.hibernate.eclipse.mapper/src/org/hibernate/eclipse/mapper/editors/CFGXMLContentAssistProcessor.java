9ccdd228a63d25e1cbbd330010ecff929af96a2a
Max Rydahl Andersen <max@hibernate.org>
2005-12-02 15:37:38 +0000

    cfg.xml now only do property name completion on property name tags. reveng.xml no longer has faulty cfg.xml codecompletion
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@616 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.hibernate.eclipse.mapper/src/org/hibernate/eclipse/mapper/editors/CFGXMLContentAssistProcessor.java
+++ plugins/org.hibernate.eclipse.mapper/src/org/hibernate/eclipse/mapper/editors/CFGXMLContentAssistProcessor.java
@@ -2,10 +2,10 @@ package org.hibernate.eclipse.mapper.editors;
 
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
-import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jface.text.contentassist.CompletionProposal;
 import org.eclipse.wst.xml.ui.internal.contentassist.ContentAssistRequest;
 import org.hibernate.eclipse.mapper.extractor.HBMInfoExtractor;
@@ -25,14 +25,17 @@ public class CFGXMLContentAssistProcessor extends HibernateContentAssistProcesso
 
 	
 	protected List getAttributeValueProposals(String attributeName, String matchString, int offset, ContentAssistRequest contentAssistRequest) {
-		List types = this.extractor.findMatchingPropertyTypes(matchString);
-		
-		List proposals = new ArrayList(types.size() );		
-		for (Iterator iter = types.iterator(); iter.hasNext();) {
-			String element = (String) iter.next();
-			proposals.add(new CompletionProposal(element, offset, matchString.length(), element.length(), null, null, null, null) );
+		if("property".equals(contentAssistRequest.getNode().getNodeName()) && "name".equals(attributeName)) {
+			List types = this.extractor.findMatchingPropertyTypes(matchString);
+			
+			List proposals = new ArrayList(types.size() );		
+			for (Iterator iter = types.iterator(); iter.hasNext();) {
+				String element = (String) iter.next();
+				proposals.add(new CompletionProposal(element, offset, matchString.length(), element.length(), null, null, null, null) );
+			}
+			return proposals;
 		}
-		return proposals;
+		return Collections.EMPTY_LIST;
 	}
 
 	
