8a6049b14c5781ca4f3ee36a5914db659cc538a7
Marshall Culpepper <mculpepper@jboss.com>
2006-04-19 06:05:15 +0000

    added a new property "exporter list"
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@921 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.hibernate.eclipse.console/src/org/hibernate/eclipse/launch/HibernateLaunchConstants.java
+++ plugins/org.hibernate.eclipse.console/src/org/hibernate/eclipse/launch/HibernateLaunchConstants.java
@@ -37,5 +37,6 @@ public class HibernateLaunchConstants
    public static final String ATTR_ENABLE_TEMPLATE_DIR = ATTR_PREFIX + "templatepathenabled";
    public static final String ATTR_TEMPLATE_DIR = ATTR_PREFIX + "templatepath";
    public static final String ATTR_PREFER_BASIC_COMPOSITE_IDS = ATTR_PREFIX + "prefercompositeids";
+   public static final String ATTR_EXPORTER_LIST = ATTR_PREFIX + "exporterlist";
    
 }
