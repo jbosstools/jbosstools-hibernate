e8b029edc3f1d3dc18e340cf253c2bdfa72584e0
Max Rydahl Andersen <max.andersen@jboss.com>
2006-04-24 10:25:39 +0000

    misc
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@929 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.hibernate.eclipse.console.test/src/org/hibernate/eclipse/console/test/HibernateProjectTests.java
+++ tests/org.hibernate.eclipse.console.test/src/org/hibernate/eclipse/console/test/HibernateProjectTests.java
@@ -1,18 +1,10 @@
 package org.hibernate.eclipse.console.test;
 
-import org.eclipse.core.resources.ProjectScope;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.preferences.IScopeContext;
-import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
-import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.ide.IDE;
-import org.hibernate.eclipse.console.utils.ProjectUtils;
 import org.hibernate.eclipse.mapper.editors.HBMXMLContentAssistProcessor;
 import org.osgi.service.prefs.BackingStoreException;
-import org.osgi.service.prefs.Preferences;
 
 public class HibernateProjectTests extends HibernateConsoleTest {
 
