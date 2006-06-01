02627728023e7963da0aecaac6b7131842f19296
Max Rydahl Andersen <max.andersen@jboss.com>
2006-06-01 20:43:02 +0000

    HBX-671 @NamedQuery/@NamedQueries HQL validation
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@1012 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ plugins/org.hibernate.eclipse.jdt.apt.ui/src/org/hibernate/eclipse/jdt/apt/ui/HQLAnnotationProcessor.java
@@ -0,0 +1,176 @@
+package org.hibernate.eclipse.jdt.apt.ui;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.jdt.apt.core.env.EclipseAnnotationProcessorEnvironment;
+import org.eclipse.jdt.apt.core.env.Phase;
+import org.hibernate.console.ConsoleConfiguration;
+import org.hibernate.eclipse.nature.HibernateNature;
+import org.hibernate.engine.query.HQLQueryPlan;
+import org.hibernate.impl.SessionFactoryImpl;
+
+import com.sun.mirror.apt.AnnotationProcessor;
+import com.sun.mirror.apt.AnnotationProcessorEnvironment;
+import com.sun.mirror.apt.Messager;
+import com.sun.mirror.declaration.AnnotationMirror;
+import com.sun.mirror.declaration.AnnotationTypeDeclaration;
+import com.sun.mirror.declaration.AnnotationTypeElementDeclaration;
+import com.sun.mirror.declaration.AnnotationValue;
+import com.sun.mirror.declaration.Declaration;
+import com.sun.mirror.type.AnnotationType;
+
+public class HQLAnnotationProcessor implements AnnotationProcessor
+{
+	final EclipseAnnotationProcessorEnvironment	env;
+
+	public HQLAnnotationProcessor(AnnotationProcessorEnvironment env)
+	{
+		if(env instanceof EclipseAnnotationProcessorEnvironment) {
+			this.env = (EclipseAnnotationProcessorEnvironment) env;
+		} else {
+			this.env = null;
+		}
+	}
+	
+	static ConsoleConfiguration getConsoleConfiguration(HibernateNature hibernateNature) {
+		
+		if(hibernateNature!=null) {
+			return hibernateNature.getDefaultConsoleConfiguration();
+		} else {
+			return null;
+		}
+	}
+
+	private ConsoleConfiguration getConsoleConfiguration() {
+		return getConsoleConfiguration(HibernateNature.getHibernateNature( env.getJavaProject() ));
+	}
+	
+
+	public void process()
+	{
+		try {
+			if(env==null) {
+				return; // quick stop if not running inside eclipse.
+			}
+
+			if(env.getPhase()==Phase.RECONCILE) {
+				return; // quick stop if user "editing"
+			}
+			
+			ConsoleConfiguration cc = getConsoleConfiguration();
+			
+			Messager messager = env.getMessager();
+
+			Set types = new HashSet();
+			AnnotationTypeDeclaration jpa_queries = (AnnotationTypeDeclaration)this.env.getTypeDeclaration("javax.persistence.NamedQueries");
+			types.addAll(env.getDeclarationsAnnotatedWith(jpa_queries));
+
+			AnnotationTypeDeclaration hibernate_queries = (AnnotationTypeDeclaration)this.env.getTypeDeclaration("org.hibernate.annotations.NamedQueries");
+			types.addAll( env.getDeclarationsAnnotatedWith(hibernate_queries) );
+
+			AnnotationTypeDeclaration jpa_namedQuery = (AnnotationTypeDeclaration)this.env.getTypeDeclaration("javax.persistence.NamedQuery");
+			types.addAll( env.getDeclarationsAnnotatedWith(jpa_namedQuery) );
+
+			AnnotationTypeDeclaration hibernate_namedQuery = (AnnotationTypeDeclaration)this.env.getTypeDeclaration("org.hibernate.annotations.NamedQuery");
+			types.addAll( env.getDeclarationsAnnotatedWith(hibernate_namedQuery) );				
+
+			handleNamedQueryTypes( cc, messager, types );
+		} catch (NullPointerException npe) {
+			env.getMessager().printError( npe.toString() );
+			npe.printStackTrace();
+		}
+
+	}
+
+	private void handleNamedQueryTypes(ConsoleConfiguration cc, Messager messager, Collection annotatedTypes) {
+		for (Iterator iter = annotatedTypes.iterator(); iter.hasNext();) {
+			Declaration decl = (Declaration) iter.next();
+
+			Collection mirrors = decl.getAnnotationMirrors();
+
+			for (Iterator iterator = mirrors.iterator(); iterator.hasNext();) {
+				AnnotationMirror mirror = (AnnotationMirror) iterator.next();
+				AnnotationType annotationType = mirror.getAnnotationType();
+				String simpleName = annotationType.getDeclaration().getSimpleName();
+				if(simpleName.equals( "NamedQueries" )) {
+					Map valueMap = mirror.getElementValues();
+					Set valueSet = valueMap.entrySet();
+
+					for (Iterator it3 = valueSet.iterator(); it3.hasNext();) {
+						Map.Entry annoKeyValue = (Map.Entry) it3.next();
+
+						AnnotationValue annoValue = (AnnotationValue) annoKeyValue.getValue();	
+
+						AnnotationTypeElementDeclaration atedec = (AnnotationTypeElementDeclaration) annoKeyValue.getKey();
+						if (atedec.getSimpleName().equals("value")) {
+							Object queryValueList = annoValue.getValue();			
+							if (queryValueList instanceof Collection) {
+								Collection values = (Collection) queryValueList;
+								for (Iterator val = values.iterator(); val.hasNext();) {
+									Object element = val.next();
+									if(element instanceof AnnotationValue) {
+										AnnotationValue el = (AnnotationValue) element;
+										if(el.getValue() instanceof AnnotationMirror) {
+											AnnotationMirror annMirror = (AnnotationMirror) el.getValue();	
+											
+											handleNamedQuery( cc, messager, annMirror );
+											
+										}																				
+									}
+								}
+								
+							}
+						}						
+					}				
+				} else if (simpleName.equals("NamedQuery")) {
+					handleNamedQuery( cc, messager, mirror );	
+				}
+				
+			}
+		}
+	}
+
+	private void handleNamedQuery(ConsoleConfiguration cc, Messager messager, AnnotationMirror mirror) {
+		Map valueMap = mirror.getElementValues();
+		Set valueSet = valueMap.entrySet();
+
+		for (Iterator it3 = valueSet.iterator(); it3.hasNext();) {
+			Map.Entry annoKeyValue = (Map.Entry) it3.next();
+
+			AnnotationValue annoValue = (AnnotationValue) annoKeyValue.getValue();	
+
+			AnnotationTypeElementDeclaration atedec = (AnnotationTypeElementDeclaration) annoKeyValue.getKey();
+			handleSingleQuery( cc, messager, annoValue, atedec );						
+		}
+	}
+
+	private void handleSingleQuery(ConsoleConfiguration cc, Messager messager, AnnotationValue annoValue, AnnotationTypeElementDeclaration atedec) {
+		if (atedec.getSimpleName().equals("query")) {
+			Object query = annoValue.getValue();			
+			if (query instanceof String) {
+				checkQuery( cc, messager, annoValue, query );
+			}
+		}
+	}
+
+	private void checkQuery(ConsoleConfiguration cc, Messager messager, AnnotationValue annoValue, Object query) {
+		try {
+			if(cc!=null && cc.isSessionFactoryCreated()) {
+				new HQLQueryPlan((String)query, false, Collections.EMPTY_MAP, (SessionFactoryImpl)cc.getSessionFactory());
+			} else {											
+				messager.printWarning( annoValue.getPosition(), "Could not verify syntax. SessionFactory not created." );
+			}
+		} catch(RuntimeException re) {
+			messager.printError(annoValue.getPosition(), re.getMessage());	
+		}
+	}
+
+
+	
+
+}
