7ec7e0cceec73a8fb2f71707f5c5669717cf8550
Mikle Dryakhlenkov <dma@exadel.com>
2007-06-26 10:09:05 +0000

    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@2182 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ sampleprojects/org.jboss.tools.hibernate.sampleproject.mappingtypes/src/mapping/declaration/id/MappedClassWithId.java
@@ -0,0 +1,13 @@
+package mapping.declaration.id;
+
+public class MappedClassWithId {
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
