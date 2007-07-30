181d26d77220388a7e622ed24a6cbe8aa67c7345
Mikle Dryakhlenkov <dma@exadel.com>
2007-07-30 15:40:23 +0000

    EXIN-369:  Saving positions and states (visible/hidden) of diagram elements
    Creating file for save.
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@2718 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.hibernate.ui.view/src/org/jboss/tools/hibernate/ui/view/views/OpenDiagramActionDelegate.java
+++ plugins/org.jboss.tools.hibernate.ui.view/src/org/jboss/tools/hibernate/ui/view/views/OpenDiagramActionDelegate.java
@@ -12,6 +12,12 @@ package org.jboss.tools.hibernate.ui.view.views;
 
 import java.util.HashMap;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationType;
+import org.eclipse.debug.core.ILaunchManager;
+import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.TreeSelection;
@@ -21,6 +27,9 @@ import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.ide.IDE;
 import org.eclipse.ui.internal.ObjectPluginAction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.console.ConsoleConfiguration;
+import org.hibernate.eclipse.console.utils.ProjectUtils;
+import org.hibernate.eclipse.launch.IConsoleConfigurationLaunchConstants;
 import org.jboss.tools.hibernate.ui.view.ViewPlugin;
 
 public class OpenDiagramActionDelegate implements IObjectActionDelegate {
@@ -33,9 +42,29 @@ public class OpenDiagramActionDelegate implements IObjectActionDelegate {
     	ObjectPluginAction objectPluginAction = (ObjectPluginAction)action;
     	Object rootClass = ((TreeSelection)objectPluginAction.getSelection()).getFirstElement();
 		ObjectEditorInput input = (ObjectEditorInput)hashMap.get(rootClass);
-		Configuration configuration = (Configuration)(((TreeSelection)objectPluginAction.getSelection()).getPaths()[0]).getSegment(1);
+//		Configuration configuration = (Configuration)(((TreeSelection)objectPluginAction.getSelection()).getPaths()[0]).getSegment(1);
+		ConsoleConfiguration consoleConfiguration = (ConsoleConfiguration)(((TreeSelection)objectPluginAction.getSelection()).getPaths()[0]).getSegment(0);
+		
+		ILaunchManager launchManager = DebugPlugin.getDefault().getLaunchManager();
+
+		ILaunchConfigurationType launchConfigurationType = launchManager.getLaunchConfigurationType( "org.hibernate.eclipse.launch.ConsoleConfigurationLaunchConfigurationType" );
+		ILaunchConfiguration[] launchConfigurations;
+		IJavaProject proj = null;
+		try {
+			launchConfigurations = launchManager.getLaunchConfigurations( launchConfigurationType );
+			for (int i = 0; i < launchConfigurations.length; i++) { // can't believe there is no look up by name API
+				ILaunchConfiguration launchConfiguration = launchConfigurations[i];
+				if(launchConfiguration.getName().equals(consoleConfiguration.getName())) {
+					proj = ProjectUtils.findJavaProject(launchConfiguration.getAttribute(IConsoleConfigurationLaunchConstants.PROJECT_NAME, ""));
+				}
+			}								
+		} catch (CoreException e1) {
+			ViewPlugin.getDefault().logError("Can't find java project.", e1);
+		}
+			
+			
 		if(input == null) {
-			input = new ObjectEditorInput(configuration, rootClass);
+			input = new ObjectEditorInput(consoleConfiguration, rootClass, proj);
 			hashMap.put(rootClass, input);
 		}
 		try {
