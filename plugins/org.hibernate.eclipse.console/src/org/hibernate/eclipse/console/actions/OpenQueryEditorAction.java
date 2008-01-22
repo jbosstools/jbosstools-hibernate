2cd094c69822e6bbaee19914e5ace615b92c636b
Dmitry Geraskov <dgeraskov@exadel.com>
2008-01-22 14:25:15 +0000

    http://jira.jboss.com/jira/browse/JBIDE-1665 select ConsoleConfiguration in combobox
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@5867 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.hibernate.eclipse.console/src/org/hibernate/eclipse/console/actions/OpenQueryEditorAction.java
+++ plugins/org.hibernate.eclipse.console/src/org/hibernate/eclipse/console/actions/OpenQueryEditorAction.java
@@ -1,8 +1,14 @@
 package org.hibernate.eclipse.console.actions;
 
+import java.io.FileNotFoundException;
 import java.util.Iterator;
 
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.TreePath;
+import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.swt.widgets.Event;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.actions.SelectionListenerAction;
 import org.hibernate.HibernateException;
 import org.hibernate.console.ConsoleConfiguration;
@@ -20,17 +26,19 @@ public abstract class OpenQueryEditorAction extends SelectionListenerAction {
 
 	protected void doRun() {
 		boolean showed = false;
-		for (Iterator i = getSelectedNonResources().iterator(); i.hasNext();) {
-			try {
-				Object node = i.next();
-				if(node instanceof ConsoleConfiguration) {
-					final ConsoleConfiguration config = (ConsoleConfiguration) node;
-					openQueryEditor( config, "" );
-					showed = true;
-				} 
-			} catch(HibernateException he) {
-				HibernateConsolePlugin.getDefault().showError(null, "Exception while trying to open HQL editor", he);
-			}
+		IStructuredSelection sel = getStructuredSelection();
+		if (sel instanceof TreeSelection){
+			TreePath[] paths = ((TreeSelection)sel).getPaths();
+			for (int i = 0; i < paths.length; i++) {
+				TreePath path = paths[i];
+				ConsoleConfiguration config = (ConsoleConfiguration) path.getSegment(0);
+				try {
+				  openQueryEditor( config, "" );
+				} catch(HibernateException he) {
+					HibernateConsolePlugin.getDefault().showError(null, "Exception while trying to open HQL editor", he);
+				}
+				showed = true;
+			}			
 		}
 		
 		if(!showed) {
