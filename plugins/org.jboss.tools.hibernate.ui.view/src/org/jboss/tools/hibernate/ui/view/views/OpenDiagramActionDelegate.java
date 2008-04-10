fddc30e18a082aaaf2791b3e6b40273ebff2161a
Vitali Yemialyanchyk <vyemialyanchyk@exadel.com>
2008-04-10 11:10:00 +0000

    http://jira.jboss.com/jira/browse/JBIDE-1559
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@7479 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.hibernate.ui.view/src/org/jboss/tools/hibernate/ui/view/views/OpenDiagramActionDelegate.java
+++ plugins/org.jboss.tools.hibernate.ui.view/src/org/jboss/tools/hibernate/ui/view/views/OpenDiagramActionDelegate.java
@@ -11,6 +11,10 @@
 package org.jboss.tools.hibernate.ui.view.views;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jface.action.IAction;
@@ -23,27 +27,39 @@ import org.eclipse.ui.internal.ObjectPluginAction;
 import org.hibernate.console.ConsoleConfiguration;
 import org.hibernate.eclipse.console.utils.ProjectUtils;
 import org.hibernate.mapping.PersistentClass;
+import org.hibernate.mapping.RootClass;
 import org.jboss.tools.hibernate.ui.view.ViewPlugin;
 
 public class OpenDiagramActionDelegate extends OpenActionDelegate {
+
 	private HashMap hashMap = new HashMap();
 
 	public void run(IAction action) {
     	ObjectPluginAction objectPluginAction = (ObjectPluginAction)action;
+    	Map mapCC_PCs = new HashMap();
     	TreePath[] paths = ((TreeSelection)objectPluginAction.getSelection()).getPaths();
     	for (int i = 0; i < paths.length; i++) {
     		Object last_el = paths[i].getLastSegment();
         	if (last_el instanceof PersistentClass) {
     			PersistentClass persClass = (PersistentClass) last_el;
     			ConsoleConfiguration consoleConfiguration = (ConsoleConfiguration)(paths[i].getFirstSegment());
-    	    	
-    	    	try {
-    	    		openEditor(persClass, consoleConfiguration);
-    	    	} catch (PartInitException e) {
-    				ViewPlugin.getDefault().logError("Can't open mapping view.", e);		//$NON-NLS-1$
-    			} 
+    			Set setPC = (Set)mapCC_PCs.get(consoleConfiguration);
+    			if (null == setPC) {
+    				setPC = new HashSet();
+    				mapCC_PCs.put(consoleConfiguration, setPC);
+    			}
+    			setPC.add(persClass);
     		}    
 		}    		
+    	for (Iterator it = mapCC_PCs.keySet().iterator(); it.hasNext(); ) {
+    		ConsoleConfiguration consoleConfiguration = (ConsoleConfiguration)it.next();
+    		Set setPC = (Set)mapCC_PCs.get(consoleConfiguration);
+	    	try {
+	    		openEditor(setPC, consoleConfiguration);
+	    	} catch (PartInitException e) {
+				ViewPlugin.getDefault().logError("Can't open mapping view.", e);		//$NON-NLS-1$
+			} 
+    	}
 	}
 
 	public IEditorPart openEditor(PersistentClass persClass,
@@ -56,6 +72,33 @@ public class OpenDiagramActionDelegate extends OpenActionDelegate {
 		if(input == null) {
 			input = new ObjectEditorInput(consoleConfiguration, persClass.getRootClass(), proj);
 			hashMap.put(persClass.getRootClass(), input);
+		}
+
+		return IDE.openEditor(ViewPlugin.getPage(),input ,"org.jboss.tools.hibernate.ui.veditor.editors.visualeditor");		//$NON-NLS-1$
+	}
+
+	public IEditorPart openEditor(Set setPC, ConsoleConfiguration consoleConfiguration) throws PartInitException {
+		
+		if (0 >= setPC.size()) {
+			return null;
+		}
+		
+		RootClass[] rcArr = new RootClass[setPC.size()];
+		String id = "";
+		PersistentClass persClass = null;
+		int i = 0;
+    	for (Iterator it = setPC.iterator(); it.hasNext(); ) {
+    		persClass = (PersistentClass)it.next();
+    		id += "@" + persClass.toString();
+    		rcArr[i++] = persClass.getRootClass();
+    	}
+		
+		ObjectEditorInput input = (ObjectEditorInput)hashMap.get(id);
+		IJavaProject proj = ProjectUtils.findJavaProject(consoleConfiguration);
+			
+		if (null == input) {
+			input = new ObjectEditorInput(consoleConfiguration, rcArr, proj);
+			hashMap.put(id, input);
 		}
 
 		return IDE.openEditor(ViewPlugin.getPage(),input ,"org.jboss.tools.hibernate.ui.veditor.editors.visualeditor");		//$NON-NLS-1$
