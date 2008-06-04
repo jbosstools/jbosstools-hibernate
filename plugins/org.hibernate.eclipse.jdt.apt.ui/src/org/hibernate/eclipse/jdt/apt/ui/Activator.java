0c277c70940bec791e58fbec30d92363ed103d52
Vitali Yemialyanchyk <vyemialyanchyk@exadel.com>
2008-06-04 14:58:16 +0000

    http://opensource.atlassian.com/projects/hibernate/browse/HBX-500
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@8547 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.hibernate.eclipse.jdt.apt.ui/src/org/hibernate/eclipse/jdt/apt/ui/Activator.java
+++ plugins/org.hibernate.eclipse.jdt.apt.ui/src/org/hibernate/eclipse/jdt/apt/ui/Activator.java
@@ -30,7 +30,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.hibernate.eclipse.jdt.apt.ui";
+	public static final String PLUGIN_ID = "org.hibernate.eclipse.jdt.apt.ui"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
