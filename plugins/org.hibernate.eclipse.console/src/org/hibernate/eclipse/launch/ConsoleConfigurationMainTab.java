e4c208bdc3bc10b0f9099455bc7a2b7c47566148
Vitali Yemialyanchyk <vyemialyanchyk@exadel.com>
2009-02-19 16:57:34 +0000

    configurationFilename -> propertyFilename bugfix
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@13685 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.hibernate.eclipse.console/src/org/hibernate/eclipse/launch/ConsoleConfigurationMainTab.java
+++ plugins/org.hibernate.eclipse.console/src/org/hibernate/eclipse/launch/ConsoleConfigurationMainTab.java
@@ -430,7 +430,7 @@ public class ConsoleConfigurationMainTab extends ConsoleConfigurationTab {
 		if (propertyFilename.length() > 0) {
 			//IResource resource = ResourcesPlugin.getWorkspace().getRoot().findMember(propertyFilename);
 			//String msg = checkForFile(HibernateConsoleMessages.ConsoleConfigurationMainTab_property_file_2, resource);
-			String msg = PathHelper.checkFile(configurationFilename, HibernateConsoleMessages.ConsoleConfigurationMainTab_property_file_2, true);
+			String msg = PathHelper.checkFile(propertyFilename, HibernateConsoleMessages.ConsoleConfigurationMainTab_property_file_2, true);
 			if(msg!=null) {
 				setErrorMessage(msg);
 				return false;
