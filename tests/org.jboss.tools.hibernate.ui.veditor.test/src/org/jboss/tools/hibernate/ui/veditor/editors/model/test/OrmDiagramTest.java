485d3a456d5af19b7f1df9ebe752d16f2782e106
Vitali Yemialyanchyk <vyemialyanchyk@exadel.com>
2009-04-09 13:22:12 +0000

    JBIDE-4148 & JBIDE-4138 - unit test for OrmDiagram save & load
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@14632 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ tests/org.jboss.tools.hibernate.ui.veditor.test/src/org/jboss/tools/hibernate/ui/veditor/editors/model/test/OrmDiagramTest.java
@@ -0,0 +1,111 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2009 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.hibernate.ui.veditor.editors.model.test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.core.IJavaProject;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.console.ConsoleConfiguration;
+import org.hibernate.mapping.RootClass;
+import org.jboss.tools.hibernate.ui.veditor.editors.model.OrmDiagram;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.lib.legacy.ClassImposteriser;
+
+import junit.framework.TestCase;
+
+/**
+ * for OrmDiagram class functionality test
+ * 
+ * @author Vitali Yemialyanchyk
+ */
+public class OrmDiagramTest extends TestCase {
+	
+	public Mockery context = new Mockery() {
+		{
+			setImposteriser(ClassImposteriser.INSTANCE);
+		}
+	};
+
+	public void testLoadAndSave() {
+		
+		final ConsoleConfiguration consoleConfiguration = context.mock(ConsoleConfiguration.class);
+		final Configuration configuration = context.mock(Configuration.class);
+		final RootClass ioe = context.mock(RootClass.class);
+		final IJavaProject javaProject = context.mock(IJavaProject.class);
+		final List<Object> emptyList = new ArrayList<Object>();
+		final Iterator<Object> emptyListIterator = emptyList.iterator();
+		final IProject project = context.mock(IProject.class);
+
+		context.checking(new Expectations() {
+			{
+				oneOf(consoleConfiguration).getConfiguration();
+				will(returnValue(configuration));
+
+				oneOf(ioe).getEntityName();
+				will(returnValue("testEntityName")); //$NON-NLS-1$
+
+				oneOf(ioe).getEntityName();
+				will(returnValue("")); //$NON-NLS-1$
+
+				oneOf(ioe).getEntityName();
+				will(returnValue("")); //$NON-NLS-1$
+
+				oneOf(ioe).getIdentifierProperty();
+				will(returnValue(null));
+
+				oneOf(ioe).getIdentifier();
+				will(returnValue(null));
+
+				oneOf(ioe).getPropertyIterator();
+				will(returnValue(emptyListIterator));
+
+				oneOf(ioe).getTable();
+				will(returnValue(null));
+
+				oneOf(ioe).getSubclassIterator();
+				will(returnValue(emptyListIterator));
+
+				oneOf(ioe).getIdentifier();
+				will(returnValue(null));
+
+				oneOf(ioe).getJoinIterator();
+				will(returnValue(emptyListIterator));
+				
+				allowing(javaProject).getProject();
+				will(returnValue(project));
+
+				allowing(project).getLocation();
+				will(returnValue(Path.fromOSString(""))); //$NON-NLS-1$
+				
+				allowing(ioe).getClassName();
+				will(returnValue("ClassName")); //$NON-NLS-1$
+				
+				allowing(consoleConfiguration).getName();
+				will(returnValue("CCName")); //$NON-NLS-1$
+
+				allowing(ioe).getEntityName();
+				will(returnValue("")); //$NON-NLS-1$
+			}
+		});
+		final OrmDiagram ormDiagram = new OrmDiagram(consoleConfiguration, ioe, javaProject);
+		ormDiagram.save();
+		// GENERAL TEST:
+		// check for all expectations
+		context.assertIsSatisfied();
+	}
+
+}
