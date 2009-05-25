4742545f586c2726fca56e3f328203ca1b3fbf21
Dmitry Geraskov <dgeraskov@exadel.com>
2009-05-25 08:06:27 +0000

    https://jira.jboss.org/jira/browse/JBIDE-3507 added copyright
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@15483 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.hibernate.jpt.ui/src/org/jboss/tools/hibernate/jpt/ui/internal/platform/HibernateJpaUiFactory.java
+++ plugins/org.jboss.tools.hibernate.jpt.ui/src/org/jboss/tools/hibernate/jpt/ui/internal/platform/HibernateJpaUiFactory.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2009 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.jboss.tools.hibernate.jpt.ui.internal.platform;
 
 import java.util.ArrayList;
@@ -18,6 +28,10 @@ import org.jboss.tools.hibernate.jpt.core.internal.context.basic.BasicHibernateP
 import org.jboss.tools.hibernate.jpt.ui.internal.persistence.details.HibernatePropertiesComposite;
 import org.jboss.tools.hibernate.jpt.ui.xpl.PersistenceUnitGeneralComposite;
 
+/**
+ * @author Dmitry Geraskov
+ * 
+ */
 @SuppressWarnings("restriction")
 public class HibernateJpaUiFactory extends GenericJpaUiFactory {
 
