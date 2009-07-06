27e36dc5fa0ef15525a9842352715878502d4579
Sean Flanigan <sflaniga@redhat.com>
2009-07-06 01:01:23 +0000

    https://jira.jboss.org/jira/browse/JBIDE-3557 Externalize English strings
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@16390 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ plugins/org.jboss.tools.hibernate.xml.ui/src/org/jboss/tools/hibernate/ui/xml/form/Messages.java
@@ -0,0 +1,108 @@
+package org.jboss.tools.hibernate.ui.xml.form;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "org.jboss.tools.hibernate.ui.xml.form.messages"; //$NON-NLS-1$
+	public static String HibConfig3CacheFormLayoutData_Folder;
+	public static String HibConfig3CacheFormLayoutData_Item;
+	public static String HibConfig3CacheFormLayoutData_List;
+	public static String HibConfig3EventFormLayoutData_Class;
+	public static String HibConfig3EventFormLayoutData_Folder;
+	public static String HibConfig3EventFormLayoutData_EventHeader;
+	public static String HibConfig3EventFormLayoutData_List;
+	public static String HibConfig3EventFormLayoutData_ListenerFolder;
+	public static String HibConfig3EventFormLayoutData_ListenerList;
+	public static String HibConfig3EventFormLayoutData_Type;
+	public static String HibConfig3FileFormLayoutData_Header;
+	public static String HibConfig3MappingFormLayoutData_Folder;
+	public static String HibConfig3MappingFormLayoutData_List;
+	public static String HibConfig3MappingFormLayoutData_Item;
+	public static String HibConfig3PropertyFormLayoutData_Name;
+	public static String HibConfig3PropertyFormLayoutData_Folder;
+	public static String HibConfig3PropertyFormLayoutData_List;
+	public static String HibConfig3SessionFormLayoutData_Header;
+	public static String Hibernate3AnyFormLayoutData_Advanced;
+	public static String Hibernate3AnyFormLayoutData_General;
+	public static String Hibernate3AnyFormLayoutData_List;
+	public static String Hibernate3ColumnFormLayoutData_List;
+	public static String Hibernate3FormulaFormLayoutData_Formula;
+	public static String Hibernate3FormulaFormLayoutData_FormulaList;
+	public static String Hibernate3IdFormLayoutData_Advanced;
+	public static String Hibernate3IdFormLayoutData_CollectionID;
+	public static String Hibernate3IdFormLayoutData_CompositeID;
+	public static String Hibernate3IdFormLayoutData_CompositeIndex;
+	public static String Hibernate3IdFormLayoutData_IDKeyProperties;
+	public static String Hibernate3IdFormLayoutData_IndexKeyProperties;
+	public static String Hibernate3KeyFormLayoutData_Advanced;
+	public static String Hibernate3KeyFormLayoutData_CompositeMapKey;
+	public static String Hibernate3KeyFormLayoutData_Index;
+	public static String Hibernate3KeyFormLayoutData_Key;
+	public static String Hibernate3KeyFormLayoutData_KeyManyToOne;
+	public static String Hibernate3KeyFormLayoutData_KeyProperties;
+	public static String Hibernate3KeyFormLayoutData_KeyProperty;
+	public static String Hibernate3KeyFormLayoutData_ListIndex;
+	public static String Hibernate3KeyFormLayoutData_MapKey;
+	public static String Hibernate3KeyFormLayoutData_MapKeyManyToMany;
+	public static String Hibernate3ListFormLayoutData_Array;
+	public static String Hibernate3ListFormLayoutData_ArrayAdvanced;
+	public static String Hibernate3ListFormLayoutData_ArrayElement;
+	public static String Hibernate3ListFormLayoutData_ArrayElementKind;
+	public static String Hibernate3ListFormLayoutData_ArrayIndex;
+	public static String Hibernate3ListFormLayoutData_ArrayIndexKind;
+	public static String Hibernate3ListFormLayoutData_ArrayKey;
+	public static String Hibernate3ListFormLayoutData_List;
+	public static String Hibernate3ListFormLayoutData_ListAdvanced;
+	public static String Hibernate3ListFormLayoutData_ListElement;
+	public static String Hibernate3ListFormLayoutData_ListElementKind;
+	public static String Hibernate3ListFormLayoutData_ListIndex;
+	public static String Hibernate3ListFormLayoutData_ListIndexKind;
+	public static String Hibernate3ListFormLayoutData_ListKey;
+	public static String Hibernate3ManyToOneFormLayoutData_Advanced;
+	public static String Hibernate3ManyToOneFormLayoutData_ManyToOne;
+	public static String Hibernate3MapFormLayoutData_Advanced;
+	public static String Hibernate3MapFormLayoutData_Element;
+	public static String Hibernate3MapFormLayoutData_ElementKind;
+	public static String Hibernate3MapFormLayoutData_Index;
+	public static String Hibernate3MapFormLayoutData_IndexKind;
+	public static String Hibernate3MapFormLayoutData_Key;
+	public static String Hibernate3MapFormLayoutData_Map;
+	public static String Hibernate3MetaFormLayoutData_Attr;
+	public static String Hibernate3MetaFormLayoutData_Class;
+	public static String Hibernate3MetaFormLayoutData_Folder;
+	public static String Hibernate3MetaFormLayoutData_List;
+	public static String Hibernate3MetaFormLayoutData_TuplizersFolder;
+	public static String Hibernate3MetaFormLayoutData_TuplizersList;
+	public static String Hibernate3MetaFormLayoutData_Value;
+	public static String Hibernate3OneToOneFormLayoutData_Advanced;
+	public static String Hibernate3OneToOneFormLayoutData_OneToOne;
+	public static String Hibernate3PropertyFormLayoutData_Advanced;
+	public static String Hibernate3PropertyFormLayoutData_Property;
+	public static String Hibernate3SetFormLayoutData_Bag;
+	public static String Hibernate3SetFormLayoutData_BagAdvanced;
+	public static String Hibernate3SetFormLayoutData_BagElement;
+	public static String Hibernate3SetFormLayoutData_BagElementKind;
+	public static String Hibernate3SetFormLayoutData_BagKey;
+	public static String Hibernate3SetFormLayoutData_Set;
+	public static String Hibernate3SetFormLayoutData_SetAdvanced;
+	public static String Hibernate3SetFormLayoutData_SetElement;
+	public static String Hibernate3SetFormLayoutData_SetElementKind;
+	public static String Hibernate3SetFormLayoutData_SetKey;
+	public static String Hibernate3SQLQueryFormLayoutData_QueryAdvanced;
+	public static String Hibernate3SQLQueryFormLayoutData_QueryReturns;
+	public static String Hibernate3SQLQueryFormLayoutData_ResultSet;
+	public static String Hibernate3SQLQueryFormLayoutData_ResultSetAdvanced;
+	public static String Hibernate3SQLQueryFormLayoutData_ResultSetReturns;
+	public static String Hibernate3SQLQueryFormLayoutData_ResultSetReturnScalars;
+	public static String Hibernate3SQLQueryFormLayoutData_QueryReturnScalars;
+	public static String Hibernate3SQLQueryFormLayoutData_SQLQuery;
+	public static String Hibernate3SQLQueryFormLayoutData_SyncFolder;
+	public static String Hibernate3SQLQueryFormLayoutData_SyncList;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
