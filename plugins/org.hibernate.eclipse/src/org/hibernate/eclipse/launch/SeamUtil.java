3dce22dad173c935c1556039869c648bb2dd2dfb
Vitali Yemialyanchyk <vyemialyanchyk@exadel.com>
2009-07-07 18:25:15 +0000

    https://jira.jboss.org/jira/browse/JBIDE-4531 - SeamUtil is necessary for org.jboss.tools.seam.ui\src\org\jboss\tools\seam\ui\wizard\SeamGenerateEnitiesWizard
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@16475 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ plugins/org.hibernate.eclipse/src/org/hibernate/eclipse/launch/SeamUtil.java
@@ -0,0 +1,27 @@
+package org.hibernate.eclipse.launch;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Do not remove this file! 
+ * This file is necessary for SeamGenerateEnitiesWizard.java
+ * org.jboss.tools.seam.ui\src\org\jboss\tools\seam\ui\wizard\SeamGenerateEnitiesWizard
+ * for more information: JBIDE-4531
+ * This is "hack" to get seam gen code generation work 
+ * without launching in a separate process. 
+ */
+public class SeamUtil {
+	public String lower(String name)
+	{
+		return name.substring(0, 1).toLowerCase() + name.substring(1);
+	}
+	public String upper(String name)
+	{
+		return name.substring(0, 1).toUpperCase() + name.substring(1);
+	}
+	public Set set()
+	{
+		return new HashSet();
+	}
+}
