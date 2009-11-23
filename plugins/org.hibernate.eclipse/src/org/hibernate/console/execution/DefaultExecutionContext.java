91751fa3f113bfb9a470a022df3438b6cc39bc7a
Vitali Yemialyanchyk <vyemialyanchyk@exadel.com>
2009-11-23 17:21:03 +0000

    http://opensource.atlassian.com/projects/hibernate/browse/HBX-936 - revert changes back, could be unsafe for core part, cause it change classpath order, should be released in a better manner
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@18788 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.hibernate.eclipse/src/org/hibernate/console/execution/DefaultExecutionContext.java
+++ plugins/org.hibernate.eclipse/src/org/hibernate/console/execution/DefaultExecutionContext.java
@@ -21,6 +21,7 @@
  */
 package org.hibernate.console.execution;
 
+import java.net.URLClassLoader;
 import java.util.Map;
 import java.util.WeakHashMap;
 
@@ -30,13 +31,13 @@ import org.hibernate.eclipse.logging.CurrentContext;
 
 public class DefaultExecutionContext implements ExecutionContext {
 
-	final private ClassLoader configurationClassLoader;
+	final private URLClassLoader configurationClassLoader;
 	private volatile int installs;
 	private Map<Thread, ClassLoader> previousLoaders = new WeakHashMap<Thread, ClassLoader>();
 
 	final String key;
 
-	public DefaultExecutionContext(String key, ClassLoader loader) {
+	public DefaultExecutionContext(String key, URLClassLoader loader) {
 		configurationClassLoader = loader;
 		this.key = key;
 	}
