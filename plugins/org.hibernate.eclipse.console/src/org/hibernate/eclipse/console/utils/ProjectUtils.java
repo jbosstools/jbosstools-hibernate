6f6a0bf6e35e1ae0ca0ef05faaf8517e18ba824e
Vitali Yemialyanchyk <vyemialyanchyk@exadel.com>
2010-02-25 18:20:19 +0000

    https://jira.jboss.org/jira/browse/JBIDE-5944 - bugfix with ProjectUtils.removeProjectNature - fixed
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@20496 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.hibernate.eclipse.console/src/org/hibernate/eclipse/console/utils/ProjectUtils.java
+++ plugins/org.hibernate.eclipse.console/src/org/hibernate/eclipse/console/utils/ProjectUtils.java
@@ -148,9 +148,10 @@ public class ProjectUtils {
 
 			String[] natures = description.getNatureIds();
 	        String[] newNatures = new String[natures.length - 1];
-	        for(int i = 0; i < natures.length; i++) {
-	            if (!natures[i].equals(nature) )
-	                newNatures[i] = natures[i];
+	        for(int i = 0, j = 0; i < natures.length; i++) {
+	            if (!natures[i].equals(nature)) {
+	                newNatures[j++] = natures[i];
+	            }
 	        }
 	        description.setNatureIds(newNatures);
 	        project.setDescription(description, monitor);
