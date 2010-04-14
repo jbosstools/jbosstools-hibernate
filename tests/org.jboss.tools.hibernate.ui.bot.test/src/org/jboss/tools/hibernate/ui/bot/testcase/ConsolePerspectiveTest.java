3ed13034ce82bdd6735289d68809b1a6c50f984d
Jiri Peterka <peterka.jiri@gmail.com>
2010-04-14 14:15:36 +0000

    Initial OpenPerspectiveElement test added
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@21470 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/ui/bot/testcase/ConsolePerspectiveTest.java
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/ui/bot/testcase/ConsolePerspectiveTest.java
@@ -12,6 +12,7 @@ package org.jboss.tools.hibernate.ui.bot.testcase;
 
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.jboss.tools.hibernate.ui.bot.testsuite.HibernateTest;
+import org.jboss.tools.ui.bot.ext.gen.ActionItem;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -19,11 +20,21 @@ import org.junit.runner.RunWith;
 public class ConsolePerspectiveTest extends HibernateTest {
 
 	/**
-	 * TC 17
+	 * TC 17 - Check presence of basic hibernate views
 	 */
 	@Test
 	public void openPerspectiveElements() {
-
+		bot.viewByTitle("Welcome").close();
+		
+		open.perspective(ActionItem.Perspective.HIBERNATE.LABEL);
+		bot.sleep(TIME_1S);
+		
+		open.viewOpen(ActionItem.View.HibernateHibernateConfigurations.LABEL);
+		bot.sleep(TIME_1S);
+		open.viewOpen(ActionItem.View.HibernateHibernateDynamicSQLPreview.LABEL);
+		bot.sleep(TIME_1S);
+		open.viewOpen(ActionItem.View.HibernateHibernateQueryResult.LABEL);
+		bot.sleep(TIME_1S);
 	}
 
 }
