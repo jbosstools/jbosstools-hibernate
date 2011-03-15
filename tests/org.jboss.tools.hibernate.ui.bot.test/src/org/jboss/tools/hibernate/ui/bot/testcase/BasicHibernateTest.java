3ec783723baceafa258590547b2a1b01531ef16f
Jiri Peterka <peterka.jiri@gmail.com>
2011-03-15 16:05:04 +0000

    hibernatebot: basic hibernate template test added
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@29795 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/ui/bot/testcase/BasicHibernateTest.java
@@ -0,0 +1,35 @@
+package org.jboss.tools.hibernate.ui.bot.testcase;
+
+import org.jboss.tools.hibernate.ui.bot.testsuite.HibernateTest;
+import org.jboss.tools.ui.bot.ext.config.Annotations.DB;
+import org.jboss.tools.ui.bot.ext.config.Annotations.SWTBotTestRequires;
+import org.jboss.tools.ui.bot.ext.gen.ActionItem.Perspective;
+import org.jboss.tools.ui.bot.ext.gen.ActionItem.View.HibernateHibernateConfigurations;
+import org.jboss.tools.ui.bot.ext.types.IDELabel;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Basic Hibernate tests
+ */
+@SWTBotTestRequires( db=@DB,perspective="Hibernate")
+public class BasicHibernateTest extends HibernateTest {
+	@BeforeClass
+	/**
+	 * Setup prerequisites for this test
+	 */
+	public static void setUpTest() {
+
+		eclipse.maximizeActiveShell();
+		eclipse.closeView(IDELabel.View.WELCOME);
+		
+		prepareProject();
+		util.waitForNonIgnoredJobs();
+	}
+	
+	@Test
+	public void testItI() {
+		open.viewOpen(HibernateHibernateConfigurations.LABEL);
+		open.perspective(Perspective.HIBERNATE.LABEL);
+	}
+}
