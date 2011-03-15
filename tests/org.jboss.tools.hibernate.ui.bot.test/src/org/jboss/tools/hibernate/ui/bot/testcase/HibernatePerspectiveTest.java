ef12ee405dc63271b93e4ef5ec5e928bb7c1f5e2
Jiri Peterka <peterka.jiri@gmail.com>
2011-03-15 15:21:50 +0000

    hibernatebot: perspective test added
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@29791 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/ui/bot/testcase/HibernatePerspectiveTest.java
@@ -0,0 +1,46 @@
+ /*******************************************************************************
+  * Copyright (c) 2007-2009 Red Hat, Inc.
+  * Distributed under license by Red Hat, Inc. All rights reserved.
+  * This program is made available under the terms of the
+  * Eclipse Public License v1.0 which accompanies this distribution,
+  * and is available at http://www.eclipse.org/legal/epl-v10.html
+  *
+  * Contributor:
+  *     Red Hat, Inc. - initial API and implementation
+  ******************************************************************************/
+package org.jboss.tools.hibernate.ui.bot.testcase;
+
+import org.jboss.tools.hibernate.ui.bot.testsuite.HibernateTest;
+import org.jboss.tools.ui.bot.ext.config.Annotations.SWTBotTestRequires;
+import org.jboss.tools.ui.bot.ext.gen.ActionItem;
+import org.jboss.tools.ui.bot.ext.types.IDELabel;
+import org.junit.Test;
+
+@SWTBotTestRequires(clearProjects = false,  perspective="Hibernate")
+public class HibernatePerspectiveTest extends HibernateTest {
+
+	/**
+	 * TC 17 - Check presence of basic hibernate views
+	 */
+	@Test
+	public void openPerspectiveElements() {
+		eclipse.closeView(IDELabel.View.WELCOME);
+		
+		open.perspective(ActionItem.Perspective.HIBERNATE.LABEL);
+		bot.sleep(TIME_1S);		
+		open.viewOpen(ActionItem.View.HibernateHibernateConfigurations.LABEL);		
+		bot.sleep(TIME_1S);
+		open.viewOpen(ActionItem.View.HibernateHibernateDynamicSQLPreview.LABEL);
+		bot.sleep(TIME_1S);
+		open.viewOpen(ActionItem.View.HibernateHibernateQueryResult.LABEL);
+		bot.sleep(TIME_1S);
+	}
+	
+	@Test 
+	public void checkHibernateTree() {
+		open.perspective(ActionItem.Perspective.HIBERNATE.LABEL);
+		bot.sleep(TIME_1S);
+		
+		open.viewOpen(ActionItem.View.HibernateHibernateConfigurations.LABEL);		
+	}
+}
