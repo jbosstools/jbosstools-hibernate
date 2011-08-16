c61aed96a3a6f2149e8dd2e2688a0b2f654fd095
Dmitry Geraskov <dgeraskov@exadel.com>
2011-08-16 09:41:07 +0000

    https://issues.jboss.org/browse/JBIDE-9547 JPA: Can't remove @NamedNativeQuery#resultClass field from ui
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@33966 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.hibernate.jpt.ui/src/org/jboss/tools/hibernate/jpt/ui/internal/mapping/details/HibernateNamedNativeQueryPropertyComposite.java
+++ plugins/org.jboss.tools.hibernate.jpt.ui/src/org/jboss/tools/hibernate/jpt/ui/internal/mapping/details/HibernateNamedNativeQueryPropertyComposite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009-2010 Red Hat, Inc.
+ * Copyright (c) 2009-2011 Red Hat, Inc.
  * Distributed under license by Red Hat, Inc. All rights reserved.
  * This program is made available under the terms of the
  * Eclipse Public License v1.0 which accompanies this distribution,
@@ -57,6 +57,9 @@ public class HibernateNamedNativeQueryPropertyComposite extends
 
 					@Override
 					protected void setValue_(String value) {
+						if (value.length() == 0) {
+							value = null;
+						}
 						this.subject.setResultClass(value);
 					}
 				};
