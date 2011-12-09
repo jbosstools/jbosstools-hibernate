bd798a86632126f1f6ba432baad5eff2aa9404b3
Jiri Peterka <peterka.jiri@gmail.com>
2011-12-09 10:56:44 +0000

    [hb-bot] Mapping diagram test update
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@37154 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/ui/bot/testcase/MappingsDiagramTest.java
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/ui/bot/testcase/MappingsDiagramTest.java
@@ -10,19 +10,49 @@
   ******************************************************************************/
 package org.jboss.tools.hibernate.ui.bot.testcase;
 
+import org.eclipse.swtbot.swt.finder.SWTBot;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
 import org.jboss.tools.hibernate.ui.bot.testsuite.HibernateTest;
+import org.jboss.tools.hibernate.ui.bot.testsuite.Project;
+import org.jboss.tools.ui.bot.ext.config.Annotations.DB;
 import org.jboss.tools.ui.bot.ext.config.Annotations.Require;
+import org.jboss.tools.ui.bot.ext.gen.ActionItem;
+import org.jboss.tools.ui.bot.ext.types.IDELabel;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-@Require(clearProjects  = false, perspective="Hibernate")
+@Require( db=@DB, perspective="Hibernate")
 public class MappingsDiagramTest extends HibernateTest {
 
+	@BeforeClass
 	/**
-	 * TC 21
+	 * Setup prerequisites for this test
+	 */
+	public static void setUpTest() {
+
+		eclipse.maximizeActiveShell();
+		eclipse.closeView(IDELabel.View.WELCOME);
+		
+		prepareProject();
+		prepareConsole();
+
+		util.waitForNonIgnoredJobs();
+	}
+	
+	/**
+	 * Test mapping diagram
 	 */
 	@Test
 	public void testDiagram() {
-
+		
+		CodeGenerationLauncherTest test = new CodeGenerationLauncherTest();
+		test.generate();
+		
+		SWTBot viewBot = open.viewOpen(ActionItem.View.HibernateHibernateConfigurations.LABEL).bot();	
+		SWTBotTreeItem console = viewBot.tree().expandNode(Project.PROJECT_NAME);
+		bot.sleep(TIME_1S);	
+		
+		console.contextMenu("Mapping Diagram").click();
 	}
 
 }
