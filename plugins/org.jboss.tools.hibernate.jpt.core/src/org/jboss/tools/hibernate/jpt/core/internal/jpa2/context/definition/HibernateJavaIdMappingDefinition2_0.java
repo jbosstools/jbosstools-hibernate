0811248e4d38d035aa388ee7990652bb1710944d
Dmitry Geraskov <dgeraskov@exadel.com>
2012-03-07 07:13:28 +0000

    Added copyrigths
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@39331 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.hibernate.jpt.core/src/org/jboss/tools/hibernate/jpt/core/internal/jpa2/context/definition/HibernateJavaIdMappingDefinition2_0.java
+++ plugins/org.jboss.tools.hibernate.jpt.core/src/org/jboss/tools/hibernate/jpt/core/internal/jpa2/context/definition/HibernateJavaIdMappingDefinition2_0.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011-2012 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.jboss.tools.hibernate.jpt.core.internal.jpa2.context.definition;
 
 import org.eclipse.jpt.jpa.core.context.java.JavaAttributeMappingDefinition;
@@ -5,6 +15,11 @@ import org.eclipse.jpt.jpa.core.context.java.JavaPersistentAttribute;
 import org.eclipse.jpt.jpa.core.internal.context.java.JavaAttributeMappingDefinitionWrapper;
 import org.jboss.tools.hibernate.jpt.core.internal.context.definition.HibernateJavaIdMappingDefinition;
 
+/**
+ * 
+ * @author Dmitry Geraskov (geraskov@gmail.com)
+ *
+ */
 public class HibernateJavaIdMappingDefinition2_0 extends JavaAttributeMappingDefinitionWrapper {
 
 	private static final HibernateJavaIdMappingDefinition DELEGATE = HibernateJavaIdMappingDefinition.instance();
--- plugins/org.jboss.tools.hibernate.jpt.core/src/org/jboss/tools/hibernate/jpt/core/internal/jpa2/context/definition/HibernateJavaManyToOneMappingDefinition2_0.java
+++ plugins/org.jboss.tools.hibernate.jpt.core/src/org/jboss/tools/hibernate/jpt/core/internal/jpa2/context/definition/HibernateJavaManyToOneMappingDefinition2_0.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011-2012 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.jboss.tools.hibernate.jpt.core.internal.jpa2.context.definition;
 
 import org.eclipse.jpt.common.utility.internal.iterables.ArrayIterable;
@@ -8,6 +18,11 @@ import org.eclipse.jpt.jpa.core.jpa2.resource.java.MapsId2_0Annotation;
 import org.eclipse.jpt.jpa.core.resource.java.IdAnnotation;
 import org.jboss.tools.hibernate.jpt.core.internal.context.definition.HibernateJavaManyToOneMappingDefinition;
 
+/**
+ * 
+ * @author Dmitry Geraskov (geraskov@gmail.com)
+ *
+ */
 public class HibernateJavaManyToOneMappingDefinition2_0	extends JavaAttributeMappingDefinitionWrapper
 {
 	private static final JavaAttributeMappingDefinition DELEGATE = HibernateJavaManyToOneMappingDefinition.instance();
