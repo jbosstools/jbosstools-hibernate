7f5e77b6c1c0f3524ed2467d21e38056e6124526
Jiri Peterka <peterka.jiri@gmail.com>
2012-03-16 11:02:34 +0000

    Hibernate perspective/views test added
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@39558 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hb/ui/bot/test/perspective/PerspectiveTest.java
@@ -0,0 +1,28 @@
+package org.jboss.tools.hb.ui.bot.test.perspective;
+
+import org.jboss.tools.hb.ui.bot.test.HibernateBaseTest;
+import org.jboss.tools.hibernate.ui.bot.testsuite.HibernateTest;
+import org.jboss.tools.ui.bot.ext.config.Annotations.Require;
+import org.jboss.tools.ui.bot.ext.gen.ActionItem;
+import org.jboss.tools.ui.bot.ext.types.IDELabel;
+import org.junit.Test;
+
+/**
+ * Hibernate perspective ui bot test
+ * 
+ * @author jpeterka
+ * 
+ */
+@Require(clearProjects = false, perspective = "Hibernate")
+public class PerspectiveTest extends HibernateBaseTest {
+		
+	@Test
+	public void openPerspectiveElements() {
+		eclipse.closeView(IDELabel.View.WELCOME);
+
+		open.perspective(ActionItem.Perspective.HIBERNATE.LABEL);
+		open.viewOpen(ActionItem.View.HibernateHibernateConfigurations.LABEL);
+		open.viewOpen(ActionItem.View.HibernateHibernateDynamicSQLPreview.LABEL);
+		open.viewOpen(ActionItem.View.HibernateHibernateQueryResult.LABEL);
+	}
+}
