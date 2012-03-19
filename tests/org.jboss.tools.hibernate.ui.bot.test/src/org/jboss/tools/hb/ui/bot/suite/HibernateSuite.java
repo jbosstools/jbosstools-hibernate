39fdf289221bf0861bf9b7f24f00d5573f83edb9
Jiri Peterka <peterka.jiri@gmail.com>
2012-03-19 20:16:58 +0000

    Hibernate bot testsuite update (test classes added)
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@39641 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hb/ui/bot/suite/HibernateSuite.java
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hb/ui/bot/suite/HibernateSuite.java
@@ -3,6 +3,16 @@ package org.jboss.tools.hb.ui.bot.suite;
 import org.jboss.tools.hb.ui.bot.test.configuration.CreateConfigurationFileTest;
 import org.jboss.tools.hb.ui.bot.test.configuration.EditConfigurationFileTest;
 import org.jboss.tools.hb.ui.bot.test.console.CreateConsoleConfigurationTest;
+import org.jboss.tools.hb.ui.bot.test.console.EditConsoleConfigurationTest;
+import org.jboss.tools.hb.ui.bot.test.diagram.MappingDiagramTest;
+import org.jboss.tools.hb.ui.bot.test.generation.CreateCodeGenerationConfiguration;
+import org.jboss.tools.hb.ui.bot.test.generation.JPADDLGenerationTest;
+import org.jboss.tools.hb.ui.bot.test.generation.JPAEntitiesGenerationTest;
+import org.jboss.tools.hb.ui.bot.test.jpa.CreateJPAProjectTest;
+import org.jboss.tools.hb.ui.bot.test.jpa.EditPersistenceXMLTest;
+import org.jboss.tools.hb.ui.bot.test.mappingfile.CreateMappingFileTest;
+import org.jboss.tools.hb.ui.bot.test.mappingfile.EditMappingFileTest;
+import org.jboss.tools.hb.ui.bot.test.perspective.JPAPerspectiveTest;
 import org.jboss.tools.hb.ui.bot.test.perspective.PerspectiveTest;
 import org.jboss.tools.ui.bot.ext.RequirementAwareSuite;
 import org.junit.runner.RunWith;
@@ -10,11 +20,28 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(RequirementAwareSuite.class)
 @SuiteClasses({
+	/* Configuration */
 	CreateConfigurationFileTest.class, 
 	EditConfigurationFileTest.class,
+	/* console */
+	CreateConsoleConfigurationTest.class,
+	EditConsoleConfigurationTest.class,
+	/* diagram */
+	MappingDiagramTest.class,
+	/* generation */
+	CreateCodeGenerationConfiguration.class,
+	JPADDLGenerationTest.class,
+	JPAEntitiesGenerationTest.class,
+	/* jpa */
+	CreateJPAProjectTest.class,
+	EditPersistenceXMLTest.class,
+	/* mappingfile */
+	CreateMappingFileTest.class,
+	EditMappingFileTest.class,
+	/* perspective */
+	JPAPerspectiveTest.class,
 	PerspectiveTest.class,
-	CreateConsoleConfigurationTest.class
-	})
+ 	})
 public class HibernateSuite {
 
 }
