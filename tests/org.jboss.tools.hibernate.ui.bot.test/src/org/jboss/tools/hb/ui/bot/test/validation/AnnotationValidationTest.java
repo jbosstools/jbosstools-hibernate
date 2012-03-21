50543e9717f841c5b168c398fa665c3ea8140e39
Jiri Peterka <peterka.jiri@gmail.com>
2012-03-21 07:46:34 +0000

    HB annotation validation test extended to recheck fixed state
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@39682 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hb/ui/bot/test/validation/AnnotationValidationTest.java
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hb/ui/bot/test/validation/AnnotationValidationTest.java
@@ -1,9 +1,12 @@
 package org.jboss.tools.hb.ui.bot.test.validation;
 
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotEditor;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
 import org.jboss.tools.hb.ui.bot.common.ProjectExplorer;
 import org.jboss.tools.hb.ui.bot.test.HibernateBaseTest;
 import org.jboss.tools.ui.bot.ext.config.Annotations.Require;
+import org.jboss.tools.ui.bot.ext.helper.StringHelper;
 import org.jboss.tools.ui.bot.ext.view.ProblemsView;
 import org.junit.Test;
 
@@ -28,15 +31,29 @@ public class AnnotationValidationTest extends HibernateBaseTest {
 	}
 
 	private void checkGenericGeneratorValidation() {
-		ProjectExplorer.open(prj, "src",pkg);
+		String resource = "GeneratorValidationEntity.java";
+		ProjectExplorer.open(prj, "src", pkg, resource);
 		
 		String desc = "No generator named \"mygen\" is defined in the persistence unit";
 		String path = "/" + prj + "/src/org/validation";
-		String resource = "GeneratorValidationEntity.java";
 		String type = "JPA Problem";
 		
 		SWTBotTreeItem[] items = null;
 		items = ProblemsView.getFilteredErrorsTreeItems(bot, desc, path, resource, type);				
 		assertTrue(items.length == 1);
+		
+		// fix 
+		SWTBotEditor editor = bot.editorByTitle(resource);
+		editor.show();
+		StringHelper sh = new StringHelper(editor.toTextEditor().getText());
+		Point pos = sh.getPositionAfter("\"mygen\"");
+		editor.toTextEditor().selectRange(pos.y, pos.x, 0);
+		editor.toTextEditor().insertText("erator");
+		editor.save();
+		util.waitForAll();
+		
+		// check
+		items = ProblemsView.getFilteredErrorsTreeItems(bot, desc, path, resource, type);				
+		assertTrue(items.length == 0);
 	}
 }
