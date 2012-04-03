4c9c97a921683658d59fc05e009a3cc69988f0bc
Jiri Peterka <peterka.jiri@gmail.com>
2012-04-03 07:22:55 +0000

    HB ui bot test HQL Editor Test fixed and check added + prj resource
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@39987 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hb/ui/bot/test/hql/HQLEditorTest.java
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hb/ui/bot/test/hql/HQLEditorTest.java
@@ -1,10 +1,14 @@
 package org.jboss.tools.hb.ui.bot.test.hql;
 
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotEditor;
+import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTable;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
 import org.jboss.tools.hb.ui.bot.common.ConfigurationFile;
 import org.jboss.tools.hb.ui.bot.common.ConsoleConfiguration;
 import org.jboss.tools.hb.ui.bot.test.HibernateBaseTest;
+import org.jboss.tools.ui.bot.ext.gen.ActionItem;
 import org.junit.Test;
 
 public class HQLEditorTest extends HibernateBaseTest {
@@ -19,18 +23,29 @@ public class HQLEditorTest extends HibernateBaseTest {
 		ConfigurationFile.create(new String[]{"hibernate40", "src"}, "hibernate.cfg.xml",false);
 		
 		executeHQLQuery();
+		checkHQLQueryResult();
 	}
 
 	private void executeHQLQuery() {
-		SWTBotTreeItem item = ConsoleConfiguration.selectNode("pre-hibernate40", "Configuration", clazz);
+		SWTBotTreeItem item = ConsoleConfiguration.selectNode("pre-hibernate40", "Configuration");
+
 		item.contextMenu("HQL Editor").click();
 		SWTBotEditor editor = bot.editorByTitle(console);
 		editor.setFocus();
+		editor.toTextEditor().setText("from " + clazz);
+		editor.save();
 		bot.toolbarButtonWithTooltip("Run HQL").click();
 		
-		bot.sleep(TIME_10S);
+		String osf= "Open Session factory";
+		bot.waitForShell(osf);
+		SWTBotShell shell = bot.shell(osf);
+		shell.bot().button("Yes").click();		
+	}
+	
+	private void checkHQLQueryResult() {
+		SWTBotView qr = open.viewOpen(ActionItem.View.HibernateHibernateQueryResult.LABEL);
+		SWTBotTable table = qr.bot().table();
+		assertTrue(table.rowCount() == 0);		
 	}
 
-
-	
 }
