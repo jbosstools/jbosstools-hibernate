ec09a72ed90d5dc3a6a6b60300752e51168b8468
Jiri Peterka <peterka.jiri@gmail.com>
2012-04-20 06:53:15 +0000

    Added new tests into HB ui bot test suite
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@40372 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hb/ui/bot/suite/HibernateSuite.java
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hb/ui/bot/suite/HibernateSuite.java
@@ -1,19 +1,25 @@
 package org.jboss.tools.hb.ui.bot.suite;
 
+import org.jboss.tools.hb.ui.bot.test.ant.ExportAntCodeGenerationTest;
 import org.jboss.tools.hb.ui.bot.test.configuration.CreateConfigurationFileTest;
 import org.jboss.tools.hb.ui.bot.test.configuration.EditConfigurationFileTest;
 import org.jboss.tools.hb.ui.bot.test.console.CreateConsoleConfigurationTest;
 import org.jboss.tools.hb.ui.bot.test.console.EditConsoleConfigurationTest;
+import org.jboss.tools.hb.ui.bot.test.criteria.CriteriaEditorTest;
 import org.jboss.tools.hb.ui.bot.test.diagram.MappingDiagramTest;
 import org.jboss.tools.hb.ui.bot.test.generation.CreateCodeGenerationConfiguration;
 import org.jboss.tools.hb.ui.bot.test.generation.JPADDLGenerationTest;
 import org.jboss.tools.hb.ui.bot.test.generation.JPAEntitiesGenerationTest;
+import org.jboss.tools.hb.ui.bot.test.generation.RunSchemaExportTest;
+import org.jboss.tools.hb.ui.bot.test.hql.HQLEditorTest;
+import org.jboss.tools.hb.ui.bot.test.jpa.CreateJPAEntityTest;
 import org.jboss.tools.hb.ui.bot.test.jpa.CreateJPAProjectTest;
 import org.jboss.tools.hb.ui.bot.test.jpa.EditPersistenceXMLTest;
 import org.jboss.tools.hb.ui.bot.test.mappingfile.CreateMappingFileTest;
 import org.jboss.tools.hb.ui.bot.test.mappingfile.EditMappingFileTest;
 import org.jboss.tools.hb.ui.bot.test.perspective.JPAPerspectiveTest;
 import org.jboss.tools.hb.ui.bot.test.perspective.PerspectiveTest;
+import org.jboss.tools.hb.ui.bot.test.reveng.CreateRevengFileTest;
 import org.jboss.tools.hb.ui.bot.test.validation.AnnotationValidationTest;
 import org.jboss.tools.hb.ui.bot.test.view.JPADetailViewTest;
 import org.jboss.tools.hb.ui.bot.test.view.PackageInfoTest;
@@ -23,20 +29,28 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(RequirementAwareSuite.class)
 @SuiteClasses({
+	/* Ant */
+	ExportAntCodeGenerationTest.class,
 	/* Configuration */
 	CreateConfigurationFileTest.class, 
 	EditConfigurationFileTest.class,
 	/* console */
 	CreateConsoleConfigurationTest.class,
 	EditConsoleConfigurationTest.class,
+	/* criteria */
+	CriteriaEditorTest.class,
 	/* diagram */
 	MappingDiagramTest.class,
 	/* generation */
 	CreateCodeGenerationConfiguration.class,
 	JPADDLGenerationTest.class,
 	JPAEntitiesGenerationTest.class,
+	RunSchemaExportTest.class,
+	/* hql */
+	HQLEditorTest.class,
 	/* jpa */
 	CreateJPAProjectTest.class,
+	CreateJPAEntityTest.class,
 	EditPersistenceXMLTest.class,
 	/* mappingfile */
 	CreateMappingFileTest.class,
@@ -44,6 +58,8 @@ import org.junit.runners.Suite.SuiteClasses;
 	/* perspective */
 	JPAPerspectiveTest.class,
 	PerspectiveTest.class,
+	/* reveng */
+	CreateRevengFileTest.class,
 	/* validation */
 	AnnotationValidationTest.class,
 	/* view */
