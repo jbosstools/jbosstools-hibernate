4aed63225de7c2d04e93e830ecd8119e2561fac8
Jiri Peterka <peterka.jiri@gmail.com>
2012-04-24 09:39:33 +0000

    EditConsoleConfigurationTest class removed from hb ui bot test
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@40443 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hb/ui/bot/test/console/EditConsoleConfigurationTest.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.tools.hb.ui.bot.test.console;
-
-import org.jboss.tools.hb.ui.bot.test.HibernateBaseTest;
-import org.jboss.tools.ui.bot.ext.config.Annotations.Require;
-
-@Require(clearProjects = true)
-public class EditConsoleConfigurationTest extends HibernateBaseTest {
-
-}
