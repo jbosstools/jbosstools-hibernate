d3eddd76b6595503393178c4c336239bfd7eb57e
Jiri Peterka <peterka.jiri@gmail.com>
2012-05-02 11:24:30 +0000

    All tests included in HB  JPASuite
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@40712 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hb/ui/bot/suite/JPASuite.java
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hb/ui/bot/suite/JPASuite.java
@@ -1,6 +1,8 @@
 package org.jboss.tools.hb.ui.bot.suite;
 
 import org.jboss.tools.hb.ui.bot.test.jpa.CreateJPAEntityTest;
+import org.jboss.tools.hb.ui.bot.test.jpa.CreateJPAProjectTest;
+import org.jboss.tools.hb.ui.bot.test.jpa.EditPersistenceXMLTest;
 import org.jboss.tools.ui.bot.ext.RequirementAwareSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
@@ -8,7 +10,8 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(RequirementAwareSuite.class)
 //@SuiteClasses({CreateJPAProjectTest.class})
 //@SuiteClasses({EditPersistenceXMLTest.class})
-@SuiteClasses({CreateJPAEntityTest.class})
+//@SuiteClasses({CreateJPAEntityTest.class})
+@SuiteClasses({CreateJPAProjectTest.class,CreateJPAEntityTest.class,EditPersistenceXMLTest.class})
 public class JPASuite {
 
 }
