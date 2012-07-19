f633744db29f6a1d692edcfcac15d6db6a4dff88
Jiri Peterka <peterka.jiri@gmail.com>
2012-07-19 08:26:13 +0000

    Mapping diagram editor check added into the test
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@42645 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hb/ui/bot/test/diagram/MappingDiagramTest.java
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hb/ui/bot/test/diagram/MappingDiagramTest.java
@@ -1,5 +1,8 @@
 package org.jboss.tools.hb.ui.bot.test.diagram;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
 import org.jboss.tools.hb.ui.bot.common.Tree;
@@ -18,19 +21,27 @@ import org.junit.Test;
 public class MappingDiagramTest extends HibernateBaseTest {
 	
 	final String hc = "hibernate35";
-	
+
+	/**
+	 * Test imports projects and check if mapping diagram can be opened
+	 */
 	@Test
 	public void showMappingDiagram() {
 		importTestProject("/resources/prj/hibernatelib");
 		importTestProject("/resources/prj/hibernate35");
 		util.waitForAll();
 		openDiagram();
+		bot.sleep(TIME_10S);
 	}
 
 	private void openDiagram() {
 		SWTBotView hcv = open.viewOpen(ActionItem.View.HibernateHibernateConfigurations.LABEL);
 		SWTBotTreeItem item = Tree.select(hcv.bot(), hc, "Configuration");
 		item.contextMenu("Mapping Diagram").click();
+		String title = bot.activeEditor().getTitle();
 		
+		Pattern pattern = Pattern.compile(hc + ".*");
+		Matcher matcher = pattern.matcher(title);
+		assertTrue("Mapping diagram editor must be found",matcher.matches());
 	}
 }
