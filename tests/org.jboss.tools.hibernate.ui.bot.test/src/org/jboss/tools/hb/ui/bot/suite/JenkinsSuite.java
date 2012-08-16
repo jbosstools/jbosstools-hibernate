9487f12e6ee91d95d0ac068a8332a8288e0a8e5f
Jiri Peterka <peterka.jiri@gmail.com>
2012-08-16 07:29:10 +0000

    Suite for jenkins test and profile added & pom update
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@43065 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hb/ui/bot/suite/JenkinsSuite.java
@@ -0,0 +1,20 @@
+package org.jboss.tools.hb.ui.bot.suite;
+
+import org.jboss.tools.hb.ui.bot.test.perspective.PerspectiveTest;
+import org.jboss.tools.ui.bot.ext.RequirementAwareSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Suite of tests for to be executed on jenkins slave
+ * Same test from Hibernate suite are not here because of known issues
+ * @author jpeterka
+ *
+ */
+@RunWith(RequirementAwareSuite.class)
+@SuiteClasses({
+	PerspectiveTest.class
+ 	})
+public class JenkinsSuite {
+
+}
