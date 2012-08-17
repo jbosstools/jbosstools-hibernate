738414966cecb48fe802e2965281d257d2b7de52
Jiri Peterka <peterka.jiri@gmail.com>
2012-08-17 07:32:41 +0000

    Test added into hb ui bot jenkins suite
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@43082 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hb/ui/bot/suite/JenkinsSuite.java
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hb/ui/bot/suite/JenkinsSuite.java
@@ -2,6 +2,7 @@ package org.jboss.tools.hb.ui.bot.suite;
 
 import org.jboss.tools.hb.ui.bot.test.configuration.CreateConfigurationFileTest;
 import org.jboss.tools.hb.ui.bot.test.perspective.PerspectiveTest;
+import org.jboss.tools.hb.ui.bot.test.reveng.CreateRevengFileTest;
 import org.jboss.tools.ui.bot.ext.RequirementAwareSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
@@ -15,7 +16,9 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(RequirementAwareSuite.class)
 @SuiteClasses({
 	PerspectiveTest.class,
-	CreateConfigurationFileTest.class
+	CreateConfigurationFileTest.class,
+	//EditConfigurationFileTest.class - Multipage editor must be fixed
+	CreateRevengFileTest.class
  	})
 public class JenkinsSuite {
 
