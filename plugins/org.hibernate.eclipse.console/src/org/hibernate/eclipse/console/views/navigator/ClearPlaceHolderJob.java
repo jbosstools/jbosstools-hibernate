a9791f3c405d9e12a09638107deda3097f4df852
Nick Boldt <nboldt@redhat.com>
2012-09-30 06:22:36 +0000

    https://issues.jboss.org/browse/JBIDE-12427 fix dual-license issue (now only EPL) and update copyright date to 2012 with note about previous mistaken license terms
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@44157 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.hibernate.eclipse.console/src/org/hibernate/eclipse/console/views/navigator/ClearPlaceHolderJob.java
+++ plugins/org.hibernate.eclipse.console/src/org/hibernate/eclipse/console/views/navigator/ClearPlaceHolderJob.java
@@ -1,30 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2004 IBM Corporation and others.
+ * Copyright (c) 2003-2012 Red Hat, Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:/*
- * JBoss, Home of Professional Open Source
- * Copyright 2005, JBoss Inc., and individual contributors as indicated
- * by the @authors tag. See the copyright.txt in the distribution for a
- * full listing of individual contributors.
- *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * Contributors:
+ *     IBM Corporation (and others) - Initial implementation, 2003-2004
+ *     JBoss by Red Hat - Initial contribution, 2005 (was erroneously licensed under LGPL 2.1)
  */
 package org.hibernate.eclipse.console.views.navigator;
 
