8c6eb36cb462a4318e1ac2853fb9b83299549dcf
Koen Aers <koen.aers@gmail.com>
2014-02-18 17:18:48 +0100

    JBIDE-15333: Bundle Hibernate 4.3 (initial commit)

---
+++ plugins/org.jboss.tools.hibernate4_3/src/org/jboss/tools/hibernate4_3/HibernateExtension4_3.java
@@ -0,0 +1,316 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.hibernate4_3;
+
+import java.net.URL;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.eclipse.osgi.util.NLS;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.boot.registry.internal.StandardServiceRegistryImpl;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Settings;
+import org.hibernate.console.ConsoleConfigClassLoader;
+import org.hibernate.console.ConsoleMessages;
+import org.hibernate.console.QueryInputModel;
+import org.hibernate.console.QueryPage;
+import org.hibernate.console.execution.DefaultExecutionContext;
+import org.hibernate.console.execution.ExecutionContext;
+import org.hibernate.console.execution.ExecutionContext.Command;
+import org.hibernate.console.ext.HibernateException;
+import org.hibernate.console.ext.HibernateExtension;
+import org.hibernate.console.preferences.ConsoleConfigurationPreferences;
+import org.hibernate.console.preferences.PreferencesClassPathUtils;
+import org.hibernate.service.ServiceRegistry;
+
+/**
+ * 
+ * @author Dmitry Geraskov
+ *
+ */
+public class HibernateExtension4_3 implements HibernateExtension {
+	
+	private ConsoleConfigClassLoader classLoader = null;
+
+	private ExecutionContext executionContext;
+	
+	private ConsoleConfigurationPreferences prefs;
+	
+	private Configuration configuration;
+	
+	private SessionFactory sessionFactory;
+	
+	private ServiceRegistry serviceRegistry;
+	
+	private Map<String, FakeDelegatingDriver> fakeDrivers = new HashMap<String, FakeDelegatingDriver>();
+
+	public HibernateExtension4_3() {
+	}
+
+	@Override
+	public String getHibernateVersion() {
+		return "4.3";
+	}
+	
+	@Override
+	public QueryPage executeHQLQuery(final String hql,
+			final QueryInputModel queryParameters) {
+		return (QueryPage)execute(new Command() {
+			public Object execute() {
+				Session session = sessionFactory.openSession();
+				QueryPage qp = new HQLQueryPage(HibernateExtension4_3.this, hql,queryParameters);
+				qp.setSession(session);
+				return qp;
+			}
+		});
+	}
+
+	@Override
+	public QueryPage executeCriteriaQuery(final String criteriaCode,
+			final QueryInputModel model) {
+		return (QueryPage)execute(new Command() {
+			public Object execute() {
+				Session session = sessionFactory.openSession();
+				QueryPage qp = new JavaPage(HibernateExtension4_3.this,criteriaCode,model);
+				qp.setSession(session);
+				return qp;
+			}
+		});
+	}
+
+	/**
+	 * @param ConsoleConfigurationPreferences the prefs to set
+	 */
+	public void setConsoleConfigurationPreferences(ConsoleConfigurationPreferences prefs) {
+		this.prefs = prefs;
+	}
+	
+	public void build() {
+		configuration = buildWith(null, true);
+	}
+
+	@Override
+	public void buildSessionFactory() {
+		execute(new Command() {
+			public Object execute() {
+				if (sessionFactory != null) {
+					throw new HibernateException("Factory was not closed before attempt to build a new Factory");
+				}
+				serviceRegistry =  new StandardServiceRegistryBuilder()
+					.applySettings( configuration.getProperties())
+					.build();
+				sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+				return null;
+			}
+		});
+	}
+
+	@Override
+	public boolean closeSessionFactory() {
+		boolean res = false;
+		if (sessionFactory != null) {
+			sessionFactory.close();
+			sessionFactory = null;
+			( (StandardServiceRegistryImpl) serviceRegistry ).destroy();
+			serviceRegistry = null;
+			res = true;
+		}
+		return res;
+	}
+
+	public Configuration buildWith(final Configuration cfg, final boolean includeMappings) {
+		reinitClassLoader();
+		//TODO handle user libraries here
+		executionContext = new DefaultExecutionContext(prefs.getName(), classLoader);
+		Configuration result = (Configuration)execute(new Command() {
+			public Object execute() {
+				ConfigurationFactory cf = new ConfigurationFactory(prefs, fakeDrivers);
+				return cf.createConfiguration(cfg, includeMappings);
+			}
+		});
+		return result;
+	}
+	
+	/**
+	 * Create class loader - so it uses the original urls list from preferences. 
+	 */
+	protected void reinitClassLoader() {
+		//the class loader caches user's compiled classes
+		//need to rebuild it on every console configuration rebuild to pick up latest versions.
+		final URL[] customClassPathURLs = PreferencesClassPathUtils.getCustomClassPathURLs(prefs);
+		cleanUpClassLoader();
+		classLoader = createClassLoader(customClassPathURLs);
+	}
+	
+	protected ConsoleConfigClassLoader createClassLoader(final URL[] customClassPathURLs) {
+		ConsoleConfigClassLoader classLoader = AccessController.doPrivileged(new PrivilegedAction<ConsoleConfigClassLoader>() {
+			public ConsoleConfigClassLoader run() {
+				return new ConsoleConfigClassLoader(customClassPathURLs, Thread.currentThread().getContextClassLoader()) {
+					protected Class<?> findClass(String name) throws ClassNotFoundException {
+						try {
+							return super.findClass(name);
+						} catch (ClassNotFoundException cnfe) {
+							throw cnfe;
+						} catch (IllegalStateException e){
+							e.printStackTrace();
+							throw e;
+						}
+					}
+		
+					protected synchronized Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+						try {
+							return super.loadClass(name, resolve);
+						} catch (ClassNotFoundException cnfe) {
+							throw cnfe;
+						}
+					}
+		
+					public Class<?> loadClass(String name) throws ClassNotFoundException {
+						try {
+							return super.loadClass(name);
+						} catch (ClassNotFoundException cnfe) {
+							throw cnfe;
+						}
+					}
+					
+					public URL getResource(String name) {
+					      return super.getResource(name);
+					}
+				};
+			}
+		});
+		return classLoader;
+	}
+
+	public String getName() {
+		return prefs.getName();
+	}
+	
+	public ExecutionContext getExecutionContext() {
+		return executionContext;
+	}
+	
+	public Object execute(Command c) {
+		if (executionContext != null) {
+			return executionContext.execute(c);
+		}
+		final String msg = NLS.bind(ConsoleMessages.ConsoleConfiguration_null_execution_context, getName());
+		throw new HibernateException(msg);
+	}
+
+	@Override
+	public boolean reset() {
+		boolean resetted = false;
+		// reseting state
+		if (configuration != null) {
+			configuration = null;
+			resetted = true;
+		}
+		
+		resetted = resetted | closeSessionFactory() | cleanUpClassLoader();
+
+		executionContext = null;
+		return resetted;
+	}
+	
+	protected boolean cleanUpClassLoader() {
+		boolean resetted = false;
+		if (executionContext != null) {
+			executionContext.execute(new Command() {
+				public Object execute() {
+					Iterator<FakeDelegatingDriver> it = fakeDrivers.values().iterator();
+					while (it.hasNext()) {
+						try {
+							DriverManager.deregisterDriver(it.next());
+						} catch (SQLException e) {
+							// ignore
+						}
+					}
+					return null;
+				}
+			});
+		}
+		if (fakeDrivers.size() > 0) {
+			fakeDrivers.clear();
+			resetted = true;
+		}
+		ClassLoader classLoaderTmp = classLoader;
+		while (classLoaderTmp != null) {
+			if (classLoaderTmp instanceof ConsoleConfigClassLoader) {
+				((ConsoleConfigClassLoader)classLoaderTmp).close();
+				resetted = true;
+			}
+			classLoaderTmp = classLoaderTmp.getParent();
+		}
+		if (classLoader != null) {
+			classLoader = null;
+			resetted = true;
+		}
+		return resetted;
+	}
+
+	@Override
+	public boolean hasConfiguration() {
+		return configuration != null;
+	}
+
+	/**
+	 * @return
+	 */
+	public Configuration getConfiguration() {
+		return configuration;
+	}
+	
+	public Settings getSettings(final Configuration cfg, final ServiceRegistry serviceRegisrty) {
+		return (Settings) execute(new Command() {
+			public Object execute() {
+				return cfg.buildSettings(serviceRegisrty);
+			}
+		});
+	}
+
+	@Override
+	public boolean isSessionFactoryCreated() {
+		return sessionFactory != null;
+	}
+	
+	public String generateSQL(final String query) {
+		return QueryHelper.generateSQL(executionContext, sessionFactory, query);
+	}
+
+	@Override
+	public void buildMappings() {
+		execute(new Command() {
+			public Object execute() {
+				getConfiguration().buildMappings();
+				return null;
+			}
+		});
+	}
+	
+	@Override
+	public boolean hasExecutionContext() {
+		return executionContext != null;
+	}
+	
+	@Override
+	public String getConsoleConfigurationName() {
+		return prefs.getName();
+	}
+}
---
+++ plugins/org.jboss.tools.hibernate4_3/src/org/jboss/tools/hibernate4_3/QueryHelper.java
@@ -0,0 +1,93 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.hibernate4_3;
+
+import java.util.Collections;
+import java.util.Iterator;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.console.execution.ExecutionContext;
+import org.hibernate.eclipse.console.HibernateConsoleMessages;
+import org.hibernate.eclipse.console.utils.QLFormatHelper;
+import org.hibernate.engine.query.spi.HQLQueryPlan;
+import org.hibernate.hql.spi.QueryTranslator;
+import org.hibernate.internal.SessionFactoryImpl;
+import org.hibernate.type.Type;
+import org.hibernate.util.xpl.StringHelper;
+
+public class QueryHelper {
+	
+	
+	static String generateSQL(ExecutionContext executionContext, final SessionFactory sessionFactory, final String query) {
+
+		if(StringHelper.isEmpty(query)) return ""; //$NON-NLS-1$
+
+		String result = (String) executionContext.execute(new ExecutionContext.Command() {
+			@SuppressWarnings("unchecked")
+			public Object execute() {
+				try {
+					SessionFactoryImpl sfimpl = (SessionFactoryImpl) sessionFactory; // hack - to get to the actual queries..
+					StringBuffer str = new StringBuffer(256);
+					HQLQueryPlan plan = new HQLQueryPlan(query, false, Collections.EMPTY_MAP, sfimpl);
+
+					QueryTranslator[] translators = plan.getTranslators();
+					for (int i = 0; i < translators.length; i++) {
+						QueryTranslator translator = translators[i];
+						if(translator.isManipulationStatement()) {
+							str.append(HibernateConsoleMessages.DynamicSQLPreviewView_manipulation_of + i + ":"); //$NON-NLS-1$
+							Iterator<?> iterator = translator.getQuerySpaces().iterator();
+							while ( iterator.hasNext() ) {
+								Object qspace = iterator.next();
+								str.append(qspace);
+								if(iterator.hasNext()) { str.append(", "); } //$NON-NLS-1$
+							}
+
+						} else {
+							Type[] returnTypes = translator.getReturnTypes();
+							str.append(i +": "); //$NON-NLS-1$
+							for (int j = 0; j < returnTypes.length; j++) {
+								Type returnType = returnTypes[j];
+								str.append(returnType.getName());
+								if(j<returnTypes.length-1) { str.append(", "); }							 //$NON-NLS-1$
+							}
+						}
+						str.append("\n-----------------\n"); //$NON-NLS-1$
+						Iterator<?> sqls = translator.collectSqlStrings().iterator();
+						while ( sqls.hasNext() ) {
+							String sql = (String) sqls.next();
+							str.append(QLFormatHelper.formatForScreen(sql));
+							str.append("\n\n");	 //$NON-NLS-1$
+						}
+					}
+					return str.toString();
+				} catch(Throwable t) {
+					StringBuffer msgs = new StringBuffer();
+
+					Throwable cause = t;
+					while(cause!=null) {
+						msgs.append(t);
+						if(cause.getCause()==cause) {
+							cause=null;
+						} else {
+							cause = cause.getCause();
+							if(cause!=null) msgs.append(HibernateConsoleMessages.DynamicSQLPreviewView_caused_by);
+						}
+					}
+					return msgs.toString();
+				}
+
+			}
+		});
+
+		return result;
+	}
+
+}
