632769bde2e03b411ca1e151f530ee60c722cbea
Koen Aers <koen.aers@gmail.com>
2014-06-03 11:37:49 +0200

    remove unused classes

--- plugins/org.hibernate.eclipse/src/org/hibernate/console/spi/HibernateSessionFactory.java
+++
@@ -1,7 +0,0 @@
-package org.hibernate.console.spi;
-
-public interface HibernateSessionFactory {
-	
-	void close();
-
-}
--- plugins/org.hibernate.eclipse/src/org/hibernate/console/spi/HibernateSettings.java
+++
@@ -1,5 +0,0 @@
-package org.hibernate.console.spi;
-
-public interface HibernateSettings {
-
-}
