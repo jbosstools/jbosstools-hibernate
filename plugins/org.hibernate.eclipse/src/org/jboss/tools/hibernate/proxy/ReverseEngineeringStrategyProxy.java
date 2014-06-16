38fbcd36eb50fdee3353c00efc5b9094a3407251
Koen Aers <koen.aers@gmail.com>
2014-06-16 12:16:48 +0200

    add IReverseEngineeringStrategy and ReverseEngineeringStrategyProxy

---
+++ plugins/org.hibernate.eclipse/src/org/jboss/tools/hibernate/proxy/ReverseEngineeringStrategyProxy.java
@@ -0,0 +1,8 @@
+package org.jboss.tools.hibernate.proxy;
+
+import org.jboss.tools.hibernate.spi.IReverseEngineeringStrategy;
+
+public class ReverseEngineeringStrategyProxy implements
+		IReverseEngineeringStrategy {
+
+}
