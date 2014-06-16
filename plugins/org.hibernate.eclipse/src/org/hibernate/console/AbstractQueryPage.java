5ed1496ee87eeea1a6fda2fb8f66be79066bc1a2
Koen Aers <koen.aers@gmail.com>
2014-06-16 12:16:03 +0200

    organize imports

--- plugins/org.hibernate.eclipse/src/org/hibernate/console/AbstractQueryPage.java
+++ plugins/org.hibernate.eclipse/src/org/hibernate/console/AbstractQueryPage.java
@@ -26,7 +26,6 @@ import java.beans.PropertyChangeSupport;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.Session;
 import org.hibernate.console.ext.HibernateExtension;
 
 /**
