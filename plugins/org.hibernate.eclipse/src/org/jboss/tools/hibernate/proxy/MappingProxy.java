3c24f5d435fbd6a37b66578ece93392d5d7fab63
Koen Aers <koen.aers@gmail.com>
2014-06-24 12:12:35 +0200

    add IMapping interface and MappingProxy implementation

---
+++ plugins/org.hibernate.eclipse/src/org/jboss/tools/hibernate/proxy/MappingProxy.java
@@ -0,0 +1,18 @@
+package org.jboss.tools.hibernate.proxy;
+
+import org.hibernate.engine.Mapping;
+import org.jboss.tools.hibernate.spi.IMapping;
+
+public class MappingProxy implements IMapping {
+	
+	private Mapping target = null;
+	
+	public MappingProxy(Mapping m) {
+		target = m;
+	}
+
+	Mapping getTarget() {
+		return target;
+	}
+
+}
