1bdafffe6240edd6d610b755b9ac8b18fca0e820
Koen Aers <koen.aers@gmail.com>
2014-07-08 11:34:32 +0200

    add IPersistentClass and PersistentClassProxy

---
+++ plugins/org.hibernate.eclipse/src/org/jboss/tools/hibernate/proxy/PersistentClassProxy.java
@@ -0,0 +1,156 @@
+package org.jboss.tools.hibernate.proxy;
+
+import java.util.Iterator;
+
+import org.hibernate.mapping.Join;
+import org.hibernate.mapping.PersistentClass;
+import org.hibernate.mapping.Property;
+import org.hibernate.mapping.RootClass;
+import org.hibernate.mapping.Subclass;
+import org.jboss.tools.hibernate.spi.IPersistentClass;
+import org.jboss.tools.hibernate.spi.IProperty;
+import org.jboss.tools.hibernate.spi.ITable;
+import org.jboss.tools.hibernate.spi.IValue;
+
+public class PersistentClassProxy implements IPersistentClass {
+	
+	private PersistentClass target = null;
+	private IPersistentClass rootClass = null;
+	private IPersistentClass superClass = null;
+	private ITable table = null;
+	private IValue discriminator = null;
+	private IValue identifier = null;
+	private IProperty version = null;
+
+	public PersistentClassProxy(PersistentClass persistentClass) {
+		target = persistentClass;
+	}
+
+	public PersistentClass getTarget() {
+		return target;
+	}
+
+	@Override
+	public String getClassName() {
+		return target.getClassName();
+	}
+
+	@Override
+	public String getEntityName() {
+		return target.getEntityName();
+	}
+
+	@Override
+	public boolean isAssignableToRootClass() {
+		return RootClass.class.isAssignableFrom(target.getClass());
+	}
+
+	@Override
+	public boolean isRootClass() {
+		return target.getClass() == RootClass.class;
+	}
+
+	@Override
+	public Property getIdentifierProperty() {
+		return target.getIdentifierProperty();
+	}
+
+	@Override
+	public boolean hasIdentifierProperty() {
+		return target.hasIdentifierProperty();
+	}
+
+	@Override
+	public boolean isInstanceOfRootClass() {
+		return target instanceof RootClass;
+	}
+
+	@Override
+	public boolean isInstanceOfSubclass() {
+		return target instanceof Subclass;
+	}
+
+	@Override
+	public String getNodeName() {
+		return target.getNodeName();
+	}
+
+	@Override
+	public IPersistentClass getRootClass() {
+		if (rootClass == null) {
+			rootClass = new PersistentClassProxy(target.getRootClass());
+		}
+		return rootClass;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Iterator<Property> getPropertyClosureIterator() {
+		return target.getPropertyClosureIterator();
+	}
+
+	@Override
+	public IPersistentClass getSuperclass() {
+		if (superClass != null) {
+			superClass = new PersistentClassProxy(target.getSuperclass());
+		}
+		return superClass;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Iterator<Property> getPropertyIterator() {
+		return target.getPropertyIterator();
+	}
+
+	@Override
+	public Property getProperty(String string) {
+		return target.getProperty(string);
+	}
+
+	@Override
+	public ITable getTable() {
+		if (table == null) {
+			table = new TableProxy(target.getTable());
+		}
+		return table;
+	}
+
+	@Override
+	public boolean isAbstract() {
+		return target.isAbstract();
+	}
+
+	@Override
+	public IValue getDiscriminator() {
+		if (discriminator == null) {
+			discriminator = new ValueProxy(target.getDiscriminator());
+		}
+		return discriminator;
+	}
+
+	@Override
+	public IValue getIdentifier() {
+		if (identifier == null) {
+			identifier = new ValueProxy(target.getIdentifier());
+		}
+		return identifier;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Iterator<Join> getJoinIterator() {
+		return target.getJoinIterator();
+	}
+
+	@Override
+	public IProperty getVersion() {
+		if (version == null) {
+			version = new PropertyProxy(target.getVersion());
+		}
+		return version;
+	}
+	
+	
+
+}
---
+++ plugins/org.hibernate.eclipse/src/org/jboss/tools/hibernate/spi/IPersistentClass.java
@@ -0,0 +1,31 @@
+package org.jboss.tools.hibernate.spi;
+
+import java.util.Iterator;
+
+import org.hibernate.mapping.Join;
+import org.hibernate.mapping.Property;
+
+public interface IPersistentClass {
+
+	String getClassName();
+	String getEntityName();
+	boolean isAssignableToRootClass();
+	boolean isRootClass();
+	Property getIdentifierProperty();
+	boolean hasIdentifierProperty();
+	boolean isInstanceOfRootClass();
+	boolean isInstanceOfSubclass();
+	String getNodeName();
+	IPersistentClass getRootClass();
+	Iterator<Property> getPropertyClosureIterator();
+	IPersistentClass getSuperclass();
+	Iterator<Property> getPropertyIterator();
+	Property getProperty(String string);
+	ITable getTable();
+	boolean isAbstract();
+	IValue getDiscriminator();
+	IValue getIdentifier();
+	Iterator<Join> getJoinIterator();
+	IProperty getVersion();
+
+}
