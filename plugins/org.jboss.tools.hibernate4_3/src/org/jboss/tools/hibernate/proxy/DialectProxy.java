f789f3081b0e0c0b547749f67af3d1d7ad6455cd
Koen Aers <koen.aers@gmail.com>
2014-07-17 00:20:12 +0200

    add the missing *Proxy classes to plugin org.jboss.tools.hibernate4_3

---
+++ plugins/org.jboss.tools.hibernate4_3/src/org/jboss/tools/hibernate/proxy/DialectProxy.java
@@ -0,0 +1,33 @@
+package org.jboss.tools.hibernate.proxy;
+
+import org.hibernate.dialect.Dialect;
+import org.jboss.tools.hibernate.spi.IDialect;
+
+public class DialectProxy implements IDialect {
+	
+	private Dialect target;
+	
+	public DialectProxy(Dialect dialect) {
+		target = dialect;
+	}
+
+	Dialect getTarget() {
+		return target;
+	}
+	
+	@Override
+	public String toString() {
+		return target.toString();
+	}
+
+	@Override
+	public char openQuote() {
+		return target.openQuote();
+	}
+
+	@Override
+	public char closeQuote() {
+		return target.closeQuote();
+	}
+
+}
