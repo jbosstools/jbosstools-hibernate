93949939748a76f232a8ed3930aa8f8b3a583359
Koen Aers <koen.aers@gmail.com>
2014-08-10 13:10:42 +0200

    implement ICriteria.createCriteria(String, String), ICriteria.setMaxResults(int) and ICriteria.list()

--- plugins/org.jboss.tools.hibernate.spi/src/org/jboss/tools/hibernate/spi/ICriteria.java
+++ plugins/org.jboss.tools.hibernate.spi/src/org/jboss/tools/hibernate/spi/ICriteria.java
@@ -1,5 +1,11 @@
 package org.jboss.tools.hibernate.spi;
 
+import java.util.List;
+
 public interface ICriteria {
+
+	ICriteria createCriteria(String associationPath, String alias);
+	void setMaxResults(int intValue);
+	List<Object> list();
 
 }
--- plugins/org.jboss.tools.hibernate4_0/src/org/jboss/tools/hibernate/proxy/CriteriaProxy.java
+++ plugins/org.jboss.tools.hibernate4_0/src/org/jboss/tools/hibernate/proxy/CriteriaProxy.java
@@ -1,5 +1,7 @@
 package org.jboss.tools.hibernate.proxy;
 
+import java.util.List;
+
 import org.hibernate.Criteria;
 import org.jboss.tools.hibernate.spi.ICriteria;
 
@@ -13,6 +15,17 @@ public class CriteriaProxy implements ICriteria {
 	
 	public ICriteria createCriteria(String associationPath, String alias) {
 		return new CriteriaProxy(target.createCriteria(associationPath, alias));
+	}
+
+	@Override
+	public void setMaxResults(int intValue) {
+		target.setMaxResults(intValue);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Object> list() {
+		return target.list();
 	}
 
 }
