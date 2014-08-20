8ff46c1358c2691da24bf5e6cd04bb31936c9ac6
Koen Aers <koen.aers@gmail.com>
2014-08-20 19:42:00 +0200

    remove IHQLCompletionHandler.getCompletionProposals()

--- plugins/org.jboss.tools.hibernate.spi/src/org/jboss/tools/hibernate/spi/IHQLCompletionHandler.java
+++ plugins/org.jboss.tools.hibernate.spi/src/org/jboss/tools/hibernate/spi/IHQLCompletionHandler.java
@@ -1,12 +1,8 @@
 package org.jboss.tools.hibernate.spi;
 
-import java.util.List;
-
-import org.eclipse.jface.text.contentassist.ICompletionProposal;
 
 public interface IHQLCompletionHandler {
 
-	List<ICompletionProposal> getCompletionProposals();
 	boolean accept(IHQLCompletionProposal proposal);
 	void completionFailure(String errorMessage);
 	String getLastErrorMessage();
