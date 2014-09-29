1cf7b6aba2d4af88597ad6c73bc66a9f5265caa4
Koen Aers <koen.aers@gmail.com>
2014-09-29 13:34:26 +0200

    Add missing "//-NLS-1$" tags

--- plugins/org.hibernate.eclipse/src/org/hibernate/util/xpl/DTDEntityResolver.java
+++ plugins/org.hibernate.eclipse/src/org/hibernate/util/xpl/DTDEntityResolver.java
@@ -16,9 +16,9 @@ public class DTDEntityResolver implements EntityResolver, Serializable {
 
 	private static final Logger log = LoggerFactory.getLogger( DTDEntityResolver.class );
 
-	private static final String HIBERNATE_NAMESPACE = "http://www.hibernate.org/dtd/";
-	private static final String OLD_HIBERNATE_NAMESPACE = "http://hibernate.sourceforge.net/";
-	private static final String USER_NAMESPACE = "classpath://";
+	private static final String HIBERNATE_NAMESPACE = "http://www.hibernate.org/dtd/"; //$NON-NLS-1$
+	private static final String OLD_HIBERNATE_NAMESPACE = "http://hibernate.sourceforge.net/"; //$NON-NLS-1$
+	private static final String USER_NAMESPACE = "classpath://"; //$NON-NLS-1$
 	
 	private IService service;
 
@@ -29,28 +29,28 @@ public class DTDEntityResolver implements EntityResolver, Serializable {
 	public InputSource resolveEntity(String publicId, String systemId) {
 		InputSource source = null; // returning null triggers default behavior
 		if ( systemId != null ) {
-			log.debug( "trying to resolve system-id [" + systemId + "]" );
+			log.debug( "trying to resolve system-id [" + systemId + "]" );  //$NON-NLS-1$//$NON-NLS-2$
 			if ( systemId.startsWith( HIBERNATE_NAMESPACE ) ) {
-				log.debug( "recognized hibernate namespace; attempting to resolve on classpath under org/hibernate/" );
+				log.debug( "recognized hibernate namespace; attempting to resolve on classpath under org/hibernate/" ); //$NON-NLS-1$
 				source = resolveOnClassPath( publicId, systemId, HIBERNATE_NAMESPACE );
 			}
 			else if ( systemId.startsWith( OLD_HIBERNATE_NAMESPACE ) ) {
 				log.warn(
-						"recognized obsolete hibernate namespace " + OLD_HIBERNATE_NAMESPACE + ". Use namespace "
-								+ HIBERNATE_NAMESPACE + " instead. Refer to Hibernate 3.6 Migration Guide!"
+						"recognized obsolete hibernate namespace " + OLD_HIBERNATE_NAMESPACE + ". Use namespace " //$NON-NLS-1$ //$NON-NLS-2$
+								+ HIBERNATE_NAMESPACE + " instead. Refer to Hibernate 3.6 Migration Guide!" //$NON-NLS-1$
 				);
-				log.debug( "attempting to resolve on classpath under org/hibernate/" );
+				log.debug( "attempting to resolve on classpath under org/hibernate/" ); //$NON-NLS-1$
 				source = resolveOnClassPath( publicId, systemId, OLD_HIBERNATE_NAMESPACE );
 			}
 			else if ( systemId.startsWith( USER_NAMESPACE ) ) {
-				log.debug( "recognized local namespace; attempting to resolve on classpath" );
+				log.debug( "recognized local namespace; attempting to resolve on classpath" ); //$NON-NLS-1$
 				String path = systemId.substring( USER_NAMESPACE.length() );
 				InputStream stream = resolveInLocalNamespace( path );
 				if ( stream == null ) {
-					log.debug( "unable to locate [" + systemId + "] on classpath" );
+					log.debug( "unable to locate [" + systemId + "] on classpath" );  //$NON-NLS-1$//$NON-NLS-2$
 				}
 				else {
-					log.debug( "located [" + systemId + "] in classpath" );
+					log.debug( "located [" + systemId + "] in classpath" );  //$NON-NLS-1$//$NON-NLS-2$
 					source = new InputSource( stream );
 					source.setPublicId( publicId );
 					source.setSystemId( systemId );
@@ -62,16 +62,16 @@ public class DTDEntityResolver implements EntityResolver, Serializable {
 
 	private InputSource resolveOnClassPath(String publicId, String systemId, String namespace) {
 		InputSource source = null;
-		String path = "org/hibernate/" + systemId.substring( namespace.length() );
+		String path = "org/hibernate/" + systemId.substring( namespace.length() ); //$NON-NLS-1$
 		InputStream dtdStream = resolveInHibernateNamespace( path );
 		if ( dtdStream == null ) {
-			log.debug( "unable to locate [" + systemId + "] on classpath" );
-			if ( systemId.substring( namespace.length() ).indexOf( "2.0" ) > -1 ) {
-				log.error( "Don't use old DTDs, read the Hibernate 3.x Migration Guide!" );
+			log.debug( "unable to locate [" + systemId + "] on classpath" );  //$NON-NLS-1$//$NON-NLS-2$
+			if ( systemId.substring( namespace.length() ).indexOf( "2.0" ) > -1 ) { //$NON-NLS-1$
+				log.error( "Don't use old DTDs, read the Hibernate 3.x Migration Guide!" ); //$NON-NLS-1$
 			}
 		}
 		else {
-			log.debug( "located [" + systemId + "] in classpath" );
+			log.debug( "located [" + systemId + "] in classpath" );  //$NON-NLS-1$//$NON-NLS-2$
 			source = new InputSource( dtdStream );
 			source.setPublicId( publicId );
 			source.setSystemId( systemId );
@@ -93,7 +93,7 @@ public class DTDEntityResolver implements EntityResolver, Serializable {
 	}
 	
 	private InputStream getUserResourceAsStream(String resource) {
-		boolean hasLeadingSlash = resource.startsWith( "/" );
+		boolean hasLeadingSlash = resource.startsWith( "/" ); //$NON-NLS-1$
 		String stripped = hasLeadingSlash ? resource.substring(1) : resource;
 
 		InputStream stream = null;
@@ -114,7 +114,7 @@ public class DTDEntityResolver implements EntityResolver, Serializable {
 		}
 
 		if ( stream == null ) {
-			throw new HibernateException( resource + " not found" );
+			throw new HibernateException( resource + " not found" ); //$NON-NLS-1$
 		}
 
 		return stream;
