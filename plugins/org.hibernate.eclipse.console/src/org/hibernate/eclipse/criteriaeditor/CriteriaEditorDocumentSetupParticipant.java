a4a0c004c6ff9a8fc18a2d4d976229935b890599
Koen Aers <koen.aers@gmail.com>
2014-11-05 12:52:17 +0100

    Fix NPE by fconnecting the partitioner to the document earlier

--- plugins/org.hibernate.eclipse.console/src/org/hibernate/eclipse/criteriaeditor/CriteriaEditorDocumentSetupParticipant.java
+++ plugins/org.hibernate.eclipse.console/src/org/hibernate/eclipse/criteriaeditor/CriteriaEditorDocumentSetupParticipant.java
@@ -24,6 +24,8 @@ package org.hibernate.eclipse.criteriaeditor;
 import org.eclipse.jdt.ui.text.IJavaPartitions;
 import org.eclipse.jdt.ui.text.JavaTextTools;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IDocumentExtension3;
+import org.eclipse.jface.text.IDocumentPartitioner;
 import org.hibernate.eclipse.console.HibernateConsolePlugin;
 
 public class CriteriaEditorDocumentSetupParticipant {
@@ -35,6 +37,14 @@ public class CriteriaEditorDocumentSetupParticipant {
 	 */
 	public void setup( IDocument document ) {
 		JavaTextTools tools= HibernateConsolePlugin.getDefault().getJavaTextTools();
-        tools.setupJavaDocumentPartitioner(document, IJavaPartitions.JAVA_PARTITIONING);        
+		IDocumentPartitioner partitioner= tools.createDocumentPartitioner();
+		partitioner.connect(document);
+		if (document instanceof IDocumentExtension3) {
+			IDocumentExtension3 extension3= (IDocumentExtension3) document;
+			extension3.setDocumentPartitioner(IJavaPartitions.JAVA_PARTITIONING, partitioner);
+		} else {
+			document.setDocumentPartitioner(partitioner);
+		}
 	}
+	
 }
