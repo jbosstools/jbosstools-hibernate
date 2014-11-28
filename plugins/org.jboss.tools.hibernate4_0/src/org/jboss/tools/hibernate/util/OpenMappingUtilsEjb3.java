02a8c1bc45997ac11e7246dbf3457a09b2c4cf27
Koen Aers <koen.aers@gmail.com>
2014-11-28 08:37:42 +0100

    Use 'Collections.emptyMap()' and add '<?,?>' as generic parameters

--- plugins/org.jboss.tools.hibernate4_0/src/org/jboss/tools/hibernate/util/OpenMappingUtilsEjb3.java
+++ plugins/org.jboss.tools.hibernate4_0/src/org/jboss/tools/hibernate/util/OpenMappingUtilsEjb3.java
@@ -43,7 +43,7 @@ public class OpenMappingUtilsEjb3 {
 	
 	public static final String META_INF_PERS_XML = "META-INF/persistence.xml"; //$NON-NLS-1$
 	public static final String META_INF_ORM_XML = "META-INF/orm.xml"; //$NON-NLS-1$
-	private static final Map EMPTY_MAP = Collections.unmodifiableMap( new HashMap(0) );
+	private static final Map<?,?> EMPTY_MAP = Collections.emptyMap();
 
 	private OpenMappingUtilsEjb3() {}
 	
