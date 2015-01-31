c42f4cf9bbeaa9cd1ffaadde89889b61acb3a7b3
Koen Aers <koen.aers@gmail.com>
2015-01-31 16:57:19 +0100

    ArtifactCollectorProxy now extends AbstractArtifactCollector

--- plugins/org.jboss.tools.hibernate.runtime.v_4_0/src/org/jboss/tools/hibernate/proxy/ArtifactCollectorProxy.java
+++ plugins/org.jboss.tools.hibernate.runtime.v_4_0/src/org/jboss/tools/hibernate/proxy/ArtifactCollectorProxy.java
@@ -1,33 +1,5 @@
 package org.jboss.tools.hibernate.proxy;
 
-import java.io.File;
-import java.util.Set;
+import org.jboss.tools.hibernate.runtime.common.AbstractArtifactCollector;
 
-import org.hibernate.tool.hbm2x.ArtifactCollector;
-import org.jboss.tools.hibernate.runtime.spi.IArtifactCollector;
-
-public class ArtifactCollectorProxy implements IArtifactCollector {
-	
-	private ArtifactCollector target = new ArtifactCollector();
-	
-	public Object getTarget() {
-		return target;
-	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public Set<String> getFileTypes() {
-		return target.getFileTypes();
-	}
-
-	@Override
-	public void formatFiles() {
-		target.formatFiles();
-	}
-
-	@Override
-	public File[] getFiles(String type) {
-		return target.getFiles(type);
-	}
-
-}
+public class ArtifactCollectorProxy extends AbstractArtifactCollector {}
