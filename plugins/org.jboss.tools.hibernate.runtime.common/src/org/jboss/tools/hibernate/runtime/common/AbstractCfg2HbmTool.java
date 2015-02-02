b00dffe6590e5f310b454a76cc67739a241e03bc
Koen Aers <koen.aers@gmail.com>
2015-02-02 07:54:09 +0100

    Create AbstractCfg2HbmTool class that implements ICfg2HbmTool

---
+++ plugins/org.jboss.tools.hibernate.runtime.common/src/org/jboss/tools/hibernate/runtime/common/AbstractCfg2HbmTool.java
@@ -0,0 +1,41 @@
+package org.jboss.tools.hibernate.runtime.common;
+
+import org.jboss.tools.hibernate.runtime.common.internal.Util;
+import org.jboss.tools.hibernate.runtime.spi.ICfg2HbmTool;
+import org.jboss.tools.hibernate.runtime.spi.IPersistentClass;
+import org.jboss.tools.hibernate.runtime.spi.IProperty;
+
+public abstract class AbstractCfg2HbmTool implements ICfg2HbmTool {
+
+	private Object target = null;
+	
+	private Object getTarget() {
+		if (target == null) {
+			target = Util.getInstance(getTargetClassName(), this);
+		}
+		return target;
+	}
+	
+	protected String getTargetClassName() {
+		return "org.hibernate.tool.hbm2x.Cfg2HbmTool";
+	}
+	
+	@Override
+	public String getTag(IPersistentClass persistentClass) {
+		return (String)Util.invokeMethod(
+				getTarget(), 
+				"getTag", 
+				new Class[] { IPersistentClass.class }, 
+				new Object[] { persistentClass } );
+	}
+
+	@Override
+	public String getTag(IProperty property) {
+		return (String)Util.invokeMethod(
+				getTarget(), 
+				"getTag", 
+				new Class[] { IProperty.class }, 
+				new Object[] { property });
+	}
+
+}
