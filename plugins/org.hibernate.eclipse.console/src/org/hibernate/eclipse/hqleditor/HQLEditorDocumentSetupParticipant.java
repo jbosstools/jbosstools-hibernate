5d7067134579f8f84244d2e715043c525548e1ee
Koen Aers <koen.aers@gmail.com>
2015-02-27 17:38:03 +0100

    Replace deprecated org.eclipse.jface.text.rules.DefaultPartitioner by org.eclipse.jface.text.rules.FastPartitioner

--- plugins/org.hibernate.eclipse.console/src/org/hibernate/eclipse/hqleditor/HQLEditorDocumentSetupParticipant.java
+++ plugins/org.hibernate.eclipse.console/src/org/hibernate/eclipse/hqleditor/HQLEditorDocumentSetupParticipant.java
@@ -25,7 +25,7 @@ import org.eclipse.core.filebuffers.IDocumentSetupParticipant;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IDocumentExtension3;
 import org.eclipse.jface.text.IDocumentPartitioner;
-import org.eclipse.jface.text.rules.DefaultPartitioner;
+import org.eclipse.jface.text.rules.FastPartitioner;
 
 /**
  * This class implements the <code>IDocumentSetupParticipant</code> interface in
@@ -42,7 +42,7 @@ public class HQLEditorDocumentSetupParticipant implements IDocumentSetupParticip
 	public void setup( IDocument document ) {
 		if (document instanceof IDocumentExtension3) {
 			IDocumentExtension3 extension3 = (IDocumentExtension3) document;
-			partitioner = new DefaultPartitioner( new HQLPartitionScanner(), HQLPartitionScanner.HQL_PARTITION_TYPES );
+			partitioner = new FastPartitioner( new HQLPartitionScanner(), HQLPartitionScanner.HQL_PARTITION_TYPES );
             partitioner.connect( document );
 			extension3.setDocumentPartitioner( HQLSourceViewerConfiguration.HQL_PARTITIONING, partitioner );
 		}
