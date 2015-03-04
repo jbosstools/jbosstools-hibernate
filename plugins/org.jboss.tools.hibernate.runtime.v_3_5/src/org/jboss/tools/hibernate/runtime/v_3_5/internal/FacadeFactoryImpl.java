431393c2c76e25c771800591097a1a6583110aa7
Koen Aers <koen.aers@gmail.com>
2015-03-04 18:05:17 +0100

    Add FacadeFactoryImpl in org.jboss.tools.hibernate.runtime.v_3_5

---
+++ plugins/org.jboss.tools.hibernate.runtime.v_3_5/src/org/jboss/tools/hibernate/runtime/v_3_5/internal/FacadeFactoryImpl.java
@@ -0,0 +1,7 @@
+package org.jboss.tools.hibernate.runtime.v_3_5.internal;
+
+import org.jboss.tools.hibernate.runtime.common.AbstractFacadeFactory;
+
+public class FacadeFactoryImpl extends AbstractFacadeFactory {
+	
+}
