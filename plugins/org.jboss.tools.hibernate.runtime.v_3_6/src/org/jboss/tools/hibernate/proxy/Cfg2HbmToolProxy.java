cf7b156f45c353f7fdf7c0886f95e9e4e73ecc9d
Koen Aers <koen.aers@gmail.com>
2015-03-04 16:44:50 +0100

    Cfg2HbmToolProxy extends AbstractCfg2HbmToolFacade and uses its implementation

--- plugins/org.jboss.tools.hibernate.runtime.v_3_6/src/org/jboss/tools/hibernate/proxy/Cfg2HbmToolProxy.java
+++ plugins/org.jboss.tools.hibernate.runtime.v_3_6/src/org/jboss/tools/hibernate/proxy/Cfg2HbmToolProxy.java
@@ -1,28 +1,5 @@
 package org.jboss.tools.hibernate.proxy;
 
-import org.hibernate.tool.hbm2x.Cfg2HbmTool;
-import org.jboss.tools.hibernate.runtime.spi.ICfg2HbmTool;
-import org.jboss.tools.hibernate.runtime.spi.IPersistentClass;
-import org.jboss.tools.hibernate.runtime.spi.IProperty;
+import org.jboss.tools.hibernate.runtime.common.AbstractCfg2HbmToolFacade;
 
-public class Cfg2HbmToolProxy implements ICfg2HbmTool {
-	
-	private Cfg2HbmTool target = null;
-	
-	public Cfg2HbmToolProxy() {
-		target = new Cfg2HbmTool();
-	}
-
-	@Override
-	public String getTag(IPersistentClass persistentClass) {
-		assert persistentClass instanceof PersistentClassProxy;
-		return target.getTag(((PersistentClassProxy)persistentClass).getTarget());
-	}
-
-	@Override
-	public String getTag(IProperty property) {
-		assert property instanceof PropertyProxy;
-		return target.getTag(((PropertyProxy)property).getTarget());
-	}
-
-}
+public class Cfg2HbmToolProxy extends AbstractCfg2HbmToolFacade {}
