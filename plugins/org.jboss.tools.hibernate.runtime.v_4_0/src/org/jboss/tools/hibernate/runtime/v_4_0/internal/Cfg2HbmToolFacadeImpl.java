0c3cb6cb35984ee4bcb308ce6d286e1d432487af
Koen Aers <koen.aers@gmail.com>
2015-03-04 16:54:47 +0100

    Rename Cfg2HbmToolProxy into Cfg2HbmToolFacadeImpl

--- plugins/org.jboss.tools.hibernate.runtime.v_4_0/src/org/jboss/tools/hibernate/runtime/v_4_0/internal/Cfg2HbmToolFacadeImpl.java
+++ plugins/org.jboss.tools.hibernate.runtime.v_4_0/src/org/jboss/tools/hibernate/runtime/v_4_0/internal/Cfg2HbmToolFacadeImpl.java
@@ -2,4 +2,4 @@ package org.jboss.tools.hibernate.runtime.v_4_0.internal;
 
 import org.jboss.tools.hibernate.runtime.common.AbstractCfg2HbmToolFacade;
 
-public class Cfg2HbmToolProxy extends AbstractCfg2HbmToolFacade {}
+public class Cfg2HbmToolFacadeImpl extends AbstractCfg2HbmToolFacade {}
