807f32a5c720b5caeaf616f5430973edea7a687a
Koen Aers <koen.aers@gmail.com>
2015-03-04 16:30:44 +0100

    Rename ArtifactCollectorImpl into ArtifactCollectorFacadeImpl for org.jboss.tools.hibernate.runtime.v_3_6"

--- plugins/org.jboss.tools.hibernate.runtime.v_3_6/src/org/jboss/tools/hibernate/runtime/v_3_6/internal/ArtifactCollectorFacadeImpl.java
+++ plugins/org.jboss.tools.hibernate.runtime.v_3_6/src/org/jboss/tools/hibernate/runtime/v_3_6/internal/ArtifactCollectorFacadeImpl.java
@@ -2,4 +2,4 @@ package org.jboss.tools.hibernate.runtime.v_3_6.internal;
 
 import org.jboss.tools.hibernate.runtime.common.AbstractArtifactCollectorFacade;
 
-public class ArtifactCollectorImpl extends AbstractArtifactCollectorFacade {}
+public class ArtifactCollectorFacadeImpl extends AbstractArtifactCollectorFacade {}
