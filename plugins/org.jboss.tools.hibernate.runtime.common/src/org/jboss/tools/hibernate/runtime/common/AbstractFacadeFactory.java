dfdf898a8e4a107ee6b8b03a5ccfc13de4c86243
Koen Aers <koen.aers@gmail.com>
2015-03-04 17:37:29 +0100

    Create AbstractFacadeFactory as a common ancestor of the IFacadeFactory implementations

---
+++ plugins/org.jboss.tools.hibernate.runtime.common/src/org/jboss/tools/hibernate/runtime/common/AbstractFacadeFactory.java
@@ -0,0 +1,7 @@
+package org.jboss.tools.hibernate.runtime.common;
+
+import org.jboss.tools.hibernate.runtime.spi.IFacadeFactory;
+
+public abstract class AbstractFacadeFactory implements IFacadeFactory {
+
+}
