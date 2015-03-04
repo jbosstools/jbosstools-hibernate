ac7cbb62728899b517f99fe5fc1fe7bc93f2030b
Koen Aers <koen.aers@gmail.com>
2015-03-04 12:15:28 +0100

    Use 'getTarget(IProperty)' method

--- plugins/org.jboss.tools.hibernate.runtime.v_3_5/src/org/jboss/tools/hibernate/proxy/Cfg2HbmToolProxy.java
+++ plugins/org.jboss.tools.hibernate.runtime.v_3_5/src/org/jboss/tools/hibernate/proxy/Cfg2HbmToolProxy.java
@@ -1,5 +1,6 @@
 package org.jboss.tools.hibernate.proxy;
 
+import org.hibernate.mapping.Property;
 import org.hibernate.tool.hbm2x.Cfg2HbmTool;
 import org.jboss.tools.hibernate.runtime.common.AbstractCfg2HbmTool;
 import org.jboss.tools.hibernate.runtime.common.Util;
@@ -32,7 +33,7 @@ public class Cfg2HbmToolProxy extends AbstractCfg2HbmTool {
 	@Override
 	public String getTag(IProperty property) {
 		assert property instanceof PropertyProxy;
-		return ((Cfg2HbmTool)getTarget()).getTag(((PropertyProxy)property).getTarget());
+		return ((Cfg2HbmTool)getTarget()).getTag(((Property)getTarget(property)));
 	}
 
 }
