19796ddbbfd80dd13dfbc49c3b1f208efb663b29
Koen Aers <koen.aers@gmail.com>
2015-03-04 18:11:22 +0100

    Add createArtifactCollector() method to IFacadeFactory implementations

--- plugins/org.jboss.tools.hibernate.runtime.v_3_5/src/org/jboss/tools/hibernate/runtime/v_3_5/internal/FacadeFactoryImpl.java
+++ plugins/org.jboss.tools.hibernate.runtime.v_3_5/src/org/jboss/tools/hibernate/runtime/v_3_5/internal/FacadeFactoryImpl.java
@@ -1,7 +1,12 @@
 package org.jboss.tools.hibernate.runtime.v_3_5.internal;
 
 import org.jboss.tools.hibernate.runtime.common.AbstractFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IArtifactCollector;
 
 public class FacadeFactoryImpl extends AbstractFacadeFactory {
+	
+	public IArtifactCollector createArtifactCollector() {
+		return new ArtifactCollectorFacadeImpl();
+	}
 	
 }
--- plugins/org.jboss.tools.hibernate.runtime.v_4_3/src/org/jboss/tools/hibernate/runtime/v_4_3/internal/FacadeFactoryImpl.java
+++ plugins/org.jboss.tools.hibernate.runtime.v_4_3/src/org/jboss/tools/hibernate/runtime/v_4_3/internal/FacadeFactoryImpl.java
@@ -1,7 +1,12 @@
 package org.jboss.tools.hibernate.runtime.v_4_3.internal;
 
 import org.jboss.tools.hibernate.runtime.common.AbstractFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IArtifactCollector;
 
 public class FacadeFactoryImpl extends AbstractFacadeFactory {
+	
+	public IArtifactCollector createArtifactCollector() {
+		return new ArtifactCollectorFacadeImpl();
+	}
 	
 }
