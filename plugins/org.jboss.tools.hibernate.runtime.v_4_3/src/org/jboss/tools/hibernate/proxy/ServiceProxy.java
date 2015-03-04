1e74cb0d084cb966db6796c7e1c1108b9dd62081
Koen Aers <koen.aers@gmail.com>
2015-03-04 17:00:31 +0100

    Rename Cfg2HbmToolProxy into Cfg2HbmToolFacadeImpl

--- plugins/org.jboss.tools.hibernate.runtime.v_4_3/src/org/jboss/tools/hibernate/proxy/ServiceProxy.java
+++ plugins/org.jboss.tools.hibernate.runtime.v_4_3/src/org/jboss/tools/hibernate/proxy/ServiceProxy.java
@@ -85,7 +85,7 @@ import org.jboss.tools.hibernate.runtime.spi.ITableIdentifier;
 import org.jboss.tools.hibernate.runtime.spi.ITypeFactory;
 import org.jboss.tools.hibernate.runtime.spi.IValue;
 import org.jboss.tools.hibernate.runtime.v_4_3.internal.ArtifactCollectorFacadeImpl;
-import org.jboss.tools.hibernate.runtime.v_4_3.internal.Cfg2HbmToolProxy;
+import org.jboss.tools.hibernate.runtime.v_4_3.internal.Cfg2HbmToolFacadeImpl;
 import org.jboss.tools.hibernate.util.OpenMappingUtilsEjb3;
 import org.xml.sax.EntityResolver;
 
@@ -305,7 +305,7 @@ public class ServiceProxy implements IService {
 
 	@Override
 	public ICfg2HbmTool newCfg2HbmTool() {
-		return new Cfg2HbmToolProxy();
+		return new Cfg2HbmToolFacadeImpl();
 	}
 
 	@Override
