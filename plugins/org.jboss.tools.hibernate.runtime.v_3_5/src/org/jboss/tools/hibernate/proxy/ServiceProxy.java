14460670a940d5107df23347cc1d02de032c6c75
Koen Aers <koen.aers@gmail.com>
2015-03-04 15:35:14 +0100

    Move Cfg2HbmToolProxy from org.jboss.tools.hibernate.proxy to org.jboss.tools.hibernate.runtime.v_3_5.internal

--- plugins/org.jboss.tools.hibernate.runtime.v_3_5/src/org/jboss/tools/hibernate/proxy/ServiceProxy.java
+++ plugins/org.jboss.tools.hibernate.runtime.v_3_5/src/org/jboss/tools/hibernate/proxy/ServiceProxy.java
@@ -78,6 +78,7 @@ import org.jboss.tools.hibernate.runtime.spi.ITableIdentifier;
 import org.jboss.tools.hibernate.runtime.spi.ITypeFactory;
 import org.jboss.tools.hibernate.runtime.spi.IValue;
 import org.jboss.tools.hibernate.runtime.v_3_5.internal.ArtifactCollectorFacadeImpl;
+import org.jboss.tools.hibernate.runtime.v_3_5.internal.Cfg2HbmToolProxy;
 import org.jboss.tools.hibernate.util.OpenMappingUtilsEjb3;
 import org.xml.sax.EntityResolver;
 
