70801a2de8741addf05db78eefede2b2d74133fe
Koen Aers <koen.aers@gmail.com>
2015-03-05 15:06:21 +0100

    Pull up 'createArtifactCollector()' and 'createCfg2HbmTool()' to AbstractFacadeFactory

--- plugins/org.jboss.tools.hibernate.runtime.common/src/org/jboss/tools/hibernate/runtime/common/AbstractFacadeFactory.java
+++ plugins/org.jboss.tools.hibernate.runtime.common/src/org/jboss/tools/hibernate/runtime/common/AbstractFacadeFactory.java
@@ -1,7 +1,17 @@
 package org.jboss.tools.hibernate.runtime.common;
 
+import org.jboss.tools.hibernate.runtime.spi.IArtifactCollector;
+import org.jboss.tools.hibernate.runtime.spi.ICfg2HbmTool;
 import org.jboss.tools.hibernate.runtime.spi.IFacadeFactory;
 
 public abstract class AbstractFacadeFactory implements IFacadeFactory {
 
+	public IArtifactCollector createArtifactCollector() {
+		return new AbstractArtifactCollectorFacade(this) {};
+	}
+	
+	public ICfg2HbmTool createCfg2HbmTool() {
+		return new AbstractCfg2HbmToolFacade(this) {};
+	}
+	
 }
--- plugins/org.jboss.tools.hibernate.runtime.v_4_3/src/org/jboss/tools/hibernate/runtime/v_4_3/internal/FacadeFactoryImpl.java
+++ plugins/org.jboss.tools.hibernate.runtime.v_4_3/src/org/jboss/tools/hibernate/runtime/v_4_3/internal/FacadeFactoryImpl.java
@@ -1,23 +1,11 @@
 package org.jboss.tools.hibernate.runtime.v_4_3.internal;
 
-import org.jboss.tools.hibernate.runtime.common.AbstractArtifactCollectorFacade;
-import org.jboss.tools.hibernate.runtime.common.AbstractCfg2HbmToolFacade;
 import org.jboss.tools.hibernate.runtime.common.AbstractFacadeFactory;
-import org.jboss.tools.hibernate.runtime.spi.IArtifactCollector;
-import org.jboss.tools.hibernate.runtime.spi.ICfg2HbmTool;
 
 public class FacadeFactoryImpl extends AbstractFacadeFactory {
 	
 	public ClassLoader getClassLoader() {
 		return FacadeFactoryImpl.class.getClassLoader();
-	}
-	
-	public IArtifactCollector createArtifactCollector() {
-		return new AbstractArtifactCollectorFacade(this) {};
-	}
-	
-	public ICfg2HbmTool createCfg2HbmTool() {
-		return new AbstractCfg2HbmToolFacade(this) {};
 	}
 	
 }
