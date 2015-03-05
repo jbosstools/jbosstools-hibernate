31c52d3617c26ed1ce0e78cc8e55089831871988
Koen Aers <koen.aers@gmail.com>
2015-03-05 14:13:44 +0100

    Add getClassLoader() method to IFacadeFactory interface

--- plugins/org.jboss.tools.hibernate.runtime.spi/src/org/jboss/tools/hibernate/runtime/spi/IFacadeFactory.java
+++ plugins/org.jboss.tools.hibernate.runtime.spi/src/org/jboss/tools/hibernate/runtime/spi/IFacadeFactory.java
@@ -2,6 +2,7 @@ package org.jboss.tools.hibernate.runtime.spi;
 
 public interface IFacadeFactory {
 	
+	ClassLoader getClassLoader();
 	IArtifactCollector createArtifactCollector();
 	ICfg2HbmTool createCfg2HbmTool();
 
