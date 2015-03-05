fbef8833edb0c6e9b3ef6beda4c5df146c1031d7
Koen Aers <koen.aers@gmail.com>
2015-03-05 08:47:24 +0100

    Add createCfg2HbmTool() method to IFacadeFactory implementations

--- plugins/org.jboss.tools.hibernate.runtime.v_3_5/src/org/jboss/tools/hibernate/runtime/v_3_5/internal/FacadeFactoryImpl.java
+++ plugins/org.jboss.tools.hibernate.runtime.v_3_5/src/org/jboss/tools/hibernate/runtime/v_3_5/internal/FacadeFactoryImpl.java
@@ -2,11 +2,16 @@ package org.jboss.tools.hibernate.runtime.v_3_5.internal;
 
 import org.jboss.tools.hibernate.runtime.common.AbstractFacadeFactory;
 import org.jboss.tools.hibernate.runtime.spi.IArtifactCollector;
+import org.jboss.tools.hibernate.runtime.spi.ICfg2HbmTool;
 
 public class FacadeFactoryImpl extends AbstractFacadeFactory {
 	
 	public IArtifactCollector createArtifactCollector() {
 		return new ArtifactCollectorFacadeImpl(this);
+	}
+	
+	public ICfg2HbmTool createCfg2HbmTool() {
+		return new Cfg2HbmToolFacadeImpl();
 	}
 	
 }
