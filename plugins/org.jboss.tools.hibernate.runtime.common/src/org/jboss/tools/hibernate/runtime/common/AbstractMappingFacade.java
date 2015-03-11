4853772a7a52493e91e8145257484c3816cf5018
Koen Aers <koen.aers@gmail.com>
2015-03-11 15:23:39 +0100

    Create class AbstractMappingFacade as subclass of AbstractFacade and implementing IMapping

---
+++ plugins/org.jboss.tools.hibernate.runtime.common/src/org/jboss/tools/hibernate/runtime/common/AbstractMappingFacade.java
@@ -0,0 +1,12 @@
+package org.jboss.tools.hibernate.runtime.common;
+
+import org.jboss.tools.hibernate.runtime.spi.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IMapping;
+
+public class AbstractMappingFacade extends AbstractFacade implements IMapping {
+
+	public AbstractMappingFacade(IFacadeFactory facadeFactory, Object target) {
+		super(facadeFactory, target);
+	}
+
+}
