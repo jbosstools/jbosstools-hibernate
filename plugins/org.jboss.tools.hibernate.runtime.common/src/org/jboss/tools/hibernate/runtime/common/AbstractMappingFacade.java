e71ff803e7b875361ff67bea9e1b698001a6e4ea
Koen Aers <koen.aers@gmail.com>
2015-03-12 08:29:01 +0100

    Make AbstractMappingFacade an abstract class

--- plugins/org.jboss.tools.hibernate.runtime.common/src/org/jboss/tools/hibernate/runtime/common/AbstractMappingFacade.java
+++ plugins/org.jboss.tools.hibernate.runtime.common/src/org/jboss/tools/hibernate/runtime/common/AbstractMappingFacade.java
@@ -3,9 +3,13 @@ package org.jboss.tools.hibernate.runtime.common;
 import org.jboss.tools.hibernate.runtime.spi.IFacadeFactory;
 import org.jboss.tools.hibernate.runtime.spi.IMapping;
 
-public class AbstractMappingFacade extends AbstractFacade implements IMapping {
+public abstract class AbstractMappingFacade 
+extends AbstractFacade 
+implements IMapping {
 
-	public AbstractMappingFacade(IFacadeFactory facadeFactory, Object target) {
+	public AbstractMappingFacade(
+			IFacadeFactory facadeFactory, 
+			Object target) {
 		super(facadeFactory, target);
 	}
 
