51deb540528771b3614e0c47515448f795150de6
Koen Aers <koen.aers@gmail.com>
2015-03-13 08:32:29 +0100

    Pull up method 'setSettings(IReverseEngineeringSettings)' to AbstractReverseEngineeringStrategyFacade

--- plugins/org.jboss.tools.hibernate.runtime.common/src/org/jboss/tools/hibernate/runtime/common/AbstractReverseEngineeringStrategyFacade.java
+++ plugins/org.jboss.tools.hibernate.runtime.common/src/org/jboss/tools/hibernate/runtime/common/AbstractReverseEngineeringStrategyFacade.java
@@ -1,6 +1,7 @@
 package org.jboss.tools.hibernate.runtime.common;
 
 import org.jboss.tools.hibernate.runtime.spi.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IReverseEngineeringSettings;
 import org.jboss.tools.hibernate.runtime.spi.IReverseEngineeringStrategy;
 
 public abstract class AbstractReverseEngineeringStrategyFacade 
@@ -11,6 +12,26 @@ implements IReverseEngineeringStrategy {
 			IFacadeFactory facadeFactory, 
 			Object target) {
 		super(facadeFactory, target);
+	}
+
+	@Override
+	public void setSettings(IReverseEngineeringSettings settings) {
+		assert settings instanceof IFacade;
+		Util.invokeMethod(
+				getTarget(), 
+				"setSettings", 
+				new Class[] { getReverseEngineeringSettingsClass() }, 
+				new Object[] { ((IFacade)settings).getTarget() });
+	}
+	
+	protected Class<?> getReverseEngineeringSettingsClass() {
+		return Util.getClass(
+				getReverseEngineeringSettingsClassName(), 
+				getFacadeFactoryClassLoader());
+	}
+	
+	protected String getReverseEngineeringSettingsClassName() {
+		return "org.hibernate.cfg.reveng.ReverseEngineeringSettings";
 	}
 
 }
--- plugins/org.jboss.tools.hibernate.runtime.v_4_0/src/org/jboss/tools/hibernate/proxy/ReverseEngineeringStrategyProxy.java
+++ plugins/org.jboss.tools.hibernate.runtime.v_4_0/src/org/jboss/tools/hibernate/proxy/ReverseEngineeringStrategyProxy.java
@@ -1,11 +1,8 @@
 package org.jboss.tools.hibernate.proxy;
 
-import org.hibernate.cfg.reveng.ReverseEngineeringSettings;
 import org.hibernate.cfg.reveng.ReverseEngineeringStrategy;
 import org.jboss.tools.hibernate.runtime.common.AbstractReverseEngineeringStrategyFacade;
-import org.jboss.tools.hibernate.runtime.common.IFacade;
 import org.jboss.tools.hibernate.runtime.spi.IFacadeFactory;
-import org.jboss.tools.hibernate.runtime.spi.IReverseEngineeringSettings;
 
 public class ReverseEngineeringStrategyProxy 
 extends AbstractReverseEngineeringStrategyFacade  {
@@ -18,12 +15,6 @@ extends AbstractReverseEngineeringStrategyFacade  {
 
 	public ReverseEngineeringStrategy getTarget() {
 		return (ReverseEngineeringStrategy)super.getTarget();
-	}
-
-	@Override
-	public void setSettings(IReverseEngineeringSettings settings) {
-		assert settings instanceof IFacade;
-		getTarget().setSettings((ReverseEngineeringSettings)((IFacade)settings).getTarget());
 	}
 
 }
