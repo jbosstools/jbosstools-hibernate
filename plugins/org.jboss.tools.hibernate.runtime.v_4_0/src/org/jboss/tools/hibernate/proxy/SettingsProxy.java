756f68cbac9baac6a4fb2464465bb62530292747
Koen Aers <koen.aers@gmail.com>
2015-03-25 08:38:54 +0100

    SettingsProxy classes now inherit from AbstractSettingsFacade

--- plugins/org.jboss.tools.hibernate.runtime.v_4_0/src/org/jboss/tools/hibernate/proxy/SettingsProxy.java
+++ plugins/org.jboss.tools.hibernate.runtime.v_4_0/src/org/jboss/tools/hibernate/proxy/SettingsProxy.java
@@ -1,14 +1,17 @@
 package org.jboss.tools.hibernate.proxy;
 
 import org.hibernate.cfg.Settings;
+import org.jboss.tools.hibernate.runtime.common.AbstractSettingsFacade;
 import org.jboss.tools.hibernate.runtime.spi.IFacadeFactory;
-import org.jboss.tools.hibernate.runtime.spi.ISettings;
 
-public class SettingsProxy implements ISettings {
+public class SettingsProxy extends AbstractSettingsFacade {
 	
 	private Settings target;
 
-	public SettingsProxy(IFacadeFactory facadeFactory, Settings settings) {
+	public SettingsProxy(
+			IFacadeFactory facadeFactory, 
+			Settings settings) {
+		super(facadeFactory, settings);
 		target = settings;
 	}
 
