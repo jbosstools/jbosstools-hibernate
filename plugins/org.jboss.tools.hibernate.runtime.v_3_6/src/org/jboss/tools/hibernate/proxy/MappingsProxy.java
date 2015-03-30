80cd5d16d56f8a7d03c0da59c80a18b3c5de924a
Koen Aers <koen.aers@gmail.com>
2015-03-30 12:16:29 +0200

    MappingsProxy classes extend AbstractMappingsFacade

--- plugins/org.jboss.tools.hibernate.runtime.v_3_6/src/org/jboss/tools/hibernate/proxy/MappingsProxy.java
+++ plugins/org.jboss.tools.hibernate.runtime.v_3_6/src/org/jboss/tools/hibernate/proxy/MappingsProxy.java
@@ -1,17 +1,18 @@
 package org.jboss.tools.hibernate.proxy;
 
 import org.hibernate.cfg.Mappings;
+import org.jboss.tools.hibernate.runtime.common.AbstractMappingsFacade;
 import org.jboss.tools.hibernate.runtime.spi.IFacadeFactory;
-import org.jboss.tools.hibernate.runtime.spi.IMappings;
 import org.jboss.tools.hibernate.runtime.spi.IPersistentClass;
 
-public class MappingsProxy implements IMappings {
+public class MappingsProxy extends AbstractMappingsFacade {
 	
 	private Mappings target = null;
 
 	public MappingsProxy(
 			IFacadeFactory facadeFactory,
 			Mappings mappings) {
+		super(facadeFactory, mappings);
 		target = mappings;
 	}
 
