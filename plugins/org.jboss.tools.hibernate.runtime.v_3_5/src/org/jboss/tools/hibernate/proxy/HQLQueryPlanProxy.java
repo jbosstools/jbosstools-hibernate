0740b03d4bf94c35c8ff8782b6a11fdc66512f03
Koen Aers <koen.aers@gmail.com>
2015-04-14 15:06:46 +0200

    Use 'getTarget()' accessor, remove 'target' field in HQLQueryPlanProxy classes

--- plugins/org.jboss.tools.hibernate.runtime.v_3_5/src/org/jboss/tools/hibernate/proxy/HQLQueryPlanProxy.java
+++ plugins/org.jboss.tools.hibernate.runtime.v_3_5/src/org/jboss/tools/hibernate/proxy/HQLQueryPlanProxy.java
@@ -10,12 +10,10 @@ import org.jboss.tools.hibernate.runtime.spi.IQueryTranslator;
 
 public class HQLQueryPlanProxy extends AbstractHQLQueryPlanFacade {
 	
-	private HQLQueryPlan target = null;
 	private IQueryTranslator[] translators = null;
 	
 	public HQLQueryPlanProxy(IFacadeFactory facadeFactory, HQLQueryPlan queryPlan) {
 		super(facadeFactory, queryPlan);
-		target = queryPlan;
 	}
 	
 	public HQLQueryPlan getTarget() {
@@ -31,7 +29,7 @@ public class HQLQueryPlanProxy extends AbstractHQLQueryPlanFacade {
 	}
 	
 	private void initializeTranslators() {
-		QueryTranslator[] origin = target.getTranslators();
+		QueryTranslator[] origin = getTarget().getTranslators();
 		ArrayList<IQueryTranslator> destination = 
 				new ArrayList<IQueryTranslator>(origin.length);
 		for (QueryTranslator translator : origin) {
