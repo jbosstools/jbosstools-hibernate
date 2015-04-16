1c4f006bd1f47dfa11f38b65b9ec066bbb96233e
Koen Aers <koen.aers@gmail.com>
2015-04-16 10:29:20 +0200

    Use 'getTarget()' accessor instead of direct field access, remove unused field 'target' in HQLCodeAssistProxy classes

--- plugins/org.jboss.tools.hibernate.runtime.v_3_5/src/org/jboss/tools/hibernate/proxy/HQLCodeAssistProxy.java
+++ plugins/org.jboss.tools.hibernate.runtime.v_3_5/src/org/jboss/tools/hibernate/proxy/HQLCodeAssistProxy.java
@@ -9,13 +9,10 @@ import org.jboss.tools.hibernate.runtime.spi.IHQLCompletionHandler;
 
 public class HQLCodeAssistProxy extends AbstractHQLCodeAssistFacade {
 	
-	HQLCodeAssist target = null;
-
 	public HQLCodeAssistProxy(
 			IFacadeFactory facadeFactory,
 			HQLCodeAssist hqlCodeAssist) {
 		super(facadeFactory, hqlCodeAssist);
-		target = hqlCodeAssist;
 	}
 	
 	public HQLCodeAssist getTarget() {
@@ -25,7 +22,7 @@ public class HQLCodeAssistProxy extends AbstractHQLCodeAssistFacade {
 	@Override
 	public void codeComplete(String query, int currentOffset,
 			IHQLCompletionHandler handler) {
-		target.codeComplete(query, currentOffset, new HQLCompletionRequestor(handler));
+		getTarget().codeComplete(query, currentOffset, new HQLCompletionRequestor(handler));
 	}
 	
 	private class HQLCompletionRequestor implements IHQLCompletionRequestor {		
