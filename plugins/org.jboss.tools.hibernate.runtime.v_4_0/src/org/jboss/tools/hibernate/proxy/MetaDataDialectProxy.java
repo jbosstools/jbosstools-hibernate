55b47430bcf29243924d9d7709bee7e719ba071f
Koen Aers <koen.aers@gmail.com>
2015-04-20 18:14:49 +0200

    MetaDataDialectProxy classes extend AbstractMetaDataDialectFacade

--- plugins/org.jboss.tools.hibernate.runtime.v_4_0/src/org/jboss/tools/hibernate/proxy/MetaDataDialectProxy.java
+++ plugins/org.jboss.tools.hibernate.runtime.v_4_0/src/org/jboss/tools/hibernate/proxy/MetaDataDialectProxy.java
@@ -1,14 +1,15 @@
 package org.jboss.tools.hibernate.proxy;
 
 import org.hibernate.cfg.reveng.dialect.MetaDataDialect;
+import org.jboss.tools.hibernate.runtime.common.AbstractMetaDataDialectFacade;
 import org.jboss.tools.hibernate.runtime.spi.IFacadeFactory;
-import org.jboss.tools.hibernate.runtime.spi.IMetaDataDialect;
 
-public class MetaDataDialectProxy implements IMetaDataDialect {
+public class MetaDataDialectProxy extends AbstractMetaDataDialectFacade {
 
 	private MetaDataDialect target = null;
 
 	public MetaDataDialectProxy(IFacadeFactory facadeFactory, MetaDataDialect mdd) {
+		super(facadeFactory, mdd);
 		target = mdd;
 	}
 
