6fa3a9411ee388779038fbfbee3fd0f567a8754a
Koen Aers <koen.aers@gmail.com>
2015-04-27 08:41:01 +0200

    Pull up method 'getElementType' from CollectionMetadataFacadeImpl classes to AbstractCollectionMetadataFacade

--- plugins/org.jboss.tools.hibernate.runtime.common/src/org/jboss/tools/hibernate/runtime/common/AbstractCollectionMetadataFacade.java
+++ plugins/org.jboss.tools.hibernate.runtime.common/src/org/jboss/tools/hibernate/runtime/common/AbstractCollectionMetadataFacade.java
@@ -2,15 +2,30 @@ package org.jboss.tools.hibernate.runtime.common;
 
 import org.jboss.tools.hibernate.runtime.spi.ICollectionMetadata;
 import org.jboss.tools.hibernate.runtime.spi.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IType;
 
 public abstract class AbstractCollectionMetadataFacade 
 extends AbstractFacade 
 implements ICollectionMetadata {
 
+	private IType elementType = null;
+
 	public AbstractCollectionMetadataFacade(
 			IFacadeFactory facadeFactory, 
 			Object target) {
 		super(facadeFactory, target);
+	}
+
+	@Override
+	public IType getElementType() {
+		if (elementType == null) {
+			elementType = getFacadeFactory().createType(getTargetElementType());
+		}
+		return elementType;
+	}
+	
+	private Object getTargetElementType() {
+		return Util.invokeMethod(getTarget(), "getElementType", new Class[] {}, new Object[] {});
 	}
 
 }
--- plugins/org.jboss.tools.hibernate.runtime.v_3_6/src/org/jboss/tools/hibernate/runtime/v_3_6/internal/CollectionMetadataFacadeImpl.java
+++ plugins/org.jboss.tools.hibernate.runtime.v_3_6/src/org/jboss/tools/hibernate/runtime/v_3_6/internal/CollectionMetadataFacadeImpl.java
@@ -3,24 +3,13 @@ package org.jboss.tools.hibernate.runtime.v_3_6.internal;
 import org.hibernate.metadata.CollectionMetadata;
 import org.jboss.tools.hibernate.runtime.common.AbstractCollectionMetadataFacade;
 import org.jboss.tools.hibernate.runtime.spi.IFacadeFactory;
-import org.jboss.tools.hibernate.runtime.spi.IType;
 
 public class CollectionMetadataFacadeImpl extends AbstractCollectionMetadataFacade {
 	
-	private IType elementType = null;
-
 	public CollectionMetadataFacadeImpl(
 			IFacadeFactory facadeFactory,
 			CollectionMetadata value) {
 		super(facadeFactory, value);
-	}
-
-	@Override
-	public IType getElementType() {
-		if (elementType == null) {
-			elementType = getFacadeFactory().createType(((CollectionMetadata)getTarget()).getElementType());
-		}
-		return elementType;
 	}
 
 }
