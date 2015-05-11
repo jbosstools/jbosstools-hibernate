d735dcf40a6c0379f2908cfaf89493d941955568
Koen Aers <koen.aers@gmail.com>
2015-05-11 20:25:25 +0200

    POJOClassFacadeImpl classes extend AbstractPOJOClassFacade

--- plugins/org.jboss.tools.hibernate.runtime.v_3_5/src/org/jboss/tools/hibernate/proxy/POJOClassFacadeImpl.java
+++ plugins/org.jboss.tools.hibernate.runtime.v_3_5/src/org/jboss/tools/hibernate/proxy/POJOClassFacadeImpl.java
@@ -1,14 +1,15 @@
 package org.jboss.tools.hibernate.proxy;
 
 import org.hibernate.tool.hbm2x.pojo.POJOClass;
+import org.jboss.tools.hibernate.runtime.common.AbstractPOJOClassFacade;
 import org.jboss.tools.hibernate.runtime.spi.IFacadeFactory;
-import org.jboss.tools.hibernate.runtime.spi.IPOJOClass;
 
-public class POJOClassFacadeImpl implements IPOJOClass {
+public class POJOClassFacadeImpl extends AbstractPOJOClassFacade {
 	
 	private POJOClass target = null;
 	
 	public POJOClassFacadeImpl(IFacadeFactory facadeFactory, POJOClass pojoClass) {
+		super(facadeFactory, pojoClass);
 		target = pojoClass;
 	}
 
