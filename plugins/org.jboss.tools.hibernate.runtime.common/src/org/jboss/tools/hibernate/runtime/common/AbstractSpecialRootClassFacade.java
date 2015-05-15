f045e1cf71096b9150ff85480d2ff23cf58fe264
Koen Aers <koen.aers@gmail.com>
2015-05-15 12:06:50 +0200

    Pull up method 'generate()' from the SpecialRootClassFacadeImpl classes to AbstractSpecialRootClassFacade

--- plugins/org.jboss.tools.hibernate.runtime.common/src/org/jboss/tools/hibernate/runtime/common/AbstractSpecialRootClassFacade.java
+++ plugins/org.jboss.tools.hibernate.runtime.common/src/org/jboss/tools/hibernate/runtime/common/AbstractSpecialRootClassFacade.java
@@ -1,7 +1,11 @@
 package org.jboss.tools.hibernate.runtime.common;
 
+import java.util.Iterator;
+
 import org.jboss.tools.hibernate.runtime.spi.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IPersistentClass;
 import org.jboss.tools.hibernate.runtime.spi.IProperty;
+import org.jboss.tools.hibernate.runtime.spi.IValue;
 
 public abstract class AbstractSpecialRootClassFacade 
 extends AbstractPersistentClassFacade {
@@ -13,6 +17,44 @@ extends AbstractPersistentClassFacade {
 			IFacadeFactory facadeFactory, 
 			Object target) {
 		super(facadeFactory, target);
+	}
+	
+	protected void generate() {
+		if (property == null) {
+			return;
+		}
+		IValue propVal = property.getValue();
+		IValue component = null;
+		if (propVal != null && propVal.isCollection()) {
+			IValue collection = propVal;
+			component = collection.getElement();
+		} else if (propVal.isComponent()) {
+			component = propVal;
+		}
+		if (component != null) {
+			setClassName(component.getComponentClassName());
+			setEntityName(component.getComponentClassName());
+			IPersistentClass ownerClass = component.getOwner();
+			if (component.getParentProperty() != null) {
+				Object newProperty = Util.getInstance(
+						getPropertyClassName(), 
+						getFacadeFactoryClassLoader());
+				parentProperty = getFacadeFactory().createProperty(newProperty);
+				parentProperty.setName(component.getParentProperty());
+				parentProperty.setPersistentClass(ownerClass);
+			}
+			Iterator<IProperty> iterator = component.getPropertyIterator();
+			while (iterator.hasNext()) {
+				IProperty property = iterator.next();
+				if (property != null) {
+					addProperty(property);
+				}
+			}
+		}
+	}
+
+	protected String getPropertyClassName() {
+		return "org.hibernate.mapping.Property";
 	}
 
 }
--- plugins/org.jboss.tools.hibernate.runtime.v_3_5/src/org/jboss/tools/hibernate/runtime/v_3_5/internal/SpecialRootClassFacadeImpl.java
+++ plugins/org.jboss.tools.hibernate.runtime.v_3_5/src/org/jboss/tools/hibernate/runtime/v_3_5/internal/SpecialRootClassFacadeImpl.java
@@ -1,14 +1,9 @@
 package org.jboss.tools.hibernate.runtime.v_3_5.internal;
 
-import java.util.Iterator;
-
-import org.hibernate.mapping.Property;
 import org.hibernate.mapping.RootClass;
 import org.jboss.tools.hibernate.runtime.common.AbstractSpecialRootClassFacade;
 import org.jboss.tools.hibernate.runtime.spi.IFacadeFactory;
-import org.jboss.tools.hibernate.runtime.spi.IPersistentClass;
 import org.jboss.tools.hibernate.runtime.spi.IProperty;
-import org.jboss.tools.hibernate.runtime.spi.IValue;
 
 public class SpecialRootClassFacadeImpl extends AbstractSpecialRootClassFacade {
 
@@ -18,37 +13,6 @@ public class SpecialRootClassFacadeImpl extends AbstractSpecialRootClassFacade {
 		super(facadeFactory, new RootClass());
 		this.property = property;
 		generate();
-	}
-
-	private void generate() {
-		if (property == null) {
-			return;
-		}
-		IValue propVal = property.getValue();
-		IValue component = null;
-		if (propVal != null && propVal.isCollection()) {
-			IValue collection = propVal;
-			component = collection.getElement();
-		} else if (propVal.isComponent()) {
-			component = propVal;
-		}
-		if (component != null) {
-			setClassName(component.getComponentClassName());
-			setEntityName(component.getComponentClassName());
-			IPersistentClass ownerClass = component.getOwner();
-			if (component.getParentProperty() != null) {
-				parentProperty = getFacadeFactory().createProperty(new Property());
-				parentProperty.setName(component.getParentProperty());
-				parentProperty.setPersistentClass(ownerClass);
-			}
-			Iterator<IProperty> iterator = component.getPropertyIterator();
-			while (iterator.hasNext()) {
-				IProperty property = iterator.next();
-				if (property != null) {
-					addProperty(property);
-				}
-			}
-		}
 	}
 
 	public IProperty getParentProperty() {
