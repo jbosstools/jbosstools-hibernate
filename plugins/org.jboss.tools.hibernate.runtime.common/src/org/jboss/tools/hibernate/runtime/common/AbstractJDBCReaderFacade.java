df041e7934a85739c76303bbfcd89acaf830e157
Koen Aers <koen.aers@gmail.com>
2015-05-21 09:55:04 +0200

    Create method 'createProgressListener(IProgressListener progressListener)' in AbstractJDBCReaderFacade

--- plugins/org.jboss.tools.hibernate.runtime.common/src/org/jboss/tools/hibernate/runtime/common/AbstractJDBCReaderFacade.java
+++ plugins/org.jboss.tools.hibernate.runtime.common/src/org/jboss/tools/hibernate/runtime/common/AbstractJDBCReaderFacade.java
@@ -1,7 +1,12 @@
 package org.jboss.tools.hibernate.runtime.common;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 import org.jboss.tools.hibernate.runtime.spi.IFacadeFactory;
 import org.jboss.tools.hibernate.runtime.spi.IJDBCReader;
+import org.jboss.tools.hibernate.runtime.spi.IProgressListener;
 
 public abstract class AbstractJDBCReaderFacade 
 extends AbstractFacade 
@@ -13,4 +18,35 @@ implements IJDBCReader {
 		super(facadeFactory, target);
 	}
 
+	public Object createProgressListener(IProgressListener progressListener) {
+		return Proxy.newProxyInstance(
+				getFacadeFactoryClassLoader(), 
+				new Class[] { getProgressListenerClass() }, 
+				new ProgressListenerInvocationHandler(progressListener));
+	}
+	
+	public Class<?> getProgressListenerClass() {
+		return Util.getClass(
+				getProgressListenerClassName(), 
+				getFacadeFactoryClassLoader());
+	}
+	
+	public String getProgressListenerClassName() {
+		return "org.hibernate.cfg.reveng.ProgressListener";
+	}
+	
+	public class ProgressListenerInvocationHandler implements InvocationHandler {		
+		public Object target;		
+		public ProgressListenerInvocationHandler(Object target) {
+			this.target = target;
+		}
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+			if ("startSubTask".equals(method.getName())) {
+				method.invoke(target, args);
+			}
+			return null;
+		}		
+	}
+		
 }
