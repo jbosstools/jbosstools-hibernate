3eeaeb4af52008dceb470d48befaee769db5a887
Koen Aers <koen.aers@gmail.com>
2015-05-26 08:21:51 +0200

    Make method 'superExportPOJO' public in the MappingExporterExtension classes

--- plugins/org.jboss.tools.hibernate.runtime.v_4_3/src/org/jboss/tools/hibernate/proxy/HibernateMappingExporterExtension.java
+++ plugins/org.jboss.tools.hibernate.runtime.v_4_3/src/org/jboss/tools/hibernate/proxy/HibernateMappingExporterExtension.java
@@ -24,7 +24,7 @@ extends HibernateMappingExporter {
 		delegateExporter = delegate;
 	}
 
-	void superExportPOJO(Map<Object, Object> map, POJOClass pojoClass) {
+	public void superExportPOJO(Map<Object, Object> map, POJOClass pojoClass) {
 		super.exportPOJO(map, pojoClass);
 	}
 	@SuppressWarnings({ "rawtypes", "unchecked" })
