c9993f1a0fb87657dfbb966e9e826533906480fd
Koen Aers <koen.aers@gmail.com>
2015-05-26 08:43:50 +0200

    Pull up 'setGlobalSettings(IHibernateMappingGlobalSettings hmgs)' from HibernateMappingExporterProxy to AbstractHibernateMappingExporter

--- plugins/org.jboss.tools.hibernate.runtime.common/src/org/jboss/tools/hibernate/runtime/common/AbstractHibernateMappingExporterFacade.java
+++ plugins/org.jboss.tools.hibernate.runtime.common/src/org/jboss/tools/hibernate/runtime/common/AbstractHibernateMappingExporterFacade.java
@@ -2,6 +2,7 @@ package org.jboss.tools.hibernate.runtime.common;
 
 import org.jboss.tools.hibernate.runtime.spi.IFacadeFactory;
 import org.jboss.tools.hibernate.runtime.spi.IHibernateMappingExporter;
+import org.jboss.tools.hibernate.runtime.spi.IHibernateMappingGlobalSettings;
 
 public abstract class AbstractHibernateMappingExporterFacade 
 extends AbstractFacade 
@@ -11,6 +12,30 @@ implements IHibernateMappingExporter {
 			IFacadeFactory facadeFactory, 
 			Object target) {
 		super(facadeFactory, target);
+	}
+
+	@Override
+	public void setGlobalSettings(IHibernateMappingGlobalSettings hmgs) {
+		Object hmgsTarget = Util.invokeMethod(
+				hmgs, 
+				"getTarget", 
+				new Class[] {}, 
+				new Object[] {});
+		Util.invokeMethod(
+				getTarget(), 
+				"setGlobalSettings", 
+				new Class[] { getHibernateMappingGlobalSettingsClass() }, 
+				new Object[] { hmgsTarget });
+	}
+	
+	protected Class<?> getHibernateMappingGlobalSettingsClass() {
+		return Util.getClass(
+				getHibernateMappingGlobalSettingsClassName(), 
+				getFacadeFactoryClassLoader());
+	}
+	
+	protected String getHibernateMappingGlobalSettingsClassName() {
+		return "org.hibernate.tool.hbm2x.HibernateMappingGlobalSettings";
 	}
 
 }
--- plugins/org.jboss.tools.hibernate.runtime.v_4_0/src/org/jboss/tools/hibernate/proxy/HibernateMappingExporterProxy.java
+++ plugins/org.jboss.tools.hibernate.runtime.v_4_0/src/org/jboss/tools/hibernate/proxy/HibernateMappingExporterProxy.java
@@ -3,13 +3,11 @@ package org.jboss.tools.hibernate.proxy;
 import java.io.File;
 import java.util.Map;
 
-import org.hibernate.tool.hbm2x.HibernateMappingGlobalSettings;
 import org.hibernate.tool.hbm2x.pojo.POJOClass;
 import org.jboss.tools.hibernate.runtime.common.AbstractHibernateMappingExporterFacade;
 import org.jboss.tools.hibernate.runtime.common.IFacade;
 import org.jboss.tools.hibernate.runtime.spi.IExportPOJODelegate;
 import org.jboss.tools.hibernate.runtime.spi.IFacadeFactory;
-import org.jboss.tools.hibernate.runtime.spi.IHibernateMappingGlobalSettings;
 import org.jboss.tools.hibernate.runtime.spi.IPOJOClass;
 import org.jboss.tools.hibernate.runtime.v_4_0.internal.HibernateMappingExporterExtension;
 
@@ -22,12 +20,6 @@ public class HibernateMappingExporterProxy extends AbstractHibernateMappingExpor
 			HibernateMappingExporterExtension hibernateMappingExporter) {
 		super(facadeFactory, hibernateMappingExporter);
 		target = hibernateMappingExporter;
-	}
-
-	@Override
-	public void setGlobalSettings(IHibernateMappingGlobalSettings hmgs) {
-		assert hmgs instanceof IFacade;
-		target.setGlobalSettings((HibernateMappingGlobalSettings)((IFacade)hmgs).getTarget());
 	}
 
 	@Override
