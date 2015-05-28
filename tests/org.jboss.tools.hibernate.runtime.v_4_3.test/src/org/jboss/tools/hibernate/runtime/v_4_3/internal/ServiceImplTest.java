83efdf48830f6c42cf5b4158c6c6c65baaf0a5d6
Koen Aers <koen.aers@gmail.com>
2015-05-28 09:35:10 +0200

    Add test for ServiceImpl.newAnnotationConfiguration()

--- tests/org.jboss.tools.hibernate.runtime.v_4_3.test/src/org/jboss/tools/hibernate/runtime/v_4_3/internal/ServiceImplTest.java
+++ tests/org.jboss.tools.hibernate.runtime.v_4_3.test/src/org/jboss/tools/hibernate/runtime/v_4_3/internal/ServiceImplTest.java
@@ -1,5 +1,13 @@
 package org.jboss.tools.hibernate.runtime.v_4_3.internal;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ServiceImplTest {
+
+	@Test
+	public void testNewAnnotationConfiguration() {
+		Assert.assertTrue(false);
+	}
 
 }
