83e527a9c48695765920b9d1620d8ccb27b03243
Koen Aers <koen.aers@gmail.com>
2015-05-28 11:49:08 +0200

    Fix failing ServiceImplTest.testNewAnnotationConfiguration() in org.jboss.tools.hibernate.runtime.v_4_3.test

--- tests/org.jboss.tools.hibernate.runtime.v_4_3.test/src/org/jboss/tools/hibernate/runtime/v_4_3/internal/ServiceImplTest.java
+++ tests/org.jboss.tools.hibernate.runtime.v_4_3.test/src/org/jboss/tools/hibernate/runtime/v_4_3/internal/ServiceImplTest.java
@@ -1,13 +1,22 @@
 package org.jboss.tools.hibernate.runtime.v_4_3.internal;
 
+import org.hibernate.cfg.Configuration;
+import org.jboss.tools.hibernate.runtime.common.IFacade;
+import org.jboss.tools.hibernate.runtime.spi.IConfiguration;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class ServiceImplTest {
+	
+	private ServiceImpl service = new ServiceImpl();
 
 	@Test
 	public void testNewAnnotationConfiguration() {
-		Assert.assertTrue(false);
+		IConfiguration configuration = service.newAnnotationConfiguration();
+		Assert.assertNotNull(configuration);
+		Object target = ((IFacade)configuration).getTarget();
+		Assert.assertNotNull(target);
+		Assert.assertTrue(target instanceof Configuration);
 	}
-
+	
 }
