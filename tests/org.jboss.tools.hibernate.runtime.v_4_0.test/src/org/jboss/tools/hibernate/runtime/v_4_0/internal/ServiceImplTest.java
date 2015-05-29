c138862d156c46430b451f7963535a1260b0f88b
Koen Aers <koen.aers@gmail.com>
2015-05-29 16:57:58 +0200

    Add test fragment for org.jboss.tools.hibernate.runtime.v_4_0

---
+++ tests/org.jboss.tools.hibernate.runtime.v_4_0.test/src/org/jboss/tools/hibernate/runtime/v_4_0/internal/ServiceImplTest.java
@@ -0,0 +1,22 @@
+package org.jboss.tools.hibernate.runtime.v_4_0.internal;
+
+import org.hibernate.cfg.Configuration;
+import org.jboss.tools.hibernate.runtime.common.IFacade;
+import org.jboss.tools.hibernate.runtime.spi.IConfiguration;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ServiceImplTest {
+	
+	private ServiceImpl service = new ServiceImpl();
+
+	@Test
+	public void testNewAnnotationConfiguration() {
+		IConfiguration configuration = service.newAnnotationConfiguration();
+		Assert.assertNotNull(configuration);
+		Object target = ((IFacade)configuration).getTarget();
+		Assert.assertNotNull(target);
+		Assert.assertTrue(target instanceof Configuration);
+	}
+	
+}
