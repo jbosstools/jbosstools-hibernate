2b6e4daa0d6d37e64c221a7e02702e72d67893e2
Koen Aers <koen.aers@gmail.com>
2015-06-05 11:18:41 +0200

    Create ArtifactCollectorFacadeTest class in org.jboss.tools.hibernate.runtime.v_3_5.test

---
+++ tests/org.jboss.tools.hibernate.runtime.v_3_5.test/src/org/jboss/tools/hibernate/runtime/v_3_5/internal/ArtifactCollectorFacadeTest.java
@@ -0,0 +1,40 @@
+package org.jboss.tools.hibernate.runtime.v_3_5.internal;
+
+import java.util.Collections;
+import java.util.Set;
+
+import org.hibernate.tool.hbm2x.ArtifactCollector;
+import org.jboss.tools.hibernate.runtime.common.AbstractArtifactCollectorFacade;
+import org.jboss.tools.hibernate.runtime.spi.IArtifactCollector;
+import org.junit.Assert;
+
+public class ArtifactCollectorFacadeTest {
+	
+	private static Set<String> FILE_TYPES = Collections.emptySet();
+	
+	private String methodName = null;
+	private Object[] arguments = null;
+	
+	private IArtifactCollector artifactCollector = null; 
+	
+	public void setUp() {
+		methodName = null;
+		arguments = null;
+		artifactCollector = new AbstractArtifactCollectorFacade(null, new TestArtifactCollector()) {};
+	}
+	
+	public void testGetFileTypes() {
+		Assert.assertSame(FILE_TYPES, artifactCollector.getFileTypes());
+		Assert.assertEquals("getFileTypes", methodName);
+		Assert.assertArrayEquals(new Object[] {}, arguments);
+	}
+
+	private class TestArtifactCollector extends ArtifactCollector {
+		public Set<String> getFileTypes() {
+			methodName = "getFileTypes";
+			arguments = new Object[] {};
+			return FILE_TYPES;
+		}
+	}
+	
+}
