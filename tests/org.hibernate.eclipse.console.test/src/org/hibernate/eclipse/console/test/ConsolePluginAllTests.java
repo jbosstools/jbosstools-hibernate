63643fce30310d4c6e7386d4fe616413d3a07aa5
Koen Aers <koen.aers@gmail.com>
2015-08-14 14:28:26 +0200

    JBIDE-20450: Re-enable PerspectiveTest

--- tests/org.hibernate.eclipse.console.test/src/org/hibernate/eclipse/console/test/ConsolePluginAllTests.java
+++ tests/org.hibernate.eclipse.console.test/src/org/hibernate/eclipse/console/test/ConsolePluginAllTests.java
@@ -22,7 +22,7 @@ public class ConsolePluginAllTests {
 
 		suite.addTestSuite(KnownConfigurationsTest.class);
 		suite.addTestSuite(QueryParametersTest.class);
-//		suite.addTestSuite(PerspectiveTest.class);
+		suite.addTestSuite(PerspectiveTest.class);
 //		suite.addTestSuite(ConsoleConfigurationTest.class);
 //		suite.addTestSuite(JavaFormattingTest.class);
 //		suite.addTestSuite(RefactoringTest.class);
