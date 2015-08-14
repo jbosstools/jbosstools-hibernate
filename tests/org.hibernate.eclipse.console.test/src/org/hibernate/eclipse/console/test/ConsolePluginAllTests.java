f1566ac742a140f3708eac53a32ca365834c97fb
Koen Aers <koen.aers@gmail.com>
2015-08-14 17:08:52 +0200

    JBIDE-20450: Re-enable MappingTestsJpa and disable MappingTestsCore

--- tests/org.hibernate.eclipse.console.test/src/org/hibernate/eclipse/console/test/ConsolePluginAllTests.java
+++ tests/org.hibernate.eclipse.console.test/src/org/hibernate/eclipse/console/test/ConsolePluginAllTests.java
@@ -29,8 +29,8 @@ public class ConsolePluginAllTests {
 		suite.addTestSuite(HQLEditorTest.class);
 		suite.addTestSuite(CriteriaEditorTest.class);
 		
-		suite.addTestSuite(MappingTestsCore.class);
-//		suite.addTestSuite(MappingTestsJpa.class);
+//		suite.addTestSuite(MappingTestsCore.class);
+		suite.addTestSuite(MappingTestsJpa.class);
 //		suite.addTestSuite(MappingTestsAnnotations.class);
 //		suite.addTestSuite(HQLEditorPreferencePageTest.class);
 		
