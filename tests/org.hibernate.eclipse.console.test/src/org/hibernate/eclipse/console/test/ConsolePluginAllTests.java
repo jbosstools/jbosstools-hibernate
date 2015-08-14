112fe4b228747e23651f3ca8fc33aa10d974f9ea
Koen Aers <koen.aers@gmail.com>
2015-08-14 15:20:34 +0200

    JBIDE-20450: Re-enable RefactoringTest

--- tests/org.hibernate.eclipse.console.test/src/org/hibernate/eclipse/console/test/ConsolePluginAllTests.java
+++ tests/org.hibernate.eclipse.console.test/src/org/hibernate/eclipse/console/test/ConsolePluginAllTests.java
@@ -25,7 +25,7 @@ public class ConsolePluginAllTests {
 		suite.addTestSuite(PerspectiveTest.class);
 		suite.addTestSuite(ConsoleConfigurationTest.class);
 		suite.addTestSuite(JavaFormattingTest.class);
-//		suite.addTestSuite(RefactoringTest.class);
+		suite.addTestSuite(RefactoringTest.class);
 //		suite.addTestSuite(HQLEditorTest.class);
 //		suite.addTestSuite(CriteriaEditorTest.class);
 		
