727e106071d32a33eba5bebe9588d94b385fa998
Koen Aers <koen.aers@gmail.com>
2016-01-14 10:54:22 +0100

    JBIDE-20446: Add initial implementation of org.jboss.tools.hibernate.runtime.v_5_0.internal.ServiceImpl

---
+++ plugins/org.jboss.tools.hibernate.runtime.v_5_0/src/org/jboss/tools/hibernate/runtime/v_5_0/internal/ServiceImpl.java
@@ -0,0 +1,340 @@
+package org.jboss.tools.hibernate.runtime.v_5_0.internal;
+
+import java.io.File;
+import java.sql.Connection;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import org.jboss.tools.hibernate.runtime.common.AbstractService;
+import org.jboss.tools.hibernate.runtime.spi.IArtifactCollector;
+import org.jboss.tools.hibernate.runtime.spi.ICfg2HbmTool;
+import org.jboss.tools.hibernate.runtime.spi.IColumn;
+import org.jboss.tools.hibernate.runtime.spi.IConfiguration;
+import org.jboss.tools.hibernate.runtime.spi.IDatabaseCollector;
+import org.jboss.tools.hibernate.runtime.spi.IDialect;
+import org.jboss.tools.hibernate.runtime.spi.IEnvironment;
+import org.jboss.tools.hibernate.runtime.spi.IExporter;
+import org.jboss.tools.hibernate.runtime.spi.IHQLCodeAssist;
+import org.jboss.tools.hibernate.runtime.spi.IHQLQueryPlan;
+import org.jboss.tools.hibernate.runtime.spi.IHibernateMappingExporter;
+import org.jboss.tools.hibernate.runtime.spi.IHibernateMappingGlobalSettings;
+import org.jboss.tools.hibernate.runtime.spi.IJDBCReader;
+import org.jboss.tools.hibernate.runtime.spi.IMetaDataDialect;
+import org.jboss.tools.hibernate.runtime.spi.INamingStrategy;
+import org.jboss.tools.hibernate.runtime.spi.IOverrideRepository;
+import org.jboss.tools.hibernate.runtime.spi.IPersistentClass;
+import org.jboss.tools.hibernate.runtime.spi.IPrimaryKey;
+import org.jboss.tools.hibernate.runtime.spi.IProperty;
+import org.jboss.tools.hibernate.runtime.spi.IReverseEngineeringSettings;
+import org.jboss.tools.hibernate.runtime.spi.IReverseEngineeringStrategy;
+import org.jboss.tools.hibernate.runtime.spi.ISchemaExport;
+import org.jboss.tools.hibernate.runtime.spi.ISessionFactory;
+import org.jboss.tools.hibernate.runtime.spi.ISettings;
+import org.jboss.tools.hibernate.runtime.spi.ITable;
+import org.jboss.tools.hibernate.runtime.spi.ITableFilter;
+import org.jboss.tools.hibernate.runtime.spi.ITableIdentifier;
+import org.jboss.tools.hibernate.runtime.spi.ITypeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IValue;
+import org.xml.sax.EntityResolver;
+
+public class ServiceImpl extends AbstractService {
+
+	@Override
+	public IConfiguration newAnnotationConfiguration() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IConfiguration newJpaConfiguration(String entityResolver, String persistenceUnit,
+			Map<Object, Object> overrides) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IConfiguration newDefaultConfiguration() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IHibernateMappingExporter newHibernateMappingExporter(IConfiguration hcfg, File file) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ISchemaExport newSchemaExport(IConfiguration hcfg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IHQLCodeAssist newHQLCodeAssist(IConfiguration hcfg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IConfiguration newJDBCMetaDataConfiguration() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IExporter createExporter(String exporterClassName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IArtifactCollector newArtifactCollector() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IHQLQueryPlan newHQLQueryPlan(String query, boolean shallow, ISessionFactory sessionFactory) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ITypeFactory newTypeFactory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public INamingStrategy newNamingStrategy(String strategyClassName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IOverrideRepository newOverrideRepository() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ITableFilter newTableFilter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IReverseEngineeringSettings newReverseEngineeringSettings(IReverseEngineeringStrategy res) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IReverseEngineeringStrategy newDefaultReverseEngineeringStrategy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IJDBCReader newJDBCReader(IConfiguration configuration, ISettings settings,
+			IReverseEngineeringStrategy strategy) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IReverseEngineeringStrategy newReverseEngineeringStrategy(String strategyName,
+			IReverseEngineeringStrategy delegate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getReverseEngineeringStrategyClassName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IDatabaseCollector newDatabaseCollector(IMetaDataDialect metaDataDialect) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ICfg2HbmTool newCfg2HbmTool() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IProperty newProperty() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ITable newTable(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IColumn newColumn(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IDialect newDialect(Properties properties, Connection connection) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Class<?> getDriverManagerConnectionProviderClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IEnvironment getEnvironment() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IValue newSimpleValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IValue newPrimitiveArray(IPersistentClass persistentClass) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IValue newArray(IPersistentClass persistentClass) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IValue newBag(IPersistentClass persistentClass) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IValue newList(IPersistentClass persistentClass) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IValue newMap(IPersistentClass persistentClass) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IValue newSet(IPersistentClass persistentClass) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IValue newManyToOne(ITable table) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IValue newOneToMany(IPersistentClass persistentClass) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IValue newOneToOne(IPersistentClass persistentClass) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IPersistentClass newSingleTableSubclass(IPersistentClass persistentClass) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IPersistentClass newJoinedSubclass(IPersistentClass persistentClass) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IPersistentClass newSpecialRootClass(IProperty ormElement) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IPersistentClass newRootClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IPrimaryKey newPrimaryKey() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IHibernateMappingGlobalSettings newHibernateMappingGlobalSettings() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ITableIdentifier createTableIdentifier(ITable table) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ITableIdentifier newTableIdentifier(String catalog, String schema, String typename) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isInitialized(Object object) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<String> getJPAMappingFilePaths(String persistenceUnitName, EntityResolver entityResolver) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Class<?> getClassWithoutInitializingProxy(Object reflectedObject) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClassLoader getClassLoader() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- tests/org.jboss.tools.hibernate.runtime.v_5_0.test/src/org/jboss/tools/hibernate/runtime/v_5_0/internal/ServiceImplTest.java
+++ tests/org.jboss.tools.hibernate.runtime.v_5_0.test/src/org/jboss/tools/hibernate/runtime/v_5_0/internal/ServiceImplTest.java
@@ -1,11 +1,15 @@
 package org.jboss.tools.hibernate.runtime.v_5_0.internal;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ServiceImplTest {
 	
+	private ServiceImpl service = new ServiceImpl();
+	
 	@Test
 	public void test() {
+		Assert.assertNotNull(service);
 	}
 	
 }
