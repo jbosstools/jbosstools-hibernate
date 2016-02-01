045684437e61881a41c9b29950df4fe35d555fa2
Koen Aers <koen.aers@gmail.com>
2016-02-01 15:45:18 +0100

    JBIDE-20446: Add a test for org.jboss.tools.hibernate.runtime.v_5_0.internal.ConfigurationFacadeImpl.addFile()

--- tests/org.jboss.tools.hibernate.runtime.v_5_0.test/src/org/jboss/tools/hibernate/runtime/v_5_0/internal/ConfigurationFacadeTest.java
+++ tests/org.jboss.tools.hibernate.runtime.v_5_0.test/src/org/jboss/tools/hibernate/runtime/v_5_0/internal/ConfigurationFacadeTest.java
@@ -1,7 +1,16 @@
 package org.jboss.tools.hibernate.runtime.v_5_0.internal;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.List;
+
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.jaxb.hbm.spi.JaxbHbmHibernateMapping;
+import org.hibernate.boot.jaxb.spi.Binding;
 import org.hibernate.cfg.Configuration;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.v_5_0.test.MetadataHelper;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,6 +18,13 @@ import org.junit.Test;
 public class ConfigurationFacadeTest {
 
 	private static final IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
+
+	private static final String TEST_HBM_STRING =
+			"<hibernate-mapping>" +
+			"  <class name='Foo'>" + 
+			"    <id name='id'/>" + 
+			"  </class>" + 
+			"</hibernate-mapping>";
 
 	private ConfigurationFacadeImpl configurationFacade = null;
 	private Configuration configuration = null;
@@ -28,4 +44,20 @@ public class ConfigurationFacadeTest {
 		Assert.assertEquals("bar", foo);
 	}
 
+	@Test
+	public void testAddFile() throws Exception {
+		File testFile = File.createTempFile("test", "tmp");
+		testFile.deleteOnExit();
+		FileWriter fileWriter = new FileWriter(testFile);
+		fileWriter.write(TEST_HBM_STRING);
+		fileWriter.close();
+		configurationFacade.addFile(testFile);
+		MetadataSources mds = MetadataHelper.getMetadataSources(configuration);
+		Assert.assertEquals(1, mds.getXmlBindings().size());
+		Binding<?> binding = mds.getXmlBindings().get(0);
+		Assert.assertEquals(
+				testFile.getAbsolutePath(), 
+				binding.getOrigin().getName());
+	}
+	
 }
