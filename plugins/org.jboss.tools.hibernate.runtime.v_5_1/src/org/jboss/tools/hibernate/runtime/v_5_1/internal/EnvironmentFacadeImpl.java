a6216bf13bbe32f496eb94859199ce4df2602500
Koen Aers <koen.aers@gmail.com>
2016-02-29 15:58:42 +0100

    JBIDE-21735: Add EnvironmentFacadeTest and EnvironmentFacadeImpl classes

---
+++ plugins/org.jboss.tools.hibernate.runtime.v_5_1/src/org/jboss/tools/hibernate/runtime/v_5_1/internal/EnvironmentFacadeImpl.java
@@ -0,0 +1,19 @@
+package org.jboss.tools.hibernate.runtime.v_5_1.internal;
+
+import org.hibernate.cfg.Environment;
+import org.jboss.tools.hibernate.runtime.common.AbstractEnvironmentFacade;
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+
+public class EnvironmentFacadeImpl extends AbstractEnvironmentFacade {
+
+	public EnvironmentFacadeImpl(
+			IFacadeFactory facadeFactory) {
+		super(facadeFactory, null);
+	}
+
+	@Override
+	public String getTransactionManagerStrategy() {
+		return Environment.TRANSACTION_COORDINATOR_STRATEGY;
+	}
+
+}
