77d48ede7a0dd1c6ac2e525e299a22b12656238b
Koen Aers <koen.aers@gmail.com>
2016-04-21 17:16:41 +0200

    Revert "JBIDE-22168: Make sure tempFile is deleted on Windows as well"
    This reverts commit 7d399e0b46a82e2820581c18ec247adb9a93f8fe.

--- plugins/org.jboss.tools.hibernate.runtime.v_5_1/src/org/jboss/tools/hibernate/runtime/v_5_1/internal/ConfigurationFacadeImpl.java
+++ plugins/org.jboss.tools.hibernate.runtime.v_5_1/src/org/jboss/tools/hibernate/runtime/v_5_1/internal/ConfigurationFacadeImpl.java
@@ -76,7 +76,7 @@ public class ConfigurationFacadeImpl extends AbstractConfigurationFacade {
 		File tempFile = null;
 		IConfiguration result = null;
 		try {
-			tempFile = File.createTempFile(document.toString() + ".cfg", "xml");
+			tempFile = File.createTempFile(document.toString(), "cfg.xml");
 			DOMSource domSource = new DOMSource(document);
 			StringWriter stringWriter = new StringWriter();
 			StreamResult stream = new StreamResult(stringWriter);
@@ -89,12 +89,8 @@ public class ConfigurationFacadeImpl extends AbstractConfigurationFacade {
 			result = configure(tempFile);
 		} catch(IOException | TransformerException e) {
 			throw new RuntimeException("Problem while configuring", e);
-		}
-		System.out.println("*********** Checking for tempFile: " + tempFile.getAbsolutePath() + " ***************************");
-		if (tempFile.exists()) {
-			System.out.println("*********** Deleting tempFile: " + tempFile.getAbsolutePath() + " ***************************");
-			boolean success = tempFile.delete();
-			System.out.println("*********** ===> " + success + "*************");
+		} finally {
+			tempFile.delete();
 		}
 		return result;
 	}
