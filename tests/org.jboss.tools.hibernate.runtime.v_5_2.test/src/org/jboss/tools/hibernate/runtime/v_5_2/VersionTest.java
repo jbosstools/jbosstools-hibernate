31c532f3e0e98348d91673bdff74499d60db8ac9
Koen Aers <koen.aers@gmail.com>
2016-06-06 11:01:31 +0200

    JBIDE-22542: Add support for Hibernate 5.2 - Implement VersionTest.testVersion()

--- tests/org.jboss.tools.hibernate.runtime.v_5_2.test/src/org/jboss/tools/hibernate/runtime/v_5_2/VersionTest.java
+++ tests/org.jboss.tools.hibernate.runtime.v_5_2.test/src/org/jboss/tools/hibernate/runtime/v_5_2/VersionTest.java
@@ -1,12 +1,13 @@
 package org.jboss.tools.hibernate.runtime.v_5_2;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class VersionTest {
 	
 	@Test
 	public void testVersion() {
-		
+		Assert.assertEquals("5.2.0.Alpha1", org.hibernate.tool.Version.VERSION);
 	}
 
 }
