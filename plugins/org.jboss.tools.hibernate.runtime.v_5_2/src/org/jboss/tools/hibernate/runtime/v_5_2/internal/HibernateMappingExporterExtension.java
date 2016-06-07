7a21227189c7292e6e49cc38e201305f72d567b5
Koen Aers <koen.aers@gmail.com>
2016-06-07 14:45:26 +0200

    JBIDE-22542: Add support for Hibernate 5.2 - Add org.jboss.tools.hibernate.runtime.v_5_2.internal.ServiceImplTest and implement org.jboss.tools.hibernate.runtime.v_5_2.internal.ServiceImpl

---
+++ plugins/org.jboss.tools.hibernate.runtime.v_5_2/src/org/jboss/tools/hibernate/runtime/v_5_2/internal/HibernateMappingExporterExtension.java
@@ -0,0 +1,41 @@
+package org.jboss.tools.hibernate.runtime.v_5_2.internal;
+
+import java.io.File;
+import java.util.Map;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.tool.hbm2x.HibernateMappingExporter;
+import org.hibernate.tool.hbm2x.pojo.POJOClass;
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IExportPOJODelegate;
+
+public class HibernateMappingExporterExtension 
+extends HibernateMappingExporter {
+	
+	private IExportPOJODelegate delegateExporter;
+	private IFacadeFactory facadeFactory;
+	
+	public HibernateMappingExporterExtension(IFacadeFactory facadeFactory, Configuration cfg, File file) {
+		super(cfg, file);
+		this.facadeFactory = facadeFactory;
+	}
+	
+	public void setDelegate(IExportPOJODelegate delegate) {
+		delegateExporter = delegate;
+	}
+
+	public void superExportPOJO(Map<String, Object> map, POJOClass pojoClass) {
+		super.exportPOJO(map, pojoClass);
+	}
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Override
+	protected void exportPOJO(Map map, POJOClass pojoClass) {
+		if (delegateExporter == null) {
+			super.exportPOJO(map, pojoClass);
+		} else {
+			delegateExporter.exportPOJO(
+					(Map<Object, Object>)map, 
+					facadeFactory.createPOJOClass(pojoClass));
+		}
+	}
+}
---
+++ plugins/org.jboss.tools.hibernate.runtime.v_5_2/src/org/jboss/tools/hibernate/runtime/v_5_2/internal/JPAConfiguration.java
@@ -0,0 +1,62 @@
+package org.jboss.tools.hibernate.runtime.v_5_2.internal;
+
+import java.util.Map;
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.jpa.boot.internal.EntityManagerFactoryBuilderImpl;
+
+public class JPAConfiguration extends Configuration {
+	
+	private Metadata metadata = null;
+	
+	private String persistenceUnit;
+	
+	public JPAConfiguration(
+			String persistenceUnit, 
+			Map<Object, Object> properties) {
+		this.persistenceUnit = persistenceUnit;
+		if (properties != null) {
+			getProperties().putAll(properties);
+		}
+	}
+	
+	public Metadata getMetadata() {
+		if (metadata == null) {
+			EntityManagerFactoryBuilderImpl entityManagerFactoryBuilder = 
+					HibernateToolsPersistenceProvider
+						.createEntityManagerFactoryBuilder(
+								persistenceUnit, 
+								getProperties());
+			EntityManagerFactory entityManagerFactory = 
+					entityManagerFactoryBuilder.build();
+			metadata = entityManagerFactoryBuilder.getMetadata();
+			getProperties().putAll(entityManagerFactory.getProperties());
+		}
+		return metadata;
+	}
+	
+	@Override
+	public SessionFactory buildSessionFactory() {
+		return getMetadata().buildSessionFactory();
+	}
+	
+	@Override
+	public Configuration setProperties(Properties properties) {
+		super.setProperties(properties);
+		metadata = null;
+		return this;
+	}
+	
+	@Override
+	public Configuration addProperties(Properties properties) {
+		super.addProperties(properties);
+		metadata = null;
+		return this;
+	}
+	
+}
