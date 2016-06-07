3a3e66e07f314db79de474146c263bcd02b8fd6d
Koen Aers <koen.aers@gmail.com>
2016-06-07 19:44:04 +0200

    JBIDE-22542: Add support for Hibernate 5.2 - Implement org.jboss.tools.hibernate.runtime.v_5_2.internal.ConfigurationFacadeTest

---
+++ plugins/org.jboss.tools.hibernate.runtime.v_5_2/src/org/jboss/tools/hibernate/runtime/v_5_2/internal/MappingsFacadeImpl.java
@@ -0,0 +1,27 @@
+package org.jboss.tools.hibernate.runtime.v_5_2.internal;
+
+import org.jboss.tools.hibernate.runtime.common.IFacade;
+import org.jboss.tools.hibernate.runtime.spi.IConfiguration;
+import org.jboss.tools.hibernate.runtime.spi.IMappings;
+import org.jboss.tools.hibernate.runtime.spi.IPersistentClass;
+
+public class MappingsFacadeImpl implements IFacade, IMappings {
+	
+	ConfigurationFacadeImpl configuration = null;
+	
+	public MappingsFacadeImpl(IConfiguration configuration) {
+		this.configuration = (ConfigurationFacadeImpl)configuration;
+	}
+
+	@Override
+	public void addClass(IPersistentClass persistentClass) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public Object getTarget() {
+		// From Hibernate 5.0 on the Mappings class has been removed
+		return null;
+	}
+
+}
