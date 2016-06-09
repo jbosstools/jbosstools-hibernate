bad4626c15561bf3956d294a46e644c08edc8361
Koen Aers <koen.aers@gmail.com>
2016-06-09 18:00:44 +0200

    BIDE-22542: Add support for Hibernate 5.2 - Implement org.jboss.tools.hibernate.runtime.v_5_2.internal.SettingsFacadeTest

---
+++ tests/org.jboss.tools.hibernate.runtime.v_5_2.test/src/org/jboss/tools/hibernate/runtime/v_5_2/internal/SettingsFacadeTest.java
@@ -0,0 +1,39 @@
+package org.jboss.tools.hibernate.runtime.v_5_2.internal;
+
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.cfg.Environment;
+import org.jboss.tools.hibernate.runtime.common.AbstractSettingsFacade;
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.ISettings;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SettingsFacadeTest {
+	
+	private static final IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
+	
+	private ISettings settingsFacade = null;
+	
+	@Before
+	public void setUp() {
+		settingsFacade = new AbstractSettingsFacade(FACADE_FACTORY, new Settings()) {};
+	}
+	
+	@Test
+	public void testGetDefaultCatalogName() {
+		Assert.assertEquals(
+				Environment.getProperties().getProperty(
+						AvailableSettings.DEFAULT_CATALOG), 
+				settingsFacade.getDefaultCatalogName());
+	}
+
+	@Test
+	public void testGetDefaultSchemaName() {
+		Assert.assertEquals(
+				Environment.getProperties().getProperty(
+						AvailableSettings.DEFAULT_SCHEMA), 
+				settingsFacade.getDefaultSchemaName());
+	}
+
+}
