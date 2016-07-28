176eac45760e9639c7e076a026013f1020ebce0e
Koen Aers <koen.aers@gmail.com>
2016-07-28 17:09:44 +0200

    JBIDE-22849: Update Hibernate Tools dependencies to 4.3.5.Final, 5.0.1.Final, 5.1.0.Beta2 and 5.2.0.Alpha3

--- tests/org.jboss.tools.hibernate.runtime.v_5_2.test/src/org/jboss/tools/hibernate/runtime/v_5_2/VersionTest.java
+++ tests/org.jboss.tools.hibernate.runtime.v_5_2.test/src/org/jboss/tools/hibernate/runtime/v_5_2/VersionTest.java
@@ -7,7 +7,7 @@ public class VersionTest {
 	
 	@Test
 	public void testVersion() {
-		Assert.assertEquals("5.2.0.Alpha2", org.hibernate.tool.Version.VERSION);
+		Assert.assertEquals("5.2.0.Alpha3", org.hibernate.tool.Version.VERSION);
 	}
 
 }
