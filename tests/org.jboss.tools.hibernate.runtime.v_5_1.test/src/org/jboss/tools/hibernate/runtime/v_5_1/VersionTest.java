5cfb2359ad2fb92579278033942547f8500f7dda
Koen Aers <koen.aers@gmail.com>
2016-08-24 07:51:38 +0200

    JBIDE-23027: Update Hibernate Tools dependency to 5.1.0.CR1 - Add test that verifies the change

---
+++ tests/org.jboss.tools.hibernate.runtime.v_5_1.test/src/org/jboss/tools/hibernate/runtime/v_5_1/VersionTest.java
@@ -0,0 +1,18 @@
+package org.jboss.tools.hibernate.runtime.v_5_1;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class VersionTest {
+	
+	@Test
+	public void testToolsVersion() {
+		Assert.assertEquals("5.1.0.CR1", org.hibernate.tool.Version.VERSION);
+	}
+	
+	@Test
+	public void testCoreVersion() {
+		Assert.assertEquals("5.1.1.Final", org.hibernate.Version.getVersionString());
+	}
+
+}
