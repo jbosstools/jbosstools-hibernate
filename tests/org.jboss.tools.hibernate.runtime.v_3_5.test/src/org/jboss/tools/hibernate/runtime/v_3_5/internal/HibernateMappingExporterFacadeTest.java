081930ae65953ea5eaa79e2b14287e38b9100f41
Koen Aers <koen.aers@gmail.com>
2016-09-07 14:39:13 +0200

    JBIDE-23096: Remove org.jboss.tools.hibernate.runtime.v_3_5.internal.HibernateMappingExporterFacadeTesttestSetGlobalSettings()

--- tests/org.jboss.tools.hibernate.runtime.v_3_5.test/src/org/jboss/tools/hibernate/runtime/v_3_5/internal/HibernateMappingExporterFacadeTest.java
+++ tests/org.jboss.tools.hibernate.runtime.v_3_5.test/src/org/jboss/tools/hibernate/runtime/v_3_5/internal/HibernateMappingExporterFacadeTest.java
@@ -1,16 +1,11 @@
 package org.jboss.tools.hibernate.runtime.v_3_5.internal;
 
-import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 
 import org.hibernate.tool.hbm2x.HibernateMappingExporter;
-import org.hibernate.tool.hbm2x.HibernateMappingGlobalSettings;
 import org.jboss.tools.hibernate.runtime.common.AbstractHibernateMappingExporterFacade;
-import org.jboss.tools.hibernate.runtime.common.IFacade;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
 import org.jboss.tools.hibernate.runtime.spi.IHibernateMappingExporter;
-import org.jboss.tools.hibernate.runtime.spi.IHibernateMappingGlobalSettings;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -57,28 +52,6 @@ public class HibernateMappingExporterFacadeTest {
 				FACADE_FACTORY, 
 				enhancer.create()) {};
 		reset();
-	}
-	
-	@Test
-	public void testSetGlobalSettings() {
-		final HibernateMappingGlobalSettings hibernateMappingGlobalSettings = new HibernateMappingGlobalSettings();
-		IHibernateMappingGlobalSettings hibernateMappingGlobalSettingsFacade = 
-				(IHibernateMappingGlobalSettings)Proxy.newProxyInstance(
-						FACADE_FACTORY.getClassLoader(), 
-						new Class[] { IHibernateMappingGlobalSettings.class, IFacade.class }, 
-						new InvocationHandler() {
-							@Override
-							public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-								if ("getTarget".equals(method.getName())) {
-									return hibernateMappingGlobalSettings;
-								} else {
-									return null;
-								}
-							}						
-						} );
-		hibernateMappingExporterFacade.setGlobalSettings(hibernateMappingGlobalSettingsFacade);
-		Assert.assertEquals("setGlobalSettings", methodName);
-		Assert.assertArrayEquals(new Object[] { hibernateMappingGlobalSettings }, arguments);
 	}
 	
 	@Test
