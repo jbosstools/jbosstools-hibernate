6932090312aeb56a4c24668f6a1c41948d85ec0b
Koen Aers <koen.aers@gmail.com>
2016-10-05 09:10:26 +0200

    JBIDE-23287: Update Hibernate Tools dependencies to 5.0.3.Final, 5.1.0.Final and 5.2.0.CR1

--- tests/org.jboss.tools.hibernate.runtime.v_5_0.test/src/org/jboss/tools/hibernate/runtime/v_5_0/VersionTest.java
+++ tests/org.jboss.tools.hibernate.runtime.v_5_0.test/src/org/jboss/tools/hibernate/runtime/v_5_0/VersionTest.java
@@ -7,12 +7,12 @@ public class VersionTest {
 	
 	@Test
 	public void testToolsVersion() {
-		Assert.assertEquals("5.0.2.Final", org.hibernate.tool.Version.VERSION);
+		Assert.assertEquals("5.0.3.Final", org.hibernate.tool.Version.VERSION);
 	}
 	
 	@Test
 	public void testCoreVersion() {
-		Assert.assertEquals("5.0.10.Final", org.hibernate.Version.getVersionString());
+		Assert.assertEquals("5.0.11.Final", org.hibernate.Version.getVersionString());
 	}
 
 }
--- tests/org.jboss.tools.hibernate.runtime.v_5_1.test/src/org/jboss/tools/hibernate/runtime/v_5_1/VersionTest.java
+++ tests/org.jboss.tools.hibernate.runtime.v_5_1.test/src/org/jboss/tools/hibernate/runtime/v_5_1/VersionTest.java
@@ -7,12 +7,12 @@ public class VersionTest {
 	
 	@Test
 	public void testToolsVersion() {
-		Assert.assertEquals("5.1.0.CR1", org.hibernate.tool.Version.VERSION);
+		Assert.assertEquals("5.1.0.Final", org.hibernate.tool.Version.VERSION);
 	}
 	
 	@Test
 	public void testCoreVersion() {
-		Assert.assertEquals("5.1.1.Final", org.hibernate.Version.getVersionString());
+		Assert.assertEquals("5.1.2.Final", org.hibernate.Version.getVersionString());
 	}
 
 }
