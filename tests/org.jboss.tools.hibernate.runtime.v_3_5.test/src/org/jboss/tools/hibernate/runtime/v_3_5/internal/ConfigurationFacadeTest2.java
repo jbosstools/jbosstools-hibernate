0b5671e883dffdd3f439271d9a449b7ff82bee47
Koen Aers <koen.aers@gmail.com>
2016-10-08 13:38:26 +0200

    JBIDE-23305: Reimplement org.jboss.tools.hibernate.runtime.v_3_5.internal.ConfigurationFacadeTest - initial implementation

---
+++ tests/org.jboss.tools.hibernate.runtime.v_3_5.test/src/org/jboss/tools/hibernate/runtime/v_3_5/internal/ConfigurationFacadeTest2.java
@@ -0,0 +1,31 @@
+package org.jboss.tools.hibernate.runtime.v_3_5.internal;
+
+import org.hibernate.cfg.Configuration;
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IConfiguration;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ConfigurationFacadeTest2 {
+
+	private static final IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
+
+	private IConfiguration configurationFacade = null;
+	private Configuration configuration = null;
+
+	@Before
+	public void setUp() {
+		configuration = new Configuration();
+		configurationFacade = FACADE_FACTORY.createConfiguration(configuration);
+	}
+	
+	@Test
+	public void testGetProperty() {
+		Assert.assertNull(configuration.getProperty("foo"));
+		configurationFacade.setProperty("foo", "bar");
+		Assert.assertEquals("bar", configuration.getProperty("foo"));
+	}
+	
+	
+}
