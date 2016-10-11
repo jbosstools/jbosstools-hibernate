fe5405e2af51b141c68afc9101c8508f569b7580
Koen Aers <koen.aers@gmail.com>
2016-10-11 18:51:49 +0200

    JBIDE-23305: Reimplement org.jboss.tools.hibernate.runtime.v_5_2.internal.ConfigurationFacadeTest - tests for ConfigurationFacadeImpl#getProperty(), ConfigurationFacadeImpl#getProperties(), ConfigurationFacadeImpl#setProperty(String,String) and ConfigurationFacadeImpl#addFile(File)

---
+++ tests/org.jboss.tools.hibernate.runtime.v_5_2.test/src/org/jboss/tools/hibernate/runtime/v_5_2/internal/ConfigurationFacadeTest2.java
@@ -0,0 +1,78 @@
+package org.jboss.tools.hibernate.runtime.v_5_2.internal;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Properties;
+
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.jaxb.spi.Binding;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.tool.util.MetadataHelper;
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IConfiguration;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ConfigurationFacadeTest2 {
+	
+	private static final String TEST_HBM_XML_STRING =
+			"<!DOCTYPE hibernate-mapping PUBLIC" +
+			"		'-//Hibernate/Hibernate Mapping DTD 3.0//EN'" +
+			"		'http://www.hibernate.org/dtd/hibernate-mapping-3.0.dtd'>" +
+			"<hibernate-mapping package='org.jboss.tools.hibernate.runtime.v_5_2.internal'>" +
+			"  <class name='ConfigurationFacadeTest2$Foo'>" + 
+			"    <id name='id'/>" +
+			"  </class>" +
+			"</hibernate-mapping>";
+	
+	static class Foo {
+		public String id;
+	}
+	
+	private static final IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
+
+	private IConfiguration configurationFacade = null;
+	private Configuration configuration = null;
+
+	@Before
+	public void setUp() {
+		configuration = new Configuration();
+		configurationFacade = FACADE_FACTORY.createConfiguration(configuration);
+	}
+	
+	@Test
+	public void testGetProperty() {
+		Assert.assertNull(configurationFacade.getProperty("foo"));
+		configuration.setProperty("foo", "bar");
+		Assert.assertEquals("bar", configurationFacade.getProperty("foo"));
+	}
+
+	@Test 
+	public void testSetProperties() {
+		Properties testProperties = new Properties();
+		Assert.assertNotSame(testProperties, configuration.getProperties());
+		Assert.assertSame(
+				configurationFacade, 
+				configurationFacade.setProperties(testProperties));
+		Assert.assertSame(testProperties, configuration.getProperties());
+	}
+	
+	@Test
+	public void testAddFile() throws Exception {
+		File testFile = File.createTempFile("test", "hbm.xml");
+		PrintWriter printWriter = new PrintWriter(testFile);
+		printWriter.write(TEST_HBM_XML_STRING);
+		printWriter.close();
+		MetadataSources metadataSources = MetadataHelper.getMetadataSources(configuration);
+		Assert.assertTrue(metadataSources.getXmlBindings().isEmpty());
+		Assert.assertSame(
+				configurationFacade,
+				configurationFacade.addFile(testFile));
+		Assert.assertFalse(metadataSources.getXmlBindings().isEmpty());
+		Binding<?> binding = metadataSources.getXmlBindings().iterator().next();
+		Assert.assertEquals(testFile.getAbsolutePath(), binding.getOrigin().getName());
+		Assert.assertTrue(testFile.delete());
+	}
+	
+}
