a608243ba88f9c63eb71c2a561e825dc0a810f90
Koen Aers <koen.aers@gmail.com>
2016-11-24 11:06:38 +0100

    JBIDE-23508: Create tests for org.jboss.tools.hibernate.runtime.spi.IValue - Add tests for IValue#getCollectionElement()

--- tests/org.jboss.tools.hibernate.runtime.v_5_2.test/src/org/jboss/tools/hibernate/runtime/v_5_2/internal/ValueFacadeTest.java
+++ tests/org.jboss.tools.hibernate.runtime.v_5_2.test/src/org/jboss/tools/hibernate/runtime/v_5_2/internal/ValueFacadeTest.java
@@ -3,6 +3,7 @@ package org.jboss.tools.hibernate.runtime.v_5_2.internal;
 import org.hibernate.mapping.Set;
 import org.hibernate.mapping.SimpleValue;
 import org.hibernate.mapping.Value;
+import org.jboss.tools.hibernate.runtime.common.IFacade;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
 import org.jboss.tools.hibernate.runtime.spi.IValue;
 import org.junit.Assert;
@@ -33,6 +34,20 @@ public class ValueFacadeTest {
 		valueTarget = new Set(null, null);
 		valueFacade = FACADE_FACTORY.createValue(valueTarget);
 		Assert.assertTrue(valueFacade.isCollection());
+	}
+
+	@Test
+	public void testGetCollectionElement() {
+		valueTarget = new SimpleValue(null);
+		valueFacade = FACADE_FACTORY.createValue(valueTarget);
+		IValue collectionElement = valueFacade.getCollectionElement();
+		Assert.assertNull(collectionElement);
+		Set set = new Set(null, null);
+		set.setElement(valueTarget);
+		valueFacade = FACADE_FACTORY.createValue(set);
+		collectionElement = valueFacade.getCollectionElement();
+		Assert.assertNotNull(collectionElement);
+		Assert.assertSame(valueTarget, ((IFacade)collectionElement).getTarget());
 	}
 
 }
