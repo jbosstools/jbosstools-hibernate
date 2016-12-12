2946554adee936919434f21f00c3cdcb0ac0e8c8
Koen Aers <koen.aers@gmail.com>
2016-12-12 19:08:30 +0100

    JBIDE-23630: Fix the failing version test

--- tests/org.jboss.tools.hibernate.runtime.v_5_2.test/src/org/jboss/tools/hibernate/runtime/v_5_2/VersionTest.java
+++ tests/org.jboss.tools.hibernate.runtime.v_5_2.test/src/org/jboss/tools/hibernate/runtime/v_5_2/VersionTest.java
@@ -7,12 +7,12 @@ public class VersionTest {
 	
 	@Test
 	public void testToolsVersion() {
-		Assert.assertEquals("5.2.0.CR1", org.hibernate.tool.Version.VERSION);
+		Assert.assertEquals("5.2.0.Final", org.hibernate.tool.Version.VERSION);
 	}
 
 	@Test
 	public void testCoreVersion() {
-		Assert.assertEquals("5.2.3.Final", org.hibernate.Version.getVersionString());
+		Assert.assertEquals("5.2.5.Final", org.hibernate.Version.getVersionString());
 	}
 
 }
