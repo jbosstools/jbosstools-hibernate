5b72aa77a37eaa23af5e82f88e97c9061a23a28e
Koen Aers <koen.aers@gmail.com>
2017-01-04 15:07:31 +0100

    JBIDE-23664: Add tests for org.jboss.tools.hibernate.runtime.spi.ITypeFactory in the runtime providers - Add test for ITypeFactory#getBooleanType()

---
+++ tests/org.jboss.tools.hibernate.runtime.v_3_5.test/src/org/jboss/tools/hibernate/runtime/v_3_5/internal/TypeFactoryFacadeTest.java
@@ -0,0 +1,23 @@
+package org.jboss.tools.hibernate.runtime.v_3_5.internal;
+
+import org.hibernate.Hibernate;
+import org.hibernate.type.Type;
+import org.jboss.tools.hibernate.runtime.common.IFacade;
+import org.jboss.tools.hibernate.runtime.spi.IType;
+import org.jboss.tools.hibernate.runtime.spi.ITypeFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TypeFactoryFacadeTest {
+	
+	private ITypeFactory typeFactoryFacade = new FacadeFactoryImpl().createTypeFactory();
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testgetBooleanType() {
+		Type typeTarget = Hibernate.BOOLEAN;
+		IType typeFacade = typeFactoryFacade.getBooleanType();
+		Assert.assertSame(typeTarget, ((IFacade)typeFacade).getTarget());
+	}
+	
+}
---
+++ tests/org.jboss.tools.hibernate.runtime.v_5_2.test/src/org/jboss/tools/hibernate/runtime/v_5_2/internal/TypeFactoryFacadeTest.java
@@ -0,0 +1,22 @@
+package org.jboss.tools.hibernate.runtime.v_5_2.internal;
+
+import org.hibernate.type.StandardBasicTypes;
+import org.hibernate.type.Type;
+import org.jboss.tools.hibernate.runtime.common.IFacade;
+import org.jboss.tools.hibernate.runtime.spi.IType;
+import org.jboss.tools.hibernate.runtime.spi.ITypeFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TypeFactoryFacadeTest {
+	
+	private ITypeFactory typeFactoryFacade = new FacadeFactoryImpl().createTypeFactory();
+	
+	@Test
+	public void testgetBooleanType() {
+		Type typeTarget = StandardBasicTypes.BOOLEAN;
+		IType typeFacade = typeFactoryFacade.getBooleanType();
+		Assert.assertSame(typeTarget, ((IFacade)typeFacade).getTarget());
+	}
+	
+}
