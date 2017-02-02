b4a1d41afa34345b9bfaf495883391f43609feac
Koen Aers <koen.aers@gmail.com>
2017-02-02 12:27:43 +0100

    JBIDE-23836: Create tests for org.jboss.tools.hibernate.runtime.spi.IType : Add test for IType#toString(Object)

---
+++ tests/org.jboss.tools.hibernate.runtime.v_5_0.test/src/org/jboss/tools/hibernate/runtime/v_5_0/internal/TypeFacadeTest.java
@@ -0,0 +1,27 @@
+package org.jboss.tools.hibernate.runtime.v_5_0.internal;
+
+import org.hibernate.type.ArrayType;
+import org.hibernate.type.ClassType;
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IType;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TypeFacadeTest {
+
+	private static IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
+	
+	@Test
+	public void testToString() {
+		IType typeFacade = null;
+		ClassType classType = new ClassType();
+		typeFacade = FACADE_FACTORY.createType(classType);
+		Assert.assertEquals(
+				TypeFacadeTest.class.getName(), 
+				typeFacade.toString(TypeFacadeTest.class));
+		ArrayType arrayType = new ArrayType(null, null, null, String.class);
+		typeFacade = FACADE_FACTORY.createType(arrayType);
+		Assert.assertNull(typeFacade.toString(new String[] { "foo", "bar" }));
+	}
+	
+}
