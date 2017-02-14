0c93093925b16b58c905e7bdad93f5336a1f0f1a
Koen Aers <koen.aers@gmail.com>
2017-02-14 13:39:27 +0100

    JBIDE-23894: Add tests for org.jboss.tools.hibernate.runtime.spi.ITableFilter in the runtime providers - Add test for ITableFilter#setExclude(Boolean)

---
+++ tests/org.jboss.tools.hibernate.runtime.v_5_0.test/src/org/jboss/tools/hibernate/runtime/v_5_0/internal/TableFilterFacadeTest.java
@@ -0,0 +1,22 @@
+package org.jboss.tools.hibernate.runtime.v_5_0.internal;
+
+import org.hibernate.cfg.reveng.TableFilter;
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.ITableFilter;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TableFilterFacadeTest {
+
+	private static IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
+	
+	@Test
+	public void testSetExclude() {
+		TableFilter tableFilter = new TableFilter();
+		ITableFilter tableFilterFacade = FACADE_FACTORY.createTableFilter(tableFilter);
+		Assert.assertNull(tableFilter.getExclude());
+		tableFilterFacade.setExclude(true);
+		Assert.assertTrue(tableFilter.getExclude());
+	}
+	
+}
