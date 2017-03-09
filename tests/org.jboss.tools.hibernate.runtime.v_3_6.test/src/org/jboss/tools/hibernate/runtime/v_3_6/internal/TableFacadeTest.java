2f9d9ca8bd68b7ca3c0f4dff1ae5e80b4f4eaf48
Koen Aers <koen.aers@gmail.com>
2017-03-09 11:26:55 +0100

    JBIDE-24045: Create tests for the implementations of org.jboss.tools.hibernate.runtime.spi.ITable - Add test for ITable#getName()

---
+++ tests/org.jboss.tools.hibernate.runtime.v_3_6.test/src/org/jboss/tools/hibernate/runtime/v_3_6/internal/TableFacadeTest.java
@@ -0,0 +1,22 @@
+package org.jboss.tools.hibernate.runtime.v_3_6.internal;
+
+import org.hibernate.mapping.Table;
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.ITable;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TableFacadeTest {
+
+	private static IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
+	
+	@Test
+	public void testGetName() {
+		Table table = new Table();
+		ITable tableFacade = FACADE_FACTORY.createTable(table);
+		Assert.assertNull(tableFacade.getName());
+		table.setName("foo");
+		Assert.assertEquals("foo", tableFacade.getName());
+	}
+	
+}
