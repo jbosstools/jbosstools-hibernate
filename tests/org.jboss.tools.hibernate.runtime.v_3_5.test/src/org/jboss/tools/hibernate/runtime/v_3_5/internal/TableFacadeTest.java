4225a7f9844922e1fcbefd915e520691fd764236
Koen Aers <koen.aers@gmail.com>
2017-03-10 11:54:53 +0100

    JBIDE-24045: Create tests for the implementations of org.jboss.tools.hibernate.runtime.spi.ITable - Add test for ITable#addColumn(IColumn)

--- tests/org.jboss.tools.hibernate.runtime.v_3_5.test/src/org/jboss/tools/hibernate/runtime/v_3_5/internal/TableFacadeTest.java
+++ tests/org.jboss.tools.hibernate.runtime.v_3_5.test/src/org/jboss/tools/hibernate/runtime/v_3_5/internal/TableFacadeTest.java
@@ -1,7 +1,9 @@
 package org.jboss.tools.hibernate.runtime.v_3_5.internal;
 
+import org.hibernate.mapping.Column;
 import org.hibernate.mapping.Table;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IColumn;
 import org.jboss.tools.hibernate.runtime.spi.ITable;
 import org.junit.Assert;
 import org.junit.Test;
@@ -17,6 +19,17 @@ public class TableFacadeTest {
 		Assert.assertNull(tableFacade.getName());
 		table.setName("foo");
 		Assert.assertEquals("foo", tableFacade.getName());
+	}
+	
+	@Test
+	public void testAddColumn() {
+		Table table = new Table();
+		ITable tableFacade = FACADE_FACTORY.createTable(table);
+		Column column = new Column("foo");
+		IColumn columnFacade = FACADE_FACTORY.createColumn(column);
+		Assert.assertNull(table.getColumn(column));
+		tableFacade.addColumn(columnFacade);
+		Assert.assertSame(column, table.getColumn(column));
 	}
 	
 }
