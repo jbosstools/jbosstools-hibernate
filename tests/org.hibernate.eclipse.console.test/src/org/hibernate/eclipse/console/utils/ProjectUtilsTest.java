366d790deb99cc75baf9d69328d06e1cacdc39c2
Koen Aers <koen.aers@gmail.com>
2017-03-15 11:38:13 +0100

    JBIDE-24077: NullPointerException in ProjectUtils.availablePersistenceUnits - Add a test that demonstrates the problem

---
+++ tests/org.hibernate.eclipse.console.test/src/org/hibernate/eclipse/console/utils/ProjectUtilsTest.java
@@ -0,0 +1,42 @@
+package org.hibernate.eclipse.console.utils;
+
+import java.io.ByteArrayInputStream;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ProjectUtilsTest {
+	
+	@Test
+	public void testAvailablePersistenceUnits() throws Exception {
+		// First test the main case 
+		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject("test");
+		project.create(null);
+		project.open(null);
+		IFolder folder = project.getFolder("META-INF");
+		folder.create(true, true, null);
+		IFile file = folder.getFile("persistence.xml");
+		String contents = "<persistence><persistence-unit name='foo'/></persistence>";
+		file.create(new ByteArrayInputStream(contents.getBytes()), false, null);
+		IProjectDescription description = project.getDescription();
+		description.setNatureIds(new String[] { JavaCore.NATURE_ID });
+		project.setDescription(description, null);
+		IJavaProject javaProject = JavaCore.create(project);
+		javaProject.setOutputLocation(project.getFullPath(), null);
+		Assert.assertArrayEquals(
+				new String[] { "foo" },
+				ProjectUtils.availablePersistenceUnits(javaProject));
+		// Second test the edge case
+		Assert.assertArrayEquals(
+				new String[0], 
+				ProjectUtils.availablePersistenceUnits(null));
+	}
+
+}
