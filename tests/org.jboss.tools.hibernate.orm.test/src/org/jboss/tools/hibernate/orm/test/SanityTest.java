be05e2cd0a5be306a38330e20a3c182d5ac066a4
Koen Aers <koen.aers@gmail.com>
2017-11-08 06:55:42 +0100

    JBIDE-25386: Consolidate tests from 'org.hibernate.eclipse.console.test' into new test plugin 'org.jboss.tools.hibernate.orm.test' - test plugin creation
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ tests/org.jboss.tools.hibernate.orm.test/src/org/jboss/tools/hibernate/orm/test/SanityTest.java
@@ -0,0 +1,13 @@
+package org.jboss.tools.hibernate.orm.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SanityTest {
+	
+	@Test
+	public void testSanity() {
+		Assert.assertTrue(true);
+	}
+
+}
