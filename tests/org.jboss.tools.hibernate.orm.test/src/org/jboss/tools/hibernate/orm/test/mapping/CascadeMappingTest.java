67093af539b80344b34c5be30aa46e1f94387b20
Koen Aers <koen.aers@gmail.com>
2017-12-11 18:15:32 +0100

    JBIDE-25386: Consolidate tests from 'org.hibernate.eclipse.console.test' into new test plugin 'org.jboss.tools.hibernate.orm.test' - Move test package 'mapping.cascade' to be tested in org.jboss.tools.hibernate.orm.test.mapping.CascadeMappingTest
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ tests/org.jboss.tools.hibernate.orm.test/src/org/jboss/tools/hibernate/orm/test/mapping/CascadeMappingTest.java
@@ -0,0 +1,69 @@
+package org.jboss.tools.hibernate.orm.test.mapping;
+
+import org.jboss.tools.hibernate.orm.test.utils.MappingTestHelper;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+public class CascadeMappingTest {
+	
+	private static final String CFG_XML = 
+		"<!DOCTYPE hibernate-configuration PUBLIC                                                                     \n" +
+		"	'-//Hibernate/Hibernate Configuration DTD 3.0//EN'                                                        \n" +
+		"	'http://hibernate.sourceforge.net/hibernate-configuration-3.0.dtd'>                                       \n" +
+		"                                                                                                             \n" +
+		"<hibernate-configuration>                                                                                    \n" +
+		"	<session-factory>                                                                                         \n" +
+		"       <property name='dialect'>org.hibernate.dialect.HSQLDialect</property>                                \n" +
+		"       <mapping resource='/mapping/cascade/Job.hbm.xml' /> \n" +
+		"       <mapping resource='/mapping/cascade/JobBatch.hbm.xml' /> \n" +
+		"	</session-factory>                                                                                        \n" +
+		"</hibernate-configuration>                                                                                    " ;
+	
+	private static final String packageName = "mapping.cascade";
+	
+	@Rule
+	public TestName testName = new TestName();
+	
+	private MappingTestHelper mappingTestHelper = null;
+	
+	@Before
+	public void setUp() throws Exception {
+		mappingTestHelper = new MappingTestHelper(CFG_XML, packageName, testName);
+		mappingTestHelper.setUp();
+	}
+	
+	@After
+	public void tearDown() {
+		mappingTestHelper.tearDown();
+		mappingTestHelper = null;
+	}
+	
+	@Test
+	public void testCheckConsoleConfiguration() {
+		mappingTestHelper.testCheckConsoleConfiguration();
+	}
+
+	@Test
+	public void testOpenMappingDiagram() {
+		mappingTestHelper.testOpenMappingDiagram();
+	}
+	
+	@Test
+	public void testOpenMappingFileTest() {
+		mappingTestHelper.testOpenMappingFileTest();
+	}
+
+	@Test
+	public void testOpenSourceFileTest() {
+		mappingTestHelper.testOpenSourceFileTest();
+	}
+
+	@Test
+	public void testHbmExportExceptionTest() throws Exception {
+		mappingTestHelper.testHbmExportExceptionTest();
+	}
+	
+}
