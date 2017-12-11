10a4f0806178aef83327ce6ad78453a78ad1b7fd
Koen Aers <koen.aers@gmail.com>
2017-12-11 18:23:18 +0100

    JBIDE-25386: Consolidate tests from 'org.hibernate.eclipse.console.test' into new test plugin 'org.jboss.tools.hibernate.orm.test' - Rename org.jboss.tools.hibernate.orm.test.mapping.PropertyRefMappingTest to org.hboss.tools.hibernate.orm.test.mapping.AbstractEmbeddedComponentPropertyRefMappingTest
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- tests/org.jboss.tools.hibernate.orm.test/src/org/jboss/tools/hibernate/orm/test/mapping/AbstractEmbeddedComponentPropertyRefMappingTest.java
+++ tests/org.jboss.tools.hibernate.orm.test/src/org/jboss/tools/hibernate/orm/test/mapping/AbstractEmbeddedComponentPropertyRefMappingTest.java
@@ -7,7 +7,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
 
-public class PropertyRefMappingTest {
+public class AbstractEmbeddedComponentPropertyRefMappingTest {
 	
 	private static final String CFG_XML = 
 		"<!DOCTYPE hibernate-configuration PUBLIC                                                                     \n" +
