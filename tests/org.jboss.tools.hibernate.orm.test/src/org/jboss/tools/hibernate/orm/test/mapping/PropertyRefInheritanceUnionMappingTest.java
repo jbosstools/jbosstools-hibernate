6f20f5668b432678ccf2d940ea78e09e685a998e
Koen Aers <koen.aers@gmail.com>
2018-01-26 17:32:24 +0100

    JBIDE-25386: Consolidate tests from 'org.hibernate.eclipse.console.test' into new test plugin 'org.jboss.tools.hibernate.orm.test' - Move test package 'mapping.propertyref.inheritence.union' to be tested in org.jboss.tools.hibernate.orm.test.mapping.PropertyRefInheritenceUnionMappingTest
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ tests/org.jboss.tools.hibernate.orm.test/src/org/jboss/tools/hibernate/orm/test/mapping/PropertyRefInheritanceUnionMappingTest.java
@@ -0,0 +1,68 @@
+package org.jboss.tools.hibernate.orm.test.mapping;
+
+import org.jboss.tools.hibernate.orm.test.utils.MappingTestHelper;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+public class PropertyRefInheritanceUnionMappingTest {
+	
+	private static final String CFG_XML = 
+		"<!DOCTYPE hibernate-configuration PUBLIC                                                                     \n" +
+		"	'-//Hibernate/Hibernate Configuration DTD 3.0//EN'                                                        \n" +
+		"	'http://hibernate.sourceforge.net/hibernate-configuration-3.0.dtd'>                                       \n" +
+		"                                                                                                             \n" +
+		"<hibernate-configuration>                                                                                    \n" +
+		"	<session-factory>                                                                                         \n" +
+		"       <property name='dialect'>org.hibernate.dialect.HSQLDialect</property>                                \n" +
+		"       <mapping resource='/mapping/propertyref/inheritence/union/Person.hbm.xml' /> \n" +
+		"	</session-factory>                                                                                        \n" +
+		"</hibernate-configuration>                                                                                    " ;
+	
+	private static final String packageName = "mapping.propertyref.inheritence.union";
+	
+	@ClassRule
+	public static TestName testName = new TestName();
+	
+	private static MappingTestHelper mappingTestHelper = null;
+	
+	@BeforeClass
+	public static void setUp() throws Exception {
+		mappingTestHelper = new MappingTestHelper(CFG_XML, packageName, testName);
+		mappingTestHelper.setUp();
+	}
+	
+	@AfterClass
+	public static void tearDown() {
+		mappingTestHelper.tearDown();
+		mappingTestHelper = null;
+	}
+	
+	@Test
+	public void testCheckConsoleConfiguration() {
+		mappingTestHelper.testCheckConsoleConfiguration();
+	}
+
+	@Test
+	public void testOpenMappingDiagram() {
+		mappingTestHelper.testOpenMappingDiagram();
+	}
+	
+	@Test
+	public void testOpenMappingFileTest() {
+		mappingTestHelper.testOpenMappingFileTest();
+	}
+
+	@Test
+	public void testOpenSourceFileTest() {
+		mappingTestHelper.testOpenSourceFileTest();
+	}
+
+	@Test
+	public void testHbmExportExceptionTest() throws Exception {
+		mappingTestHelper.testHbmExportExceptionTest();
+	}
+	
+}
