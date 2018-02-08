17df2a276e9e429e0465318d7da9e9bd275b2395
Koen Aers <koen.aers@gmail.com>
2018-02-08 12:15:28 +0100

    JBIDE-25690: Create a Runtime Plugin for Hibernate 5.3 - Creation of test fragment
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ tests/org.jboss.tools.hibernate.runtime.v_5_3.test/src/org/jboss/tools/hibernate/runtime/v_5_3/VersionTest.java
@@ -0,0 +1,13 @@
+package org.jboss.tools.hibernate.runtime.v_5_3;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class VersionTest {
+	
+	@Test
+	public void testCoreVersion() {
+		Assert.assertEquals("5.3.0.Beta2", "5.3.0.Beta2");
+	}
+
+}
