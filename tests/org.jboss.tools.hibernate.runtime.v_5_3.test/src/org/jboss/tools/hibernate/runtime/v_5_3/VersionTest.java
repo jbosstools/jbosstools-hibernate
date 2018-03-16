08f7e7e10e51b42694af909c66d440bf5b1bf071
Koen Aers <koen.aers@gmail.com>
2018-03-16 09:01:50 +0100

    JBIDE-25690: Create a Runtime Plugin for Hibernate 5.3 - Properly implement version test
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- tests/org.jboss.tools.hibernate.runtime.v_5_3.test/src/org/jboss/tools/hibernate/runtime/v_5_3/VersionTest.java
+++ tests/org.jboss.tools.hibernate.runtime.v_5_3.test/src/org/jboss/tools/hibernate/runtime/v_5_3/VersionTest.java
@@ -6,8 +6,13 @@ import org.junit.Test;
 public class VersionTest {
 	
 	@Test
+	public void testToolsVersion() {
+		Assert.assertEquals("5.3.0.Beta1", org.hibernate.tool.Version.VERSION);
+	}
+
+	@Test
 	public void testCoreVersion() {
-		Assert.assertEquals("5.3.0.Beta2", "5.3.0.Beta2");
+		Assert.assertEquals("5.3.0.Beta2", org.hibernate.Version.getVersionString());
 	}
 
 }
