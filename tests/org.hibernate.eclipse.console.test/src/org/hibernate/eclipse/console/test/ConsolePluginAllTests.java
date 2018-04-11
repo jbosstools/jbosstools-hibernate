6a6f06edfa79ab7c346b5bf5a2aaefe38c7a98bd
Koen Aers <koen.aers@gmail.com>
2018-04-11 10:38:48 +0200

    JBIDE-25386: Consolidate tests from 'org.hibernate.eclipse.console.test' into new test plugin 'org.jboss.tools.hibernate.orm.test' - Remove 'org.hibernate.eclipse.console.test.mappingproject.MappingTestsCore' and other unnecessary files
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- tests/org.hibernate.eclipse.console.test/src/org/hibernate/eclipse/console/test/ConsolePluginAllTests.java
+++ tests/org.hibernate.eclipse.console.test/src/org/hibernate/eclipse/console/test/ConsolePluginAllTests.java
@@ -3,7 +3,6 @@ package org.hibernate.eclipse.console.test;
 import java.io.IOException;
 
 import org.hibernate.eclipse.console.test.mappingproject.MappingTestsAnnotations;
-import org.hibernate.eclipse.console.test.mappingproject.MappingTestsCore;
 import org.hibernate.eclipse.console.test.mappingproject.MappingTestsJpa;
 
 import junit.framework.Test;
@@ -14,8 +13,6 @@ public class ConsolePluginAllTests {
 	public static Test suite() throws IOException {
 		TestSuite suite = new TestSuite(
 				ConsoleTestMessages.ConsolePluginAllTests_test_for );
-
-		suite.addTestSuite(MappingTestsCore.class);
 		suite.addTestSuite(MappingTestsJpa.class);
 		suite.addTestSuite(MappingTestsAnnotations.class);
 		
