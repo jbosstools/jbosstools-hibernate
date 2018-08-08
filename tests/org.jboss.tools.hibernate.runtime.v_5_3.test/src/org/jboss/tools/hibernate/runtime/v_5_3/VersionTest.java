18d0c4318584d91b667e433ae4cef626347007a5
Koen Aers <koen.aers@gmail.com>
2018-08-08 14:53:11 +0200

    JBIDE-26295: Update hibernate tools dependency of org.jboss.tools.hibernate.runtime.v_5_3 to version 5.3.4.Final
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- tests/org.jboss.tools.hibernate.runtime.v_5_3.test/src/org/jboss/tools/hibernate/runtime/v_5_3/VersionTest.java
+++ tests/org.jboss.tools.hibernate.runtime.v_5_3.test/src/org/jboss/tools/hibernate/runtime/v_5_3/VersionTest.java
@@ -7,12 +7,12 @@ public class VersionTest {
 	
 	@Test
 	public void testToolsVersion() {
-		Assert.assertEquals("5.3.3.Final", org.hibernate.tool.Version.VERSION);
+		Assert.assertEquals("5.3.4.Final", org.hibernate.tool.Version.VERSION);
 	}
 
 	@Test
 	public void testCoreVersion() {
-		Assert.assertEquals("5.3.3.Final", org.hibernate.Version.getVersionString());
+		Assert.assertEquals("5.3.4.Final", org.hibernate.Version.getVersionString());
 	}
 
 }
