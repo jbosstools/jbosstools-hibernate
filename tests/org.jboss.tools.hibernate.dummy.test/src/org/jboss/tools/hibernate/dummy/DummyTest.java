8d367989684447fc84186cc8b96491e934f718cb
Koen Aers <koen.aers@gmail.com>
2019-02-20 09:34:28 +0100

    JBIDE-26543: Reorganize plugins and test plugins in a more coherent module hierarchy - Add 'dummy' test to 'tests' to make sure at least one test remains after moving all the tests (otherwise the build breaks)
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ tests/org.jboss.tools.hibernate.dummy.test/src/org/jboss/tools/hibernate/dummy/DummyTest.java
@@ -0,0 +1,13 @@
+package org.jboss.tools.hibernate.dummy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DummyTest {
+
+	@Test
+	public void testDummy() {
+		Assert.assertNull(null);
+	}
+	
+}
