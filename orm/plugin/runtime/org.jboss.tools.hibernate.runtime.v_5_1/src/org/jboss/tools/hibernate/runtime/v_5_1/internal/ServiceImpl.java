ce69a90b452b498b0e7d5a2cb3a0dd75a22870cf
Koen Aers <koen.aers@gmail.com>
2020-07-02 14:17:07 +0200

    JBIDE-26543: Reorganize plugins and test plugins in a more coherent module hierarchy - Make remaining tests in org.hibernate.eclipse.jdt.ui.test run with 5.1 runtime
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_5_1/src/org/jboss/tools/hibernate/runtime/v_5_1/internal/ServiceImpl.java
+++ orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_5_1/src/org/jboss/tools/hibernate/runtime/v_5_1/internal/ServiceImpl.java
@@ -84,6 +84,7 @@ import org.jboss.tools.hibernate.runtime.spi.ITable;
 import org.jboss.tools.hibernate.runtime.spi.ITableFilter;
 import org.jboss.tools.hibernate.runtime.spi.ITypeFactory;
 import org.jboss.tools.hibernate.runtime.spi.IValue;
+import org.jboss.tools.hibernate.runtime.v_5_1.internal.util.DummyMetadataImplementor;
 import org.xml.sax.EntityResolver;
 
 public class ServiceImpl extends AbstractService {
@@ -306,7 +307,7 @@ public class ServiceImpl extends AbstractService {
 
 	@Override
 	public IValue newSimpleValue() {
-		return facadeFactory.createValue(new SimpleValue(null));
+		return facadeFactory.createValue(new SimpleValue(DummyMetadataImplementor.INSTANCE));
 	}
 
 	@Override
