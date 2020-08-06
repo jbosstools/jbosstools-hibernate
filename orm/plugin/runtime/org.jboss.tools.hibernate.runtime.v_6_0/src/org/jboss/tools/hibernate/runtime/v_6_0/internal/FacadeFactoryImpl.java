ba889dae42c51732c9028db6238d81ffb78ae23b
Koen Aers <koen.aers@gmail.com>
2020-08-06 07:44:30 +0200

    JBIDE-27439: Set up a runtime plugin for the Hibernate 6.0.x releases - FacadeFactoryÌmpl extends AbstractFacadeFactory and implements IFacadeFactory
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_6_0/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/FacadeFactoryImpl.java
+++ orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_6_0/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/FacadeFactoryImpl.java
@@ -1,5 +1,292 @@
 package org.jboss.tools.hibernate.runtime.v_6_0.internal;
 
-public class FacadeFactoryImpl {
+import org.jboss.tools.hibernate.runtime.common.AbstractFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IArtifactCollector;
+import org.jboss.tools.hibernate.runtime.spi.ICfg2HbmTool;
+import org.jboss.tools.hibernate.runtime.spi.IClassMetadata;
+import org.jboss.tools.hibernate.runtime.spi.ICollectionMetadata;
+import org.jboss.tools.hibernate.runtime.spi.IColumn;
+import org.jboss.tools.hibernate.runtime.spi.IConfiguration;
+import org.jboss.tools.hibernate.runtime.spi.ICriteria;
+import org.jboss.tools.hibernate.runtime.spi.IDatabaseCollector;
+import org.jboss.tools.hibernate.runtime.spi.IEntityMetamodel;
+import org.jboss.tools.hibernate.runtime.spi.IEnvironment;
+import org.jboss.tools.hibernate.runtime.spi.IExporter;
+import org.jboss.tools.hibernate.runtime.spi.IForeignKey;
+import org.jboss.tools.hibernate.runtime.spi.IGenericExporter;
+import org.jboss.tools.hibernate.runtime.spi.IHQLCodeAssist;
+import org.jboss.tools.hibernate.runtime.spi.IHQLCompletionProposal;
+import org.jboss.tools.hibernate.runtime.spi.IHQLQueryPlan;
+import org.jboss.tools.hibernate.runtime.spi.IHbm2DDLExporter;
+import org.jboss.tools.hibernate.runtime.spi.IHibernateMappingExporter;
+import org.jboss.tools.hibernate.runtime.spi.IJDBCReader;
+import org.jboss.tools.hibernate.runtime.spi.IJoin;
+import org.jboss.tools.hibernate.runtime.spi.INamingStrategy;
+import org.jboss.tools.hibernate.runtime.spi.IOverrideRepository;
+import org.jboss.tools.hibernate.runtime.spi.IPOJOClass;
+import org.jboss.tools.hibernate.runtime.spi.IPersistentClass;
+import org.jboss.tools.hibernate.runtime.spi.IPrimaryKey;
+import org.jboss.tools.hibernate.runtime.spi.IProperty;
+import org.jboss.tools.hibernate.runtime.spi.IQuery;
+import org.jboss.tools.hibernate.runtime.spi.IQueryExporter;
+import org.jboss.tools.hibernate.runtime.spi.IQueryTranslator;
+import org.jboss.tools.hibernate.runtime.spi.IReverseEngineeringSettings;
+import org.jboss.tools.hibernate.runtime.spi.IReverseEngineeringStrategy;
+import org.jboss.tools.hibernate.runtime.spi.ISchemaExport;
+import org.jboss.tools.hibernate.runtime.spi.ISession;
+import org.jboss.tools.hibernate.runtime.spi.ISessionFactory;
+import org.jboss.tools.hibernate.runtime.spi.ITable;
+import org.jboss.tools.hibernate.runtime.spi.ITableFilter;
+import org.jboss.tools.hibernate.runtime.spi.IType;
+import org.jboss.tools.hibernate.runtime.spi.ITypeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IValue;
+
+public class FacadeFactoryImpl  extends AbstractFacadeFactory {
+
+	@Override
+	public ClassLoader getClassLoader() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IArtifactCollector createArtifactCollector(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ICfg2HbmTool createCfg2HbmTool(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public INamingStrategy createNamingStrategy(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IReverseEngineeringSettings createReverseEngineeringSettings(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IReverseEngineeringStrategy createReverseEngineeringStrategy(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IOverrideRepository createOverrideRepository(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ISchemaExport createSchemaExport(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IGenericExporter createGenericExporter(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IHbm2DDLExporter createHbm2DDLExporter(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IQueryExporter createQueryExporter(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ITableFilter createTableFilter(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IExporter createExporter(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IClassMetadata createClassMetadata(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ICollectionMetadata createCollectionMetadata(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IColumn createColumn(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IConfiguration createConfiguration(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ICriteria createCriteria(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IDatabaseCollector createDatabaseCollector(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IEntityMetamodel createEntityMetamodel(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IEnvironment createEnvironment() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IForeignKey createForeignKey(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IHibernateMappingExporter createHibernateMappingExporter(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IHQLCodeAssist createHQLCodeAssist(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IHQLCompletionProposal createHQLCompletionProposal(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IHQLQueryPlan createHQLQueryPlan(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IJDBCReader createJDBCReader(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IJoin createJoin(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IPersistentClass createPersistentClass(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IPOJOClass createPOJOClass(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IPrimaryKey createPrimaryKey(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IProperty createProperty(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IQuery createQuery(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IQueryTranslator createQueryTranslator(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ISessionFactory createSessionFactory(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ISession createSession(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IPersistentClass createSpecialRootClass(IProperty property) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ITable createTable(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ITypeFactory createTypeFactory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IType createType(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IValue createValue(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
