5cd55b20d3670ebff586b353fa49333530401fab
Koen Aers <koen.aers@gmail.com>
2020-08-26 18:16:57 +0200

    JBIDE-27439: Set up a runtime plugin for the Hibernate 6.0.x releases - Initial implementation of org.jboss.tools.hibernate.runtime.v_6_0.internal.ConfigurationFacadeImpl.java
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_6_0/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/ConfigurationFacadeImpl.java
@@ -0,0 +1,12 @@
+package org.jboss.tools.hibernate.runtime.v_6_0.internal;
+
+import org.jboss.tools.hibernate.runtime.common.AbstractConfigurationFacade;
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+
+public class ConfigurationFacadeImpl extends AbstractConfigurationFacade {
+
+	public ConfigurationFacadeImpl(IFacadeFactory facadeFactory, Object target) {
+		super(facadeFactory, target);
+	}
+
+}
---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/ConfigurationFacadeTest.java
@@ -0,0 +1,30 @@
+package org.jboss.tools.hibernate.runtime.v_6_0.internal;
+
+import org.hibernate.cfg.Configuration;
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IConfiguration;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ConfigurationFacadeTest {
+
+	private static final IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
+
+	private IConfiguration configurationFacade = null;
+	private Configuration configuration = null;
+
+	@Before
+	public void setUp() {
+		configuration = new Configuration();
+		configurationFacade = new ConfigurationFacadeImpl(FACADE_FACTORY, configuration);
+	}	
+	
+	@Test
+	public void testGetProperty() {
+		Assert.assertNull(configurationFacade.getProperty("foo"));
+		configuration.setProperty("foo", "bar");
+		Assert.assertEquals("bar", configurationFacade.getProperty("foo"));
+	}
+
+}
