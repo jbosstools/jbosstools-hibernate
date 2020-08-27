a51d144e467478e3ac8a82d2886508999dff7075
Koen Aers <koen.aers@gmail.com>
2020-08-27 18:49:01 +0200

    JBIDE-27439: Set up a runtime plugin for the Hibernate 6.0.x releases - Add 'ConfigurationFacadeTest#testAddFile()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/ConfigurationFacadeTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/ConfigurationFacadeTest.java
@@ -1,16 +1,35 @@
 package org.jboss.tools.hibernate.runtime.v_6_0.internal;
 
+import java.io.File;
+import java.io.PrintWriter;
 import java.util.Properties;
 
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.jaxb.spi.Binding;
 import org.hibernate.cfg.Configuration;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
 import org.jboss.tools.hibernate.runtime.spi.IConfiguration;
+import org.jboss.tools.hibernate.runtime.v_6_0.internal.util.MetadataHelper;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class ConfigurationFacadeTest {
 
+	private static final String TEST_HBM_XML_STRING =
+			"<!DOCTYPE hibernate-mapping PUBLIC" +
+			"		'-//Hibernate/Hibernate Mapping DTD 3.0//EN'" +
+			"		'http://www.hibernate.org/dtd/hibernate-mapping-3.0.dtd'>" +
+			"<hibernate-mapping package='org.jboss.tools.hibernate.runtime.v_5_4.internal'>" +
+			"  <class name='ConfigurationFacadeTest$Foo'>" + 
+			"    <id name='id'/>" +
+			"  </class>" +
+			"</hibernate-mapping>";
+	
+	static class Foo {
+		public String id;
+	}
+	
 	private static final IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
 
 	private IConfiguration configurationFacade = null;
@@ -46,4 +65,20 @@ public class ConfigurationFacadeTest {
 		Assert.assertSame(testProperties, configuration.getProperties());
 	}
 	
+	@Test
+	public void testAddFile() throws Exception {
+		File testFile = File.createTempFile("test", "hbm.xml");
+		PrintWriter printWriter = new PrintWriter(testFile);
+		printWriter.write(TEST_HBM_XML_STRING);
+		printWriter.close();
+		MetadataSources metadataSources = MetadataHelper.getMetadataSources(configuration);
+		Assert.assertTrue(metadataSources.getXmlBindings().isEmpty());
+		Assert.assertSame(
+				configurationFacade,
+				configurationFacade.addFile(testFile));
+		Assert.assertFalse(metadataSources.getXmlBindings().isEmpty());
+		Binding<?> binding = metadataSources.getXmlBindings().iterator().next();
+		Assert.assertEquals(testFile.getAbsolutePath(), binding.getOrigin().getName());
+		Assert.assertTrue(testFile.delete());
+	}
 }
