47ecb63da5bf0e3aff4adaf3a9e9cfdfb5eeb730
Koen Aers <koen.aers@gmail.com>
2020-09-28 17:22:40 +0200

    JBIDE-27439: Set up a runtime plugin for the Hibernate 6.0.x releases - Create initial implementation of 'Hbm2DDLFacadeImpl', add 'DdlExporterFacadeTest#testSetExport()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/DdlExporterFacadeTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/DdlExporterFacadeTest.java
@@ -1,9 +1,12 @@
 package org.jboss.tools.hibernate.runtime.v_6_0.internal;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
+import org.hibernate.tool.api.export.ExporterConstants;
 import org.hibernate.tool.internal.export.ddl.DdlExporter;
-import org.jboss.tools.hibernate.runtime.common.AbstractHbm2DDLExporterFacade;
 import org.jboss.tools.hibernate.runtime.spi.IHbm2DDLExporter;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,12 +21,21 @@ public class DdlExporterFacadeTest {
 	@Before
 	public void before() {
 		ddlExporterTarget = new DdlExporter();
-		ddlExporterFacade = new AbstractHbm2DDLExporterFacade(FACADE_FACTORY, ddlExporterTarget) {};
+		ddlExporterFacade = new Hbm2DDLExporterFacadeImpl(FACADE_FACTORY, ddlExporterTarget) {};
 	}
 	
 	@Test 
 	public void testGetProperties() {
 		assertNotNull(ddlExporterFacade.getProperties());
+	}
+	
+	@Test
+	public void testSetExport() {
+		assertNull(ddlExporterFacade.getProperties().get(ExporterConstants.EXPORT_TO_DATABASE));
+		ddlExporterFacade.setExport(false);
+		assertFalse((Boolean)ddlExporterFacade.getProperties().get(ExporterConstants.EXPORT_TO_DATABASE));
+		ddlExporterFacade.setExport(true);
+		assertTrue((Boolean)ddlExporterFacade.getProperties().get(ExporterConstants.EXPORT_TO_DATABASE));
 	}
 
 }
