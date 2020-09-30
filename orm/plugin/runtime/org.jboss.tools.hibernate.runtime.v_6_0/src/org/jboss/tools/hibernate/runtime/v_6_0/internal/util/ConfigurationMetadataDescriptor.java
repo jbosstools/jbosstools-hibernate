66700bcb1dda3eefc1c586e558b95439ffd6780d
Koen Aers <koen.aers@gmail.com>
2020-09-30 13:51:42 +0200

    JBIDE-27439: Set up a runtime plugin for the Hibernate 6.0.x releases - Implementation of ConfigurationMetadataDescriptor#createMetadata()
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_6_0/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/util/ConfigurationMetadataDescriptor.java
+++ orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_6_0/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/util/ConfigurationMetadataDescriptor.java
@@ -1,9 +1,15 @@
 package org.jboss.tools.hibernate.runtime.v_6_0.internal.util;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.Properties;
 
 import org.hibernate.boot.Metadata;
+import org.hibernate.boot.internal.MetadataImpl;
+import org.hibernate.mapping.PersistentClass;
 import org.hibernate.tool.api.metadata.MetadataDescriptor;
+import org.jboss.tools.hibernate.runtime.common.IFacade;
+import org.jboss.tools.hibernate.runtime.spi.IPersistentClass;
 import org.jboss.tools.hibernate.runtime.v_6_0.internal.ConfigurationFacadeImpl;
 
 public class ConfigurationMetadataDescriptor implements MetadataDescriptor {
@@ -16,8 +22,11 @@ public class ConfigurationMetadataDescriptor implements MetadataDescriptor {
 
 	@Override
 	public Metadata createMetadata() {
-		// TODO Auto-generated method stub
-		return null;
+		Metadata result = configurationFacade.getMetadata();
+		if (result != null) {
+			result = patch(result);
+		}
+		return result;
 	}
 
 	@Override
@@ -25,4 +34,29 @@ public class ConfigurationMetadataDescriptor implements MetadataDescriptor {
 		return configurationFacade.getProperties();
 	}
 
+	private Metadata patch(Metadata metadata) {
+		try {
+			if (metadata instanceof MetadataImpl ) {
+				MetadataImpl metadataImpl = (MetadataImpl)metadata;
+				Field entityBindingMapField = metadataImpl.getClass().getDeclaredField("entityBindingMap");
+				if (entityBindingMapField != null) {
+					entityBindingMapField.setAccessible(true);
+					Object object = entityBindingMapField.get(metadataImpl);
+					if (object instanceof HashMap<?, ?>) {
+						@SuppressWarnings("unchecked")
+						HashMap<String, PersistentClass> map = (HashMap<String, PersistentClass>)object;
+						for (IPersistentClass ipc : configurationFacade.getAddedClasses()) {
+							PersistentClass pc = (PersistentClass)((IFacade)ipc).getTarget();
+							map.put(pc.getEntityName(), pc);
+						}
+					}
+				}
+			}
+			return metadata;
+		}
+		catch (Throwable t) {
+			throw new RuntimeException("Problem while creating metadata", t);
+		}
+	} 
+	
 }
--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/util/ConfigurationMetadataDescriptorTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/util/ConfigurationMetadataDescriptorTest.java
@@ -3,9 +3,16 @@ package org.jboss.tools.hibernate.runtime.v_6_0.internal.util;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertSame;
 
+import java.io.ByteArrayInputStream;
 import java.util.Properties;
 
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.dialect.Dialect;
+import org.hibernate.mapping.PersistentClass;
+import org.hibernate.mapping.RootClass;
+import org.jboss.tools.hibernate.runtime.spi.IPersistentClass;
 import org.jboss.tools.hibernate.runtime.v_6_0.internal.ConfigurationFacadeImpl;
 import org.jboss.tools.hibernate.runtime.v_6_0.internal.FacadeFactoryImpl;
 import org.junit.Before;
@@ -13,6 +20,21 @@ import org.junit.Test;
 
 public class ConfigurationMetadataDescriptorTest {
 	
+	private static final String TEST_HBM_XML_STRING =
+			"<hibernate-mapping package='org.jboss.tools.hibernate.runtime.v_6_0.internal.util'>" +
+			"  <class name='ConfigurationMetadataDescriptorTest$Foo'>" + 
+			"    <id name='id'/>" +
+			"  </class>" +
+			"</hibernate-mapping>";
+	
+	static class Foo {
+		public String id;
+	}
+	
+	public static class TestDialect extends Dialect {
+		@Override public int getVersion() { return 0; }
+	}
+		
 	private static final FacadeFactoryImpl FACADE_FACTORY = new FacadeFactoryImpl();
 	
 	private ConfigurationMetadataDescriptor configurationMetadataDescriptor = null;
@@ -38,6 +60,24 @@ public class ConfigurationMetadataDescriptorTest {
 		Properties properties = new Properties();
 		configurationTarget.setProperties(properties);
 		assertSame(properties, configurationMetadataDescriptor.getProperties());
+	}
+	
+	@Test
+	public void testCreateMetadata() {
+		MetadataSources metadataSources = new MetadataSources();
+		metadataSources.addInputStream(new ByteArrayInputStream(TEST_HBM_XML_STRING.getBytes()));
+		Configuration configuration = new Configuration(metadataSources);
+		configuration.setProperty("hibernate.dialect", TestDialect.class.getName());
+		configurationFacade = new ConfigurationFacadeImpl(FACADE_FACTORY, configuration);
+		PersistentClass persistentClass = new RootClass(null);
+		persistentClass.setEntityName("Bar");
+		IPersistentClass persistentClassFacade = 
+				FACADE_FACTORY.createPersistentClass(persistentClass);	
+		configurationFacade.addClass(persistentClassFacade);
+		configurationMetadataDescriptor = new ConfigurationMetadataDescriptor(configurationFacade);
+		Metadata metadata = configurationMetadataDescriptor.createMetadata();
+		assertNotNull(metadata.getEntityBinding(Foo.class.getName()));
+		assertNotNull(metadata.getEntityBinding("Bar"));
 	}
 	
 }
