b65b6ec1f3320a7827a5f1859978aeb3673bb7ab
Koen Aers <koen.aers@gmail.com>
2020-10-23 12:27:57 +0200

    JBIDE-27439: Set up a runtime plugin for the Hibernate 6.0.x releases - Initial implementation of class 'SessionFacadeImpl' and of test class 'SessionFacadeTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/SessionFacadeTest.java
@@ -0,0 +1,69 @@
+package org.jboss.tools.hibernate.runtime.v_6_0.internal;
+
+import static org.junit.Assert.assertSame;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import org.hibernate.Session;
+import org.hibernate.dialect.Dialect;
+import org.hibernate.engine.spi.SessionDelegatorBaseImpl;
+import org.hibernate.engine.spi.SessionImplementor;
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SessionFacadeTest {
+	
+	private static IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
+	
+	private static final String ENTITY_NAME = "entity_name";
+	
+	private Session sessionTarget = null;
+	private SessionFacadeImpl sessionFacade = null;
+	
+	@Before
+	public void before() {
+		sessionTarget = new TestSession();
+		sessionFacade = new SessionFacadeImpl(FACADE_FACTORY, sessionTarget);
+	}
+	
+	@Test
+	public void testGetEntityName() {
+		assertSame(ENTITY_NAME, sessionFacade.getEntityName(new Object()));
+	}
+	
+	private static class TestSession extends SessionDelegatorBaseImpl {
+
+		private static final long serialVersionUID = 1L;
+		
+		private static SessionImplementor createDelegate() {
+			return (SessionImplementor)Proxy.newProxyInstance(
+					TestSession.class.getClassLoader(),
+					new Class[] { SessionImplementor.class }, 
+					new InvocationHandler() {						
+						@Override
+						public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+							return null;
+						}
+					});
+		}
+		
+		public TestSession() {
+			super(createDelegate());
+		}
+		
+		@Override
+		public String getEntityName(Object o) {
+			return ENTITY_NAME;
+		}
+
+	}
+	
+	public static class TestDialect extends Dialect {
+		@Override public int getVersion() { return 0; }	
+	}
+	
+
+}
