c3bde25d7716bb8d2009b0f5b321cd297979d2a4
Koen Aers <koen.aers@gmail.com>
2020-10-26 09:32:30 +0100

    Set up a runtime plugin for the Hibernate 6.0.x releases - Add 'SessionFactoryFacadeImpl#testGetAllClassMetadata()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/SessionFactoryFacadeTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/SessionFactoryFacadeTest.java
@@ -1,26 +1,34 @@
 package org.jboss.tools.hibernate.runtime.v_6_0.internal;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.hibernate.SessionFactory;
 import org.hibernate.engine.spi.SessionFactoryDelegatingImpl;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.metadata.ClassMetadata;
+import org.jboss.tools.hibernate.runtime.common.AbstractSessionFactoryFacade;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IClassMetadata;
 import org.junit.Before;
 import org.junit.Test;
 
 public class SessionFactoryFacadeTest {
 	
-	private static IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
+	private static final IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
 	
-	private SessionFactory sessionFactoryTarget = null;
+	private TestSessionFactory sessionFactoryTarget = null;
 	private SessionFactoryFacadeImpl sessionFactoryFacade = null;
-	
+		
 	@Before
 	public void before() {
 		sessionFactoryTarget = new TestSessionFactory();
@@ -29,43 +37,85 @@ public class SessionFactoryFacadeTest {
 	
 	@Test
 	public void testClose() {
-		assertFalse(sessionFactoryTarget.isClosed());
+		assertFalse(sessionFactoryTarget.closed);
 		sessionFactoryFacade.close();
-		assertTrue(sessionFactoryTarget.isClosed());
+		assertTrue(sessionFactoryTarget.closed);
 	}
 	
-	private static class TestSessionFactory extends SessionFactoryDelegatingImpl {
+	private Map<String, ClassMetadata> allClassMetadataTargets = new HashMap<String, ClassMetadata>();
+	
+	@Test
+	public void testGetAllClassMetadata() throws Exception {
+		TestSessionFactory testSessionFactory = (TestSessionFactory)sessionFactoryTarget;
+		assertNotNull(testSessionFactory.fooClassMetadataTarget);
+		assertNotNull(testSessionFactory.barClassMetadataTarget);
+		assertNotNull(testSessionFactory.allClassMetadata);
+		ClassMetadata fooClassMetadata = testSessionFactory.allClassMetadata.get("foo");
+		assertNotNull(fooClassMetadata);
+		ClassMetadata barClassMetadata = testSessionFactory.allClassMetadata.get("bar");
+		assertNotNull(barClassMetadata);
+		Field field = AbstractSessionFactoryFacade.class.getDeclaredField("allClassMetadata");
+		field.setAccessible(true);
+		assertNull(field.get(sessionFactoryFacade));
+		Map<String, IClassMetadata> allClassMetadata = sessionFactoryFacade.getAllClassMetadata();
+		assertNotNull(field.get(sessionFactoryFacade));
+		assertEquals(2, allClassMetadata.size());
+		IClassMetadata fooFacade = allClassMetadata.get("foo");
+		assertNotNull(fooFacade);
+	}
+	
+	private class TestSessionFactory extends SessionFactoryDelegatingImpl {
 
 		private static final long serialVersionUID = 1L;
 		
-		private static SessionFactoryImplementor createDelegate() {
-			return (SessionFactoryImplementor)Proxy.newProxyInstance(
-					TestSessionFactory.class.getClassLoader(), 
-					new Class[] { SessionFactoryImplementor.class }, 
-					new InvocationHandler() {						
-						@Override
-						public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-							return null;
-						}
-					});
-		}
-		
 		private boolean closed = false;
+		private ClassMetadata fooClassMetadataTarget = null;
+		private ClassMetadata barClassMetadataTarget = null;
+		private Map<String, ClassMetadata> allClassMetadata = new HashMap<String, ClassMetadata>();
 		
 		public TestSessionFactory() {
 			super(createDelegate());
-		}
-		
-		@Override
-		public boolean isClosed() {
-			return closed;
+			allClassMetadata.put("foo", fooClassMetadataTarget = createClassMetadata("foo"));
+			allClassMetadata.put("bar", barClassMetadataTarget = createClassMetadata("bar"));
 		}
 		
 		@Override
 		public void close() {
 			closed = true;
 		}
+		
+		public Map<String, ClassMetadata> getAllClassMetadata() {
+			return allClassMetadata;
+		}
 
 	}
-
+	
+	private ClassMetadata createClassMetadata(final String entityName) {
+		ClassMetadata result = (ClassMetadata)Proxy.newProxyInstance(
+				SessionFactoryFacadeTest.class.getClassLoader(), 
+				new Class[] { ClassMetadata.class },  
+				new InvocationHandler() {
+					@Override
+					public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+						if (method.getName().equals("getEntityName")) {
+							return entityName;
+						}
+						return this;
+					}
+				});
+		return result;
+	}
+	
+	private SessionFactoryImplementor createDelegate() {
+		return (SessionFactoryImplementor)Proxy.newProxyInstance(
+				TestSessionFactory.class.getClassLoader(), 
+				new Class[] { SessionFactoryImplementor.class }, 
+				new InvocationHandler() {						
+					@Override
+					public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+						return null;
+					}
+				});
+	}
+	
 }
