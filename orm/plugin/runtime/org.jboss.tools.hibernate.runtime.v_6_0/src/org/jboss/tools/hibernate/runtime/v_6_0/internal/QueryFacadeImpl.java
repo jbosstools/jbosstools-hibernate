25c64e20091fb2112866c6dd44825b79958cf147
Koen Aers <koen.aers@gmail.com>
2020-10-28 12:31:30 +0100

    JBIDE-27439: Set up a runtime plugin for the Hibernate 6.0.x releases - Implement 'QueryFacadeTest#testSetParameter()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_6_0/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/QueryFacadeImpl.java
@@ -0,0 +1,42 @@
+package org.jboss.tools.hibernate.runtime.v_6_0.internal;
+
+import javax.persistence.TemporalType;
+
+import org.hibernate.metamodel.model.domain.AllowableParameterType;
+import org.hibernate.query.Query;
+import org.jboss.tools.hibernate.runtime.common.AbstractQueryFacade;
+import org.jboss.tools.hibernate.runtime.common.IFacade;
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IType;
+
+public class QueryFacadeImpl extends AbstractQueryFacade {
+
+	public QueryFacadeImpl(IFacadeFactory facadeFactory, Object target) {
+		super(facadeFactory, target);
+	}
+	
+	@Override
+	public void setParameter(int position, Object value, IType type) {
+		Object typeTarget = ((IFacade)type).getTarget();
+		if (typeTarget instanceof AllowableParameterType<?>) {
+			((Query<?>)getTarget()).setParameter(position, value, (AllowableParameterType<?>)typeTarget);
+		} else if (typeTarget instanceof TemporalType) {
+			((Query<?>)getTarget()).setParameter(position, value, (TemporalType)typeTarget); 
+		} else {
+			((Query<?>)getTarget()).setParameter(position, value);
+		}
+	}
+
+	@Override
+	public void setParameter(String name, Object value, IType type) {
+		Object typeTarget = ((IFacade)type).getTarget();
+		if (typeTarget instanceof AllowableParameterType<?>) {
+			((Query<?>)getTarget()).setParameter(name, value, (AllowableParameterType<?>)typeTarget);
+		} else if (typeTarget instanceof TemporalType) {
+			((Query<?>)getTarget()).setParameter(name, value, (TemporalType)typeTarget); 
+		} else {
+			((Query<?>)getTarget()).setParameter(name, value);
+		}
+	}
+
+}
--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/QueryFacadeTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/QueryFacadeTest.java
@@ -1,6 +1,7 @@
 package org.jboss.tools.hibernate.runtime.v_6_0.internal;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 
 import java.lang.reflect.InvocationHandler;
@@ -10,15 +11,18 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import javax.persistence.TemporalType;
+
 import org.hibernate.engine.spi.SessionDelegatorBaseImpl;
 import org.hibernate.engine.spi.SessionImplementor;
+import org.hibernate.metamodel.model.domain.AllowableParameterType;
 import org.hibernate.query.Query;
 import org.hibernate.query.internal.ParameterMetadataImpl;
 import org.hibernate.query.internal.QueryImpl;
 import org.hibernate.query.spi.QueryImplementor;
-import org.jboss.tools.hibernate.runtime.common.AbstractQueryFacade;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
 import org.jboss.tools.hibernate.runtime.spi.IQuery;
+import org.jboss.tools.hibernate.runtime.spi.IType;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -32,7 +36,7 @@ public class QueryFacadeTest {
 	@Before
 	public void before() {
 		queryTarget = new TestQuery<Object>();
-		queryFacade = new AbstractQueryFacade(FACADE_FACTORY, queryTarget) {};
+		queryFacade = new QueryFacadeImpl(FACADE_FACTORY, queryTarget) {};
 	}
 	
 	@Test
@@ -47,10 +51,74 @@ public class QueryFacadeTest {
 		assertEquals(Integer.MAX_VALUE, ((TestQuery<?>)queryTarget).maxResults);
 	}
 	
+	@Test
+	public void testSetParameter() {
+		Object object = new Object();
+		AllowableParameterType<?> allowableParameterTypeTarget = createAllowableParameterType();
+		TemporalType temporalType = TemporalType.TIMESTAMP;
+		IType allowableParameterTypeFacade = FACADE_FACTORY.createType(allowableParameterTypeTarget);
+		IType temporalTypeFacade = FACADE_FACTORY.createType(temporalType);
+		IType dummyTypeFacade = FACADE_FACTORY.createType(new Object());
+		assertEquals(Integer.MIN_VALUE, ((TestQuery<?>)queryTarget).position);
+		assertNull(((TestQuery<?>)queryTarget).name);
+		assertNull(((TestQuery<?>)queryTarget).value);
+		assertNull(((TestQuery<?>)queryTarget).allowableParameterType);
+		assertNull(((TestQuery<?>)queryTarget).temporalType);
+		queryFacade.setParameter(Integer.MAX_VALUE, object, allowableParameterTypeFacade);
+		assertEquals(Integer.MAX_VALUE, ((TestQuery<?>)queryTarget).position);
+		assertNull(((TestQuery<?>)queryTarget).name);
+		assertSame(object, ((TestQuery<?>)queryTarget).value);
+		assertSame(allowableParameterTypeTarget, ((TestQuery<?>)queryTarget).allowableParameterType);
+		assertNull(((TestQuery<?>)queryTarget).temporalType);
+		((TestQuery<?>)queryTarget).allowableParameterType = null;
+		queryFacade.setParameter(Integer.MIN_VALUE, null, temporalTypeFacade);
+		assertEquals(Integer.MIN_VALUE, ((TestQuery<?>)queryTarget).position);
+		assertNull(((TestQuery<?>)queryTarget).name);
+		assertNull(((TestQuery<?>)queryTarget).value);
+		assertSame(temporalType, ((TestQuery<?>)queryTarget).temporalType);
+		assertNull(((TestQuery<?>)queryTarget).allowableParameterType);
+		((TestQuery<?>)queryTarget).temporalType = null;
+		((TestQuery<?>)queryTarget).allowableParameterType = null;
+		queryFacade.setParameter(Integer.MAX_VALUE, object, dummyTypeFacade);
+		assertEquals(Integer.MAX_VALUE, ((TestQuery<?>)queryTarget).position);
+		assertNull(((TestQuery<?>)queryTarget).name);
+		assertSame(object, ((TestQuery<?>)queryTarget).value);
+		assertNull(((TestQuery<?>)queryTarget).temporalType);
+		assertNull(((TestQuery<?>)queryTarget).allowableParameterType);
+		((TestQuery<?>)queryTarget).temporalType = null;
+		((TestQuery<?>)queryTarget).allowableParameterType = null;
+		queryFacade.setParameter("foo", null, allowableParameterTypeFacade);
+		assertEquals(Integer.MAX_VALUE, ((TestQuery<?>)queryTarget).position);
+		assertEquals("foo", ((TestQuery<?>)queryTarget).name);
+		assertNull(((TestQuery<?>)queryTarget).value);
+		assertSame(allowableParameterTypeTarget, ((TestQuery<?>)queryTarget).allowableParameterType);
+		assertNull(((TestQuery<?>)queryTarget).temporalType);
+		((TestQuery<?>)queryTarget).allowableParameterType = null;
+		queryFacade.setParameter("bar", object, temporalTypeFacade);
+		assertEquals(Integer.MAX_VALUE, ((TestQuery<?>)queryTarget).position);
+		assertEquals("bar", ((TestQuery<?>)queryTarget).name);
+		assertSame(object, ((TestQuery<?>)queryTarget).value);
+		assertSame(temporalType, ((TestQuery<?>)queryTarget).temporalType);
+		assertNull(((TestQuery<?>)queryTarget).allowableParameterType);
+		((TestQuery<?>)queryTarget).temporalType = null;
+		((TestQuery<?>)queryTarget).allowableParameterType = null;
+		queryFacade.setParameter("baz", null, dummyTypeFacade);
+		assertEquals(Integer.MAX_VALUE, ((TestQuery<?>)queryTarget).position);
+		assertEquals("baz", ((TestQuery<?>)queryTarget).name);
+		assertNull(((TestQuery<?>)queryTarget).value);
+		assertNull(((TestQuery<?>)queryTarget).temporalType);
+		assertNull(((TestQuery<?>)queryTarget).allowableParameterType);
+	}
+	
 	private class TestQuery<R> extends QueryImpl<R> {
 		
 		private List<R> theList = new ArrayList<R>();
-		int maxResults = Integer.MIN_VALUE;
+		private int maxResults = Integer.MIN_VALUE;
+		private int position = Integer.MIN_VALUE;
+		private String name = null;
+		private Object value = null;
+		private AllowableParameterType<?> allowableParameterType = null;
+		private TemporalType temporalType = null;
 
 		public TestQuery() {
 			super(
@@ -69,7 +137,55 @@ public class QueryFacadeTest {
 			maxResults = i;
 			return this;
 		}
-
+		
+		@Override
+		public QueryImplementor<R> setParameter(int p, Object v) {
+			position = p;
+			value = v;
+			return this;
+		}
+		
+		@SuppressWarnings("rawtypes")
+		@Override
+		public QueryImplementor<R> setParameter(int p, Object v, AllowableParameterType t) {
+			position = p;
+			value = v;
+			allowableParameterType = t;
+			return this;
+		}
+		
+		@Override
+		public QueryImplementor<R> setParameter(int p, Object v, TemporalType t) {
+			position = p;
+			value = v;
+			temporalType = t;
+			return this;
+		}
+		
+		@Override
+		public QueryImplementor<R> setParameter(String n, Object v) {
+			name = n;
+			value = v;
+			return this;
+		}
+		
+		@SuppressWarnings("rawtypes")
+		@Override
+		public QueryImplementor<R> setParameter(String n, Object v, AllowableParameterType t) {
+			name = n;
+			value = v;
+			allowableParameterType = t;
+			return this;
+		}
+		
+		@Override
+		public QueryImplementor<R> setParameter(String n, Object v, TemporalType t) {
+			name = n;
+			value = v;
+			temporalType = t;
+			return this;
+		}
+		
 	}
 	
 	private class TestSharedSessionContractImplementor extends SessionDelegatorBaseImpl {
@@ -92,6 +208,18 @@ public class QueryFacadeTest {
 						if ("isQueryParametersValidationEnabled".equals(method.getName())) {
 							return false;
 						}
+						return null;
+					}
+		});
+	}
+	
+	private AllowableParameterType<?> createAllowableParameterType() {
+		return (AllowableParameterType<?>)Proxy.newProxyInstance(
+				getClass().getClassLoader(), 
+				new Class[] { AllowableParameterType.class }, 
+				new InvocationHandler() {
+					@Override
+					public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 						return null;
 					}
 		});
