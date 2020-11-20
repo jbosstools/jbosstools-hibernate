6f6efe6bd77a2b2083bbfe917ccfeb8b123f8468
Koen Aers <koen.aers@gmail.com>
2020-11-20 16:34:46 +0100

    JBIDE-27439: Set up a runtime plugin for the Hibernate 6.0.x releases - Implement 'ValueFacadeTest#testGetCollectionElement()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/ValueFacadeTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/ValueFacadeTest.java
@@ -1,12 +1,16 @@
 package org.jboss.tools.hibernate.runtime.v_6_0.internal;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
 import org.hibernate.mapping.BasicValue;
 import org.hibernate.mapping.Set;
 import org.hibernate.mapping.Value;
 import org.jboss.tools.hibernate.runtime.common.AbstractValueFacade;
+import org.jboss.tools.hibernate.runtime.common.IFacade;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
 import org.jboss.tools.hibernate.runtime.spi.IValue;
 import org.jboss.tools.hibernate.runtime.v_6_0.internal.util.DummyMetadataBuildingContext;
@@ -32,11 +36,25 @@ public class ValueFacadeTest {
 	@Test
 	public void testIsCollection() {
 		valueTarget = new BasicValue(DummyMetadataBuildingContext.INSTANCE);
-		valueFacade = FACADE_FACTORY.createValue(valueTarget);
+		valueFacade = new AbstractValueFacade(FACADE_FACTORY, valueTarget) {};
 		assertFalse(valueFacade.isCollection());
 		valueTarget = new Set(DummyMetadataBuildingContext.INSTANCE, null);
-		valueFacade = FACADE_FACTORY.createValue(valueTarget);
+		valueFacade = new AbstractValueFacade(FACADE_FACTORY, valueTarget) {};
 		assertTrue(valueFacade.isCollection());
+	}
+
+	@Test
+	public void testGetCollectionElement() {
+		valueTarget = new BasicValue(DummyMetadataBuildingContext.INSTANCE, null);
+		valueFacade = new AbstractValueFacade(FACADE_FACTORY, valueTarget) {};
+		IValue collectionElement = valueFacade.getCollectionElement();
+		assertNull(collectionElement);
+		Set set = new Set(DummyMetadataBuildingContext.INSTANCE, null);
+		set.setElement(valueTarget);
+		valueFacade = new AbstractValueFacade(FACADE_FACTORY, set) {};
+		collectionElement = valueFacade.getCollectionElement();
+		assertNotNull(collectionElement);
+		assertSame(valueTarget, ((IFacade)collectionElement).getTarget());
 	}
 
 }
