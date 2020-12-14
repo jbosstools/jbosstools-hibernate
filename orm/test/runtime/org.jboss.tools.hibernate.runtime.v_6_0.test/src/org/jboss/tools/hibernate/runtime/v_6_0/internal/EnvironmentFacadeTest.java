6ca7a4e40c9e48626a242fda551b575e0ea792a3
Koen Aers <koen.aers@gmail.com>
2020-12-14 19:20:15 +0100

    JBIDE-27439: Set up a runtime plugin for the Hibernate 6.0.x releases - Implementation of 'EnvironmentFacadeTest#testGetWrappedClass()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/EnvironmentFacadeTest.java
@@ -0,0 +1,25 @@
+package org.jboss.tools.hibernate.runtime.v_6_0.internal;
+
+import static org.junit.Assert.assertSame;
+
+import org.hibernate.cfg.Environment;
+import org.jboss.tools.hibernate.runtime.common.AbstractEnvironmentFacade;
+import org.jboss.tools.hibernate.runtime.spi.IEnvironment;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EnvironmentFacadeTest {
+	
+	private IEnvironment environmentFacade = null; 
+	
+	@Before
+	public void before() {
+		environmentFacade = new AbstractEnvironmentFacade(new FacadeFactoryImpl(), null) {};		
+	}
+	
+	@Test
+	public void testGetWrappedClass() {
+		assertSame(Environment.class, environmentFacade.getWrappedClass());
+	}
+
+}
