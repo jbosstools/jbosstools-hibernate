6b82b5c2d8ea511f5ada7c12835a020d78e2f08c
Koen Aers <koen.aers@gmail.com>
2020-12-16 17:49:03 +0100

    JBIDE-27439: Set up a runtime plugin for the Hibernate 6.0.x releases - Implementation of 'EnvironmentFacadeTest#testGetUser()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/EnvironmentFacadeTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/EnvironmentFacadeTest.java
@@ -18,6 +18,11 @@ public class EnvironmentFacadeTest {
 	}
 	
 	@Test
+	public void testGetUser() {
+		assertSame(Environment.USER, environmentFacade.getUser());
+	}
+	
+	@Test
 	public void testGetPass() {
 		assertSame(Environment.PASS, environmentFacade.getPass());
 	}
