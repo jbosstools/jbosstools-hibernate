2cc442b50879673f5e481991e5e2925692b09eda
Koen Aers <koen.aers@gmail.com>
2021-01-02 12:43:06 +0100

    JBIDE-27439: Set up a runtime plugin for the Hibernate 6.0.x releases - Implementation of 'EnvironmentFacadeTest#testGetConnectionProvider()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/EnvironmentFacadeTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/EnvironmentFacadeTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertSame;
 import org.hibernate.cfg.Environment;
 import org.jboss.tools.hibernate.runtime.common.AbstractEnvironmentFacade;
 import org.jboss.tools.hibernate.runtime.spi.IEnvironment;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,6 +16,11 @@ public class EnvironmentFacadeTest {
 	@Before
 	public void before() {
 		environmentFacade = new AbstractEnvironmentFacade(new FacadeFactoryImpl(), null) {};		
+	}
+	
+	@Test
+	public void testGetConnectionProvider() {
+		assertSame(Environment.CONNECTION_PROVIDER, environmentFacade.getConnectionProvider());
 	}
 	
 	@Test
