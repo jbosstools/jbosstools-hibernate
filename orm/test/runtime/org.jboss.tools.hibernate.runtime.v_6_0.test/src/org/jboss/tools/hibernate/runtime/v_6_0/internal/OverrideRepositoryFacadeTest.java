b76df05d31119b9864ed02851e67efb700bf90ed
Koen Aers <koen.aers@gmail.com>
2021-02-05 11:42:28 +0100

    JBIDE-27439: Set up a runtime plugin for the Hibernate 6.0.x releases - Implementation of 'OverrideRepositoryFacadeTest#testAddFile()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/OverrideRepositoryFacadeTest.java
@@ -0,0 +1,58 @@
+package org.jboss.tools.hibernate.runtime.v_6_0.internal;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.lang.reflect.Field;
+import java.util.List;
+
+import org.hibernate.mapping.Table;
+import org.hibernate.tool.internal.reveng.strategy.OverrideRepository;
+import org.jboss.tools.hibernate.runtime.common.AbstractOverrideRepositoryFacade;
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IOverrideRepository;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class OverrideRepositoryFacadeTest {
+
+	private static final IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
+	
+	private static final String HIBERNATE_REVERSE_ENGINEERING_XML =
+			"<?xml version='1.0' encoding='UTF-8'?>                                 "+
+			"<!DOCTYPE hibernate-reverse-engineering PUBLIC                         "+
+			"      '-//Hibernate/Hibernate Reverse Engineering DTD 3.0//EN'         "+
+			"      'http://hibernate.org/dtd/hibernate-reverse-engineering-3.0.dtd'>"+
+			"<hibernate-reverse-engineering>                                        "+
+			"    <table name='FOO'/>                                                "+
+			"</hibernate-reverse-engineering>                                       ";
+
+	private IOverrideRepository overrideRepositoryFacade = null; 
+	private OverrideRepository overrideRepository;
+	
+	@Before
+	public void setUp() {
+		overrideRepository = new OverrideRepository();
+		overrideRepositoryFacade = new AbstractOverrideRepositoryFacade(
+				FACADE_FACTORY, 
+				overrideRepository) {};
+	}
+	
+	@Test
+	public void testAddFile() throws Exception {
+		File file = File.createTempFile("addFile", "tst");
+		file.deleteOnExit();
+		FileWriter fileWriter = new FileWriter(file);
+		fileWriter.write(HIBERNATE_REVERSE_ENGINEERING_XML);
+		fileWriter.close();
+		overrideRepositoryFacade.addFile(file);
+		Field tablesField = overrideRepository.getClass().getDeclaredField("tables");
+		tablesField.setAccessible(true);
+		Object object = tablesField.get(overrideRepository);
+		List<?> tables = (List<?>)object;
+		Table table = (Table)tables.get(0);
+		Assert.assertNotNull(table);
+		Assert.assertEquals("FOO", table.getName());
+	}
+	
+}
