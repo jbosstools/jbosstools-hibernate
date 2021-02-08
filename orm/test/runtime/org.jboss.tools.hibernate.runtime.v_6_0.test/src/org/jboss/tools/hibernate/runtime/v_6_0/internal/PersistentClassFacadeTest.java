dc366b3b22aad66c274d172ac19506262f578894
Koen Aers <koen.aers@gmail.com>
2021-02-08 15:21:44 +0100

    JBIDE-27439: Set up a runtime plugin for the Hibernate 6.0.x releases - Implementation of 'PersistentClassFacadeTest#testConstruction()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/PersistentClassFacadeTest.java
@@ -0,0 +1,34 @@
+package org.jboss.tools.hibernate.runtime.v_6_0.internal;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+
+import org.hibernate.mapping.PersistentClass;
+import org.hibernate.mapping.RootClass;
+import org.jboss.tools.hibernate.runtime.common.AbstractPersistentClassFacade;
+import org.jboss.tools.hibernate.runtime.common.IFacade;
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IPersistentClass;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PersistentClassFacadeTest {
+
+	private static final IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
+	
+	private IPersistentClass persistentClassFacade = null; 
+	private PersistentClass persistentClassTarget = null;
+	
+	@Before
+	public void before() {
+		persistentClassTarget = new RootClass(null);
+		persistentClassFacade = new AbstractPersistentClassFacade(FACADE_FACTORY, persistentClassTarget) {};
+	}
+	
+	@Test
+	public void testConstruction() {
+		assertNotNull(persistentClassFacade);
+		assertSame(persistentClassTarget, ((IFacade)persistentClassFacade).getTarget());
+	}
+	
+}
