7f3a5bca78bfb8511936f3f4aaa6993125368d68
Koen Aers <koen.aers@gmail.com>
2021-03-02 16:38:08 +0100

    JBIDE-27439: Set up a runtime plugin for the Hibernate 6.0.x releases - Implementation of 'PropertyFacadeTest#testGetValue()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/PropertyFacadeTest.java
@@ -0,0 +1,60 @@
+package org.jboss.tools.hibernate.runtime.v_6_0.internal;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import org.hibernate.mapping.Property;
+import org.hibernate.mapping.Value;
+import org.jboss.tools.hibernate.runtime.common.AbstractPropertyFacade;
+import org.jboss.tools.hibernate.runtime.common.IFacade;
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IProperty;
+import org.jboss.tools.hibernate.runtime.spi.IValue;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PropertyFacadeTest {
+
+	private static final IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
+	
+	private Property propertyTarget = null;
+	private IProperty propertyFacade = null;
+
+	@Before
+	public void before() {
+		propertyTarget = new Property();
+		propertyFacade = new AbstractPropertyFacade(FACADE_FACTORY, propertyTarget) {};
+	}
+	
+	@Test
+	public void testGetValue() throws Exception {
+		Field field = AbstractPropertyFacade.class.getDeclaredField("value");
+		field.setAccessible(true);
+		assertNull(field.get(propertyFacade));
+		assertNull(propertyFacade.getValue());
+		Value valueTarget = createValue();
+		propertyTarget.setValue(valueTarget);
+		IValue valueFacade = propertyFacade.getValue();
+		assertNotNull(valueFacade);
+		assertSame(valueFacade, field.get(propertyFacade));
+		assertSame(valueTarget, ((IFacade)valueFacade).getTarget());
+	}
+
+	private Value createValue() {
+		return (Value)Proxy.newProxyInstance(
+				getClass().getClassLoader(), 
+				new Class[] { Value.class }, 
+				new InvocationHandler() {	
+					@Override
+					public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+						return null;
+					}
+		});
+	}
+}
