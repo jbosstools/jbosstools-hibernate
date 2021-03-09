60c31f1f58c548bedd52f5f7a6b57d6dcc0556f7
Koen Aers <koen.aers@gmail.com>
2021-03-09 12:40:53 +0100

    JBIDE-27439: Set up a runtime plugin for the Hibernate 6.0.x releases - Implementation of 'ServiceImpl#newJpaConfiguration(String,String)'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/ServiceImplTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/ServiceImplTest.java
@@ -1,11 +1,13 @@
 package org.jboss.tools.hibernate.runtime.v_6_0.internal;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import org.hibernate.cfg.Configuration;
 import org.jboss.tools.hibernate.runtime.common.IFacade;
 import org.jboss.tools.hibernate.runtime.spi.IConfiguration;
+import org.jboss.tools.hibernate.runtime.v_6_0.internal.util.JpaConfiguration;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -25,6 +27,17 @@ public class ServiceImplTest {
 		assertTrue(((IFacade)annotationConfiguration).getTarget() instanceof Configuration);
 	}
 
+	@Test
+	public void testNewJpaConfiguration() {
+		IConfiguration jpaConfiguration = service.newJpaConfiguration(null, "test", null);
+		assertNotNull(jpaConfiguration);
+		Object target = ((IFacade)jpaConfiguration).getTarget();
+		assertNotNull(target);
+		assertTrue(target instanceof JpaConfiguration);
+		assertEquals("test", ((JpaConfiguration)target).getPersistenceUnit());
+		
+	}
+	
 	@Test
 	public void testNewDefaultConfiguration() {
 		IConfiguration defaultConfiguration = service.newDefaultConfiguration();
