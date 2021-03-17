246c863f67e6e44d27d9a3047515667dce9e1539
Koen Aers <koen.aers@gmail.com>
2021-03-17 17:30:33 +0100

    JBIDE-27439: Set up a runtime plugin for the Hibernate 6.0.x releases - Implementation of 'SpecialRootClassFacadeTest#testGetProperty()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/SpecialRootClassFacadeTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/SpecialRootClassFacadeTest.java
@@ -1,9 +1,16 @@
 package org.jboss.tools.hibernate.runtime.v_6_0.internal;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
+import java.lang.reflect.Field;
+
+import org.hibernate.mapping.Property;
+import org.jboss.tools.hibernate.runtime.common.AbstractSpecialRootClassFacade;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IProperty;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,13 +24,23 @@ public class SpecialRootClassFacadeTest {
 	public void before() {
 		specialRootClassFacade = new SpecialRootClassFacadeImpl(
 				FACADE_FACTORY, 
-				FACADE_FACTORY.createProperty(null));
+				FACADE_FACTORY.createProperty(new Property()));
 	}
 	
 	@Test
 	public void testIsInstanceOfSpecialRootClass() {
 		assertTrue(specialRootClassFacade.isInstanceOfSpecialRootClass());
 		assertFalse(specialRootClassFacade.isInstanceOfSubclass());
+	}
+	
+	@Test
+	public void testGetProperty() throws Exception {
+		IProperty property = FACADE_FACTORY.createProperty(new Property());
+		Field field = AbstractSpecialRootClassFacade.class.getDeclaredField("property");
+		field.setAccessible(true);
+		assertNotSame(property, specialRootClassFacade.getProperty());
+		field.set(specialRootClassFacade, property);
+		assertSame(property, specialRootClassFacade.getProperty());
 	}
 	
 
