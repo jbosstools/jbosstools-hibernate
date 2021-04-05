dc6450e8303de1b0b685ef28795d43050aa363a7
Koen Aers <koen.aers@gmail.com>
2021-04-05 23:11:51 +0200

    JBIDE-27775: Use JUnit Jupiter instead of JUnit 4 - Switch test class 'org.jboss.tools.hibernate.runtime.v_6_0.internal.util.DummyMetadataDescriptorTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/util/DummyMetadataDescriptorTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/util/DummyMetadataDescriptorTest.java
@@ -1,15 +1,15 @@
 package org.jboss.tools.hibernate.runtime.v_6_0.internal.util;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Collections;
 
 import org.hibernate.boot.Metadata;
 import org.hibernate.tool.api.metadata.MetadataDescriptor;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class DummyMetadataDescriptorTest {
 	
