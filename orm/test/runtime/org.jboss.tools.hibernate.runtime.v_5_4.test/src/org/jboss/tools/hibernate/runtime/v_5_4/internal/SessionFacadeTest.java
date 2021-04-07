a66396fd8c4992cd1769cab919c968fd47bb0687
Koen Aers <koen.aers@gmail.com>
2021-04-07 14:27:26 +0200

    JBIDE-27775: Use JUnit Jupiter instead of JUnit 4 - Switch test class 'org.jboss.tools.hibernate.runtime.v_5_4.internal.SessionFacadeTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_4.test/src/org/jboss/tools/hibernate/runtime/v_5_4/internal/SessionFacadeTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_4.test/src/org/jboss/tools/hibernate/runtime/v_5_4/internal/SessionFacadeTest.java
@@ -1,13 +1,14 @@
 package org.jboss.tools.hibernate.runtime.v_5_4.internal;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
 import org.jboss.tools.hibernate.runtime.spi.ISession;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class SessionFacadeTest {
 
@@ -15,7 +16,7 @@ public class SessionFacadeTest {
 	
 	private ISession sessionFacade = null;
 	
-	@Before
+	@BeforeEach
 	public void setUp() {
 		Configuration configuration = new Configuration();
 		configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
@@ -26,7 +27,7 @@ public class SessionFacadeTest {
 	
 	@Test
 	public void testContains() {
-		Assert.assertFalse(sessionFacade.contains("foo"));
+		assertFalse(sessionFacade.contains("foo"));
 	}
 	
 }
