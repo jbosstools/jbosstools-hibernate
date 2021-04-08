4d86cd8b6a81e589b1e74c66a1ef171731b5b5c3
Koen Aers <koen.aers@gmail.com>
2021-04-08 20:21:06 +0200

    JBIDE-27775: Use JUnit Jupiter instead of JUnit 4 - Switch test class 'org.jboss.tools.hibernate.runtime.runtime.v_5_3.internal.ExporterFacadeTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_3.test/src/org/jboss/tools/hibernate/runtime/v_5_3/internal/ExporterFacadeTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_3.test/src/org/jboss/tools/hibernate/runtime/v_5_3/internal/ExporterFacadeTest.java
@@ -1,5 +1,12 @@
 package org.jboss.tools.hibernate.runtime.v_5_3.internal;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.StringWriter;
@@ -29,10 +36,8 @@ import org.jboss.tools.hibernate.runtime.spi.IGenericExporter;
 import org.jboss.tools.hibernate.runtime.spi.IHbm2DDLExporter;
 import org.jboss.tools.hibernate.runtime.spi.IQueryExporter;
 import org.jboss.tools.hibernate.runtime.v_5_3.internal.util.ConfigurationMetadataDescriptor;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class ExporterFacadeTest {
 
@@ -44,7 +49,7 @@ public class ExporterFacadeTest {
 	private String methodName = null;
 	private Object[] arguments = null;
 	
-	@Before
+	@BeforeEach
 	public void setUp() throws Exception {
 		exporter = (Exporter)Proxy.newProxyInstance(
 				FACADE_FACTORY.getClassLoader(), 
@@ -58,11 +63,11 @@ public class ExporterFacadeTest {
 		Configuration cfg = new Configuration();
 		IConfiguration configuration = new AbstractConfigurationFacade(FACADE_FACTORY, cfg) {};
 		exporterFacade.setConfiguration(configuration);
-		Assert.assertEquals("setMetadataDescriptor", methodName);
+		assertEquals("setMetadataDescriptor", methodName);
 		Object argument = arguments[0];
-		Assert.assertTrue(argument instanceof ConfigurationMetadataDescriptor);
+		assertTrue(argument instanceof ConfigurationMetadataDescriptor);
 		ConfigurationMetadataDescriptor metadataDescriptor = (ConfigurationMetadataDescriptor)argument;
-		Assert.assertSame(configuration, metadataDescriptor.getConfiguration());
+		assertSame(configuration, metadataDescriptor.getConfiguration());
 		
 	}
 	
@@ -71,24 +76,24 @@ public class ExporterFacadeTest {
 		ArtifactCollector ac = new ArtifactCollector();
 		IArtifactCollector artifactCollector = new AbstractArtifactCollectorFacade(FACADE_FACTORY, ac) {};
 		exporterFacade.setArtifactCollector(artifactCollector);
-		Assert.assertEquals("setArtifactCollector", methodName);
-		Assert.assertArrayEquals(new Object[] { ac }, arguments);
+		assertEquals("setArtifactCollector", methodName);
+		assertArrayEquals(new Object[] { ac }, arguments);
 	}
 	
 	@Test
 	public void testSetOutputDirectory() {
 		File file = new File("");
 		exporterFacade.setOutputDirectory(file);
-		Assert.assertEquals("setOutputDirectory", methodName);
-		Assert.assertArrayEquals(new Object[] { file }, arguments);
+		assertEquals("setOutputDirectory", methodName);
+		assertArrayEquals(new Object[] { file }, arguments);
 	}
 	
 	@Test
 	public void testSetTemplatePath() {
 		String[] templatePath = new String[] {};
 		exporterFacade.setTemplatePath(templatePath);
-		Assert.assertEquals("setTemplatePath", methodName);
-		Assert.assertArrayEquals(new Object[] { templatePath }, arguments);
+		assertEquals("setTemplatePath", methodName);
+		assertArrayEquals(new Object[] { templatePath }, arguments);
 	}
 	
 	@Test
@@ -113,47 +118,47 @@ public class ExporterFacadeTest {
 		exporter.setArtifactCollector(artifactCollector);
 		exporterFacade = new ExporterFacadeImpl(FACADE_FACTORY, exporter);
 		exporterFacade.start();
-		Assert.assertEquals("start", methodName);
+		assertEquals("start", methodName);
 	}
 	
 	@Test
 	public void testGetProperties() {
-		Assert.assertNull(exporterFacade.getProperties());
-		Assert.assertEquals("getProperties", methodName);
-		Assert.assertNull(arguments);
+		assertNull(exporterFacade.getProperties());
+		assertEquals("getProperties", methodName);
+		assertNull(arguments);
 	}
 	
 	@Test
 	public void testGetGenericExporter() {
 		IGenericExporter genericExporter = exporterFacade.getGenericExporter();
-		Assert.assertNull(genericExporter);
+		assertNull(genericExporter);
 		exporter = new GenericExporter();
 		exporterFacade = new AbstractExporterFacade(FACADE_FACTORY, exporter) {};
 		genericExporter = exporterFacade.getGenericExporter();
-		Assert.assertNotNull(genericExporter);
-		Assert.assertSame(exporter, ((IFacade)genericExporter).getTarget());
+		assertNotNull(genericExporter);
+		assertSame(exporter, ((IFacade)genericExporter).getTarget());
 	}
 	
 	@Test
 	public void testGetHbm2DDLExporter() {
 		IHbm2DDLExporter hbm2DDLExporter = exporterFacade.getHbm2DDLExporter();
-		Assert.assertNull(hbm2DDLExporter);
+		assertNull(hbm2DDLExporter);
 		exporter = new Hbm2DDLExporter();
 		exporterFacade = new AbstractExporterFacade(FACADE_FACTORY, exporter) {};
 		hbm2DDLExporter = exporterFacade.getHbm2DDLExporter();
-		Assert.assertNotNull(hbm2DDLExporter);
-		Assert.assertSame(exporter, ((IFacade)hbm2DDLExporter).getTarget());
+		assertNotNull(hbm2DDLExporter);
+		assertSame(exporter, ((IFacade)hbm2DDLExporter).getTarget());
 	}
 	
 	@Test
 	public void testGetQueryExporter() {
 		IQueryExporter queryExporter = exporterFacade.getQueryExporter();
-		Assert.assertNull(queryExporter);
+		assertNull(queryExporter);
 		exporter = new QueryExporter();
 		exporterFacade = new AbstractExporterFacade(FACADE_FACTORY, exporter) {};
 		queryExporter = exporterFacade.getQueryExporter();
-		Assert.assertNotNull(queryExporter);
-		Assert.assertSame(exporter, ((IFacade)queryExporter).getTarget());
+		assertNotNull(queryExporter);
+		assertSame(exporter, ((IFacade)queryExporter).getTarget());
 	}
 	
 	@Test
@@ -165,7 +170,7 @@ public class ExporterFacadeTest {
 		exporter = new HibernateConfigurationExporter();
 		exporterFacade = FACADE_FACTORY.createExporter(exporter);
 		exporterFacade.setCustomProperties(properties);
-		Assert.assertSame(properties, ((HibernateConfigurationExporter)exporter).getCustomProperties());
+		assertSame(properties, ((HibernateConfigurationExporter)exporter).getCustomProperties());
 	}
 	
 	@Test
@@ -177,7 +182,7 @@ public class ExporterFacadeTest {
 		exporter = new HibernateConfigurationExporter();
 		exporterFacade = FACADE_FACTORY.createExporter(exporter);
 		exporterFacade.setOutput(stringWriter);
-		Assert.assertSame(stringWriter, ((HibernateConfigurationExporter)exporter).getOutput());
+		assertSame(stringWriter, ((HibernateConfigurationExporter)exporter).getOutput());
 	}
 		
 	private class TestInvocationHandler implements InvocationHandler {
