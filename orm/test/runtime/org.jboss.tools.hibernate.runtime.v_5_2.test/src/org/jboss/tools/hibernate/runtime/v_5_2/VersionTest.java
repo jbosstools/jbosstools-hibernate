2d4ddc2e1ad480b0b43c7e4b929480c89e760d56
Koen Aers <koen.aers@gmail.com>
2021-04-12 11:45:20 +0200

    JBIDE-27775: Use JUnit Jupiter instead of JUnit 4 - Switch test class 'org.jboss.tools.hibernate.runtime.runtime.v_5_2.VersionTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_2.test/src/org/jboss/tools/hibernate/runtime/v_5_2/VersionTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_2.test/src/org/jboss/tools/hibernate/runtime/v_5_2/VersionTest.java
@@ -1,18 +1,19 @@
 package org.jboss.tools.hibernate.runtime.v_5_2;
 
-import org.junit.Assert;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class VersionTest {
 	
 	@Test
 	public void testToolsVersion() {
-		Assert.assertEquals("5.2.12.Final", org.hibernate.tool.Version.VERSION);
+		assertEquals("5.2.12.Final", org.hibernate.tool.Version.VERSION);
 	}
 
 	@Test
 	public void testCoreVersion() {
-		Assert.assertEquals("5.2.18.Final", org.hibernate.Version.getVersionString());
+		assertEquals("5.2.18.Final", org.hibernate.Version.getVersionString());
 	}
 
 }
