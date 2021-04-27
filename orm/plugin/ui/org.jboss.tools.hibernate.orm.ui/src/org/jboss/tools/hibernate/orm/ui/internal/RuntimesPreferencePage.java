f9a2f825f0f4a88b26a8c5e9818fe9f0af3a45aa
Koen Aers <koen.aers@gmail.com>
2021-04-27 09:35:27 +0200

    JBIDE-27827: Create a preference page to enable/disable contributed Hibernate runtime implementations - Initial contribution of a runtimes preference page
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/plugin/ui/org.jboss.tools.hibernate.orm.ui/src/org/jboss/tools/hibernate/orm/ui/internal/RuntimesPreferencePage.java
@@ -0,0 +1,20 @@
+package org.jboss.tools.hibernate.orm.ui.internal;
+
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+public class RuntimesPreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
+
+	@Override
+	public void init(IWorkbench workbench) {
+	}
+
+	@Override
+	protected Control createContents(Composite composite) {
+		return null;
+	}
+
+}
