193f67fed89b1bb8183ef5a977eb96b6d540530d
Koen Aers <koen.aers@gmail.com>
2021-04-29 19:23:07 +0200

    JBIDE-27827: Create a preference page to enable/disable contributed Hibernate runtime implementations - Add a dropdown box to select the default runtime
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/plugin/ui/org.jboss.tools.hibernate.orm.ui/src/org/jboss/tools/hibernate/orm/ui/internal/RuntimesPreferencePage.java
+++ orm/plugin/ui/org.jboss.tools.hibernate.orm.ui/src/org/jboss/tools/hibernate/orm/ui/internal/RuntimesPreferencePage.java
@@ -4,6 +4,7 @@ import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
@@ -16,6 +17,7 @@ import org.jboss.tools.hibernate.runtime.spi.RuntimeServiceManager;
 public class RuntimesPreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
 	
 	private TableItem[] tableItems = new TableItem[RuntimeServiceManager.getVersions().length];
+	private Combo defaultRuntimeCombo = null;
 
 	@Override
 	public void init(IWorkbench workbench) {
@@ -24,17 +26,11 @@ public class RuntimesPreferencePage extends PreferencePage implements IWorkbench
 	@Override
 	protected Control createContents(Composite parent) {
 		Composite composite = new Composite(parent, SWT.NONE);
-		composite.setLayout(new GridLayout(1, false));
-		Label label = new Label(composite, SWT.NONE);
-		label.setText("Check to enable or uncheck to disable the Hibernate runtime");
-		Table table = new Table(composite, SWT.CHECK | SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
-		for (int i = 0; i < tableItems.length; i++) {
-			String version = RuntimeServiceManager.getVersions()[i];
-			tableItems[i] = new TableItem(table, SWT.FILL);
-			tableItems[i].setText(version);
-			tableItems[i].setChecked(RuntimeServiceManager.isServiceEnabled(version));	
-		}
-		table.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		composite.setLayout(new GridLayout(2, false));
+		createAllRuntimesLabel(composite);
+		createAllRuntimesTable(composite);
+		createDefaultRuntimeLabel(composite);
+		createDefaultRuntimeCombo(composite);
 		return composite;
 	}
 	
@@ -44,6 +40,47 @@ public class RuntimesPreferencePage extends PreferencePage implements IWorkbench
 			RuntimeServiceManager.enableService(tableItem.getText(), tableItem.getChecked());
 		}
 		return super.performOk();
+	}
+	
+	private void createAllRuntimesLabel(Composite parent) {
+		Label allRuntimesLabel = new Label(parent, SWT.NONE);
+		allRuntimesLabel.setText("Check to enable or uncheck to disable the Hibernate runtime");
+		GridData gridData = new GridData(SWT.FILL, SWT.CENTER, true, false);
+		gridData.horizontalSpan = 2;
+		allRuntimesLabel.setLayoutData(gridData);	
+	}
+	
+	private void createAllRuntimesTable(Composite parent) {
+		Table allRuntimesTable = new Table(parent, SWT.CHECK | SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+		for (int i = 0; i < tableItems.length; i++) {
+			String version = RuntimeServiceManager.getVersions()[i];
+			tableItems[i] = new TableItem(allRuntimesTable, SWT.FILL);
+			tableItems[i].setText(version);
+			tableItems[i].setChecked(RuntimeServiceManager.isServiceEnabled(version));	
+		}
+		GridData gridData = new GridData(SWT.FILL, SWT.CENTER, true, false);
+		gridData.horizontalSpan = 2;
+		allRuntimesTable.setLayoutData(gridData);	
+	}
+	
+	private void createDefaultRuntimeLabel(Composite parent) {
+		Label defaultRuntimeLabel = new Label(parent, SWT.NONE);
+		defaultRuntimeLabel.setText("Default Hibernate runtime:");
+	}
+	
+	private void createDefaultRuntimeCombo(Composite parent) {
+		defaultRuntimeCombo = new Combo(parent, SWT.DROP_DOWN | SWT.BORDER);
+		defaultRuntimeCombo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		refreshDefaultRuntimeCombo();
+	}
+	
+	private void refreshDefaultRuntimeCombo() {
+		for (int i = 0; i < tableItems.length; i++) {
+			if (tableItems[i].getChecked()) {
+				defaultRuntimeCombo.add(tableItems[i].getText());
+			}
+			defaultRuntimeCombo.setText(RuntimeServiceManager.getVersions()[RuntimeServiceManager.getVersions().length - 1]);
+		}
 	}
 	
 }
