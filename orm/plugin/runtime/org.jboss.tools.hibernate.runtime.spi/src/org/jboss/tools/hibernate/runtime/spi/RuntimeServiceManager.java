23ba1f09b61ef259532c7e05ef82895229443c58
Koen Aers <koen.aers@gmail.com>
2021-04-29 13:46:10 +0200

    JBIDE-27827: Create a preference page to enable/disable contributed Hibernate runtime implementations - Track enabled runtimes in RuntimeServiceManager
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/plugin/runtime/org.jboss.tools.hibernate.runtime.spi/src/org/jboss/tools/hibernate/runtime/spi/RuntimeServiceManager.java
+++ orm/plugin/runtime/org.jboss.tools.hibernate.runtime.spi/src/org/jboss/tools/hibernate/runtime/spi/RuntimeServiceManager.java
@@ -3,7 +3,9 @@ package org.jboss.tools.hibernate.runtime.spi;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
@@ -11,7 +13,9 @@ import org.eclipse.core.runtime.IExtension;
 import org.eclipse.core.runtime.IExtensionPoint;
 import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.jboss.tools.hibernate.spi.internal.HibernateServicePlugin;
+import org.osgi.service.prefs.Preferences;
 
 public class RuntimeServiceManager {
 	
@@ -19,7 +23,8 @@ public class RuntimeServiceManager {
 
 	private static IService DEFAULT_SERVICE = null;
 	private static Map<String, IService> SERVICES_MAP = null;
-	private static String[] VERSIONS = null;
+	private static String[] ALL_VERSIONS = null;
+	private static Set<String> ENABLED_VERSIONS = null;
 	
 	public static IService findService(String hibernateVersion) {
 		if (SERVICES_MAP == null) {
@@ -29,10 +34,10 @@ public class RuntimeServiceManager {
 	}
 	
 	public static String[] getVersions() {
-		if (SERVICES_MAP == null) {
+		if (ALL_VERSIONS == null) {
 			initialize();
 		}
-		return VERSIONS;
+		return ALL_VERSIONS;
 	}
 	
 	public static IService getDefault() {
@@ -42,11 +47,18 @@ public class RuntimeServiceManager {
 		return DEFAULT_SERVICE;
 	}
 
-	public static boolean isServiceEnabled(String serviceName) {
-		return true;
+	public static boolean isServiceEnabled(String version) {
+		return ENABLED_VERSIONS.contains(version);
 	}
 	
 	private static void initialize() {
+		initializeServicesMap();
+		initializeAllVersions();
+		initializeEnabledVersions();
+		initializeDefaultService();
+	}
+	
+	private static void initializeServicesMap() {
 		SERVICES_MAP = new HashMap<String, IService>();
 		IExtensionRegistry extensionRegistry = Platform.getExtensionRegistry();
 		IExtensionPoint extensionPoint = extensionRegistry.getExtensionPoint(SERVICES_EXTENSION_ID);
@@ -62,11 +74,30 @@ public class RuntimeServiceManager {
 					HibernateServicePlugin.getDefault().log(e);
 				}
 			}
-		}
+		}		
+	}
+	
+	private static void initializeAllVersions() {
 		ArrayList<String> list = new ArrayList<String>(SERVICES_MAP.keySet());
 		Collections.sort(list);
-		VERSIONS = list.toArray(new String[list.size()]);
-		DEFAULT_SERVICE = SERVICES_MAP.get(VERSIONS[VERSIONS.length - 1]);
+		ALL_VERSIONS = list.toArray(new String[list.size()]);
+	}
+	
+	private static void initializeEnabledVersions() {
+		ENABLED_VERSIONS = new HashSet<String>();
+		for (String version : ALL_VERSIONS) {
+			if ((getPreferences().getBoolean(version, true))) {
+				ENABLED_VERSIONS.add(version);
+			}
+		}		
+	}
+	
+	private static void initializeDefaultService() {
+		DEFAULT_SERVICE = SERVICES_MAP.get(ALL_VERSIONS[ALL_VERSIONS.length - 1]);
+	}
+	
+	private static Preferences getPreferences() {
+		return InstanceScope.INSTANCE.getNode("org.jboss.tools.hibernate.runtime.spi");
 	}
 	
 }
