3c3a5d354ff6b99833301a04b74226c6fed5f870
Koen Aers <koen.aers@gmail.com>
2021-05-04 16:33:16 +0200

    JBIDE-27834: Transform class 'org.jboss.tools.hibernate.runtime.spi.RuntimeServiceManager' into a singleton - Make 'RuntimeServiceManager#getDefaultVersion()' non static
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.spi.test/src/org/jboss/tools/hibernate/spi/RuntimeServiceManagerTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.spi.test/src/org/jboss/tools/hibernate/spi/RuntimeServiceManagerTest.java
@@ -1,14 +1,26 @@
 package org.jboss.tools.hibernate.spi;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 
 import org.jboss.tools.hibernate.runtime.spi.IService;
 import org.jboss.tools.hibernate.runtime.spi.RuntimeServiceManager;
 import org.jboss.tools.hibernate.spi.internal.TestService;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class RuntimeServiceManagerTest {
+	
+	private RuntimeServiceManager runtimeServiceManager = null;
+	
+	@Before
+	public void before() throws Exception {
+		Constructor<RuntimeServiceManager> constructor = 
+				RuntimeServiceManager.class.getDeclaredConstructor(new Class[] {});
+		constructor.setAccessible(true);
+		runtimeServiceManager = constructor.newInstance(new Object[] {});
+	}
 
 	@Test
 	public void testFindService() {
@@ -18,18 +30,23 @@ public class RuntimeServiceManagerTest {
 	}
 	
 	@Test
-	public void testGetDefault() {
-		IService service = RuntimeServiceManager.getInstance().getDefaultService();
-		Assert.assertSame(RuntimeServiceManager.findService("0.0.0.Test"), service);
-	}
-	
-	@Test
 	public void testGetInstance() throws Exception {
 		Field instanceField = RuntimeServiceManager.class.getDeclaredField("INSTANCE");
 		instanceField.setAccessible(true);
 		Object instance = instanceField.get(null);
 		Assert.assertNotNull(instance);
 		Assert.assertSame(instance, RuntimeServiceManager.getInstance());
+	}
+	
+	@Test
+	public void testGetDefaultService() {
+		IService service = runtimeServiceManager.getDefaultService();
+		Assert.assertSame(RuntimeServiceManager.findService("0.0.0.Test"), service);
+	}
+	
+	@Test
+	public void testGetDefaultVersion() throws Exception {
+		Assert.assertEquals("0.0.0.Test", runtimeServiceManager.getDefaultVersion());
 	}
 	
 
