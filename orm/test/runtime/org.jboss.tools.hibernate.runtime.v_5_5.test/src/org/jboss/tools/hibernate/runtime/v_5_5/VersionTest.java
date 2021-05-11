fb42bbbd51fc168ec5164f109de3ca57ffbb1ef6
Koen Aers <koen.aers@gmail.com>
2021-05-11 12:35:30 +0200

    JBIDE-27351: Set up a runtime plugin for the upcoming Hibernate 5.5.x releases - Initial project creation of test fragment 'org.jboss.tools.hibernate.runtime.v_5_5.test'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/VersionTest.java
@@ -0,0 +1,14 @@
+package org.jboss.tools.hibernate.runtime.v_5_5;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class VersionTest {
+	
+	@Test
+	public void dummy() {
+		assertTrue(true);
+	}
+
+}
