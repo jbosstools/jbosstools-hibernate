6b7dc5f9e03d3acef647e210d845bfcbcbb19279
Koen Aers <koen.aers@gmail.com>
2021-05-12 18:41:09 +0200

    JBIDE-27351: Set up a runtime plugin for the upcoming Hibernate 5.5.x releases - Add test 'org.jboss.tools.hibernate.runtime.v_5_5.internal.FacadeFactoryTest#testCreateReverseEngineeringStrategy()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/FacadeFactoryTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/FacadeFactoryTest.java
@@ -3,8 +3,13 @@ package org.jboss.tools.hibernate.runtime.v_5_5.internal;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 import org.hibernate.cfg.DefaultNamingStrategy;
 import org.hibernate.cfg.reveng.ReverseEngineeringSettings;
+import org.hibernate.cfg.reveng.ReverseEngineeringStrategy;
 import org.hibernate.tool.hbm2x.ArtifactCollector;
 import org.hibernate.tool.hbm2x.Cfg2HbmTool;
 import org.jboss.tools.hibernate.runtime.common.IFacade;
@@ -12,6 +17,7 @@ import org.jboss.tools.hibernate.runtime.spi.IArtifactCollector;
 import org.jboss.tools.hibernate.runtime.spi.ICfg2HbmTool;
 import org.jboss.tools.hibernate.runtime.spi.INamingStrategy;
 import org.jboss.tools.hibernate.runtime.spi.IReverseEngineeringSettings;
+import org.jboss.tools.hibernate.runtime.spi.IReverseEngineeringStrategy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -62,6 +68,23 @@ public class FacadeFactoryTest {
 		ReverseEngineeringSettings res = new ReverseEngineeringSettings(null);
 		IReverseEngineeringSettings facade = facadeFactory.createReverseEngineeringSettings(res);
 		assertSame(res, ((IFacade)facade).getTarget());		
+	}
+	
+	@Test
+	public void testCreateReverseEngineeringStrategy() {
+		ReverseEngineeringStrategy res = (ReverseEngineeringStrategy)Proxy.newProxyInstance(
+				facadeFactory.getClassLoader(), 
+				new Class[] { ReverseEngineeringStrategy.class }, 
+				new TestInvocationHandler());
+		IReverseEngineeringStrategy facade = facadeFactory.createReverseEngineeringStrategy(res);
+		assertSame(res, ((IFacade)facade).getTarget());		
+	}
+	
+	private class TestInvocationHandler implements InvocationHandler {
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+			return null;
+		}	
 	}
 	
 }
