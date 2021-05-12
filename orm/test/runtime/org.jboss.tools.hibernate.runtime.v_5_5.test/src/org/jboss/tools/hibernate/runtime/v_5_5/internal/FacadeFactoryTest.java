61ec9403be237af3913cd09fba149816df9581e3
Koen Aers <koen.aers@gmail.com>
2021-05-12 18:35:19 +0200

    JBIDE-27351: Set up a runtime plugin for the upcoming Hibernate 5.5.x releases - Add test 'org.jboss.tools.hibernate.runtime.v_5_5.internal.FacadeFactoryTest#testCreateNamingStrategy()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/FacadeFactoryTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/FacadeFactoryTest.java
@@ -3,11 +3,13 @@ package org.jboss.tools.hibernate.runtime.v_5_5.internal;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
+import org.hibernate.cfg.DefaultNamingStrategy;
 import org.hibernate.tool.hbm2x.ArtifactCollector;
 import org.hibernate.tool.hbm2x.Cfg2HbmTool;
 import org.jboss.tools.hibernate.runtime.common.IFacade;
 import org.jboss.tools.hibernate.runtime.spi.IArtifactCollector;
 import org.jboss.tools.hibernate.runtime.spi.ICfg2HbmTool;
+import org.jboss.tools.hibernate.runtime.spi.INamingStrategy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -44,6 +46,13 @@ public class FacadeFactoryTest {
 		Cfg2HbmTool cfg2HbmTool = new Cfg2HbmTool();
 		ICfg2HbmTool facade = facadeFactory.createCfg2HbmTool(cfg2HbmTool);
 		assertSame(cfg2HbmTool,  ((IFacade)facade).getTarget());
+	}
+	
+	@Test
+	public void testCreateNamingStrategy() {
+		DefaultNamingStrategy namingStrategy = new DefaultNamingStrategy();
+		INamingStrategy facade = facadeFactory.createNamingStrategy(namingStrategy);
+		assertSame(namingStrategy, ((IFacade)facade).getTarget());
 	}
 	
 }
