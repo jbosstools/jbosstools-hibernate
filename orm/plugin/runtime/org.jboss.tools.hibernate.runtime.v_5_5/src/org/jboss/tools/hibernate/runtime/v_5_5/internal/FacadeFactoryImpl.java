523a6fc9c7fd747b429e380dbfa37593cfc18ee4
Koen Aers <koen.aers@gmail.com>
2021-05-12 17:53:50 +0200

    JBIDE-27351: Set up a runtime plugin for the upcoming Hibernate 5.5.x releases - Initial implementation of 'org.jboss.tools.hibernate.runtime.v_5_5.internal.FacadeFactoryImpl' and 'org.jboss.tools.hibernate.runtime.v_5_5.internal.FacadeFactoryTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_5_5/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/FacadeFactoryImpl.java
@@ -0,0 +1,21 @@
+package org.jboss.tools.hibernate.runtime.v_5_5.internal;
+
+import org.jboss.tools.hibernate.runtime.common.AbstractFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IPersistentClass;
+import org.jboss.tools.hibernate.runtime.spi.IProperty;
+
+public class FacadeFactoryImpl extends AbstractFacadeFactory {
+
+	@Override
+	public ClassLoader getClassLoader() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IPersistentClass createSpecialRootClass(IProperty property) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/FacadeFactoryTest.java
@@ -0,0 +1,22 @@
+package org.jboss.tools.hibernate.runtime.v_5_5.internal;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class FacadeFactoryTest {
+
+	private FacadeFactoryImpl facadeFactory;
+
+	@BeforeEach
+	public void beforeEach() throws Exception {
+		facadeFactory = new FacadeFactoryImpl();
+	}
+	
+	@Test
+	public void testFacadeFactoryCreation() {
+		assertNotNull(facadeFactory);
+	}
+	
+}
