4642d33a366b7a8c01d479b4876b36164d81b4d4
Koen Aers <koen.aers@gmail.com>
2021-05-12 18:31:33 +0200

    JBIDE-27351: Set up a runtime plugin for the upcoming Hibernate 5.5.x releases - Add test 'org.jboss.tools.hibernate.runtime.v_5_5.internal.FacadeFactoryTest#testCreateArtifactCollector()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/FacadeFactoryTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/FacadeFactoryTest.java
@@ -3,6 +3,9 @@ package org.jboss.tools.hibernate.runtime.v_5_5.internal;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
+import org.hibernate.tool.hbm2x.ArtifactCollector;
+import org.jboss.tools.hibernate.runtime.common.IFacade;
+import org.jboss.tools.hibernate.runtime.spi.IArtifactCollector;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -27,4 +30,11 @@ public class FacadeFactoryTest {
 				facadeFactory.getClassLoader());
 	}
 	
+	@Test
+	public void testCreateArtifactCollector() {
+		ArtifactCollector artifactCollector = new ArtifactCollector();
+		IArtifactCollector facade = facadeFactory.createArtifactCollector(artifactCollector);
+		assertSame(artifactCollector, ((IFacade)facade).getTarget());
+	}
+		
 }
