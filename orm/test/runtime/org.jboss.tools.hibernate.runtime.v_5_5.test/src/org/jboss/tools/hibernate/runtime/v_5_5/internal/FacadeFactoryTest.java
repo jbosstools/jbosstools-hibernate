181a1752fbbfa2c0e8d088848fe3c8f31c70db59
Koen Aers <koen.aers@gmail.com>
2021-05-12 18:33:40 +0200

    JBIDE-27351: Set up a runtime plugin for the upcoming Hibernate 5.5.x releases - Add test 'org.jboss.tools.hibernate.runtime.v_5_5.internal.FacadeFactoryTest#testCreateCfg2HbmTool()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/FacadeFactoryTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/FacadeFactoryTest.java
@@ -4,8 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 import org.hibernate.tool.hbm2x.ArtifactCollector;
+import org.hibernate.tool.hbm2x.Cfg2HbmTool;
 import org.jboss.tools.hibernate.runtime.common.IFacade;
 import org.jboss.tools.hibernate.runtime.spi.IArtifactCollector;
+import org.jboss.tools.hibernate.runtime.spi.ICfg2HbmTool;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -37,4 +39,11 @@ public class FacadeFactoryTest {
 		assertSame(artifactCollector, ((IFacade)facade).getTarget());
 	}
 		
+	@Test
+	public void testCreateCfg2HbmTool() {
+		Cfg2HbmTool cfg2HbmTool = new Cfg2HbmTool();
+		ICfg2HbmTool facade = facadeFactory.createCfg2HbmTool(cfg2HbmTool);
+		assertSame(cfg2HbmTool,  ((IFacade)facade).getTarget());
+	}
+	
 }
