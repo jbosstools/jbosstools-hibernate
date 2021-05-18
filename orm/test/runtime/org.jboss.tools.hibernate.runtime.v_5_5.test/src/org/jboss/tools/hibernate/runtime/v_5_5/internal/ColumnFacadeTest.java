b7ba09198a231a68e1e2869f7981c232d4fabdbf
Koen Aers <koen.aers@gmail.com>
2021-05-18 17:38:08 +0200

    JBIDE-27351: Set up a runtime plugin for the upcoming Hibernate 5.5.x releases - Initial implementation of test class 'org.jboss.tools.hibernate.runtime.v_5_5.internal.ColumnFacadeTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/ColumnFacadeTest.java
@@ -0,0 +1,29 @@
+package org.jboss.tools.hibernate.runtime.v_5_5.internal;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.hibernate.mapping.Column;
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IColumn;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ColumnFacadeTest {
+
+	private static final IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
+	
+	private IColumn columnFacade = null; 
+	private Column column = null;
+	
+	@BeforeEach
+	public void beforeEach() {
+		column = new Column();
+		columnFacade = new ColumnFacadeImpl(FACADE_FACTORY, column);
+	}
+	
+	@Test
+	public void testConstruction() {
+		assertNotNull(columnFacade);
+	}
+	
+}
