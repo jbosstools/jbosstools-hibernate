5eae1824d12c04c79b37d4e189beb038cce0c349
Koen Aers <koen.aers@gmail.com>
2021-05-18 17:51:19 +0200

    JBIDE-27351: Set up a runtime plugin for the upcoming Hibernate 5.5.x releases - Implement test 'org.jboss.tools.hibernate.runtime.v_5_5.internal.ColumnFacadeTest#testGetSqlType()' and implementation 'org.jboss.tools.hibernate.runtime.v_5_5.internal.ColumnFacadeImpl#getSqlType(IConfiguration)'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/ColumnFacadeTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/ColumnFacadeTest.java
@@ -3,9 +3,23 @@ package org.jboss.tools.hibernate.runtime.v_5_5.internal;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import org.hibernate.boot.internal.BootstrapContextImpl;
+import org.hibernate.boot.internal.InFlightMetadataCollectorImpl;
+import org.hibernate.boot.internal.MetadataBuilderImpl.MetadataBuildingOptionsImpl;
+import org.hibernate.boot.internal.MetadataBuildingContextRootImpl;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.boot.spi.InFlightMetadataCollector;
+import org.hibernate.boot.spi.MetadataBuildingContext;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.dialect.Dialect;
 import org.hibernate.mapping.Column;
+import org.hibernate.mapping.SimpleValue;
+import org.hibernate.mapping.Table;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
 import org.jboss.tools.hibernate.runtime.spi.IColumn;
+import org.jboss.tools.hibernate.runtime.spi.IConfiguration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -35,5 +49,42 @@ public class ColumnFacadeTest {
 		column.setSqlTypeCode(Integer.MAX_VALUE);
 		assertEquals(Integer.MAX_VALUE, columnFacade.getSqlTypeCode().intValue());
 	}
+
+	@Test
+	public void testGetSqlType() {
+		assertNull(columnFacade.getSqlType());
+		column.setSqlType("foobar");
+		assertEquals("foobar", columnFacade.getSqlType());
+		Configuration configuration = new Configuration();
+		configuration.setProperty(Environment.DIALECT, TestDialect.class.getName());
+		SimpleValue value = new SimpleValue(createMetadataBuildingContext(), new Table());
+		value.setTypeName("int");
+		column.setValue(value);
+		IConfiguration configurationFacade = FACADE_FACTORY.createConfiguration(configuration);
+		column.setSqlType(null);
+		assertEquals("integer", columnFacade.getSqlType(configurationFacade));
+	}
+	
+	private MetadataBuildingContext createMetadataBuildingContext() {
+		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder();
+		builder.applySetting("hibernate.dialect", TestDialect.class.getName());
+		StandardServiceRegistry serviceRegistry = builder.build();		
+		MetadataBuildingOptionsImpl metadataBuildingOptions = 
+				new MetadataBuildingOptionsImpl(serviceRegistry);	
+		BootstrapContextImpl bootstrapContext = new BootstrapContextImpl(
+				serviceRegistry, 
+				metadataBuildingOptions);
+		metadataBuildingOptions.setBootstrapContext(bootstrapContext);
+		InFlightMetadataCollector inFlightMetadataCollector = 
+				new InFlightMetadataCollectorImpl(
+						bootstrapContext,
+						metadataBuildingOptions);
+		return new MetadataBuildingContextRootImpl(
+						bootstrapContext, 
+						metadataBuildingOptions, 
+						inFlightMetadataCollector);
+	}
+	
+	public static class TestDialect extends Dialect {}
 
 }
