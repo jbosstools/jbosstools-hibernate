1875097378f73e96abe0978ea33a8b429e480271
Koen Aers <koen.aers@gmail.com>
2021-05-19 16:03:55 +0200

    JBIDE-27351: Set up a runtime plugin for the upcoming Hibernate 5.5.x releases - Implement test 'org.jboss.tools.hibernate.runtime.v_5_5.internal.util.JdbcMetadataConfigurationTest#testGetProperties()' and implementation 'org.jboss.tools.hibernate.runtime.v_5_5.internal.util.JdbcMetadataConfiguration#getProperties()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_5_5/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/util/JdbcMetadataConfiguration.java
+++ orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_5_5/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/util/JdbcMetadataConfiguration.java
@@ -1,5 +1,13 @@
 package org.jboss.tools.hibernate.runtime.v_5_5.internal.util;
 
+import java.util.Properties;
+
 public class JdbcMetadataConfiguration {
+
+	private Properties properties = new Properties();
+
+	public Object getProperties() {
+		return properties;
+	}
 
 }
--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/util/JdbcMetadataConfigurationTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/util/JdbcMetadataConfigurationTest.java
@@ -1,6 +1,11 @@
 package org.jboss.tools.hibernate.runtime.v_5_5.internal.util;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import java.lang.reflect.Field;
+import java.util.Properties;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,8 +20,14 @@ public class JdbcMetadataConfigurationTest {
 	}
 	
 	@Test
-	public void testConstruction() {
-		assertNotNull(jdbcMetadataConfiguration);
+	public void testGetProperties() throws Exception {
+		Properties properties = new Properties();
+		Field propertiesField = JdbcMetadataConfiguration.class.getDeclaredField("properties");
+		propertiesField.setAccessible(true);	
+		assertNotNull(jdbcMetadataConfiguration.getProperties());
+		assertNotSame(properties,  jdbcMetadataConfiguration.getProperties());
+		propertiesField.set(jdbcMetadataConfiguration, properties);
+		assertSame(properties, jdbcMetadataConfiguration.getProperties());
 	}
 	
 }
