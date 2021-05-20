053b7de8030ba6996db3b5084e8126f6a9478910
Koen Aers <koen.aers@gmail.com>
2021-05-20 11:54:40 +0200

    JBIDE-27351: Set up a runtime plugin for the upcoming Hibernate 5.5.x releases - Implement test 'org.jboss.tools.hibernate.runtime.v_5_5.internal.EnvironmentFacadeTest#ttestGetConnectionProvider()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/EnvironmentFacadeTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/EnvironmentFacadeTest.java
@@ -41,4 +41,9 @@ public class EnvironmentFacadeTest {
 		assertSame(Environment.DATASOURCE, environmentFacade.getDataSource());
 	}
 	
+	@Test
+	public void testGetConnectionProvider() {
+		assertSame(Environment.CONNECTION_PROVIDER, environmentFacade.getConnectionProvider());
+	}
+	
 }
