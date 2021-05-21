d6158b53e1e6392e97d52875872d313beb6fd22f
Koen Aers <koen.aers@gmail.com>
2021-05-21 17:44:11 +0200

    JBIDE-27351: Set up a runtime plugin for the upcoming Hibernate 5.5.x releases - Implement test 'org.jboss.tools.hibernate.runtime.v_5_5.internal.ForeignKeyFacadeTest#testColumnIterator()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/ForeignKeyFacadeTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/ForeignKeyFacadeTest.java
@@ -1,12 +1,17 @@
 package org.jboss.tools.hibernate.runtime.v_5_5.internal;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
+import java.util.Iterator;
+
+import org.hibernate.mapping.Column;
 import org.hibernate.mapping.ForeignKey;
 import org.hibernate.mapping.Table;
 import org.jboss.tools.hibernate.runtime.common.AbstractForeignKeyFacade;
 import org.jboss.tools.hibernate.runtime.common.IFacade;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IColumn;
 import org.jboss.tools.hibernate.runtime.spi.IForeignKey;
 import org.jboss.tools.hibernate.runtime.spi.ITable;
 import org.junit.jupiter.api.BeforeEach;
@@ -31,6 +36,16 @@ public class ForeignKeyFacadeTest {
 		foreignKey.setReferencedTable(tableTarget);
 		ITable table = foreignKeyFacade.getReferencedTable();
 		assertSame(tableTarget, ((IFacade)table).getTarget());
+	}
+	
+	@Test
+	public void testColumnIterator() {
+		Column column = new Column();
+		foreignKey.addColumn(column);
+		Iterator<IColumn> iterator = foreignKeyFacade.columnIterator();
+		IColumn columnFacade = iterator.next();
+		assertSame(column, ((IFacade)columnFacade).getTarget());
+		assertFalse(iterator.hasNext());
 	}
 	
 }
