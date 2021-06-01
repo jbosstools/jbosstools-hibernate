12fa7af501a4965fc328a287d58debb81060573c
Koen Aers <koen.aers@gmail.com>
2021-06-01 10:39:50 +0200

    JBIDE-27351: Set up a runtime plugin for the upcoming Hibernate 5.5.x releases - Remove unneeded class 'org.jboss.tools.hibernate.runtime.v_5_5.internal.HibernateMappingExporterFacadeImpl' and implent test 'org.jboss.tools.hibernate.runtime.v_5_5.internal.HibernateMappingExporterFacadeTest#testStart()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/HibernateMappingExporterFacadeTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/HibernateMappingExporterFacadeTest.java
@@ -1,11 +1,36 @@
 package org.jboss.tools.hibernate.runtime.v_5_5.internal;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
+import org.hibernate.boot.Metadata;
+import org.hibernate.cfg.Environment;
+import org.hibernate.mapping.Column;
+import org.hibernate.mapping.PersistentClass;
+import org.hibernate.mapping.RootClass;
+import org.hibernate.mapping.SimpleValue;
+import org.hibernate.mapping.Table;
+import org.hibernate.tool.api.metadata.MetadataDescriptor;
+import org.hibernate.tool.hbm2x.AbstractExporter;
+import org.hibernate.tool.hbm2x.pojo.POJOClass;
+import org.jboss.tools.hibernate.runtime.common.AbstractHibernateMappingExporterFacade;
+import org.jboss.tools.hibernate.runtime.common.IFacade;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IExportPOJODelegate;
 import org.jboss.tools.hibernate.runtime.spi.IHibernateMappingExporter;
+import org.jboss.tools.hibernate.runtime.spi.IPOJOClass;
+import org.jboss.tools.hibernate.runtime.v_5_5.internal.util.DummyMetadataBuildingContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
@@ -24,12 +49,95 @@ public class HibernateMappingExporterFacadeTest {
 	public void beforeEach() throws Exception {
 		hibernateMappingExporter = new HibernateMappingExporterExtension(FACADE_FACTORY, null, null);
 		hibernateMappingExporterFacade = 
-				new HibernateMappingExporterFacadeImpl(FACADE_FACTORY, hibernateMappingExporter);
+				new AbstractHibernateMappingExporterFacade(FACADE_FACTORY, hibernateMappingExporter) {};
 	}
 	
 	@Test
-	public void testConstruction() {
-		assertNotNull(hibernateMappingExporterFacade);
+	public void testStart() throws Exception {
+		Field metadataDescriptorField = AbstractExporter.class.getDeclaredField("metadataDescriptor");
+		metadataDescriptorField.setAccessible(true);
+		metadataDescriptorField.set(hibernateMappingExporter, new TestMetadataDescriptor());
+		Field outputDirectoryField = AbstractExporter.class.getDeclaredField("outputdir");
+		outputDirectoryField.setAccessible(true);
+		outputDirectoryField.set(hibernateMappingExporter, outputDir);
+		final File fooHbmXml = new File(outputDir, "Foo.hbm.xml");
+		// First without a 'delegate' exporter
+		assertFalse(fooHbmXml.exists());
+		hibernateMappingExporterFacade.start();
+		assertTrue(fooHbmXml.exists());
+		assertTrue(fooHbmXml.delete());
+		// Now set a 'delegate' and invoke 'start' again
+		final File dummyDir = new File(outputDir, "dummy");
+		dummyDir.mkdir();
+		assertTrue(dummyDir.exists());
+		IExportPOJODelegate delegate = new IExportPOJODelegate() {			
+			@Override
+			public void exportPOJO(Map<Object, Object> map, IPOJOClass pojoClass) {
+				assertTrue(dummyDir.delete());
+				Map<String, Object> m = new HashMap<>();
+				for (Object key : map.keySet()) {
+					m.put((String)key, map.get(key));
+				}
+				hibernateMappingExporter.superExportPOJO(
+					m,(POJOClass)((IFacade)pojoClass).getTarget());
+			}
+		};
+		Field delegateField = HibernateMappingExporterExtension.class.getDeclaredField("delegateExporter");
+		delegateField.setAccessible(true);
+		delegateField.set(hibernateMappingExporter, delegate);
+		hibernateMappingExporterFacade.start();
+		assertFalse(dummyDir.exists());
+		assertTrue(fooHbmXml.exists());
+		assertTrue(fooHbmXml.delete());
+		assertTrue(outputDir.exists());
 	}
 	
+	private class TestMetadataDescriptor implements MetadataDescriptor {
+		@Override
+		public Metadata createMetadata() {
+			return (Metadata)Proxy.newProxyInstance(
+					getClass().getClassLoader(), 
+					new Class<?>[] { Metadata.class }, 
+					new TestInvocationHandler());
+		}
+		@Override
+		public Properties getProperties() {
+			Properties properties = new Properties();
+			properties.put(Environment.DIALECT, "org.hibernate.dialect.H2Dialect");
+			return properties;
+		}	
+	}
+	
+	private class TestInvocationHandler implements InvocationHandler {
+		private ArrayList<PersistentClass> entities = new ArrayList<PersistentClass>();
+		private ArrayList<Table> tables = new ArrayList<Table>();
+		private TestInvocationHandler() {
+			RootClass persistentClass = new RootClass(null);
+			Table table = new Table("FOO");
+			Column keyColumn = new Column("BAR");
+			SimpleValue key = new SimpleValue(DummyMetadataBuildingContext.INSTANCE, null);
+			key.setTypeName("String");
+			key.addColumn(keyColumn);
+			key.setTable(table);
+			persistentClass.setClassName("Foo");
+			persistentClass.setEntityName("Foo");
+			persistentClass.setJpaEntityName("Foo");
+			persistentClass.setTable(table);
+			persistentClass.setIdentifier(key);	
+			entities.add(persistentClass);
+			tables.add(table);
+		}
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+			if (method.getName().equals("getEntityBindings")) {
+				return entities;
+			} else if (method.getName().equals("collectTableMappings")) {
+				return tables;
+			} else if (method.getName().equals("getImports")) {
+				return Collections.emptyMap();
+			}
+			return null;
+		}		
+	}
+		
 }
