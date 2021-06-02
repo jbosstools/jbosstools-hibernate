ee79d79be4f4f241470d7671f8531b6b5e7ca056
Koen Aers <koen.aers@gmail.com>
2021-06-02 12:14:00 +0200

    JBIDE-27351: Set up a runtime plugin for the upcoming Hibernate 5.5.x releases - Implementation of test 'org.jboss.tools.hibernate.runtime.v_5_5.internal.PersistentClassFacadeTest#testGetIdentifierProperty()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/PersistentClassFacadeTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/PersistentClassFacadeTest.java
@@ -4,16 +4,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.lang.reflect.Field;
+
 import org.hibernate.mapping.PersistentClass;
+import org.hibernate.mapping.Property;
 import org.hibernate.mapping.RootClass;
 import org.hibernate.mapping.SingleTableSubclass;
 import org.jboss.tools.hibernate.runtime.common.AbstractPersistentClassFacade;
 import org.jboss.tools.hibernate.runtime.common.IFacade;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
 import org.jboss.tools.hibernate.runtime.spi.IPersistentClass;
+import org.jboss.tools.hibernate.runtime.spi.IProperty;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -68,6 +73,20 @@ public class PersistentClassFacadeTest {
 		persistentClassTarget = new RootClass(null);
 		persistentClassFacade = new AbstractPersistentClassFacade(FACADE_FACTORY, persistentClassTarget) {};
 		assertTrue(persistentClassFacade.isRootClass());
+	}
+	
+	@Test
+	public void testGetIdentifierProperty() throws Exception {
+		Field field = AbstractPersistentClassFacade.class.getDeclaredField("identifierProperty");
+		field.setAccessible(true);
+		assertNull(field.get(persistentClassFacade));
+		Property propertyTarget = new Property();
+		assertNull(persistentClassFacade.getIdentifierProperty());
+		((RootClass)persistentClassTarget).setIdentifierProperty(propertyTarget);
+		IProperty propertyFacade = persistentClassFacade.getIdentifierProperty();
+		assertNotNull(propertyFacade);
+		assertSame(propertyTarget, ((IFacade)propertyFacade).getTarget());
+		assertSame(propertyFacade, field.get(persistentClassFacade));
 	}
 	
 }
