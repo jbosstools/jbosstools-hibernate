682f9b4424b52572e9991d49953aa958bc8ca8cc
Koen Aers <koen.aers@gmail.com>
2021-06-02 10:19:32 +0200

    JBIDE-27351: Set up a runtime plugin for the upcoming Hibernate 5.5.x releases - Implementation of test 'org.jboss.tools.hibernate.runtime.v_5_5.internal.NamingStrategyFacadeTest#testCollectionTableName()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/NamingStrategyFacadeTest.java
@@ -0,0 +1,70 @@
+package org.jboss.tools.hibernate.runtime.v_5_5.internal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.hibernate.cfg.DefaultNamingStrategy;
+import org.hibernate.cfg.NamingStrategy;
+import org.jboss.tools.hibernate.runtime.common.AbstractNamingStrategyFacade;
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.INamingStrategy;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class NamingStrategyFacadeTest {
+
+	private final IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
+	
+	private NamingStrategy namingStrategyTarget = null;
+	private INamingStrategy namingStrategyFacade = null;
+	
+	@BeforeEach
+	public void beforeEach() {
+		namingStrategyTarget = new TestNamingStrategy();
+		namingStrategyFacade = new AbstractNamingStrategyFacade(FACADE_FACTORY, namingStrategyTarget) {};
+	}
+	
+	@Test
+	public void testCollectionTableName() {
+		String tableName = namingStrategyFacade.collectionTableName(
+				"FooEntity", 
+				"FooTable", 
+				"BarEntity", 
+				"BarTable", 
+				"FooBarProperty");
+		assertEquals("FooBarCollectionTableName", tableName);
+	}
+	
+	private class TestNamingStrategy extends DefaultNamingStrategy {
+		private static final long serialVersionUID = 1L;
+		@Override
+		public String collectionTableName(
+				String ownerEntity, 
+				String ownerEntityTable, 
+				String associatedEntity, 
+				String associatedEntityTable,
+				String propertyName) {
+			return "FooBarCollectionTableName";
+		}
+		@Override
+		public String columnName(String columnName) {
+			return "FooBarColumnName";
+		}
+		@Override
+		public String propertyToColumnName(String propertyName) {
+			return "BarFooPropertyColumn";
+		}
+		@Override
+		public String tableName(String tableName) {
+			return "BarFooTable";
+		}
+		@Override
+		public String joinKeyColumnName(String joinedColumn, String joinedTable) {
+			return "FooBarJoinKeyColumnName";
+		}
+		@Override
+		public String classToTableName(String className) {
+			return "FooBarClassTable";
+		}
+	}
+
+}
