5a01abaaeb2844b9ee15c08fb78b9db225ddad47
Koen Aers <koen.aers@gmail.com>
2021-06-03 19:16:35 +0200

    JBIDE-27351: Set up a runtime plugin for the upcoming Hibernate 5.5.x releases - Implementation of test 'org.jboss.tools.hibernate.runtime.v_5_5.internal.PropertyFacadeTest#testGetValue()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/PropertyFacadeTest.java
@@ -0,0 +1,78 @@
+package org.jboss.tools.hibernate.runtime.v_5_5.internal;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import org.hibernate.mapping.Property;
+import org.hibernate.mapping.Value;
+import org.hibernate.type.StringType;
+import org.hibernate.type.Type;
+import org.jboss.tools.hibernate.runtime.common.AbstractPropertyFacade;
+import org.jboss.tools.hibernate.runtime.common.IFacade;
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IProperty;
+import org.jboss.tools.hibernate.runtime.spi.IValue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PropertyFacadeTest {
+
+	private static final IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
+	
+	private static final Type TYPE = new StringType();
+	
+	private Property propertyTarget = null;
+	private IProperty propertyFacade = null;
+	private Boolean valueBoolean = false;
+
+	@BeforeEach
+	public void beforeEach() {
+		propertyTarget = new Property();
+		propertyFacade = new AbstractPropertyFacade(FACADE_FACTORY, propertyTarget) {};
+	}
+	
+	@Test
+	public void testGetValue() throws Exception {
+		Field field = AbstractPropertyFacade.class.getDeclaredField("value");
+		field.setAccessible(true);
+		assertNull(field.get(propertyFacade));
+		assertNull(propertyFacade.getValue());
+		Value valueTarget = createValue();
+		propertyTarget.setValue(valueTarget);
+		IValue valueFacade = propertyFacade.getValue();
+		assertNotNull(valueFacade);
+		assertSame(valueFacade, field.get(propertyFacade));
+		assertSame(valueTarget, ((IFacade)valueFacade).getTarget());
+	}
+	
+	private Value createValue() {
+		return (Value)Proxy.newProxyInstance(
+				getClass().getClassLoader(), 
+				new Class[] { Value.class }, 
+				new InvocationHandler() {	
+					@Override
+					public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+						if ("getType".equals(method.getName())) {
+							return TYPE;
+						}
+						if ("hasAnyInsertableColumns".equals(method.getName())) {
+							return valueBoolean;
+						}
+						if ("hasAnyUpdatableColumns".equals(method.getName())) {
+							return valueBoolean;
+						}
+						if ("isNullable".equals(method.getName())) {
+							return false;
+						}
+						return null;
+					}
+		});
+	}
+	
+}
