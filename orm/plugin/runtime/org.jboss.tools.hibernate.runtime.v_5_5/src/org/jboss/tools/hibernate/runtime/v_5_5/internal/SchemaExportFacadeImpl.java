02ab85958ba718225ae17f5916974ce750f6b1d6
Koen Aers <koen.aers@gmail.com>
2021-06-07 16:02:02 +0200

    JBIDE-27351: Set up a runtime plugin for the upcoming Hibernate 5.5.x releases - Implementation of test 'org.jboss.tools.hibernate.runtime.v_5_5.internal.SchemaExportFacadeTest#testSetConfiguration() and of method ''org.jboss.tools.hibernate.runtime.v_5_5.internal.SchemaExportFacadeImpl#setConfiguration(IConfiguration)'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_5_5/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/SchemaExportFacadeImpl.java
+++ orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_5_5/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/SchemaExportFacadeImpl.java
@@ -1,12 +1,20 @@
 package org.jboss.tools.hibernate.runtime.v_5_5.internal;
 
+import org.hibernate.boot.Metadata;
 import org.jboss.tools.hibernate.runtime.common.AbstractSchemaExportFacade;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IConfiguration;
 
 public class SchemaExportFacadeImpl extends AbstractSchemaExportFacade {
 
+	private Metadata metadata = null;
+
 	public SchemaExportFacadeImpl(IFacadeFactory facadeFactory, Object target) {
 		super(facadeFactory, target);
+	}
+
+	public void setConfiguration(IConfiguration configuration) {
+		metadata = ((ConfigurationFacadeImpl)configuration).getMetadata();
 	}
 
 }
--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/SchemaExportFacadeTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/SchemaExportFacadeTest.java
@@ -1,13 +1,16 @@
 package org.jboss.tools.hibernate.runtime.v_5_5.internal;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
+import java.lang.reflect.Field;
+
+import org.hibernate.boot.Metadata;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.dialect.Dialect;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
-import org.jboss.tools.hibernate.runtime.common.AbstractSchemaExportFacade;
-import org.jboss.tools.hibernate.runtime.common.IFacade;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
-import org.jboss.tools.hibernate.runtime.spi.ISchemaExport;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,20 +18,36 @@ public class SchemaExportFacadeTest {
 
 	private static final IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
 
-	
-	private ISchemaExport schemaExportFacade = null;
+	private SchemaExportFacadeImpl schemaExportFacade = null;
 	private SchemaExport schemaExportTarget = null;
 	
 	@BeforeEach
 	public void before() {
 		schemaExportTarget = new SchemaExport();
-		schemaExportFacade = new AbstractSchemaExportFacade(FACADE_FACTORY, schemaExportTarget) {};
+		schemaExportFacade = new SchemaExportFacadeImpl(FACADE_FACTORY, schemaExportTarget);
 	}
 	
 	@Test
-	public void testCreation() {
+	public void testCreation() throws Exception {
 		assertNotNull(schemaExportFacade);
-		assertSame(((IFacade)schemaExportFacade).getTarget(), schemaExportTarget);
+		Field metadataField = SchemaExportFacadeImpl.class.getDeclaredField("metadata");
+		metadataField.setAccessible(true);
+		assertNull(metadataField.get(schemaExportFacade));
 	}
+	
+	@Test
+	public void testSetConfiguration() throws Exception {
+		Field metadataField = SchemaExportFacadeImpl.class.getDeclaredField("metadata");
+		metadataField.setAccessible(true);
+		Configuration configurationTarget = new Configuration();
+		configurationTarget.setProperty("hibernate.dialect", TestDialect.class.getName());
+		ConfigurationFacadeImpl configuration = new ConfigurationFacadeImpl(FACADE_FACTORY, configurationTarget);
+		Metadata metadata = configuration.getMetadata();
+		assertNull(metadataField.get(schemaExportFacade));
+		schemaExportFacade.setConfiguration(configuration);
+		assertSame(metadata, metadataField.get(schemaExportFacade));
+	}
+	
+	public static class TestDialect extends Dialect {}
 	
 }
