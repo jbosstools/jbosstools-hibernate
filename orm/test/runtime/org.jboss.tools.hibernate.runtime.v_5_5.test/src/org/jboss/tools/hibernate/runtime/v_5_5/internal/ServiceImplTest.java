5827a18699119bb2072346aec3b943b63f88e151
Koen Aers <koen.aers@gmail.com>
2021-06-07 17:34:39 +0200

    JBIDE-27351: Set up a runtime plugin for the upcoming Hibernate 5.5.x releases - Implementation of test 'org.jboss.tools.hibernate.runtime.v_5_5.internal.ServiceImplTest#testNewDefaultConfiguration()' and of method 'org.jboss.tools.hibernate.runtime.v_5_5.internal.ServiceImpl#newDefaultConfiguration()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/ServiceImplTest.java
@@ -0,0 +1,28 @@
+package org.jboss.tools.hibernate.runtime.v_5_5.internal;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.hibernate.cfg.Configuration;
+import org.jboss.tools.hibernate.runtime.common.IFacade;
+import org.jboss.tools.hibernate.runtime.spi.IConfiguration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ServiceImplTest {
+
+	private ServiceImpl service = null;
+	
+	@BeforeEach
+	public void beforeEach() {
+		service = new ServiceImpl();
+	}
+	
+	@Test
+	public void testNewDefaultConfiguration() {
+		IConfiguration defaultConfiguration = service.newDefaultConfiguration();
+		assertNotNull(defaultConfiguration);
+		assertTrue(((IFacade)defaultConfiguration).getTarget() instanceof Configuration);
+	}
+
+}
