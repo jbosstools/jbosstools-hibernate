6c82a5e70b3e65d4233197ce4cfef13661e7cb85
Koen Aers <koen.aers@gmail.com>
2021-06-17 10:11:55 +0200

    JBIDE-27351: Set up a runtime plugin for the upcoming Hibernate 5.5.x releases - Implementation of test 'org.jboss.tools.hibernate.runtime.v_5_5.internal.TableFacadeTest#testAddColumn()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/TableFacadeTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/TableFacadeTest.java
@@ -2,9 +2,12 @@ package org.jboss.tools.hibernate.runtime.v_5_5.internal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
+import org.hibernate.mapping.Column;
 import org.hibernate.mapping.Table;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IColumn;
 import org.jboss.tools.hibernate.runtime.spi.ITable;
 import org.junit.jupiter.api.Test;
 
@@ -19,6 +22,17 @@ public class TableFacadeTest {
 		assertNull(tableFacade.getName());
 		table.setName("foo");
 		assertEquals("foo", tableFacade.getName());
+	}
+	
+	@Test
+	public void testAddColumn() {
+		Table table = new Table();
+		ITable tableFacade = FACADE_FACTORY.createTable(table);
+		Column column = new Column("foo");
+		IColumn columnFacade = FACADE_FACTORY.createColumn(column);
+		assertNull(table.getColumn(column));
+		tableFacade.addColumn(columnFacade);
+		assertSame(column, table.getColumn(column));
 	}
 	
 }
