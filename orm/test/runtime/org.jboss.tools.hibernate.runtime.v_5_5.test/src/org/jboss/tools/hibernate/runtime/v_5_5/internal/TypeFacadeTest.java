f8f1022b742b6ad886cf11079981c1d8a89fc5a7
Koen Aers <koen.aers@gmail.com>
2021-06-18 11:54:01 +0200

    JBIDE-27351: Set up a runtime plugin for the upcoming Hibernate 5.5.x releases - Implementation of test 'org.jboss.tools.hibernate.runtime.v_5_5.internal.TypeFacadeTest#testToString()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/TypeFacadeTest.java
@@ -0,0 +1,29 @@
+package org.jboss.tools.hibernate.runtime.v_5_5.internal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.hibernate.type.ArrayType;
+import org.hibernate.type.ClassType;
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IType;
+import org.junit.jupiter.api.Test;
+
+public class TypeFacadeTest {
+
+	private static IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
+	
+	@Test
+	public void testToString() {
+		IType typeFacade = null;
+		ClassType classType = new ClassType();
+		typeFacade = FACADE_FACTORY.createType(classType);
+		assertEquals(
+				TypeFacadeTest.class.getName(), 
+				typeFacade.toString(TypeFacadeTest.class));
+		ArrayType arrayType = new ArrayType("foo", "bar", String.class);
+		typeFacade = FACADE_FACTORY.createType(arrayType);
+		assertNull(typeFacade.toString(new String[] { "foo", "bar" }));
+	}
+	
+}
