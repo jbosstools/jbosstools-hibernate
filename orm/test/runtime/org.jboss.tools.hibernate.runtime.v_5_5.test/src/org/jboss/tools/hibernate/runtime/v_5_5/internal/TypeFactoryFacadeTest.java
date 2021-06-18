6ea0e78fb269351292caab92321cbb68ac319fd9
Koen Aers <koen.aers@gmail.com>
2021-06-18 13:54:06 +0200

    JBIDE-27351: Set up a runtime plugin for the upcoming Hibernate 5.5.x releases - Implementation of test 'org.jboss.tools.hibernate.runtime.v_5_5.internal.TypeFactoryFacadeTest#testGetBooleanType()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/TypeFactoryFacadeTest.java
@@ -0,0 +1,23 @@
+package org.jboss.tools.hibernate.runtime.v_5_5.internal;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import org.hibernate.type.StandardBasicTypes;
+import org.hibernate.type.Type;
+import org.jboss.tools.hibernate.runtime.common.IFacade;
+import org.jboss.tools.hibernate.runtime.spi.IType;
+import org.jboss.tools.hibernate.runtime.spi.ITypeFactory;
+import org.junit.jupiter.api.Test;
+
+public class TypeFactoryFacadeTest {
+
+	private ITypeFactory typeFactoryFacade = new FacadeFactoryImpl().createTypeFactory();
+	
+	@Test
+	public void testGetBooleanType() {
+		Type typeTarget = StandardBasicTypes.BOOLEAN;
+		IType typeFacade = typeFactoryFacade.getBooleanType();
+		assertSame(typeTarget, ((IFacade)typeFacade).getTarget());
+	}
+	
+}
