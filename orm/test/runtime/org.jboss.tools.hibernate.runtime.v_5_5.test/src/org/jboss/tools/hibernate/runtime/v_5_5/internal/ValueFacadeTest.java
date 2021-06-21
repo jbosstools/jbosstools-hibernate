f90ad579e43ad04dde2429b1c118bb11df25392a
Koen Aers <koen.aers@gmail.com>
2021-06-21 11:39:16 +0200

    JBIDE-27351: Set up a runtime plugin for the upcoming Hibernate 5.5.x releases - Implementation of test 'org.jboss.tools.hibernate.runtime.v_5_5.internal.ValueFacadeTest#testGetCollectionElement()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/ValueFacadeTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_5.test/src/org/jboss/tools/hibernate/runtime/v_5_5/internal/ValueFacadeTest.java
@@ -1,11 +1,15 @@
 package org.jboss.tools.hibernate.runtime.v_5_5.internal;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.hibernate.mapping.Set;
 import org.hibernate.mapping.SimpleValue;
 import org.hibernate.mapping.Value;
+import org.jboss.tools.hibernate.runtime.common.IFacade;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
 import org.jboss.tools.hibernate.runtime.spi.IValue;
 import org.jboss.tools.hibernate.runtime.v_5_5.internal.util.DummyMetadataBuildingContext;
@@ -36,6 +40,20 @@ public class ValueFacadeTest {
 		valueTarget = new Set(DummyMetadataBuildingContext.INSTANCE, null);
 		valueFacade = FACADE_FACTORY.createValue(valueTarget);
 		assertTrue(valueFacade.isCollection());
+	}
+
+	@Test
+	public void testGetCollectionElement() {
+		valueTarget = new SimpleValue(DummyMetadataBuildingContext.INSTANCE, null);
+		valueFacade = FACADE_FACTORY.createValue(valueTarget);
+		IValue collectionElement = valueFacade.getCollectionElement();
+		assertNull(collectionElement);
+		Set set = new Set(DummyMetadataBuildingContext.INSTANCE, null);
+		set.setElement(valueTarget);
+		valueFacade = FACADE_FACTORY.createValue(set);
+		collectionElement = valueFacade.getCollectionElement();
+		assertNotNull(collectionElement);
+		assertSame(valueTarget, ((IFacade)collectionElement).getTarget());
 	}
 
 }
