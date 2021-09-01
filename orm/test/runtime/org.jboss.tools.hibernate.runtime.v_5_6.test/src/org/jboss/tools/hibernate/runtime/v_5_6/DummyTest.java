2033b135588ce2335c04ae1e0d2e54c172b1f769
Koen Aers <koen.aers@gmail.com>
2021-09-01 20:02:39 +0200

    JBIDE-27986: Set up a runtime plugin for the Hibernate 5.6.x releases - Initial creation of runtime test fragment 'org.jboss.tools.hibernate.runtime.v_5_6.test'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_6.test/src/org/jboss/tools/hibernate/runtime/v_5_6/DummyTest.java
@@ -0,0 +1,15 @@
+package org.jboss.tools.hibernate.runtime.v_5_6;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+	
+	@Test
+	public void testAllIsFine() {
+		assertTrue(true);
+	}
+	
+}
