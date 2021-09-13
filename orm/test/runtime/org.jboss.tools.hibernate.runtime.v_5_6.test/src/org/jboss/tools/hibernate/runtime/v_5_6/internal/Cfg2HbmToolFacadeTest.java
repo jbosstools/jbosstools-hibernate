d94a02a023c46428ec2946e21138693b7ba7690e
Koen Aers <koen.aers@gmail.com>
2021-09-13 18:29:10 +0200

    JBIDE-27986: Set up a runtime plugin for the Hibernate 5.6.x releases - Add test class 'org.jboss.tools.hibernate.runtime.v_5_6.internal.Cfg2HbmToolFacadeTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_6.test/src/org/jboss/tools/hibernate/runtime/v_5_6/internal/Cfg2HbmToolFacadeTest.java
@@ -0,0 +1,71 @@
+package org.jboss.tools.hibernate.runtime.v_5_6.internal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import org.hibernate.boot.spi.MetadataBuildingContext;
+import org.hibernate.mapping.PersistentClass;
+import org.hibernate.mapping.Property;
+import org.hibernate.mapping.RootClass;
+import org.hibernate.mapping.SimpleValue;
+import org.hibernate.mapping.Table;
+import org.hibernate.tool.hbm2x.Cfg2HbmTool;
+import org.jboss.tools.hibernate.runtime.common.AbstractCfg2HbmToolFacade;
+import org.jboss.tools.hibernate.runtime.common.AbstractPropertyFacade;
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.ICfg2HbmTool;
+import org.jboss.tools.hibernate.runtime.spi.IPersistentClass;
+import org.jboss.tools.hibernate.runtime.spi.IProperty;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Cfg2HbmToolFacadeTest {
+
+	private static final IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
+
+	private ICfg2HbmTool cfg2HbmToolFacade = null; 
+	private Cfg2HbmTool cfg2HbmTool = null;
+	
+	@BeforeEach
+	public void beforeEach() {
+		cfg2HbmTool = new Cfg2HbmTool();
+		cfg2HbmToolFacade = new AbstractCfg2HbmToolFacade(FACADE_FACTORY, cfg2HbmTool) {};
+	}
+
+	@Test
+	public void testGetTagPersistentClass() {
+		PersistentClass target = new RootClass(null);
+		IPersistentClass persistentClass = FACADE_FACTORY.createPersistentClass(target);
+		assertEquals("class", cfg2HbmToolFacade.getTag(persistentClass));
+	}
+
+	@Test
+	public void testGetTagProperty() throws Exception {
+		RootClass rc = new RootClass(null);
+		Property p = new Property();
+		Table t = new Table();
+		SimpleValue sv = new SimpleValue(createMetadaBuildingContext(), t);
+		sv.setTypeName("foobar");
+		p.setValue(sv);
+		p.setPersistentClass(rc);
+		rc.setVersion(p);
+		IProperty property = new AbstractPropertyFacade(FACADE_FACTORY, p) {};
+		assertEquals("version", cfg2HbmToolFacade.getTag(property));
+	}
+	
+	private MetadataBuildingContext createMetadaBuildingContext() {
+		return (MetadataBuildingContext)Proxy.newProxyInstance(
+				getClass().getClassLoader(), 
+				new Class[] { MetadataBuildingContext.class }, 
+				new InvocationHandler() {			
+					@Override
+					public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+						return null;
+					}
+		});
+	}
+	
+}
