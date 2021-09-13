e9cf04f2e09966eb3ed42955a47af92f6af0ce9e
Koen Aers <koen.aers@gmail.com>
2021-09-13 23:48:20 +0200

    JBIDE-27986: Set up a runtime plugin for the Hibernate 5.6.x releases - Add test class 'org.jboss.tools.hibernate.runtime.v_5_6.internal.CollectionMetadataFacadeTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_6.test/src/org/jboss/tools/hibernate/runtime/v_5_6/internal/CollectionMetadataFacadeTest.java
@@ -0,0 +1,62 @@
+package org.jboss.tools.hibernate.runtime.v_5_6.internal;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import org.hibernate.metadata.CollectionMetadata;
+import org.hibernate.type.Type;
+import org.jboss.tools.hibernate.runtime.common.AbstractCollectionMetadataFacade;
+import org.jboss.tools.hibernate.runtime.common.IFacade;
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.ICollectionMetadata;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CollectionMetadataFacadeTest {
+	
+	private static final IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
+	
+	private CollectionMetadata collectionMetadataTarget = null;
+	private ICollectionMetadata collectionMetadataFacade = null;
+	
+	@BeforeEach
+	public void beforeEach() {
+		collectionMetadataTarget = createCollectionMetadata();
+		collectionMetadataFacade = new AbstractCollectionMetadataFacade(
+				FACADE_FACTORY, collectionMetadataTarget) {};
+	}
+	
+	@Test
+	public void testGetElementType() {
+		assertSame(elementType, ((IFacade)collectionMetadataFacade.getElementType()).getTarget());
+	}
+	
+	private final Type elementType = (Type)Proxy.newProxyInstance(
+			getClass().getClassLoader(), 
+			new Class[] { Type.class },
+			new InvocationHandler() {		
+				@Override
+				public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+					return null;
+				}
+			});
+	
+	private CollectionMetadata createCollectionMetadata() {
+		return (CollectionMetadata)Proxy.newProxyInstance(
+				getClass().getClassLoader(), 
+				new Class[] { CollectionMetadata.class }, 
+				new InvocationHandler() {
+					@Override
+					public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+						if ("getElementType".equals(method.getName())) {
+							
+						}
+						return elementType;
+					}
+				});
+	}
+	
+}
