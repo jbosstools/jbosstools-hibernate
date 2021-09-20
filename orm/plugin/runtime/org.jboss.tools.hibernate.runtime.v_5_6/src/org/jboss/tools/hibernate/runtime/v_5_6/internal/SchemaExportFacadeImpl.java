3d95ffd7ace07ba3bca4940adebc72a6393c9906
Koen Aers <koen.aers@gmail.com>
2021-09-20 15:49:43 +0200

    JBIDE-27986: Set up a runtime plugin for the Hibernate 5.6.x releases - Add test class 'org.jboss.tools.hibernate.runtime.v_5_6.internal.SchemaExportFacadeTest' and implementation 'org.jboss.tools.hibernate.runtime.v_5_6.internal.SchemaExportFacadeImpl'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_5_6/src/org/jboss/tools/hibernate/runtime/v_5_6/internal/SchemaExportFacadeImpl.java
@@ -0,0 +1,29 @@
+package org.jboss.tools.hibernate.runtime.v_5_6.internal;
+
+import java.util.EnumSet;
+
+import org.hibernate.boot.Metadata;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
+import org.hibernate.tool.schema.TargetType;
+import org.jboss.tools.hibernate.runtime.common.AbstractSchemaExportFacade;
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IConfiguration;
+
+public class SchemaExportFacadeImpl extends AbstractSchemaExportFacade {
+
+	private Metadata metadata = null;
+
+	public SchemaExportFacadeImpl(IFacadeFactory facadeFactory, Object target) {
+		super(facadeFactory, target);
+	}
+
+	public void setConfiguration(IConfiguration configuration) {
+		metadata = ((ConfigurationFacadeImpl)configuration).getMetadata();
+	}
+
+	@Override
+	public void create() {
+		((SchemaExport)getTarget()).create(EnumSet.of(TargetType.DATABASE), metadata);
+	}
+
+}
