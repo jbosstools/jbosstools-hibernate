09666c68c1f24defb5d65ed7e63e5379eafe334b
Koen Aers <koen.aers@gmail.com>
2021-10-12 20:54:37 +0200

    JBIDE-28049: Investigate the stack traces during the runtime tests and adapt the configuration where needed - Use 'org.jboss.tools.hibernate.runtime.v_5_4.internal.util.MockConnectionProvider' and 'org.jboss.tools.hibernate.runtime.v_5_4.internal.util.MockDialect' in class 'org.jboss.tools.hibernate.runtime.v_5_4.internal.SessionFacadeTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_4.test/src/org/jboss/tools/hibernate/runtime/v_5_4/internal/SessionFacadeTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_4.test/src/org/jboss/tools/hibernate/runtime/v_5_4/internal/SessionFacadeTest.java
@@ -4,9 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.cfg.Configuration;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
 import org.jboss.tools.hibernate.runtime.spi.ISession;
+import org.jboss.tools.hibernate.runtime.v_5_4.internal.util.MockConnectionProvider;
+import org.jboss.tools.hibernate.runtime.v_5_4.internal.util.MockDialect;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -19,7 +22,8 @@ public class SessionFacadeTest {
 	@BeforeEach
 	public void setUp() {
 		Configuration configuration = new Configuration();
-		configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+		configuration.setProperty(AvailableSettings.DIALECT, MockDialect.class.getName());
+		configuration.setProperty(AvailableSettings.CONNECTION_PROVIDER, MockConnectionProvider.class.getName());
 		SessionFactory sessionFactory = configuration.buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		sessionFacade = new SessionFacadeImpl(FACADE_FACTORY, session);
