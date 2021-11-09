2f96479a16a4b48910863e9cb24497bc239eb158
Koen Aers <koen.aers@gmail.com>
2021-11-09 14:37:04 +0100

    JBIDE-28172: Improve the tests of the 5.4 runtime plugin - Add test class 'org.jboss.tools.hibernate.runtime.v_5_4.internal.SchemaExportFacadeTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_5_4/src/org/jboss/tools/hibernate/runtime/v_5_4/internal/SchemaExportFacadeImpl.java
+++ orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_5_4/src/org/jboss/tools/hibernate/runtime/v_5_4/internal/SchemaExportFacadeImpl.java
@@ -3,33 +3,27 @@ package org.jboss.tools.hibernate.runtime.v_5_4.internal;
 import java.util.EnumSet;
 
 import org.hibernate.boot.Metadata;
-import org.hibernate.cfg.Configuration;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
 import org.hibernate.tool.schema.TargetType;
 import org.jboss.tools.hibernate.runtime.common.AbstractSchemaExportFacade;
-import org.jboss.tools.hibernate.runtime.common.IFacade;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
 import org.jboss.tools.hibernate.runtime.spi.IConfiguration;
-import org.jboss.tools.hibernate.runtime.v_5_4.internal.util.MetadataHelper;
 
 public class SchemaExportFacadeImpl extends AbstractSchemaExportFacade {
 	
-	private SchemaExport target = null;
 	private Metadata metadata = null;
 
 	public SchemaExportFacadeImpl(IFacadeFactory facadeFactory, Object target) {
 		super(facadeFactory, target);
-		this.target = (SchemaExport)target;
 	}
-	
+
 	public void setConfiguration(IConfiguration configuration) {
-		Configuration configurationTarget = (Configuration)((IFacade)configuration).getTarget();
-		this.metadata = MetadataHelper.getMetadata(configurationTarget);
+		metadata = ((ConfigurationFacadeImpl)configuration).getMetadata();
 	}
-	
+
 	@Override
 	public void create() {
-		target.create(EnumSet.of(TargetType.DATABASE), metadata);
+		((SchemaExport)getTarget()).create(EnumSet.of(TargetType.DATABASE), metadata);
 	}
 
 }
---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_4.test/src/org/jboss/tools/hibernate/runtime/v_5_4/internal/SchemaExportFacadeTest.java
@@ -0,0 +1,112 @@
+package org.jboss.tools.hibernate.runtime.v_5_4.internal;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.List;
+
+import org.hibernate.boot.Metadata;
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
+import org.hibernate.tool.schema.TargetType;
+import org.jboss.tools.hibernate.runtime.common.AbstractFacade;
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.v_5_4.internal.util.MockConnectionProvider;
+import org.jboss.tools.hibernate.runtime.v_5_4.internal.util.MockDialect;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SchemaExportFacadeTest {
+
+	private static final IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
+
+	private SchemaExportFacadeImpl schemaExportFacade = null;
+	private SchemaExport schemaExportTarget = null;
+	
+	@BeforeEach
+	public void before() {
+		schemaExportTarget = new SchemaExport();
+		schemaExportFacade = new SchemaExportFacadeImpl(FACADE_FACTORY, schemaExportTarget);
+	}
+	
+	@Test
+	public void testCreation() throws Exception {
+		assertNotNull(schemaExportFacade);
+		Field metadataField = SchemaExportFacadeImpl.class.getDeclaredField("metadata");
+		metadataField.setAccessible(true);
+		assertNull(metadataField.get(schemaExportFacade));
+	}
+	
+	@Test
+	public void testSetConfiguration() throws Exception {
+		Field metadataField = SchemaExportFacadeImpl.class.getDeclaredField("metadata");
+		metadataField.setAccessible(true);
+		Configuration configurationTarget = new Configuration();
+		configurationTarget.setProperty(AvailableSettings.DIALECT, MockDialect.class.getName());
+		configurationTarget.setProperty(AvailableSettings.CONNECTION_PROVIDER, MockConnectionProvider.class.getName());
+		ConfigurationFacadeImpl configuration = new ConfigurationFacadeImpl(FACADE_FACTORY, configurationTarget);
+		Metadata metadata = configuration.getMetadata();
+		assertNull(metadataField.get(schemaExportFacade));
+		schemaExportFacade.setConfiguration(configuration);
+		assertSame(metadata, metadataField.get(schemaExportFacade));
+	}
+	
+	@Test
+	public void testCreate() throws Exception {
+		TestSchemaExport target = new TestSchemaExport();
+		Field targetField = AbstractFacade.class.getDeclaredField("target");
+		targetField.setAccessible(true);
+		targetField.set(schemaExportFacade, target);
+		Field metadataField = SchemaExportFacadeImpl.class.getDeclaredField("metadata");
+		metadataField.setAccessible(true);
+		metadataField.set(schemaExportFacade, createTestMetadata());
+		assertNull(target.metadata);
+		assertNull(target.targetTypes);
+		schemaExportFacade.create();	
+		assertSame(target.metadata, metadataField.get(schemaExportFacade));
+		assertTrue(target.targetTypes.contains(TargetType.DATABASE));
+	}
+	
+	@Test
+	public void testGetExceptions() throws Exception {
+		List<Throwable> exceptions = Collections.emptyList();
+		Field exceptionsField = SchemaExport.class.getDeclaredField("exceptions");
+		exceptionsField.setAccessible(true);
+		assertNotSame(exceptions, schemaExportFacade.getExceptions());
+		exceptionsField.set(schemaExportTarget, exceptions);
+		assertSame(exceptions, schemaExportFacade.getExceptions());
+	}
+	
+	private static class TestSchemaExport extends SchemaExport {
+		Metadata metadata = null;
+		EnumSet<TargetType> targetTypes = null;
+		@Override
+		public void create(EnumSet<TargetType> targetTypes, Metadata metadata) {
+			this.targetTypes = targetTypes;
+			this.metadata = metadata;
+		}
+	}
+	
+	private static Metadata createTestMetadata() {
+		return (Metadata)Proxy.newProxyInstance(
+				SchemaExportFacadeTest.class.getClassLoader(), 
+				new Class[] { Metadata.class },  
+				new InvocationHandler() {					
+					@Override
+					public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+						return null;
+					}
+				});
+	}
+
+}
