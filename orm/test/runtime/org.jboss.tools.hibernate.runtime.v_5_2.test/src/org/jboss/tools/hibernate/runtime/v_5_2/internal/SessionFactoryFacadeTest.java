e4f815efa92de521951ee8b195cf186a4e889b65
Koen Aers <koen.aers@gmail.com>
2021-11-16 20:39:30 +0100

    JBIDE-28182: Improve the tests of the 5.2 runtime plugin - Refactor test class 'org.jboss.tools.hibernate.runtime.v_5_2.internal.SessionFactoryFacadeTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_2.test/src/org/jboss/tools/hibernate/runtime/v_5_2/internal/SessionFactoryFacadeTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_2.test/src/org/jboss/tools/hibernate/runtime/v_5_2/internal/SessionFactoryFacadeTest.java
@@ -1,146 +1,162 @@
 package org.jboss.tools.hibernate.runtime.v_5_2.internal;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.lang.reflect.Field;
+import java.util.HashSet;
 import java.util.Map;
-
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.metamodel.Metamodel;
+import java.util.Set;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.metadata.ClassMetadata;
-import org.hibernate.metadata.CollectionMetadata;
-import org.hibernate.metamodel.spi.MetamodelImplementor;
+import org.hibernate.engine.spi.SessionFactoryDelegatingImpl;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.jboss.tools.hibernate.runtime.common.AbstractSessionFactoryFacade;
 import org.jboss.tools.hibernate.runtime.common.IFacade;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
 import org.jboss.tools.hibernate.runtime.spi.IClassMetadata;
 import org.jboss.tools.hibernate.runtime.spi.ICollectionMetadata;
 import org.jboss.tools.hibernate.runtime.spi.ISession;
 import org.jboss.tools.hibernate.runtime.spi.ISessionFactory;
-import org.jboss.tools.hibernate.runtime.v_5_2.internal.test.Foo;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 public class SessionFactoryFacadeTest {
 
+	private static final String TEST_CFG_XML_STRING =
+			"<hibernate-configuration>" +
+			"  <session-factory>" + 
+			"  </session-factory>" +
+			"</hibernate-configuration>";
+	
+	private static final String TEST_HBM_XML_STRING =
+			"<hibernate-mapping package='org.jboss.tools.hibernate.runtime.v_5_2.internal'>" +
+			"  <class name='SessionFactoryFacadeTest$Foo'>" + 
+			"    <id name='id' access='field' />" +
+			"    <set name='bars' access='field' >" +
+			"      <key column='barId' />" +
+			"      <element column='barVal' type='string' />" +
+			"    </set>" +
+			"  </class>" +
+			"</hibernate-mapping>";
+	
+	static class Foo {
+		public String id;
+		public Set<String> bars = new HashSet<String>();
+	}
+	
 	private static final IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
+
+	@TempDir
+	public File tempDir = new File("tempDir");
+	
+	private SessionFactory sessionFactoryTarget = null;
+	private ISessionFactory sessionFactoryFacade = null;
+	
+	@BeforeEach
+	public void beforeEach() throws Exception {
+		File cfgXmlFile = new File(tempDir, "hibernate.cfg.xml");
+		FileWriter fileWriter = new FileWriter(cfgXmlFile);
+		fileWriter.write(TEST_CFG_XML_STRING);
+		fileWriter.close();
+		File hbmXmlFile = new File(tempDir, "Foo.hbm.xml");
+		fileWriter = new FileWriter(hbmXmlFile);
+		fileWriter.write(TEST_HBM_XML_STRING);
+		fileWriter.close();
+		Configuration configuration = new Configuration();
+		configuration.addFile(hbmXmlFile);
+		configuration.configure(cfgXmlFile);
+		sessionFactoryTarget = new TestSessionFactory(configuration.buildSessionFactory());
+		sessionFactoryFacade = new SessionFactoryFacadeImpl(FACADE_FACTORY, sessionFactoryTarget);
+	}
 	
 	@Test
 	public void testClose() {
-		Configuration configuration = new Configuration();
-		SessionFactory sessionFactory = 
-				configuration.buildSessionFactory(
-						new StandardServiceRegistryBuilder().build());
-		sessionFactory.openSession();
-		ISessionFactory sessionFactoryFacade = 
-				FACADE_FACTORY.createSessionFactory(sessionFactory);
-		assertFalse(sessionFactory.isClosed());
+		assertFalse(sessionFactoryTarget.isClosed());
 		sessionFactoryFacade.close();
-		assertTrue(sessionFactory.isClosed());
+		assertTrue(sessionFactoryTarget.isClosed());
 	}
 	
 	@Test
 	public void testGetAllClassMetadata() throws Exception {
-		Configuration configuration = new Configuration();
-		SessionFactory sessionFactory = 
-				configuration.buildSessionFactory(
-						new StandardServiceRegistryBuilder().build());
-		ISessionFactory sessionFactoryFacade = 
-				FACADE_FACTORY.createSessionFactory(sessionFactory);
-		assertTrue(sessionFactoryFacade.getAllClassMetadata().isEmpty());
-		sessionFactory.close();
-		configuration.addClass(Foo.class);
-		sessionFactory = 
-				configuration.buildSessionFactory(
-						new StandardServiceRegistryBuilder().build());
-		sessionFactoryFacade = 
-				FACADE_FACTORY.createSessionFactory(sessionFactory);
-		Map<String, IClassMetadata> allClassMetaData = 
-				sessionFactoryFacade.getAllClassMetadata();
-		assertNotNull(
-				allClassMetaData.get(
-						"org.jboss.tools.hibernate.runtime.v_5_2.internal.test.Foo"));
+		Field field = AbstractSessionFactoryFacade.class.getDeclaredField("allClassMetadata");
+		field.setAccessible(true);
+		assertNull(field.get(sessionFactoryFacade));
+		Map<String, IClassMetadata> allClassMetadata = sessionFactoryFacade.getAllClassMetadata();
+		assertNotNull(field.get(sessionFactoryFacade));
+		assertEquals(1, allClassMetadata.size());
+		assertNotNull(allClassMetadata.get(Foo.class.getName()));
 	}
 	
 	@Test
-	public void testGetAllCollectionMetadata() {
-		Configuration configuration = new Configuration();
-		SessionFactory sessionFactory = 
-				configuration.buildSessionFactory(
-						new StandardServiceRegistryBuilder().build());
-		ISessionFactory sessionFactoryFacade = 
-				FACADE_FACTORY.createSessionFactory(sessionFactory);
-		assertTrue(sessionFactoryFacade.getAllCollectionMetadata().isEmpty());
-		sessionFactory.close();
-		configuration.addClass(Foo.class);
-		sessionFactory = 
-				configuration.buildSessionFactory(
-						new StandardServiceRegistryBuilder().build());
-		sessionFactoryFacade = 
-				FACADE_FACTORY.createSessionFactory(sessionFactory);
-		Map<String, ICollectionMetadata> allCollectionMetaData = 
-				sessionFactoryFacade.getAllCollectionMetadata();
-		assertNotNull(
-				allCollectionMetaData.get(
-						"org.jboss.tools.hibernate.runtime.v_5_2.internal.test.Foo.bars"));
+	public void testGetAllCollectionMetadata() throws Exception {
+		Field field = AbstractSessionFactoryFacade.class.getDeclaredField("allCollectionMetadata");
+		field.setAccessible(true);
+		assertNull(field.get(sessionFactoryFacade));
+		Map<String, ICollectionMetadata> allCollectionMetadata = sessionFactoryFacade.getAllCollectionMetadata();
+		assertNotNull(field.get(sessionFactoryFacade));
+		assertEquals(1, allCollectionMetadata.size());
+		assertNotNull(allCollectionMetadata.get(Foo.class.getName() + ".bars"));
 	}
 	
 	@Test
-	public void testOpenSession() {
-		Configuration configuration = new Configuration();
-		SessionFactory sessionFactory = 
-				configuration.buildSessionFactory(
-						new StandardServiceRegistryBuilder().build());
-		ISessionFactory sessionFactoryFacade = 
-				FACADE_FACTORY.createSessionFactory(sessionFactory);
+	public void testOpenSession() throws Exception {
+		assertNull(((TestSessionFactory)sessionFactoryTarget).session);
 		ISession sessionFacade = sessionFactoryFacade.openSession();
-		Session session = (Session)((IFacade)sessionFacade).getTarget();
-		assertSame(sessionFactory, session.getSessionFactory());
+		assertNotNull(((TestSessionFactory)sessionFactoryTarget).session);
+		assertSame(((TestSessionFactory)sessionFactoryTarget).session, ((IFacade)sessionFacade).getTarget());
 	}
 	
 	@Test
-	public void testGetClassMetadata() {
-		Configuration configuration = new Configuration();
-		configuration.addClass(Foo.class);
-		SessionFactory sessionFactory = 
-				configuration.buildSessionFactory(
-						new StandardServiceRegistryBuilder().build());
-		Metamodel metamodel = ((EntityManagerFactory)sessionFactory).getMetamodel();	
-		ClassMetadata classMetadata = (ClassMetadata)((MetamodelImplementor)metamodel).entityPersister(Foo.class);
-		ISessionFactory sessionFactoryFacade = 
-				FACADE_FACTORY.createSessionFactory(sessionFactory);
-		assertSame(
-				classMetadata, 
-				((IFacade)sessionFactoryFacade.getClassMetadata(Foo.class)).getTarget());
-		assertSame(
-				classMetadata, 
-				((IFacade)sessionFactoryFacade.getClassMetadata(
-						"org.jboss.tools.hibernate.runtime.v_5_2.internal.test.Foo"))
-					.getTarget());
+	public void testGetClassMetadata() throws Exception {
+		Field field = AbstractSessionFactoryFacade.class.getDeclaredField("allClassMetadata");
+		field.setAccessible(true);
+		assertNull(field.get(sessionFactoryFacade));
+		assertNull(sessionFactoryFacade.getClassMetadata("foo"));
+		assertNotNull(field.get(sessionFactoryFacade));
+		field.set(sessionFactoryFacade, null);
+		assertNotNull(sessionFactoryFacade.getClassMetadata(Foo.class.getName()));
+		assertNotNull(field.get(sessionFactoryFacade));
+		field.set(sessionFactoryFacade, null);
+		assertNull(sessionFactoryFacade.getClassMetadata(Object.class));
+		assertNotNull(field.get(sessionFactoryFacade));
+		field.set(sessionFactoryFacade, null);
+		assertNotNull(sessionFactoryFacade.getClassMetadata(Foo.class));
+		assertNotNull(field.get(sessionFactoryFacade));
 	}
 	
 	@Test
-	public void testGetCollectionMetadata() {
-		Configuration configuration = new Configuration();
-		configuration.addClass(Foo.class);
-		SessionFactory sessionFactory = 
-				configuration.buildSessionFactory(
-						new StandardServiceRegistryBuilder().build());
-		ISessionFactory sessionFactoryFacade = 
-				FACADE_FACTORY.createSessionFactory(sessionFactory);
-		Metamodel metamodel = ((EntityManagerFactory)sessionFactory).getMetamodel();	
-		CollectionMetadata collectionMetadata = (CollectionMetadata)((MetamodelImplementor)metamodel)
-				.collectionPersister("org.jboss.tools.hibernate.runtime.v_5_2.internal.test.Foo.bars");
-		assertSame(
-				collectionMetadata, 
-				((IFacade)sessionFactoryFacade.getCollectionMetadata(
-						"org.jboss.tools.hibernate.runtime.v_5_2.internal.test.Foo.bars"))
-					.getTarget());
+	public void testGetCollectionMetadata() throws Exception {
+		Field field = AbstractSessionFactoryFacade.class.getDeclaredField("allCollectionMetadata");
+		field.setAccessible(true);
+		assertNull(field.get(sessionFactoryFacade));
+		assertNull(sessionFactoryFacade.getCollectionMetadata("bars"));
+		assertNotNull(field.get(sessionFactoryFacade));
+		field.set(sessionFactoryFacade, null);
+		assertNotNull(sessionFactoryFacade.getCollectionMetadata(Foo.class.getName() + ".bars"));
+		assertNotNull(field.get(sessionFactoryFacade));
+	}	
+	
+	private class TestSessionFactory extends SessionFactoryDelegatingImpl {
+		private static final long serialVersionUID = 1L;
+		Session session = null;
+		public TestSessionFactory(SessionFactory delegate) {
+			super((SessionFactoryImplementor)delegate);
+		}
+		@Override
+		public Session openSession() {
+			this.session = super.openSession();
+			return this.session;
+		}
 	}
 	
 }
