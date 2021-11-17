093e64bb73dc4136f4c7b96dfd3af75420deb578
Koen Aers <koen.aers@gmail.com>
2021-11-17 11:14:25 +0100

    JBIDE-28182: Improve the tests of the 5.2 runtime plugin - Add test class 'org.jboss.tools.hibernate.runtime.v_5_2.internal.util.DummyMetadataImplementorTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_2.test/src/org/jboss/tools/hibernate/runtime/v_5_2/internal/util/DummyMetadataImplementorTest.java
@@ -0,0 +1,23 @@
+package org.jboss.tools.hibernate.runtime.v_5_2.internal.util;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.dialect.Dialect;
+import org.hibernate.engine.jdbc.spi.JdbcServices;
+import org.junit.jupiter.api.Test;
+
+public class DummyMetadataImplementorTest {
+	
+	@Test
+	public void testInstance() {
+		assertNotNull(DummyMetadataImplementor.INSTANCE);
+		StandardServiceRegistry serviceRegistry = DummyMetadataImplementor.INSTANCE
+				.getMetadataBuildingOptions().getServiceRegistry();
+		JdbcServices jdbcServices = serviceRegistry.getService(JdbcServices.class);
+		Dialect dialect = jdbcServices.getDialect();
+		assertTrue(dialect instanceof DummyMetadataImplementor.DummyDialect);
+	}
+
+}
