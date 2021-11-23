b932f21a059b7567201ff3c5d515ee5523c5afc9
Koen Aers <koen.aers@gmail.com>
2021-11-23 15:42:20 +0200

    JBIDE-28190: Improve the tests of the 5.1 runtime plugin - Refactor test class 'org.jboss.tools.hibernate.runtime.v_5_1.internal.EntityMetamodelFacadeTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_1.test/src/org/jboss/tools/hibernate/runtime/v_5_1/internal/EntityMetamodelFacadeTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_1.test/src/org/jboss/tools/hibernate/runtime/v_5_1/internal/EntityMetamodelFacadeTest.java
@@ -1,29 +1,43 @@
 package org.jboss.tools.hibernate.runtime.v_5_1.internal;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.ArrayList;
 
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.internal.ClassLoaderAccessImpl;
+import org.hibernate.boot.internal.InFlightMetadataCollectorImpl;
+import org.hibernate.boot.internal.MetadataBuilderImpl.MetadataBuildingOptionsImpl;
+import org.hibernate.boot.internal.MetadataBuildingContextRootImpl;
+import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.boot.registry.classloading.spi.ClassLoaderService;
+import org.hibernate.boot.spi.ClassLoaderAccess;
+import org.hibernate.boot.spi.InFlightMetadataCollector;
+import org.hibernate.boot.spi.MetadataBuildingContext;
 import org.hibernate.boot.spi.MetadataImplementor;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.dialect.Dialect;
+import org.hibernate.engine.OptimisticLockStyle;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.mapping.Column;
+import org.hibernate.mapping.PersistentClass;
 import org.hibernate.mapping.RootClass;
 import org.hibernate.mapping.SimpleValue;
 import org.hibernate.mapping.Table;
-import org.hibernate.service.ServiceRegistry;
-import org.hibernate.tool.util.MetadataHelper;
 import org.hibernate.tuple.entity.EntityMetamodel;
 import org.hibernate.tuple.entity.EntityTuplizer;
+import org.hibernate.type.TypeResolver;
 import org.jboss.tools.hibernate.runtime.common.AbstractEntityMetamodelFacade;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
 import org.jboss.tools.hibernate.runtime.spi.IEntityMetamodel;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class EntityMetamodelFacadeTest {
 
@@ -33,20 +47,34 @@ public class EntityMetamodelFacadeTest {
 	
 	private IEntityMetamodel entityMetamodelFacade = null; 
 	private EntityMetamodel entityMetamodel = null;
-	
+
 	private String methodName = null;
 	private Object[] arguments = null;
 	
-	@SuppressWarnings("serial")
-	@Before
-	public void setUp() throws Exception {
-		Configuration configuration = new Configuration();
-		configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
-		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder();
-		builder.applySettings(configuration.getProperties());
-		ServiceRegistry serviceRegistry = builder.build();		
-		SessionFactoryImplementor sfi = (SessionFactoryImplementor)configuration.buildSessionFactory(serviceRegistry);
-		RootClass rc = new RootClass(null);
+	@BeforeEach
+	public void beforeEach() throws Exception {
+		entityMetamodel = createFooBarModel();
+		entityMetamodelFacade = new AbstractEntityMetamodelFacade(FACADE_FACTORY, entityMetamodel) {};
+	}
+	
+	@Test
+	public void testGetTuplizerPropertyValue() {
+		assertSame(OBJECT, entityMetamodelFacade.getTuplizerPropertyValue(OBJECT, Integer.MAX_VALUE));
+		assertEquals("getPropertyValue", methodName);
+		assertArrayEquals(new Object[] { OBJECT,  Integer.MAX_VALUE }, arguments);
+	}
+	
+	@Test
+	public void testGetPropertyIndexOrNull() {
+		assertSame(INDEX, entityMetamodelFacade.getPropertyIndexOrNull("foobar"));
+		assertEquals("getPropertyIndexOrNull", methodName);
+		assertArrayEquals(arguments, new Object[] { "foobar" });
+	}
+	
+	private PersistentClass createPersistentClass(
+			MetadataBuildingContext metadataBuildingContext,
+			MetadataImplementor metadataImplementor) {
+		RootClass rc = new RootClass(metadataBuildingContext);
 		Table t = new Table("foobar");
 		rc.setTable(t);
 		Column c = new Column("foo");
@@ -54,14 +82,46 @@ public class EntityMetamodelFacadeTest {
 		ArrayList<Column> keyList = new ArrayList<>();
 		keyList.add(c);
 		t.createUniqueKey(keyList);
-		MetadataImplementor m = (MetadataImplementor)MetadataHelper.getMetadata(configuration);
-		SimpleValue sv = new SimpleValue(m);
+		SimpleValue sv = new SimpleValue(metadataImplementor, t);
 		sv.setNullValue("null");
 		sv.setTypeName(Integer.class.getName());
 		sv.addColumn(c);
 		rc.setEntityName("foobar");
 		rc.setIdentifier(sv);
-		entityMetamodel = new EntityMetamodel(rc, null, sfi) {
+		rc.setClassName(FooBar.class.getName());
+		rc.setOptimisticLockStyle(OptimisticLockStyle.NONE);
+		return rc;
+	}
+	
+	private EntityMetamodel createFooBarModel() {
+		StandardServiceRegistryBuilder ssrb = new StandardServiceRegistryBuilder();
+		ssrb.applySetting(AvailableSettings.DIALECT, TestDialect.class.getName());
+		StandardServiceRegistry serviceRegistry = ssrb.build();		
+		MetadataBuildingOptionsImpl metadataBuildingOptions = 
+				new MetadataBuildingOptionsImpl(serviceRegistry);	
+		InFlightMetadataCollector inFlightMetadataCollector = 
+				new InFlightMetadataCollectorImpl(
+						metadataBuildingOptions, new TypeResolver());
+		ClassLoaderService classLoaderService = metadataBuildingOptions.getServiceRegistry().getService( ClassLoaderService.class );
+		ClassLoaderAccess classLoaderAccess = new ClassLoaderAccessImpl(
+				metadataBuildingOptions.getTempClassLoader(),
+				classLoaderService
+		);
+		MetadataBuildingContext metadataBuildingContext = 
+				new MetadataBuildingContextRootImpl(
+						metadataBuildingOptions, 
+						classLoaderAccess,
+						inFlightMetadataCollector);
+		MetadataSources metadataSources = new MetadataSources(serviceRegistry);
+		PersistentClass persistentClass = createPersistentClass(
+				metadataBuildingContext, 
+				(MetadataImplementor)metadataSources.buildMetadata());
+		SessionFactoryImplementor sessionFactoryImplementor = 
+				(SessionFactoryImplementor)metadataSources
+					.buildMetadata()
+					.buildSessionFactory();
+		return  new EntityMetamodel(persistentClass, null, sessionFactoryImplementor) {
+			private static final long serialVersionUID = 1L;
 			@Override public EntityTuplizer getTuplizer() {
 				return (EntityTuplizer)Proxy.newProxyInstance(
 						FACADE_FACTORY.getClassLoader(), 
@@ -73,24 +133,14 @@ public class EntityMetamodelFacadeTest {
 				arguments = new Object[] { id };
 				return INDEX;
 			}
-			
 		};
-		entityMetamodelFacade = new AbstractEntityMetamodelFacade(FACADE_FACTORY, entityMetamodel) {};
+	}
+		
+	public class FooBar {
+		public int id = 1967;
 	}
 	
-	@Test
-	public void testGetTuplizerPropertyValue() {
-		Assert.assertSame(OBJECT, entityMetamodelFacade.getTuplizerPropertyValue(OBJECT, Integer.MAX_VALUE));
-		Assert.assertEquals("getPropertyValue", methodName);
-		Assert.assertArrayEquals(new Object[] { OBJECT,  Integer.MAX_VALUE }, arguments);
-	}
-	
-	@Test
-	public void testGetPropertyIndexOrNull() {
-		Assert.assertSame(INDEX, entityMetamodelFacade.getPropertyIndexOrNull("foobar"));
-		Assert.assertEquals("getPropertyIndexOrNull", methodName);
-		Assert.assertArrayEquals(arguments, new Object[] { "foobar" });
-	}
+	public static class TestDialect extends Dialect {}
 	
 	private class TestInvocationHandler implements InvocationHandler {
 		@Override
@@ -100,5 +150,4 @@ public class EntityMetamodelFacadeTest {
 			return OBJECT;
 		}
 	}
-	
 }
