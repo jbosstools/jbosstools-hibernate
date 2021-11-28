1ac373cbbf41a3425d0b9bd2af030148ce2b2f0b
Koen Aers <koen.aers@gmail.com>
2021-11-28 16:30:42 +0100

    JBIDE-28190: Improve the tests of the 5.1 runtime plugin - Refactor test class 'org.jboss.tools.hibernate.runtime.v_5_1.internal.TableFilterFacadeTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_1.test/src/org/jboss/tools/hibernate/runtime/v_5_1/internal/TableFilterFacadeTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_1.test/src/org/jboss/tools/hibernate/runtime/v_5_1/internal/TableFilterFacadeTest.java
@@ -1,10 +1,14 @@
 package org.jboss.tools.hibernate.runtime.v_5_1.internal;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.hibernate.cfg.reveng.TableFilter;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
 import org.jboss.tools.hibernate.runtime.spi.ITableFilter;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class TableFilterFacadeTest {
 
@@ -14,72 +18,72 @@ public class TableFilterFacadeTest {
 	public void testSetExclude() {
 		TableFilter tableFilter = new TableFilter();
 		ITableFilter tableFilterFacade = FACADE_FACTORY.createTableFilter(tableFilter);
-		Assert.assertNull(tableFilter.getExclude());
+		assertNull(tableFilter.getExclude());
 		tableFilterFacade.setExclude(true);
-		Assert.assertTrue(tableFilter.getExclude());
+		assertTrue(tableFilter.getExclude());
 	}
 	
 	@Test
 	public void testSetMatchCatalog() {
 		TableFilter tableFilter = new TableFilter();
 		ITableFilter tableFilterFacade = FACADE_FACTORY.createTableFilter(tableFilter);
-		Assert.assertNotEquals("foo", tableFilter.getMatchCatalog());
+		assertNotEquals("foo", tableFilter.getMatchCatalog());
 		tableFilterFacade.setMatchCatalog("foo");
-		Assert.assertEquals("foo", tableFilter.getMatchCatalog());
+		assertEquals("foo", tableFilter.getMatchCatalog());
 	}
 		
 	@Test
 	public void testSetMatchSchema() {
 		TableFilter tableFilter = new TableFilter();
 		ITableFilter tableFilterFacade = FACADE_FACTORY.createTableFilter(tableFilter);
-		Assert.assertNotEquals("foo", tableFilter.getMatchSchema());
+		assertNotEquals("foo", tableFilter.getMatchSchema());
 		tableFilterFacade.setMatchSchema("foo");
-		Assert.assertEquals("foo", tableFilter.getMatchSchema());
+		assertEquals("foo", tableFilter.getMatchSchema());
 	}
 		
 	@Test
 	public void testSetMatchName() {
 		TableFilter tableFilter = new TableFilter();
 		ITableFilter tableFilterFacade = FACADE_FACTORY.createTableFilter(tableFilter);
-		Assert.assertNotEquals("foo", tableFilter.getMatchName());
+		assertNotEquals("foo", tableFilter.getMatchName());
 		tableFilterFacade.setMatchName("foo");
-		Assert.assertEquals("foo", tableFilter.getMatchName());
+		assertEquals("foo", tableFilter.getMatchName());
 	}
 		
 	@Test
 	public void testGetExclude() {
 		TableFilter tableFilter = new TableFilter();
 		ITableFilter tableFilterFacade = FACADE_FACTORY.createTableFilter(tableFilter);
-		Assert.assertNull(tableFilterFacade.getExclude());
+		assertNull(tableFilterFacade.getExclude());
 		tableFilter.setExclude(true);
-		Assert.assertTrue(tableFilterFacade.getExclude());
+		assertTrue(tableFilterFacade.getExclude());
 	}
 		
 	@Test
 	public void testGetMatchCatalog() {
 		TableFilter tableFilter = new TableFilter();
 		ITableFilter tableFilterFacade = FACADE_FACTORY.createTableFilter(tableFilter);
-		Assert.assertNotEquals("foo", tableFilterFacade.getMatchCatalog());
+		assertNotEquals("foo", tableFilterFacade.getMatchCatalog());
 		tableFilter.setMatchCatalog("foo");
-		Assert.assertEquals("foo", tableFilterFacade.getMatchCatalog());
+		assertEquals("foo", tableFilterFacade.getMatchCatalog());
 	}
 		
 	@Test
 	public void testGetMatchSchema() {
 		TableFilter tableFilter = new TableFilter();
 		ITableFilter tableFilterFacade = FACADE_FACTORY.createTableFilter(tableFilter);
-		Assert.assertNotEquals("foo", tableFilterFacade.getMatchSchema());
+		assertNotEquals("foo", tableFilterFacade.getMatchSchema());
 		tableFilter.setMatchSchema("foo");
-		Assert.assertEquals("foo", tableFilterFacade.getMatchSchema());
+		assertEquals("foo", tableFilterFacade.getMatchSchema());
 	}
 		
 	@Test
 	public void testGetMatchName() {
 		TableFilter tableFilter = new TableFilter();
 		ITableFilter tableFilterFacade = FACADE_FACTORY.createTableFilter(tableFilter);
-		Assert.assertNotEquals("foo", tableFilterFacade.getMatchName());
+		assertNotEquals("foo", tableFilterFacade.getMatchName());
 		tableFilter.setMatchName("foo");
-		Assert.assertEquals("foo", tableFilterFacade.getMatchName());
+		assertEquals("foo", tableFilterFacade.getMatchName());
 	}
 		
 }
