b7c9dd49474a7af0be3cb6c9ca3db0667621fbb2
Koen Aers <koen.aers@gmail.com>
2021-12-06 18:54:42 +0100

    JBIDE-28200: Improve the tests of the 5.0 runtime plugin - Add test class 'org.jboss.tools.hibernate.runtime.v_5_0.internal.JoinFacadeTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_0.test/src/org/jboss/tools/hibernate/runtime/v_5_0/internal/JoinFacadeTest.java
@@ -0,0 +1,44 @@
+package org.jboss.tools.hibernate.runtime.v_5_0.internal;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Iterator;
+
+import org.hibernate.mapping.Join;
+import org.hibernate.mapping.Property;
+import org.jboss.tools.hibernate.runtime.common.AbstractJoinFacade;
+import org.jboss.tools.hibernate.runtime.common.IFacade;
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IJoin;
+import org.jboss.tools.hibernate.runtime.spi.IProperty;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class JoinFacadeTest {
+
+	private final IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
+	
+	private IJoin joinFacade = null;
+	private Join joinTarget = null;
+	
+	@BeforeEach
+	public void beforeEach() {
+		joinTarget = new Join();
+	}
+	
+	@Test
+	public void testGetPropertyIterator() {
+		joinFacade = new AbstractJoinFacade(FACADE_FACTORY, joinTarget) {};
+		Iterator<IProperty> propertyIterator = joinFacade.getPropertyIterator();
+		assertFalse(propertyIterator.hasNext());
+		Property property = new Property();
+		joinTarget.addProperty(property);
+		joinFacade = new AbstractJoinFacade(FACADE_FACTORY, joinTarget) {};
+		propertyIterator = joinFacade.getPropertyIterator();
+		assertTrue(propertyIterator.hasNext());
+		assertSame(property, (((IFacade)propertyIterator.next()).getTarget()));
+	}
+	
+}
