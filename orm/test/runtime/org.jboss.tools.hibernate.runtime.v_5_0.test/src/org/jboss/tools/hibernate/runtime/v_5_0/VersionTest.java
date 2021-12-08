54da1bf1bc1d94568ac0518cb35d7e0d1ee95439
Koen Aers <koen.aers@gmail.com>
2021-12-08 10:00:01 +0100

    JBIDE-28200: Improve the tests of the 5.0 runtime plugin - Refactor test class 'org.jboss.tools.hibernate.runtime.v_5_0.VersionTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_0.test/src/org/jboss/tools/hibernate/runtime/v_5_0/VersionTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_0.test/src/org/jboss/tools/hibernate/runtime/v_5_0/VersionTest.java
@@ -1,18 +1,19 @@
 package org.jboss.tools.hibernate.runtime.v_5_0;
 
-import org.junit.Assert;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class VersionTest {
 	
 	@Test
 	public void testToolsVersion() {
-		Assert.assertEquals("5.0.6.Final", org.hibernate.tool.Version.VERSION);
+		assertEquals("5.0.6.Final", org.hibernate.tool.Version.VERSION);
 	}
-	
+
 	@Test
 	public void testCoreVersion() {
-		Assert.assertEquals("5.0.12.Final", org.hibernate.Version.getVersionString());
+		assertEquals("5.0.12.Final", org.hibernate.Version.getVersionString());
 	}
 
 }
