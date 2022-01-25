c6d0fc739f60089d50bf09c5c7ddeb0bef9afe38
Koen Aers <koen.aers@gmail.com>
2022-01-25 12:11:23 +0100

    JBIDE-28262: Update hibernate tools dependency of org.jboss.tools.hibernate.runtime.v_6_0 to version 6.0.0.Beta2 - Create and use class 'org.jboss.tools.hibernate.runtime.v_6_0.internal.legacy.CalendarDateType' instead of 'org.hibernate.type.CalendarDateType' which is to be removed
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/TypeFactoryFacadeTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_0.test/src/org/jboss/tools/hibernate/runtime/v_6_0/internal/TypeFactoryFacadeTest.java
@@ -10,7 +10,6 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.TimeZone;
 
-import org.hibernate.type.CalendarDateType;
 import org.hibernate.type.CalendarType;
 import org.hibernate.type.CharacterType;
 import org.hibernate.type.ClassType;
@@ -40,6 +39,7 @@ import org.jboss.tools.hibernate.runtime.v_6_0.internal.legacy.BigDecimalType;
 import org.jboss.tools.hibernate.runtime.v_6_0.internal.legacy.BigIntegerType;
 import org.jboss.tools.hibernate.runtime.v_6_0.internal.legacy.BooleanType;
 import org.jboss.tools.hibernate.runtime.v_6_0.internal.legacy.ByteType;
+import org.jboss.tools.hibernate.runtime.v_6_0.internal.legacy.CalendarDateType;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
