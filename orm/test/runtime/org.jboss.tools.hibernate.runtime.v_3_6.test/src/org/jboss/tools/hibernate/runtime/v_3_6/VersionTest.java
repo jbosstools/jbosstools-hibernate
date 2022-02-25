7d12daa603c91ff24b89f385084012c26030c862
Koen Aers <koen.aers@gmail.com>
2022-02-25 14:03:53 +0100

    JBIDE-28339: Improve the tests of the 3.6 runtime plugin - Add test class 'org.jboss.tools.hibernate.runtime.v_3_6.VersionTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_3_6.test/src/org/jboss/tools/hibernate/runtime/v_3_6/VersionTest.java
@@ -0,0 +1,19 @@
+package org.jboss.tools.hibernate.runtime.v_3_6;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class VersionTest {
+	
+	@Test
+	public void testToolsVersion() {
+		assertEquals("3.6.0.Final", org.hibernate.tool.Version.VERSION);
+	}
+
+	@Test
+	public void testCoreVersion() {
+		assertEquals("3.6.10.Final", org.hibernate.Version.getVersionString());
+	}
+
+}
