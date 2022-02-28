84ba13e91b359eb9e1e0fb4bd3cea7f9c64de756
Koen Aers <koen.aers@gmail.com>
2022-02-28 13:21:29 +0100

    JBIDE-28339: Improve the tests of the 3.6 runtime plugin - Add test class 'org.jboss.tools.hibernate.runtime.v_3_6.internal.ReverseEngineeringStrategyFacadeTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_3_6.test/src/org/jboss/tools/hibernate/runtime/v_3_6/internal/ReverseEngineeringStrategyFacadeTest.java
@@ -0,0 +1,36 @@
+package org.jboss.tools.hibernate.runtime.v_3_6.internal;
+
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import java.lang.reflect.Field;
+
+import org.hibernate.cfg.reveng.DefaultReverseEngineeringStrategy;
+import org.hibernate.cfg.reveng.ReverseEngineeringSettings;
+import org.hibernate.cfg.reveng.ReverseEngineeringStrategy;
+import org.jboss.tools.hibernate.runtime.common.AbstractReverseEngineeringStrategyFacade;
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IReverseEngineeringSettings;
+import org.jboss.tools.hibernate.runtime.spi.IReverseEngineeringStrategy;
+import org.junit.jupiter.api.Test;
+
+public class ReverseEngineeringStrategyFacadeTest {
+
+	private static IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
+	
+	@Test
+	public void testSetSettings() throws Exception {
+		ReverseEngineeringStrategy revengStrategyTarget = new DefaultReverseEngineeringStrategy();
+		ReverseEngineeringSettings revengSettingsTarget = new ReverseEngineeringSettings(revengStrategyTarget);
+		IReverseEngineeringSettings revengSettingsFacade = 
+				FACADE_FACTORY.createReverseEngineeringSettings(revengSettingsTarget);
+		IReverseEngineeringStrategy revengStrategyFacade = 
+				new AbstractReverseEngineeringStrategyFacade(FACADE_FACTORY, revengStrategyTarget) {};
+		Field field = DefaultReverseEngineeringStrategy.class.getDeclaredField("settings");
+		field.setAccessible(true);
+		assertNotSame(field.get(revengStrategyTarget), revengSettingsTarget);
+		revengStrategyFacade.setSettings(revengSettingsFacade);
+		assertSame(field.get(revengStrategyTarget), revengSettingsTarget);
+	}
+	
+}
