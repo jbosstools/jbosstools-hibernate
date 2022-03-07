4af8cc1b85f9f903f456a3fe013143b77316f00f
Koen Aers <koen.aers@gmail.com>
2022-03-07 18:58:13 +0100

    JBIDE-28344: Improve the tests of the 3.5 runtime plugin - Refactor test class 'org.jboss.tools.hibernate.runtime.v_3_5.internal.SchemaExportFacadeTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_3_5.test/src/org/jboss/tools/hibernate/runtime/v_3_5/internal/SchemaExportFacadeTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_3_5.test/src/org/jboss/tools/hibernate/runtime/v_3_5/internal/SchemaExportFacadeTest.java
@@ -1,21 +1,25 @@
 package org.jboss.tools.hibernate.runtime.v_3_5.internal;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.io.File;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+import org.h2.Driver;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
+import org.hibernate.dialect.H2Dialect;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.HibernateException;
 import org.jboss.tools.hibernate.runtime.spi.ISchemaExport;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 public class SchemaExportFacadeTest {
 
@@ -24,7 +28,7 @@ public class SchemaExportFacadeTest {
 			"		'-//Hibernate/Hibernate Mapping DTD 3.0//EN'" +
 			"		'http://www.hibernate.org/dtd/hibernate-mapping-3.0.dtd'>" +
 			"<hibernate-mapping package='org.jboss.tools.hibernate.runtime.v_3_5.internal'>" +
-			"  <class name='ConfigurationFacadeTest$Foo' table='FOO'>" + 
+			"  <class name='SchemaExportFacadeTest$Foo' table='FOO'>" + 
 			"    <id name='fooId' column='ID'/>" +
 			"  </class>" +
 			"</hibernate-mapping>";
@@ -35,16 +39,20 @@ public class SchemaExportFacadeTest {
 	
 	private static final IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
 	
-	@Rule
-	public TemporaryFolder temporaryFolder = new TemporaryFolder();
+	@TempDir
+	public File output = new File("output");
 	
 	private File fooFile;
 	private Configuration configuration;
 	
-	@Before
+	@BeforeAll
+	public static void beforeAll() throws Exception {
+		DriverManager.registerDriver(new Driver());		
+	}
+
+	@BeforeEach
 	public void before() throws Exception {
-		File folder = temporaryFolder.getRoot();
-		fooFile = new File(folder, "foo.hbm.xml");
+		fooFile = new File(output, "foo.hbm.xml");
 		PrintWriter fooWriter = new PrintWriter(fooFile);
 		fooWriter.write(FOO_HBM_XML_STRING);
 		fooWriter.close();
@@ -52,30 +60,31 @@ public class SchemaExportFacadeTest {
 		configuration.addFile(fooFile);
 	}
 	
-	@Ignore
 	@Test
 	public void testCreate() throws Exception {
 		String urlString = "jdbc:h2:mem:create_test";
 		Connection connection = DriverManager.getConnection(urlString);
 		configuration.setProperty(Environment.URL, urlString);
+		configuration.setProperty(Environment.DIALECT, H2Dialect.class.getName());
 		ISchemaExport schemaExportFacade = FACADE_FACTORY
 				.createSchemaExport(new SchemaExport(configuration));
-		Assert.assertFalse(connection.getMetaData().getTables(null, null, "FOO", null).next());
+		assertFalse(connection.getMetaData().getTables(null, null, "FOO", null).next());
 		schemaExportFacade.create();
-		Assert.assertTrue(connection.getMetaData().getTables(null, null, "FOO", null).next());
+		assertTrue(connection.getMetaData().getTables(null, null, "FOO", null).next());
 	}
 	
-	@Ignore
+	@SuppressWarnings("unchecked")
 	@Test
 	public void testGetExceptions() throws Throwable {
-		String urlString = "jdbc:h2:tcp://localhost/~/some_weird_unexistant_location";
+		String urlString = "jdbc:h2:mem:create_test";
 		configuration.setProperty(Environment.URL, urlString);
+		configuration.setProperty(Environment.DIALECT, H2Dialect.class.getName());
 		SchemaExport schemaExport = new SchemaExport(configuration);
 		ISchemaExport schemaExportFacade = FACADE_FACTORY
 				.createSchemaExport(schemaExport);
-		Assert.assertTrue(schemaExportFacade.getExceptions().isEmpty());
-		schemaExport.create(false, true);
-		Assert.assertFalse(schemaExportFacade.getExceptions().isEmpty());
+		assertTrue(schemaExportFacade.getExceptions().isEmpty());
+		schemaExport.getExceptions().add(new HibernateException("blah"));
+		assertFalse(schemaExportFacade.getExceptions().isEmpty());
 	}
 
 }
