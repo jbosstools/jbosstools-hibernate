00094065260cd0d96d84f161ae6596529e382714
Koen Aers <koen.aers@gmail.com>
2022-03-07 16:18:30 +0100

    JBIDE-28344: Improve the tests of the 3.5 runtime plugin - Add new implementation class 'org.jboss.tools.hibernate.runtime.v_3_5.internal.PersistentClassFacadeImpl'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_3_5/src/org/jboss/tools/hibernate/runtime/v_3_5/internal/PersistentClassFacadeImpl.java
@@ -0,0 +1,24 @@
+package org.jboss.tools.hibernate.runtime.v_3_5.internal;
+
+import org.jboss.tools.hibernate.runtime.common.AbstractPersistentClassFacade;
+import org.jboss.tools.hibernate.runtime.common.IFacade;
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.common.Util;
+import org.jboss.tools.hibernate.runtime.spi.IValue;
+
+public class PersistentClassFacadeImpl extends AbstractPersistentClassFacade {
+
+	public PersistentClassFacadeImpl(IFacadeFactory facadeFactory, Object target) {
+		super(facadeFactory, target);
+	}
+	
+	@Override
+	public void setKey(IValue keyValueFacade) {
+		Util.invokeMethod(
+				getTarget(), 
+				"setIdentifier", 
+				new Class[] { getKeyValueClass() }, 
+				new Object[] { ((IFacade)keyValueFacade).getTarget() });
+	}
+
+}
