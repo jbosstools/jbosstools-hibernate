3f94bbe62612dd3f43fed716f313bee24f970968
Koen Aers <koen.aers@gmail.com>
2022-07-11 20:08:01 +0200

    JBIDE-28549: Create a Runtime Plugin for Hibernate 6.1 - Add test class 'org.jboss.tools.hibernate.runtime.v_6_1.internal.util.MockDialectTest' and implement class 'org.jboss.tools.hibernate.runtime.v_6_1.internal.util.MockDialect'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_1.test/src/org/jboss/tools/hibernate/runtime/v_6_1/internal/util/MockDialectTest.java
@@ -0,0 +1,17 @@
+package org.jboss.tools.hibernate.runtime.v_6_1.internal.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.hibernate.dialect.DatabaseVersion;
+import org.junit.jupiter.api.Test;
+
+public class MockDialectTest {
+	
+	@Test
+	public void testGetVersion() {
+		DatabaseVersion version = new MockDialect().getVersion();
+		assertEquals(Integer.MAX_VALUE, version.getDatabaseMajorVersion());
+		assertEquals(Integer.MIN_VALUE, version.getDatabaseMinorVersion());
+	}
+
+}
