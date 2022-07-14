8779ee7b2667e535841941ef3ff7d5cd0a9b23f0
Koen Aers <koen.aers@gmail.com>
2022-07-14 16:19:53 +0200

    JBIDE-28549: Create a Runtime Plugin for Hibernate 6.1 - Add test class 'org.jboss.tools.hibernate.runtime.v_6_1.internal.legacy.BooleanTypeTest' and implementation class 'org.jboss.tools.hibernate.runtime.v_6_1.internal.legacy.BooleanType'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_6_1/src/org/jboss/tools/hibernate/runtime/v_6_1/internal/legacy/BooleanType.java
@@ -0,0 +1,38 @@
+package org.jboss.tools.hibernate.runtime.v_6_1.internal.legacy;
+
+import org.hibernate.Incubating;
+import org.hibernate.type.AbstractSingleColumnStandardBasicType;
+import org.hibernate.type.AdjustableBasicType;
+import org.hibernate.type.descriptor.java.BooleanJavaType;
+import org.hibernate.type.descriptor.java.JavaType;
+import org.hibernate.type.descriptor.jdbc.BooleanJdbcType;
+import org.hibernate.type.descriptor.jdbc.JdbcType;
+
+public class BooleanType extends AbstractSingleColumnStandardBasicType<Boolean>
+		implements AdjustableBasicType<Boolean> {
+	public static final BooleanType INSTANCE = new BooleanType();
+
+	public BooleanType() {
+		this(BooleanJdbcType.INSTANCE, BooleanJavaType.INSTANCE);
+	}
+
+	protected BooleanType(JdbcType jdbcType, BooleanJavaType javaTypeDescriptor) {
+		super(jdbcType, javaTypeDescriptor);
+	}
+
+	@Incubating
+	public BooleanType(JdbcType jdbcType, JavaType<Boolean> javaTypeDescriptor) {
+		super(jdbcType, javaTypeDescriptor);
+	}
+
+	@Override
+	public String getName() {
+		return "boolean";
+	}
+
+	@Override
+	public String[] getRegistrationKeys() {
+		return new String[] { getName(), boolean.class.getName(), Boolean.class.getName() };
+	}
+
+}
