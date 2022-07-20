98bf80ad1ec99b4f43f0f6c8ecba23760b11a92e
Koen Aers <koen.aers@gmail.com>
2022-07-20 14:38:47 +0200

    JBIDE-28549: Create a Runtime Plugin for Hibernate 6.1 - Add new test method 'ConfigurationFacadeTest#testAddFile()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_1.test/src/org/jboss/tools/hibernate/runtime/v_6_1/internal/ConfigurationFacadeTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_1.test/src/org/jboss/tools/hibernate/runtime/v_6_1/internal/ConfigurationFacadeTest.java
@@ -1,17 +1,33 @@
 package org.jboss.tools.hibernate.runtime.v_6_1.internal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.File;
+import java.io.PrintWriter;
+
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.jaxb.spi.Binding;
 import org.hibernate.cfg.Configuration;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
 import org.jboss.tools.hibernate.runtime.spi.IConfiguration;
+import org.jboss.tools.hibernate.runtime.v_6_1.internal.util.MetadataHelper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ConfigurationFacadeTest {
 
+	private static final String TEST_HBM_XML_STRING =
+			"<hibernate-mapping package='org.jboss.tools.hibernate.runtime.v_6_0.internal'>" +
+			"  <class name='ConfigurationFacadeTest$Foo'>" + 
+			"    <id name='id'/>" +
+			"  </class>" +
+			"</hibernate-mapping>";
+	
 	private static final IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
 
 	private IConfiguration configurationFacade = null;
@@ -36,4 +52,21 @@ public class ConfigurationFacadeTest {
 		assertEquals("bar", configurationFacade.getProperty("foo"));
 	}
 
+	@Test
+	public void testAddFile() throws Exception {
+		File testFile = File.createTempFile("test", "hbm.xml");
+		PrintWriter printWriter = new PrintWriter(testFile);
+		printWriter.write(TEST_HBM_XML_STRING);
+		printWriter.close();
+		MetadataSources metadataSources = MetadataHelper.getMetadataSources(configuration);
+		assertTrue(metadataSources.getXmlBindings().isEmpty());
+		assertSame(
+				configurationFacade,
+				configurationFacade.addFile(testFile));
+		assertFalse(metadataSources.getXmlBindings().isEmpty());
+		Binding<?> binding = metadataSources.getXmlBindings().iterator().next();
+		assertEquals(testFile.getAbsolutePath(), binding.getOrigin().getName());
+		assertTrue(testFile.delete());
+	}
+	
 }
