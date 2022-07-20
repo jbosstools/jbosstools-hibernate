b5ee2426f5a000d90f2c8525cae186e1cfbe72b5
Koen Aers <koen.aers@gmail.com>
2022-07-20 14:04:08 +0200

    JBIDE-28549: Create a Runtime Plugin for Hibernate 6.1 - Add new test method 'org.jboss.tools.hibernate.runtime.v_6_1.internal.util.MetadataHelperTest#testGetMetadata()' and implementation 'org.jboss.tools.hibernate.runtime.v_6_1.internal.util.MetadataHelper#getMetadata()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_1.test/src/org/jboss/tools/hibernate/runtime/v_6_1/internal/util/MetadataHelperTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_1.test/src/org/jboss/tools/hibernate/runtime/v_6_1/internal/util/MetadataHelperTest.java
@@ -4,7 +4,14 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
+import java.io.ByteArrayInputStream;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
+import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.cfg.Configuration;
 import org.junit.jupiter.api.Test;
 
@@ -23,4 +30,69 @@ public class MetadataHelperTest {
 		assertSame(mds1, mds2);
 	}
 	
+	@Test
+	public void testGetMetadata() {
+		 Configuration methodConfiguration = new MetadataMethodConfiguration();
+	     assertSame(
+	    		 MetadataMethodConfiguration.METADATA, 
+	    		 MetadataHelper.getMetadata(methodConfiguration));
+	     Configuration fieldConfiguration = new MetadataFieldConfiguration();
+	     assertSame(
+	    		 MetadataFieldConfiguration.METADATA, 
+	    		 MetadataHelper.getMetadata(fieldConfiguration));
+	     MetadataSources metadataSources = new MetadataSources();
+	     metadataSources.addInputStream(new ByteArrayInputStream(TEST_HBM_XML_STRING.getBytes()));
+	     Configuration configuration = new Configuration(metadataSources);
+	     configuration.setProperty(AvailableSettings.DIALECT, MockDialect.class.getName());
+	     configuration.setProperty(AvailableSettings.CONNECTION_PROVIDER, MockConnectionProvider.class.getName());
+	     Metadata metadata = MetadataHelper.getMetadata(configuration);
+	     assertNotNull(metadata.getEntityBinding("org.jboss.tools.hibernate.runtime.v_6_1.internal.util.MetadataHelperTest$Foo"));
+	}
+	
+	private static class MetadataMethodConfiguration extends Configuration {
+		static Metadata METADATA = createMetadata();
+		@SuppressWarnings("unused")
+		public Metadata getMetadata() {
+			return METADATA;
+		}
+	}
+	
+	private static class MetadataFieldConfiguration extends Configuration {
+		static Metadata METADATA = createMetadata();
+		@SuppressWarnings("unused")
+		private Metadata metadata = METADATA;
+	}
+	
+	private static Metadata createMetadata() {
+		Metadata result = null;
+		result = (Metadata) Proxy.newProxyInstance(
+				MetadataHelperTest.class.getClassLoader(), 
+				new Class[] { Metadata.class },  
+				new InvocationHandler() {				
+					@Override
+					public Object invoke(
+							Object proxy, 
+							Method method, 
+							Object[] args) throws Throwable {
+						return null;
+					}
+				});
+		return result;
+	}
+	
+	@SuppressWarnings("unused")
+	private static class Foo {
+		public String id;
+	}
+	
+	private static final String TEST_HBM_XML_STRING =
+			"<!DOCTYPE hibernate-mapping PUBLIC" +
+			"		'-//Hibernate/Hibernate Mapping DTD 3.0//EN'" +
+			"		'http://www.hibernate.org/dtd/hibernate-mapping-3.0.dtd'>" +
+			"<hibernate-mapping package='org.jboss.tools.hibernate.runtime.v_6_1.internal.util'>" +
+			"  <class name='MetadataHelperTest$Foo'>" + 
+			"    <id name='id'/>" +
+			"  </class>" +
+			"</hibernate-mapping>";
+
 }
