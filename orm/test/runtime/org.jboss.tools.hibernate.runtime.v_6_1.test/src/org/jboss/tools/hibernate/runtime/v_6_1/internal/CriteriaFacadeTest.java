04073a9571c44c1b7c58886f493fc52ec92aa11b
Koen Aers <koen.aers@gmail.com>
2022-07-26 20:55:39 +0200

    JBIDE-28549: Create a Runtime Plugin for Hibernate 6.1 - Initial implementation of new test class 'org.jboss.tools.hibernate.runtime.v_6_1.internal.CriteriaFacadeTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_1.test/src/org/jboss/tools/hibernate/runtime/v_6_1/internal/CriteriaFacadeTest.java
@@ -0,0 +1,58 @@
+package org.jboss.tools.hibernate.runtime.v_6_1.internal;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+import java.util.List;
+
+import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.ICriteria;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.Query;
+
+public class CriteriaFacadeTest {
+
+	private static final IFacadeFactory FACADE_FACTORY = new FacadeFactoryImpl();
+	
+	private static final List<Object> RESULT_LIST = Arrays.asList();
+
+	private int maxResults = Integer.MIN_VALUE;
+	
+	private ICriteria criteriaFacade = null;
+
+	@BeforeEach
+	public void beforeEach() {
+		criteriaFacade = new CriteriaFacadeImpl(FACADE_FACTORY, createTestQuery());		
+	}
+	
+	@Test
+	public void testInstance() {
+		assertNotNull(criteriaFacade);
+	}
+	
+	private Query createTestQuery() {
+		return (Query)Proxy.newProxyInstance(
+				getClass().getClassLoader(), 
+				new Class[] { Query.class }, 
+				new TestInvocationHandler());
+	}
+	
+	private class TestInvocationHandler implements InvocationHandler {
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+			String methodName = method.getName();
+			if ("setMaxResults".equals(methodName)) {
+				maxResults = (int)args[0];
+			} else if ("getResultList".equals(methodName)) {
+				return RESULT_LIST;
+			}
+			return null;
+		}	
+	}
+
+}
