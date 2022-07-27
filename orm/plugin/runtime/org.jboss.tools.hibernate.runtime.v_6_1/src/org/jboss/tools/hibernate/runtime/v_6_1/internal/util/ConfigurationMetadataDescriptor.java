6e5f540280c10c9d6e2d0dde5a7b744fcfd49007
Koen Aers <koen.aers@gmail.com>
2022-07-27 16:11:52 +0200

    JBIDE-28549: Create a Runtime Plugin for Hibernate 6.1 - Implementation of new test class 'org.jboss.tools.hibernate.runtime.v_6_1.internal.util.ConfigurationMetadataDescriptorTest' and of the implementation class 'org.jboss.tools.hibernate.runtime.v_6_1.internal.util.ConfigurationMetadataDescriptor'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_6_1/src/org/jboss/tools/hibernate/runtime/v_6_1/internal/util/ConfigurationMetadataDescriptor.java
@@ -0,0 +1,62 @@
+package org.jboss.tools.hibernate.runtime.v_6_1.internal.util;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Properties;
+
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.internal.MetadataImpl;
+import org.hibernate.mapping.PersistentClass;
+import org.hibernate.tool.api.metadata.MetadataDescriptor;
+import org.jboss.tools.hibernate.runtime.common.IFacade;
+import org.jboss.tools.hibernate.runtime.spi.IPersistentClass;
+import org.jboss.tools.hibernate.runtime.v_6_1.internal.ConfigurationFacadeImpl;
+
+public class ConfigurationMetadataDescriptor implements MetadataDescriptor {
+
+	ConfigurationFacadeImpl configurationFacade;
+	
+	public ConfigurationMetadataDescriptor(ConfigurationFacadeImpl configurationFacade) {
+		this.configurationFacade = configurationFacade;
+	}
+
+	@Override
+	public Metadata createMetadata() {
+		Metadata result = configurationFacade.getMetadata();
+		if (result != null) {
+			result = patch(result);
+		}
+		return result;
+	}
+
+	@Override
+	public Properties getProperties() {
+		return configurationFacade.getProperties();
+	}
+
+	private Metadata patch(Metadata metadata) {
+		try {
+			if (metadata instanceof MetadataImpl ) {
+				MetadataImpl metadataImpl = (MetadataImpl)metadata;
+				Field entityBindingMapField = metadataImpl.getClass().getDeclaredField("entityBindingMap");
+				if (entityBindingMapField != null) {
+					entityBindingMapField.setAccessible(true);
+					Object object = entityBindingMapField.get(metadataImpl);
+					if (object instanceof HashMap<?, ?>) {
+						@SuppressWarnings("unchecked")
+						HashMap<String, PersistentClass> map = (HashMap<String, PersistentClass>)object;
+						for (IPersistentClass ipc : configurationFacade.getAddedClasses()) {
+							PersistentClass pc = (PersistentClass)((IFacade)ipc).getTarget();
+							map.put(pc.getEntityName(), pc);
+						}
+					}
+				}
+			}
+			return metadata;
+		}
+		catch (Throwable t) {
+			throw new RuntimeException("Problem while creating metadata", t);
+		}
+	} 
+	
+}
