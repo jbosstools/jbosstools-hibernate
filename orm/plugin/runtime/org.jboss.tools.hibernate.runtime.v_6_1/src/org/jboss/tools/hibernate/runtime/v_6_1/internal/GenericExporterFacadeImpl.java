82ac0d9a3de066a0d9768afd0e2830eb979f00dc
Koen Aers <koen.aers@gmail.com>
2022-07-28 15:13:24 +0200

    JBIDE-28549: Create a Runtime Plugin for Hibernate 6.1 - Implementation of new test class 'org.jboss.tools.hibernate.runtime.v_6_1.internal.GenericExporterFacadeTest' and of the implementation class 'org.jboss.tools.hibernate.runtime.v_6_1.internal.GenericExporterFacadeImpl'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_6_1/src/org/jboss/tools/hibernate/runtime/v_6_1/internal/GenericExporterFacadeImpl.java
+++ orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_6_1/src/org/jboss/tools/hibernate/runtime/v_6_1/internal/GenericExporterFacadeImpl.java
@@ -1,5 +1,7 @@
 package org.jboss.tools.hibernate.runtime.v_6_1.internal;
 
+import org.hibernate.tool.api.export.ExporterConstants;
+import org.hibernate.tool.internal.export.common.GenericExporter;
 import org.jboss.tools.hibernate.runtime.common.AbstractGenericExporterFacade;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
 
@@ -7,6 +9,37 @@ public class GenericExporterFacadeImpl extends AbstractGenericExporterFacade {
 
 	public GenericExporterFacadeImpl(IFacadeFactory facadeFactory, Object target) {
 		super(facadeFactory, target);
+	}
+	
+	@Override 
+	public void setFilePattern(String filePattern) {
+		((GenericExporter)getTarget()).getProperties().setProperty(
+				ExporterConstants.FILE_PATTERN, 
+				filePattern);
+	}
+
+	@Override 
+	public void setTemplateName(String templateName) {
+		((GenericExporter)getTarget()).getProperties().setProperty(
+				ExporterConstants.TEMPLATE_NAME, 
+				templateName);
+	}
+
+	@Override 
+	public void setForEach(String forEach) {
+		((GenericExporter)getTarget()).getProperties().setProperty(
+				ExporterConstants.FOR_EACH, 
+				forEach);
+	}
+	
+	@Override
+	public String getFilePattern() {
+		return ((GenericExporter)getTarget()).getProperties().getProperty(ExporterConstants.FILE_PATTERN);
+	}
+	
+	@Override
+	public String getTemplateName() {
+		return ((GenericExporter)getTarget()).getProperties().getProperty(ExporterConstants.TEMPLATE_NAME);
 	}
 
 }
