3ad684f4cbbd660d20a074a63838aad7b46a3622
Koen Aers <koen.aers@gmail.com>
2022-08-03 11:34:09 +0200

    JBIDE-28549: Create a Runtime Plugin for Hibernate 6.1 - Initial implementation of new test class 'org.jboss.tools.hibernate.runtime.v_6_1.internal.ServiceImplTest' and of the implementation class 'org.jboss.tools.hibernate.runtime.v_6_1.internal.ServiceImpl'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_1.test/src/org/jboss/tools/hibernate/runtime/v_6_1/internal/ServiceImplTest.java
@@ -0,0 +1,22 @@
+package org.jboss.tools.hibernate.runtime.v_6_1.internal;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ServiceImplTest {
+
+	private ServiceImpl service = null;
+	
+	@BeforeEach
+	public void beforeEach() {
+		service = new ServiceImpl();
+	}
+	
+	@Test
+	public void testConstruction() {
+		assertNotNull(service);
+	}
+	
+}
