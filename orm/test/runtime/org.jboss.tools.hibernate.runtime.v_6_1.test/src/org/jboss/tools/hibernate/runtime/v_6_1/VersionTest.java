8e4e95699963eea6ccf21422c6eaf16cc82eaf57
Koen Aers <koen.aers@gmail.com>
2022-08-16 12:02:00 +0200

    JBIDE-28549: Create a Runtime Plugin for Hibernate 6.1 - Implementation of new test class 'org.jboss.tools.hibernate.runtime.v_6_1.VersionTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_1.test/src/org/jboss/tools/hibernate/runtime/v_6_1/VersionTest.java
@@ -0,0 +1,26 @@
+package org.jboss.tools.hibernate.runtime.v_6_1;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import org.jboss.tools.hibernate.runtime.spi.RuntimeServiceManager;
+import org.jboss.tools.hibernate.runtime.v_6_1.internal.ServiceImpl;
+import org.junit.jupiter.api.Test;
+
+public class VersionTest {
+	
+	@Test
+	public void testToolsVersion() {
+		assertEquals("6.1.0.Final", org.hibernate.tool.api.version.Version.CURRENT_VERSION);
+	}
+	
+	@Test 
+	public void testCoreVersion() {
+		assertEquals("6.1.0.Final", org.hibernate.Version.getVersionString());
+	}
+
+	@Test 
+	public void testRuntimeVersion() {
+		assertSame(RuntimeServiceManager.getInstance().findService("6.1").getClass(), ServiceImpl.class);
+	}
+}
