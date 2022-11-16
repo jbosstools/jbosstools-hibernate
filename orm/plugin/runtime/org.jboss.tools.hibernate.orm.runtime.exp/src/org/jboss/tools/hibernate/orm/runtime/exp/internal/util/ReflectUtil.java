6da46aa05f33367120511fb4c047eb028ad41be4
Koen Aers <koen.aers@gmail.com>
2022-11-16 19:09:34 +0100

    JBIDE-28628: Create an experimental Hibernate runtime that will use the new Hibernate Tools ORM to JBoss Tools adapter layer - Extract private method 'GenericFacadeFactory#lookupMethod(...)' into new class ReflectUtil and take into account possible primitive arguments
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/plugin/runtime/org.jboss.tools.hibernate.orm.runtime.exp/src/org/jboss/tools/hibernate/orm/runtime/exp/internal/util/ReflectUtil.java
@@ -0,0 +1,69 @@
+package org.jboss.tools.hibernate.orm.runtime.exp.internal.util;
+
+import java.lang.reflect.Method;
+import java.util.Map;
+
+public class ReflectUtil {
+	
+    private static final Map<Class<?>, Class<?>> primitiveWrapperMap =
+            Map.of(boolean.class, Boolean.class,
+                    byte.class, Byte.class,
+                    char.class, Character.class,
+                    double.class, Double.class,
+                    float.class, Float.class,
+                    int.class, Integer.class,
+                    long.class, Long.class,
+                    short.class, Short.class);
+
+	public static Method lookupMethod(Class<?> methodClass, String methodName, Class<?>[] argumentClasses) {
+		Method result = null;
+		Method[] methods = methodClass.getMethods();
+		for (Method candidate : methods) {
+			if (!methodName.equals(candidate.getName())) continue;
+			int parameterCount = candidate.getParameterCount();
+			if (argumentClasses == null && parameterCount == 0) {
+				result = candidate;
+				break;
+			}
+			else if (argumentClasses.length != parameterCount) {
+				continue;
+			}
+			result = candidate;		
+			Class<?>[] parameterTypes = candidate.getParameterTypes();
+			for (int i = 0; i < argumentClasses.length; i++) {
+				if (!isAssignableTo(argumentClasses[i], parameterTypes[i])) {
+					result = null;
+					break;
+				}
+			}
+			if (result != null) break;
+		}
+		if (result != null) {
+			result.setAccessible(true);
+		}
+		return result;
+	}
+	
+    private static boolean isPrimitiveWrapperOf(Class<?> targetClass, Class<?> primitive) {
+        if (!primitive.isPrimitive()) {
+            throw new IllegalArgumentException("First argument has to be primitive type");
+        }
+        return primitiveWrapperMap.get(primitive) == targetClass;
+    }
+
+    private static boolean isAssignableTo(Class<?> from, Class<?> to) {
+        if (to.isAssignableFrom(from)) {
+            return true;
+        }
+        if (from.isPrimitive()) {
+            return isPrimitiveWrapperOf(to, from);
+        }
+        if (to.isPrimitive()) {
+            return isPrimitiveWrapperOf(from, to);
+        }
+        return false;
+    }
+
+	
+
+}
