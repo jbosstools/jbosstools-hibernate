d25ad1b0b20ec2010a08b7b6f6d8084b77789187
Koen Aers <koen.aers@gmail.com>
2022-11-29 19:20:15 +0100

    JBIDE-28751: Implement and use the generic adapter for IColumn in the experimental Hibernate runtime - Add test method 'IColumnTest#testGetSqlType()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.exp.test/src/org/jboss/tools/hibernate/orm/runtime/exp/internal/IColumnTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.exp.test/src/org/jboss/tools/hibernate/orm/runtime/exp/internal/IColumnTest.java
@@ -4,10 +4,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.mapping.Column;
+import org.hibernate.mapping.Value;
+import org.hibernate.tool.orm.jbt.type.IntegerType;
+import org.hibernate.tool.orm.jbt.util.MockConnectionProvider;
+import org.hibernate.tool.orm.jbt.util.MockDialect;
 import org.jboss.tools.hibernate.orm.runtime.exp.internal.util.NewFacadeFactory;
 import org.jboss.tools.hibernate.runtime.common.IFacade;
 import org.jboss.tools.hibernate.runtime.spi.IColumn;
+import org.jboss.tools.hibernate.runtime.spi.IConfiguration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -42,6 +52,35 @@ public class IColumnTest {
 		assertNull(columnFacade.getSqlTypeCode());
 		columnTarget.setSqlTypeCode(Integer.MAX_VALUE);
 		assertEquals(Integer.MAX_VALUE, columnFacade.getSqlTypeCode().intValue());
+	}
+
+	@Test
+	public void testGetSqlType() {
+		assertNull(columnFacade.getSqlType());
+		columnTarget.setSqlType("foobar");
+		assertEquals("foobar", columnFacade.getSqlType());
+		IConfiguration configurationFacade = FACADE_FACTORY.createNativeConfiguration();
+		configurationFacade.setProperty(AvailableSettings.DIALECT, MockDialect.class.getName());
+		configurationFacade.setProperty(AvailableSettings.CONNECTION_PROVIDER, MockConnectionProvider.class.getName());
+		columnTarget.setValue(createIntegerTypeValue());
+		columnTarget.setSqlType(null);
+		assertEquals("integer", columnFacade.getSqlType(configurationFacade));
+	}
+	
+	private Value createIntegerTypeValue() {
+		return (Value)Proxy.newProxyInstance(
+				getClass().getClassLoader(), 
+				new Class[] { Value.class }, 
+				new InvocationHandler() {					
+					@Override
+					public Object invoke(Object proxy, Method method, Object[] args) 
+							throws Throwable {
+						if (method.getName().equals("getType")) {
+							return IntegerType.INSTANCE;
+						}
+						return null;
+					}
+				});
 	}
 
 }
