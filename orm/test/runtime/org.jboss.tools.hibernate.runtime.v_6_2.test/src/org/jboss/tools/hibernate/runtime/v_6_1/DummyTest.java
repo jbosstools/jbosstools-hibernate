9252411ca2b53f773c51b37d652574a70c73847a
Koen Aers <koen.aers@gmail.com>
2023-01-06 10:39:36 +0100

    JBIDE-28836: Create a plugin for the Hibernate 6.2.x runtimes - Initial setup of test fragment 'org.jboss.tools.hibernate.runtime.v.6.2.test'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_2.test/src/org/jboss/tools/hibernate/runtime/v_6_1/DummyTest.java
@@ -0,0 +1,14 @@
+package org.jboss.tools.hibernate.runtime.v_6_1;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+	
+	@Test
+	public void doTheTest() {
+		assertEquals("1", Integer.toString(1));
+	}
+	
+}
