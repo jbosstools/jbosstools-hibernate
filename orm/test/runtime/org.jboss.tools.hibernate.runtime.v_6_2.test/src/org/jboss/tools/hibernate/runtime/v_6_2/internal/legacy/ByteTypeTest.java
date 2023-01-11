2de6764f8fb3ee42bb4747ceb64d7646bc619246
Koen Aers <koen.aers@gmail.com>
2023-01-11 21:41:59 +0100

    JBIDE-28836: Create a plugin for the Hibernate 6.2.x runtimes   - Add test class 'org.jboss.hibernate.runtime.v_6_2.internal.legacy.ByteTypeTest'   - Add implementation class 'org.jboss.hibernate.runtime.v_6_2.internal.legacy.ByteType'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_2.test/src/org/jboss/tools/hibernate/runtime/v_6_2/internal/legacy/ByteTypeTest.java
@@ -0,0 +1,32 @@
+package org.jboss.tools.hibernate.runtime.v_6_2.internal.legacy;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+public class ByteTypeTest {
+	
+	@Test
+	public void testInstance() {
+		assertNotNull(ByteType.INSTANCE);
+	}
+	
+	@Test
+	public void testGetName() {
+		assertEquals("byte", ByteType.INSTANCE.getName());
+	}
+	
+	@Test
+	public void testGetRegistrationKeys() {
+		assertArrayEquals(
+				new String[] {
+					"byte",
+					"byte",
+					"java.lang.Byte"
+				},
+				ByteType.INSTANCE.getRegistrationKeys());
+	}
+
+}
