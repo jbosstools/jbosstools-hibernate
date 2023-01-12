d258266347fb959ea00f6cefa8868e866c8e641e
Koen Aers <koen.aers@gmail.com>
2023-01-12 13:05:29 +0100

    JBIDE-28836: Create a plugin for the Hibernate 6.2.x runtimes   - Add test class 'org.jboss.hibernate.runtime.v_6_2.internal.legacy.ShortTypeTest'   - Add implementation class 'org.jboss.hibernate.runtime.v_6_2.internal.legacy.ShortType'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_2.test/src/org/jboss/tools/hibernate/runtime/v_6_2/internal/legacy/ShortTypeTest.java
@@ -0,0 +1,32 @@
+package org.jboss.tools.hibernate.runtime.v_6_2.internal.legacy;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+public class ShortTypeTest {
+	
+	@Test
+	public void testInstance() {
+		assertNotNull(ShortType.INSTANCE);
+	}
+	
+	@Test
+	public void testGetName() {
+		assertEquals("short", ShortType.INSTANCE.getName());
+	}
+	
+	@Test
+	public void testGetRegistrationKeys() {
+		assertArrayEquals(
+				new String[] {
+					"short",
+					"short",
+					"java.lang.Short"
+				},
+				ShortType.INSTANCE.getRegistrationKeys());
+	}
+
+}
