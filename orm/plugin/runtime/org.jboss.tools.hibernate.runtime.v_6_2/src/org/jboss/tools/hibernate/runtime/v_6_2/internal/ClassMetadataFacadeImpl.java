32ba60deebb626d062d30d0428993fc41c94e221
Koen Aers <koen.aers@gmail.com>
2023-01-13 16:17:44 +0100

    JBIDE-28836: Create a plugin for the Hibernate 6.2.x runtimes   - Add test class 'org.jboss.hibernate.runtime.v_6_2.internal.ClassMetadataFacadeTest'   - Implement method 'ClassMetadataFacadeImpl#getTuplizerPropertyValue(Object,int)'   - Add dependency on 'org.jboss.tools.hibernate.libs.antlr4-runtime.v_4_10'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_6_2/src/org/jboss/tools/hibernate/runtime/v_6_2/internal/ClassMetadataFacadeImpl.java
+++ orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_6_2/src/org/jboss/tools/hibernate/runtime/v_6_2/internal/ClassMetadataFacadeImpl.java
@@ -1,5 +1,6 @@
 package org.jboss.tools.hibernate.runtime.v_6_2.internal;
 
+import org.hibernate.persister.entity.EntityPersister;
 import org.jboss.tools.hibernate.runtime.common.AbstractClassMetadataFacade;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
 
@@ -9,4 +10,9 @@ public class ClassMetadataFacadeImpl extends AbstractClassMetadataFacade {
 		super(facadeFactory, target);
 	}
 
+	@Override
+	public Object getTuplizerPropertyValue(Object entity, int i) {
+		return ((EntityPersister)getTarget()).getPropertyValue(entity, i);
+	}
+	
 }
