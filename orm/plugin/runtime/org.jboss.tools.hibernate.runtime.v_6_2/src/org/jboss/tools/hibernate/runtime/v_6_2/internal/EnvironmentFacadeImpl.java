79249e9a097f45b56423b2941af56d3d24b7973f
Koen Aers <koen.aers@gmail.com>
2023-01-16 13:12:47 +0100

    JBIDE-28836: Create a plugin for the Hibernate 6.2.x runtimes   - Add test class 'org.jboss.hibernate.runtime.v_6_2.internal.EnvironmentFacadeTest'   - Complete implementation class 'org.jboss.hibernate.runtime.v_6_2.internal.EnvironmentFacadeImpl'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_6_2/src/org/jboss/tools/hibernate/runtime/v_6_2/internal/EnvironmentFacadeImpl.java
+++ orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_6_2/src/org/jboss/tools/hibernate/runtime/v_6_2/internal/EnvironmentFacadeImpl.java
@@ -1,5 +1,6 @@
 package org.jboss.tools.hibernate.runtime.v_6_2.internal;
 
+import org.hibernate.cfg.Environment;
 import org.jboss.tools.hibernate.runtime.common.AbstractEnvironmentFacade;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
 
@@ -8,6 +9,11 @@ public class EnvironmentFacadeImpl extends AbstractEnvironmentFacade {
 	public EnvironmentFacadeImpl(
 			IFacadeFactory facadeFactory) {
 		super(facadeFactory, null);
+	}
+
+	@Override
+	public String getTransactionManagerStrategy() {
+		return Environment.TRANSACTION_COORDINATOR_STRATEGY;
 	}
 
 }
---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_2.test/src/org/jboss/tools/hibernate/runtime/v_6_2/internal/EnvironmentFacadeTest.java
@@ -0,0 +1,91 @@
+package org.jboss.tools.hibernate.runtime.v_6_2.internal;
+
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import org.hibernate.cfg.Environment;
+import org.jboss.tools.hibernate.runtime.spi.IEnvironment;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class EnvironmentFacadeTest {
+	
+	private IEnvironment environmentFacade = null; 
+	
+	@BeforeEach
+	public void beforeEach() {
+		environmentFacade = new EnvironmentFacadeImpl(new FacadeFactoryImpl());		
+	}
+	
+	@Test
+	public void testInstance() {
+		assertNotNull(environmentFacade);
+	}
+	
+	@Test
+	public void testGetTransactionManagerStrategy() {
+		assertSame("hibernate.transaction.coordinator_class", environmentFacade.getTransactionManagerStrategy());
+	}
+	
+	@Test
+	public void testGetDriver() {
+		assertSame(Environment.DRIVER, environmentFacade.getDriver());
+	}
+	
+	@Test
+	public void testGetHBM2DDLAuto() {
+		assertSame(Environment.HBM2DDL_AUTO, environmentFacade.getHBM2DDLAuto());
+	}
+	
+	@Test
+	public void testGetDialect() {
+		assertSame(Environment.DIALECT, environmentFacade.getDialect());
+	}
+	
+	@Test
+	public void testGetDataSource() {
+		assertSame(Environment.DATASOURCE, environmentFacade.getDataSource());
+	}
+	
+	@Test
+	public void testGetConnectionProvider() {
+		assertSame(Environment.CONNECTION_PROVIDER, environmentFacade.getConnectionProvider());
+	}
+	
+	@Test
+	public void testGetURL() {
+		assertSame(Environment.URL, environmentFacade.getURL());
+	}
+	
+	@Test
+	public void testGetUser() {
+		assertSame(Environment.USER, environmentFacade.getUser());
+	}
+	
+	@Test
+	public void testGetPass() {
+		assertSame(Environment.PASS, environmentFacade.getPass());
+	}
+	
+	@Test
+	public void testGetSessionFactoryName() {
+		assertSame(Environment.SESSION_FACTORY_NAME, environmentFacade.getSessionFactoryName());
+	}
+	
+	@Test
+	public void testGetDefaultCatalog() {
+		assertSame(Environment.DEFAULT_CATALOG, environmentFacade.getDefaultCatalog());
+	}
+	
+	@Test
+	public void testGetDefaultSchema() {
+		assertSame(Environment.DEFAULT_SCHEMA, environmentFacade.getDefaultSchema());
+	}
+	
+	@Test
+	public void testGetWrappedClass() {
+		assertSame(Environment.class, environmentFacade.getWrappedClass());
+	}
+	
+}
