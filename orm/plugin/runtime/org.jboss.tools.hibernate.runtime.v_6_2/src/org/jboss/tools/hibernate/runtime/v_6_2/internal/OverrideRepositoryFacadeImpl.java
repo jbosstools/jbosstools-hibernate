3a6d0e8897855da3aa958be3dde5c61ca624a76c
Koen Aers <koen.aers@gmail.com>
2023-01-17 09:40:58 +0100

    JBIDE-28836: Create a plugin for the Hibernate 6.2.x runtimes   - Add test class 'org.jboss.hibernate.runtime.v_6_2.internal.OverrideRepositoryFacadeTest'   - Add implementation class 'org.jboss.hibernate.runtime.v_6_2.internal.OverrideRepositoryFacadeImpl'   - Add dependency on 'org.jboss.tools.hibernate.libs.commons-collections4.v_4_4'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_6_2/src/org/jboss/tools/hibernate/runtime/v_6_2/internal/OverrideRepositoryFacadeImpl.java
+++ orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_6_2/src/org/jboss/tools/hibernate/runtime/v_6_2/internal/OverrideRepositoryFacadeImpl.java
@@ -1,12 +1,30 @@
 package org.jboss.tools.hibernate.runtime.v_6_2.internal;
 
+import org.hibernate.tool.api.reveng.RevengStrategy;
+import org.hibernate.tool.internal.reveng.strategy.OverrideRepository;
 import org.jboss.tools.hibernate.runtime.common.AbstractOverrideRepositoryFacade;
+import org.jboss.tools.hibernate.runtime.common.IFacade;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IReverseEngineeringStrategy;
 
 public class OverrideRepositoryFacadeImpl extends AbstractOverrideRepositoryFacade {
 
 	public OverrideRepositoryFacadeImpl(IFacadeFactory facadeFactory, Object target) {
 		super(facadeFactory, target);
+	}
+	
+	@Override
+	public IReverseEngineeringStrategy getReverseEngineeringStrategy(
+			IReverseEngineeringStrategy res) {
+		assert res instanceof IFacade;
+		RevengStrategy resTarget = (RevengStrategy)((IFacade)res).getTarget();
+		return getFacadeFactory().createReverseEngineeringStrategy(
+				((OverrideRepository)getTarget()).getReverseEngineeringStrategy(resTarget));
+	}
+	
+	@Override
+	protected String getTableFilterClassName() {
+		return "org.hibernate.tool.internal.reveng.strategy.TableFilter";
 	}
 	
 }
