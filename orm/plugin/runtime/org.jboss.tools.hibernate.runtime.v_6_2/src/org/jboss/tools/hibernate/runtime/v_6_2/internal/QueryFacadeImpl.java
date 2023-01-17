47a976568695e20c25b34489975ea683b95c5d82
Koen Aers <koen.aers@gmail.com>
2023-01-17 18:34:16 +0100

    JBIDE-28836: Create a plugin for the Hibernate 6.2.x runtimes   - Add test class 'org.jboss.hibernate.runtime.v_6_2.internal.QueryFacadeTest'   - Complete implementation class 'org.jboss.hibernate.runtime.v_6_2.internal.QueryFacadeImpl'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_6_2/src/org/jboss/tools/hibernate/runtime/v_6_2/internal/QueryFacadeImpl.java
+++ orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_6_2/src/org/jboss/tools/hibernate/runtime/v_6_2/internal/QueryFacadeImpl.java
@@ -1,7 +1,11 @@
 package org.jboss.tools.hibernate.runtime.v_6_2.internal;
 
+import java.util.List;
+
+import org.hibernate.query.Query;
 import org.jboss.tools.hibernate.runtime.common.AbstractQueryFacade;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IType;
 
 public class QueryFacadeImpl extends AbstractQueryFacade {
 
@@ -9,4 +13,33 @@ public class QueryFacadeImpl extends AbstractQueryFacade {
 		super(facadeFactory, target);
 	}
 	
+	@Override
+	public void setParameter(int position, Object value, IType type) {
+		((Query<?>)getTarget()).setParameter(position, value);
+	}
+
+	@Override
+	public void setParameter(String name, Object value, IType type) {
+		((Query<?>)getTarget()).setParameter(name, value);
+	}
+	
+	@Override
+	public void setParameterList(String name, List<Object> list, IType type) {
+		((Query<?>)getTarget()).setParameter(name, list);
+	}
+	
+	// TODO JBIDE-27532: Review the Query Page Viewer as the used APIs have completely changed
+	@Override
+	@Deprecated
+	public String[] getReturnAliases() {
+		return new String[0];
+	}
+
+	// TODO JBIDE-27532: Review the Query Page Viewer as the used APIs have completely changed
+	@Override
+	@Deprecated
+	public IType[] getReturnTypes() {
+		return new IType[0];
+	}
+
 }
