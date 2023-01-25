37b559034a6a94f1439cc6f4fa5fa19f33df0060
Koen Aers <koen.aers@gmail.com>
2023-01-25 18:49:48 +0100

    JBIDE-28836: Create a plugin for the Hibernate 6.2.x runtimes   - Add new test method 'org.jboss.hibernate.runtime.v_6_2.internal.ServiceImplTest#testNewSingleTableSubclass()'   - Implement method stub 'org.jboss.hibernate.runtime.v_6_2.internal.ServiceImpl#newSingleTableSubclass(IPersistentClass)'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_6_2/src/org/jboss/tools/hibernate/runtime/v_6_2/internal/ServiceImpl.java
+++ orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_6_2/src/org/jboss/tools/hibernate/runtime/v_6_2/internal/ServiceImpl.java
@@ -39,6 +39,7 @@ import org.hibernate.mapping.PrimitiveArray;
 import org.hibernate.mapping.Property;
 import org.hibernate.mapping.RootClass;
 import org.hibernate.mapping.Set;
+import org.hibernate.mapping.SingleTableSubclass;
 import org.hibernate.mapping.Table;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.tool.api.export.Exporter;
@@ -57,6 +58,7 @@ import org.hibernate.tool.internal.reveng.reader.DatabaseReader;
 import org.hibernate.tool.internal.reveng.strategy.DefaultStrategy;
 import org.hibernate.tool.internal.reveng.strategy.OverrideRepository;
 import org.hibernate.tool.internal.reveng.strategy.TableFilter;
+import org.jboss.tools.hibernate.runtime.common.AbstractPersistentClassFacade;
 import org.jboss.tools.hibernate.runtime.common.AbstractService;
 import org.jboss.tools.hibernate.runtime.common.IFacade;
 import org.jboss.tools.hibernate.runtime.common.IFacadeFactory;
@@ -431,9 +433,13 @@ public class ServiceImpl extends AbstractService {
 	}
 
 	@Override
-	public IPersistentClass newSingleTableSubclass(IPersistentClass arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public IPersistentClass newSingleTableSubclass(IPersistentClass persistentClass) {
+		IPersistentClass result = facadeFactory.createPersistentClass(
+				new SingleTableSubclass(
+						(PersistentClass)((IFacade)persistentClass).getTarget(),
+						DummyMetadataBuildingContext.INSTANCE));
+		((AbstractPersistentClassFacade)result).setSuperClass(persistentClass);
+		return result;
 	}
 
 	@Override
