32b2fc5c327b96c1b29a7281d80bb4d71d0c0a6a
Koen Aers <koen.aers@gmail.com>
2023-01-29 10:30:00 +0100

    JBIDE-28836: Create a plugin for the Hibernate 6.2.x runtimes   - Add new test method 'org.jboss.hibernate.runtime.v_6_2.internal.ServiceImplTest#testGetClassLoader()'   - Implement method stub 'org.jboss.hibernate.runtime.v_6_2.internal.ServiceImpl#getClassLoader()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_2.test/src/org/jboss/tools/hibernate/runtime/v_6_2/internal/ServiceImplTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_2.test/src/org/jboss/tools/hibernate/runtime/v_6_2/internal/ServiceImplTest.java
@@ -542,4 +542,11 @@ public class ServiceImplTest {
 				service.getClassWithoutInitializingProxy(new Object()));
 	}
 	
+	@Test
+	public void testGetClassLoader(){
+		assertSame(
+				ServiceImpl.class.getClassLoader(), 
+				service.getClassLoader());
+	}
+	
 }
