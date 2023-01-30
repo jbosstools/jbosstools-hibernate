be88a98d5bc262981f9c63b82998c3b76dd2d1bb
Koen Aers <koen.aers@gmail.com>
2023-01-30 13:54:40 +0100

    JBIDE-28836: Create a plugin for the Hibernate 6.2.x runtimes   - Add new test method 'org.jboss.tools.hibernate.runtime.v_6_2.VersionTest#testRuntimeVersion()'   - Contribute class 'org.jboss.tools.hibernate.runtime.v_6_2.internal.ServiceImpl' as the service implentation for the 6.2 runtime to the Eclipse platform
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_2.test/src/org/jboss/tools/hibernate/runtime/v_6_2/VersionTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_6_2.test/src/org/jboss/tools/hibernate/runtime/v_6_2/VersionTest.java
@@ -1,7 +1,10 @@
 package org.jboss.tools.hibernate.runtime.v_6_2;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
+import org.jboss.tools.hibernate.runtime.spi.RuntimeServiceManager;
+import org.jboss.tools.hibernate.runtime.v_6_2.internal.ServiceImpl;
 import org.junit.jupiter.api.Test;
 
 public class VersionTest {
@@ -16,4 +19,8 @@ public class VersionTest {
 		assertEquals("6.2.0.CR1", org.hibernate.Version.getVersionString());
 	}
 
+	@Test 
+	public void testRuntimeVersion() {
+		assertSame(RuntimeServiceManager.getInstance().findService("6.2").getClass(), ServiceImpl.class);
+	}
 }
