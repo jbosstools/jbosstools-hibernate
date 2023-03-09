2f9eec37494dded9c7891331393e16969f6b65e8
Koen Aers <koen.aers@gmail.com>
2023-03-09 15:04:19 +0100

    JBIDE-28760: Implement and use the generic adapter for IHQLCompletionProposal in the experimental Hibernate runtime   - Initial creation of test class 'org.jboss.tools.hibernate.orm.runtime.exp.internal.IHQLCompletionProposalTest'   - Add test method 'org.jboss.tools.hibernate.orm.runtime.exp.internal.IHQLCompletionProposalTest#testConstruction()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.exp.test/src/org/jboss/tools/hibernate/orm/runtime/exp/internal/IHQLCompletionProposalTest.java
@@ -0,0 +1,32 @@
+package org.jboss.tools.hibernate.orm.runtime.exp.internal;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import org.hibernate.tool.ide.completion.HQLCompletionProposal;
+import org.jboss.tools.hibernate.orm.runtime.exp.internal.util.NewFacadeFactory;
+import org.jboss.tools.hibernate.runtime.common.IFacade;
+import org.jboss.tools.hibernate.runtime.spi.IHQLCompletionProposal;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class IHQLCompletionProposalTest {
+	
+	private IHQLCompletionProposal hqlCompletionProposalFacade = null;
+	private HQLCompletionProposal hqlCompletionProposalTarget = null;
+	
+	@BeforeEach
+	public void beforeEach() {
+		hqlCompletionProposalTarget = new HQLCompletionProposal(0, 0);
+		hqlCompletionProposalFacade = 
+				NewFacadeFactory.INSTANCE.createHQLCompletionProposal(hqlCompletionProposalTarget);
+	}
+	
+	@Test
+	public void testConstruction() {
+		assertNotNull(hqlCompletionProposalTarget);
+		assertNotNull(hqlCompletionProposalFacade);
+		assertSame(hqlCompletionProposalTarget, ((IFacade)hqlCompletionProposalFacade).getTarget());
+	}
+
+}
