9f6990ddcb2ddc566ff1c690e711da124dde94bb
Koen Aers <koen.aers@gmail.com>
2023-03-16 08:57:42 +0100

    JBIDE-28769: Implement and use the generic adapter for ITable in the experimental Hibernate runtime   - Add new test case 'org.jboss.tools.hibernate.orm.runtime.exp.internal.ITableTest#testAddColumn()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.exp.test/src/org/jboss/tools/hibernate/orm/runtime/exp/internal/ITableTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.exp.test/src/org/jboss/tools/hibernate/orm/runtime/exp/internal/ITableTest.java
@@ -2,10 +2,15 @@ package org.jboss.tools.hibernate.orm.runtime.exp.internal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
+import org.hibernate.mapping.Column;
 import org.hibernate.mapping.Table;
 import org.jboss.tools.hibernate.orm.runtime.exp.internal.util.NewFacadeFactory;
+import org.jboss.tools.hibernate.runtime.common.AbstractTableFacade;
 import org.jboss.tools.hibernate.runtime.common.IFacade;
+import org.jboss.tools.hibernate.runtime.spi.IColumn;
 import org.jboss.tools.hibernate.runtime.spi.ITable;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -33,6 +38,15 @@ public class ITableTest {
 		assertEquals("foo", tableFacade.getName());
 		tableTarget.setName("bar");
 		assertEquals("bar", tableFacade.getName());
+	}
+	
+	@Test
+	public void testAddColumn() {
+		IColumn columnFacade = NewFacadeFactory.INSTANCE.createColumn("foo");
+		Column columnTarget = (Column)((IFacade)columnFacade).getTarget();
+		assertNull(tableTarget.getColumn(columnTarget));
+		tableFacade.addColumn(columnFacade);
+		assertSame(columnTarget, tableTarget.getColumn(columnTarget));
 	}
 	
 }
