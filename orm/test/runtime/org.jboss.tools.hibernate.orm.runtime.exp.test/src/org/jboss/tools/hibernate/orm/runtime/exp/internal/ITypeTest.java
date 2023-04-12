a8c8e00fba36a60b872b4894e933d90fab59c94f
Koen Aers <koen.aers@gmail.com>
2023-04-12 10:08:38 +0200

    JBIDE-28750: Implement and use the generic adapter for IType in the experimental Hibernate runtime   - Add new test case 'org.jboss.tools.hibernate.orm.runtime.exp.internal.IType#testToString()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.exp.test/src/org/jboss/tools/hibernate/orm/runtime/exp/internal/ITypeTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.exp.test/src/org/jboss/tools/hibernate/orm/runtime/exp/internal/ITypeTest.java
@@ -1,7 +1,9 @@
 package org.jboss.tools.hibernate.orm.runtime.exp.internal;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.hibernate.tool.orm.jbt.type.ClassType;
 import org.hibernate.tool.orm.jbt.wrp.TypeWrapperFactory;
@@ -14,6 +16,27 @@ import org.junit.jupiter.api.Test;
 public class ITypeTest {
 	
 	@Test
+	public void testToString() {
+		// first try type that is string representable
+		IType classTypeFacade = (IType)GenericFacadeFactory.createFacade(
+				IType.class, 
+				TypeWrapperFactory.createTypeWrapper(new ClassType()));
+		assertEquals(
+				TypeFacadeTest.class.getName(), 
+				classTypeFacade.toString(TypeFacadeTest.class));
+		// next try a type that cannot be represented by a string
+		try {
+			IType arrayTypeFacade = (IType)GenericFacadeFactory.createFacade(
+					IType.class, 
+					TypeWrapperFactory.createTypeWrapper(new ArrayType("foo", "bar", String.class)));
+			arrayTypeFacade.toString(new String[] { "foo", "bar" });
+			fail();
+		} catch (UnsupportedOperationException e) {
+			assertTrue(e.getMessage().contains("does not support 'toString(Object)'"));
+		}
+	}
+	
+	@Test
 	public void testIsAnyType() {
 		// first try type that is not a any type
 		IType classTypeFacade = (IType)GenericFacadeFactory.createFacade(
@@ -21,10 +44,10 @@ public class ITypeTest {
 				TypeWrapperFactory.createTypeWrapper(new ClassType()));
 		assertFalse(classTypeFacade.isAnyType());
 		// next try a any type
-		IType arrayTypeFacade = (IType)GenericFacadeFactory.createFacade(
+		IType anyTypeFacade = (IType)GenericFacadeFactory.createFacade(
 				IType.class, 
 				TypeWrapperFactory.createTypeWrapper(new AnyType(null, null, null, true)));
-		assertTrue(arrayTypeFacade.isAnyType());
+		assertTrue(anyTypeFacade.isAnyType());
 	}
 	
 	@Test
