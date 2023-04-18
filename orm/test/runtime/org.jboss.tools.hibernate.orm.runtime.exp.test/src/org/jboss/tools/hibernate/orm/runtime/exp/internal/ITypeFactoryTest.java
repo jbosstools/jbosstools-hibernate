a06bad76dcaed191bb7c95c4e76c5738a2972424
Koen Aers <koen.aers@gmail.com>
2023-04-18 16:29:28 +0200

    JBIDE-28771: Implement and use the generic adapter for ITypeFactory in the experimental Hibernate runtime   - Add new test case 'org.jboss.tools.hibernate.orm.runtime.exp.internal.ITypeFactoryTest#testGetTypeFormats()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.exp.test/src/org/jboss/tools/hibernate/orm/runtime/exp/internal/ITypeFactoryTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.exp.test/src/org/jboss/tools/hibernate/orm/runtime/exp/internal/ITypeFactoryTest.java
@@ -4,12 +4,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
+import java.text.SimpleDateFormat;
+import java.util.Currency;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
+import java.util.TimeZone;
+
 import org.hibernate.tool.orm.jbt.wrp.TypeFactoryWrapper;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 import org.hibernate.tool.orm.jbt.wrp.WrapperFactory;
 import org.hibernate.type.Type;
 import org.jboss.tools.hibernate.orm.runtime.exp.internal.util.GenericFacadeFactory;
 import org.jboss.tools.hibernate.runtime.common.IFacade;
+import org.jboss.tools.hibernate.runtime.spi.ITable;
 import org.jboss.tools.hibernate.runtime.spi.IType;
 import org.jboss.tools.hibernate.runtime.spi.ITypeFactory;
 import org.junit.jupiter.api.BeforeEach;
@@ -206,4 +214,50 @@ public class ITypeFactoryTest {
 		Type typeTarget = (Type)((Wrapper)((IFacade)typeFacade).getTarget()).getWrappedObject();
 		assertEquals("string", typeTarget.getName());
 	}
+	
+	@Test
+	public void testGetTypeFormats() {
+		Map<IType, String> typeFormats = typeFactoryFacade.getTypeFormats();
+		assertEquals(23, typeFormats.size());
+		assertEquals("true", typeFormats.get(typeFactoryFacade.getBooleanType()));
+		assertEquals("42", typeFormats.get(typeFactoryFacade.getByteType()));
+		assertEquals("42", typeFormats.get(typeFactoryFacade.getBigIntegerType()));
+		assertEquals("42", typeFormats.get(typeFactoryFacade.getShortType()));
+		assertEquals(
+				new SimpleDateFormat("yyyy-MM-dd").format(new Date()), 
+				typeFormats.get(typeFactoryFacade.getCalendarType()));
+		assertEquals(
+				new SimpleDateFormat("yyyy-MM-dd").format(new Date()), 
+				typeFormats.get(typeFactoryFacade.getCalendarDateType()));
+		assertEquals("42", typeFormats.get(typeFactoryFacade.getIntegerType()));
+		assertEquals("42", typeFormats.get(typeFactoryFacade.getBigDecimalType()));
+		assertEquals("h", typeFormats.get(typeFactoryFacade.getCharacterType()));
+		assertEquals(
+				Class.class.getName(), 
+				typeFormats.get(typeFactoryFacade.getClassType()));
+		assertEquals(
+				Currency.getInstance(Locale.getDefault()).toString(), 
+				typeFormats.get(typeFactoryFacade.getCurrencyType()));
+		assertEquals(
+				new SimpleDateFormat("yyyy-MM-dd").format(new Date()), 
+				typeFormats.get(typeFactoryFacade.getDateType()));
+		assertEquals("42.42", typeFormats.get(typeFactoryFacade.getDoubleType()));
+		assertEquals("42.42", typeFormats.get(typeFactoryFacade.getFloatType()));
+		assertEquals(
+				Locale.getDefault().toString(), 
+				typeFormats.get(typeFactoryFacade.getLocaleType()));
+		assertEquals("42", typeFormats.get(typeFactoryFacade.getLongType()));
+		assertEquals("a string", typeFormats.get(typeFactoryFacade.getStringType()));
+		assertEquals("a text", typeFormats.get(typeFactoryFacade.getTextType()));
+		assertEquals(12, typeFormats.get(typeFactoryFacade.getTimeType()).length());
+		assertEquals(
+				new SimpleDateFormat("yyyy-MM-dd").format(new Date()), 
+				typeFormats.get(typeFactoryFacade.getTimestampType()).substring(0, 10));
+		assertEquals(
+				TimeZone.getDefault().getID(), 
+				typeFormats.get(typeFactoryFacade.getTimezoneType()));
+		assertEquals("true", typeFormats.get(typeFactoryFacade.getTrueFalseType()));
+		assertEquals("true", typeFormats.get(typeFactoryFacade.getYesNoType()));
+	}
+	
 }
