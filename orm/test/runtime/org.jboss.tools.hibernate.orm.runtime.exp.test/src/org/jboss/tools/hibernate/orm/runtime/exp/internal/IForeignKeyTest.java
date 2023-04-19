f8b0364fbe5b6242daa8a794bab93f9dce90d71d
Koen Aers <koen.aers@gmail.com>
2023-04-19 08:42:52 +0200

    JBIDE-28757: Implement and use the generic adapter for IForeignKey in the experimental Hibernate runtime   - Add new test case 'org.jboss.tools.hibernate.orm.runtime.exp.internal.IForeignKeyTest#testGetReferencedTable()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.exp.test/src/org/jboss/tools/hibernate/orm/runtime/exp/internal/IForeignKeyTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.exp.test/src/org/jboss/tools/hibernate/orm/runtime/exp/internal/IForeignKeyTest.java
@@ -1,10 +1,14 @@
 package org.jboss.tools.hibernate.orm.runtime.exp.internal;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 import org.hibernate.mapping.ForeignKey;
+import org.hibernate.mapping.Table;
 import org.jboss.tools.hibernate.orm.runtime.exp.internal.util.GenericFacadeFactory;
+import org.jboss.tools.hibernate.runtime.common.IFacade;
 import org.jboss.tools.hibernate.runtime.spi.IForeignKey;
+import org.jboss.tools.hibernate.runtime.spi.ITable;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -25,4 +29,12 @@ public class IForeignKeyTest {
 		assertNotNull(foreignKeyTarget);
 	}
 
+	@Test
+	public void testGetReferencedTable() {
+		Table tableTarget = new Table("");
+		foreignKeyTarget.setReferencedTable(tableTarget);
+		ITable table = foreignKeyFacade.getReferencedTable();
+		assertSame(tableTarget, ((IFacade)table).getTarget());
+	}
+	
 }
