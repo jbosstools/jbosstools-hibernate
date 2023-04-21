091d8e4827b34efc0af92f30aec0bcf70d582e59
Koen Aers <koen.aers@gmail.com>
2023-04-21 10:12:12 +0200

    JBIDE-28763: Implement and use the generic adapter for IPrimaryKey in the experimental Hibernate runtime   - Add new test case 'org.jboss.tools.hibernate.orm.runtime.exp.internal.IPrimaryKeyTest#testGetColumns()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.exp.test/src/org/jboss/tools/hibernate/orm/runtime/exp/internal/IPrimaryKeyTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.exp.test/src/org/jboss/tools/hibernate/orm/runtime/exp/internal/IPrimaryKeyTest.java
@@ -5,11 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
+
 import org.hibernate.mapping.Column;
 import org.hibernate.mapping.PrimaryKey;
 import org.hibernate.mapping.Table;
 import org.hibernate.tool.orm.jbt.wrp.ColumnWrapper;
 import org.jboss.tools.hibernate.orm.runtime.exp.internal.util.GenericFacadeFactory;
+import org.jboss.tools.hibernate.runtime.common.IFacade;
 import org.jboss.tools.hibernate.runtime.spi.IColumn;
 import org.jboss.tools.hibernate.runtime.spi.IPrimaryKey;
 import org.junit.jupiter.api.BeforeEach;
@@ -49,6 +52,17 @@ public class IPrimaryKeyTest {
 		assertEquals(0, primaryKeyFacade.getColumnSpan());
 		primaryKeyTarget.addColumn(new Column());
 		assertEquals(1, primaryKeyFacade.getColumnSpan());
+	}
+	
+	@Test
+	public void testGetColumns() throws Exception {
+		Column columnTarget = new ColumnWrapper("foo");
+		assertTrue(primaryKeyFacade.getColumns().isEmpty());
+		primaryKeyTarget.addColumn(columnTarget);
+		List<IColumn> columnFacades = primaryKeyFacade.getColumns();
+		assertNotNull(columnFacades);
+		assertEquals(1, columnFacades.size());
+		assertSame(columnTarget, ((IFacade)columnFacades.get(0)).getTarget());
 	}
 	
 }
