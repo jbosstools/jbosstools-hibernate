d7a103b057788b2074b5ea6710ca673e3536b9fa
Koen Aers <koen.aers@gmail.com>
2023-04-24 12:52:14 +0200

    JBIDE-28758: Implement and use the generic adapter for IPOJOClass in the experimental Hibernate runtime   - Create new initial test class 'org.jboss.tools.hibernate.orm.runtime.exp.internal.IPojoClassTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.exp.test/src/org/jboss/tools/hibernate/orm/runtime/exp/internal/IPojoClassTest.java
@@ -0,0 +1,34 @@
+package org.jboss.tools.hibernate.orm.runtime.exp.internal;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.hibernate.mapping.PersistentClass;
+import org.hibernate.mapping.RootClass;
+import org.hibernate.tool.internal.export.java.Cfg2JavaTool;
+import org.hibernate.tool.internal.export.java.EntityPOJOClass;
+import org.hibernate.tool.internal.export.java.POJOClass;
+import org.hibernate.tool.orm.jbt.util.DummyMetadataBuildingContext;
+import org.jboss.tools.hibernate.orm.runtime.exp.internal.util.GenericFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IPOJOClass;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class IPojoClassTest {
+	
+	private POJOClass pojoClassTarget = null;
+	private IPOJOClass pojoClassFacade = null;
+	
+	@BeforeEach 
+	public void beforeEach() {
+		PersistentClass pc = new RootClass(DummyMetadataBuildingContext.INSTANCE);
+		pojoClassTarget = new EntityPOJOClass(pc, new Cfg2JavaTool());
+		pojoClassFacade = (IPOJOClass)GenericFacadeFactory.createFacade(IPOJOClass.class, pojoClassTarget);
+	}
+	
+	@Test
+	public void testConstruction() {
+		assertNotNull(pojoClassTarget);
+		assertNotNull(pojoClassFacade);
+	}
+
+}
