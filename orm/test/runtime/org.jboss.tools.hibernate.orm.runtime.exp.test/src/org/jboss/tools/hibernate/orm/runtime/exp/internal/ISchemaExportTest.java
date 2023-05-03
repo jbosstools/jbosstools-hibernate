05c74e1b4ab2a395feeb556d9379c0447648e423
Koen Aers <koen.aers@gmail.com>
2023-05-03 14:08:55 +0200

    JBIDE-28766: Implement and use the generic adapter for ISchemaExport in the experimental Hibernate runtime   - Add new test case 'org.jboss.tools.hibernate.orm.runtime.exp.internal.ISchemaExportTest#testCreate()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.exp.test/src/org/jboss/tools/hibernate/orm/runtime/exp/internal/ISchemaExportTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.exp.test/src/org/jboss/tools/hibernate/orm/runtime/exp/internal/ISchemaExportTest.java
@@ -1,11 +1,19 @@
 package org.jboss.tools.hibernate.orm.runtime.exp.internal;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.hibernate.tool.hbm2ddl.SchemaExport;
-import org.hibernate.tool.orm.jbt.wrp.WrapperFactory;
+import java.util.EnumSet;
+
+import org.hibernate.boot.Metadata;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.tool.orm.jbt.util.MockConnectionProvider;
+import org.hibernate.tool.orm.jbt.util.MockDialect;
+import org.hibernate.tool.orm.jbt.wrp.SchemaExportWrapper;
+import org.hibernate.tool.schema.TargetType;
 import org.jboss.tools.hibernate.orm.runtime.exp.internal.util.GenericFacadeFactory;
-import org.jboss.tools.hibernate.runtime.common.IFacade;
 import org.jboss.tools.hibernate.runtime.spi.ISchemaExport;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,14 +21,17 @@ import org.junit.jupiter.api.Test;
 public class ISchemaExportTest {
 	
 	private ISchemaExport schemaExportFacade = null;
-	private SchemaExport schemaExportTarget = null;
+	private TestSchemaExport schemaExportTarget = null;
 	
 	@BeforeEach
 	public void beforeEach() {
+		Configuration configuration = new Configuration();
+		configuration.setProperty(Environment.DIALECT, MockDialect.class.getName());
+		configuration.setProperty(Environment.CONNECTION_PROVIDER, MockConnectionProvider.class.getName());
+		schemaExportTarget = new TestSchemaExport(configuration);
 		schemaExportFacade = (ISchemaExport)GenericFacadeFactory.createFacade(
 				ISchemaExport.class, 
-				WrapperFactory.createSchemaExport());
-		schemaExportTarget = (SchemaExport)((IFacade)schemaExportFacade).getTarget();
+				schemaExportTarget);
 	}
 	
 	@Test
@@ -29,6 +40,27 @@ public class ISchemaExportTest {
 		assertNotNull(schemaExportTarget);
 	}
 	
+	@Test
+	public void testCreate() {
+		assertFalse(schemaExportTarget.created);
+		schemaExportFacade.create();
+		assertTrue(schemaExportTarget.created);
+	}
 	
+	private class TestSchemaExport extends SchemaExportWrapper {
+		
+		private boolean created = false;
 
+		public TestSchemaExport(Configuration configuration) {
+			super(configuration);
+		}
+		
+		@Override 
+		public void create(EnumSet<TargetType> targetTypes, Metadata metadata) {
+			created = true;
+		}
+
+		
+	}
+	
 }
