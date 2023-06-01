c8b06d9e2975cc8052545a92dac4bdf3285200ad
Koen Aers <koen.aers@gmail.com>
2023-06-01 16:08:34 +0200

    JBIDE-28756: Implement and use the generic adapter for IExporter in the experimental Hibernate runtime   - Add new test case 'org.jboss.tools.hibernate.orm.runtime.exp.internal.IExporterTest#testSetConfiguration()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.exp.test/src/org/jboss/tools/hibernate/orm/runtime/exp/internal/IExporterTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.exp.test/src/org/jboss/tools/hibernate/orm/runtime/exp/internal/IExporterTest.java
@@ -1,13 +1,23 @@
 package org.jboss.tools.hibernate.orm.runtime.exp.internal;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.lang.reflect.Field;
+import java.util.Properties;
+
+import org.hibernate.cfg.Configuration;
 import org.hibernate.tool.api.export.Exporter;
-import org.hibernate.tool.internal.export.ddl.DdlExporter;
+import org.hibernate.tool.api.export.ExporterConstants;
+import org.hibernate.tool.internal.export.cfg.CfgExporter;
+import org.hibernate.tool.orm.jbt.util.ConfigurationMetadataDescriptor;
 import org.hibernate.tool.orm.jbt.wrp.Wrapper;
 import org.hibernate.tool.orm.jbt.wrp.WrapperFactory;
 import org.jboss.tools.hibernate.orm.runtime.exp.internal.util.GenericFacadeFactory;
+import org.jboss.tools.hibernate.orm.runtime.exp.internal.util.NewFacadeFactory;
 import org.jboss.tools.hibernate.runtime.common.IFacade;
+import org.jboss.tools.hibernate.runtime.spi.IConfiguration;
 import org.jboss.tools.hibernate.runtime.spi.IExporter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,7 +31,7 @@ public class IExporterTest {
 	public void beforeEach() {
 		exporterFacade = (IExporter)GenericFacadeFactory.createFacade(
 				IExporter.class, 
-				WrapperFactory.createExporterWrapper(DdlExporter.class.getName()));
+				WrapperFactory.createExporterWrapper(CfgExporter.class.getName()));
 		Object exporterWrapper = ((IFacade)exporterFacade).getTarget();
 		exporterTarget = (Exporter)((Wrapper)exporterWrapper).getWrappedObject();
 	}
@@ -32,4 +42,23 @@ public class IExporterTest {
 		assertNotNull(exporterTarget);
 	}
 
+	@Test
+	public void testSetConfiguration() throws Exception {
+		Properties properties = new Properties();
+		IConfiguration configurationFacade = NewFacadeFactory.INSTANCE.createNativeConfiguration();
+		configurationFacade.setProperties(properties);
+		exporterFacade.setConfiguration(configurationFacade);	
+		assertSame(properties, ((CfgExporter)exporterTarget).getCustomProperties());
+		Object object = exporterTarget.getProperties().get(ExporterConstants.METADATA_DESCRIPTOR);
+		assertNotNull(object);
+		assertTrue(object instanceof ConfigurationMetadataDescriptor);
+		ConfigurationMetadataDescriptor configurationMetadataDescriptor = (ConfigurationMetadataDescriptor)object;
+		Field field = ConfigurationMetadataDescriptor.class.getDeclaredField("configuration");
+		field.setAccessible(true);
+		object = field.get(configurationMetadataDescriptor);
+		assertNotNull(object);
+		assertTrue(object instanceof Configuration);
+		assertSame(object, ((IFacade)configurationFacade).getTarget());
+	}
+	
 }
