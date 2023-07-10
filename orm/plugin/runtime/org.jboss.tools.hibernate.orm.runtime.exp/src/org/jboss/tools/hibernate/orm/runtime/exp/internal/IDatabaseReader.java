4e5bd5839dbbaa1101fd0d69b020e3b294254304
Koen Aers <koen.aers@gmail.com>
2023-07-10 14:05:46 +0200

    JBIDE-29047: Extract the generic functionality from the experimental runtime plugin   - Interface 'org.jboss.tools.hibernate.orm.runtime.exp.internal.IDatabaseReader' no longer implements 'org.jboss.tools.hibernate.orm.runtime.common.IFacade'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/plugin/runtime/org.jboss.tools.hibernate.orm.runtime.exp/src/org/jboss/tools/hibernate/orm/runtime/exp/internal/IDatabaseReader.java
+++ orm/plugin/runtime/org.jboss.tools.hibernate.orm.runtime.exp/src/org/jboss/tools/hibernate/orm/runtime/exp/internal/IDatabaseReader.java
@@ -3,10 +3,9 @@ package org.jboss.tools.hibernate.orm.runtime.exp.internal;
 import java.util.List;
 import java.util.Map;
 
-import org.jboss.tools.hibernate.orm.runtime.common.IFacade;
 import org.jboss.tools.hibernate.runtime.spi.ITable;
 
-public interface IDatabaseReader extends IFacade {
+public interface IDatabaseReader {
 	
 	Map<String, List<ITable>> collectDatabaseTables();
 
