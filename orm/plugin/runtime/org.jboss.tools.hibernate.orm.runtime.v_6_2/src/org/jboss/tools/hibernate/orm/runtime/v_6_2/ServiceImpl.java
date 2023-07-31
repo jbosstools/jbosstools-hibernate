487ce5bd45b6cbd598b80a2788f70d96b9376f17
Koen Aers <koen.aers@gmail.com>
2023-07-31 12:57:13 +0200

    JBIDE-29046: Move the existing Hibernate 6.2 runtime plugin and make use of the new JBoss Tools adapter   - Add new test case 'org.jboss.tools.hibernate.orm.runtime.v_6_2.ServiceImplTest#testNewRootClass()'   - Implement method 'org.jboss.tools.hibernate.orm.runtime.v_6_2.ServiceImpl#newRootClass()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/plugin/runtime/org.jboss.tools.hibernate.orm.runtime.v_6_2/src/org/jboss/tools/hibernate/orm/runtime/v_6_2/ServiceImpl.java
+++ orm/plugin/runtime/org.jboss.tools.hibernate.orm.runtime.v_6_2/src/org/jboss/tools/hibernate/orm/runtime/v_6_2/ServiceImpl.java
@@ -345,8 +345,9 @@ public class ServiceImpl implements IService {
 
 	@Override
 	public IPersistentClass newRootClass() {
-		// TODO Auto-generated method stub
-		return null;
+		return (IPersistentClass)GenericFacadeFactory.createFacade(
+				IPersistentClass.class, 
+				WrapperFactory.createRootClassWrapper());
 	}
 
 	@Override
