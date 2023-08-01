3e7c8246fafb6928fb88d4a03f7920a846ae8055
Koen Aers <koen.aers@gmail.com>
2023-08-01 13:23:29 +0200

    JBIDE-29046: Move the existing Hibernate 6.2 runtime plugin and make use of the new JBoss Tools adapter   - Add test case 'org.jboss.tools.hibernate.orm.runtime.v_6_2.VersionTest#testRuntimeVersion()'   - Contribute 'org.jboss.tools.hibernate.orm.runtime.v_6_2.ServiceImpl' to the 'org.jboss.tools.hibernate.runtime.spi.services' extension point   - Make sure contributed version is '6.2.x' to avoid confusion with '6.2'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.v_6_2.test/src/org/jboss/tools/hibernate/orm/runtime/v_6_2/VersionTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.v_6_2.test/src/org/jboss/tools/hibernate/orm/runtime/v_6_2/VersionTest.java
@@ -1,7 +1,9 @@
 package org.jboss.tools.hibernate.orm.runtime.v_6_2;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
+import org.jboss.tools.hibernate.runtime.spi.RuntimeServiceManager;
 import org.junit.jupiter.api.Test;
 
 public class VersionTest {
@@ -16,4 +18,8 @@ public class VersionTest {
 		assertEquals("6.2.7-SNAPSHOT", org.hibernate.tool.api.version.Version.CURRENT_VERSION);
 	}
 	
+	@Test 
+	public void testRuntimeVersion() {
+		assertSame(RuntimeServiceManager.getInstance().findService("6.2.x").getClass(), ServiceImpl.class);
+	}
 }
