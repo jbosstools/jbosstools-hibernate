729ac3963c8686748a8f868c659eed3131177bc4
Koen Aers <koen.aers@gmail.com>
2023-08-01 10:51:54 +0200

    JBIDE-29046: Move the existing Hibernate 6.2 runtime plugin and make use of the new JBoss Tools adapter   - Add new test case 'org.jboss.tools.hibernate.orm.runtime.v_6_2.ServiceImplTest#testGetJPAMappingFilePaths()'   - Implement method 'org.jboss.tools.hibernate.orm.runtime.v_6_2.ServiceImpl#getJPAMappingFilePaths(String,EntityResolver)'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.v_6_2.test/src/org/jboss/tools/hibernate/orm/runtime/v_6_2/ServiceImplTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.v_6_2.test/src/org/jboss/tools/hibernate/orm/runtime/v_6_2/ServiceImplTest.java
@@ -564,4 +564,10 @@ public class ServiceImplTest {
 		assertTrue(service.isInitialized(new Object()));
 	}
 	
+	@Test
+	public void testGetJPAMappingFilePaths() {
+		List<String> result = service.getJPAMappingFilePaths("test", null);
+		assertEquals(0, result.size());
+	}
+	
 }
