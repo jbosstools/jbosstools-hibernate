cd1f0126b883df5dec4bbfd1b126dd706d33c2ed
Koen Aers <koen.aers@gmail.com>
2023-08-02 13:26:18 +0200

    JBIDE-29046: Move the existing Hibernate 6.2 runtime plugin and make use of the new JBoss Tools adapter  - Remove unneeded test fragment 'org.jboss.tools.hibernate.runtime.v_6_2.test'  - Remove obsolete plugin 'org.jboss.tools.hibernate.runtime.v_6_2'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_6_2/src/org/hibernate/tool/hbm2x/HibernateConfigurationExporter.java
+++
@@ -1,5 +0,0 @@
-package org.hibernate.tool.hbm2x;
-
-import org.hibernate.tool.internal.export.cfg.CfgExporter;
-
-public class HibernateConfigurationExporter extends CfgExporter {}
--- orm/plugin/runtime/org.jboss.tools.hibernate.runtime.v_6_2/src/org/hibernate/tool/hbm2x/POJOExporter.java
+++
@@ -1,7 +0,0 @@
-package org.hibernate.tool.hbm2x;
-
-import org.hibernate.tool.internal.export.java.JavaExporter;
-
-public class POJOExporter extends JavaExporter {
-
-}
