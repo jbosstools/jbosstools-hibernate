7e69c242af8cacb57d3707f4e2c064fd172de9b4
Koen Aers <koen.aers@gmail.com>
2023-08-07 12:18:15 +0200

    JBIDE-29054: Create a Hibernate 6.3 runtime plugin   - Create new initial test class 'org.jboss.tools.hibernate.orm.runtime.v_6_3.ServiceImplTest'   - Create new initial implementation class 'org.jboss.tools.hibernate.orm.runtime.v_6_3.ServiceImpl'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.v_6_3.test/src/org/jboss/tools/hibernate/orm/runtime/v_6_3/ServiceImplTest.java
@@ -0,0 +1,22 @@
+package org.jboss.tools.hibernate.orm.runtime.v_6_3;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ServiceImplTest {
+
+	private ServiceImpl service = null;
+	
+	@BeforeEach
+	public void beforeEach() {
+		service = new ServiceImpl();
+	}
+	
+	@Test
+	public void testConstruction() {
+		assertNotNull(service);
+	}
+	
+}
