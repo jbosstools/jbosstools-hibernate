a29bc89cbfbc16b5e45e9107ab9a038a116a29bd
Koen Aers <koen.aers@gmail.com>
2023-08-09 08:46:29 +0200

    JBIDE-29054: Create a Hibernate 6.3 runtime plugin   - Contribute 'org.jboss.tools.hibernate.orm.runtime.v_6_3.ServiceImpl' to extension point 'org.jboss.tools.hibernate.runtime.spi.services' with version identifier '6.3 Preview'   - Add test case 'org.jboss.tools.hibernate.orm.runtime.v_6_3.VersionTest#testRuntimeVersion()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.v_6_3.test/src/org/jboss/tools/hibernate/orm/runtime/v_6_3/VersionTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.v_6_3.test/src/org/jboss/tools/hibernate/orm/runtime/v_6_3/VersionTest.java
@@ -1,7 +1,9 @@
 package org.jboss.tools.hibernate.orm.runtime.v_6_3;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
+import org.jboss.tools.hibernate.runtime.spi.RuntimeServiceManager;
 import org.junit.jupiter.api.Test;
 
 public class VersionTest {
@@ -16,4 +18,8 @@ public class VersionTest {
 		assertEquals("6.3.0-SNAPSHOT", org.hibernate.tool.api.version.Version.CURRENT_VERSION);
 	}
 	
+	@Test 
+	public void testRuntimeVersion() {
+		assertSame(RuntimeServiceManager.getInstance().findService("6.3 Preview").getClass(), ServiceImpl.class);
+	}
 }
