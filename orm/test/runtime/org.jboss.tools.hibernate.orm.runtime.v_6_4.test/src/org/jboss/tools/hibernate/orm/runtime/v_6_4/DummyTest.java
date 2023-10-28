7c09812abe3bcffa5d4b12ba750fd827302bd6f4
Koen Aers <koen.aers@gmail.com>
2023-10-28 13:27:48 +0200

    JBIDE-29093: Create a Hibernate runtime plugin for the 6.4 releases   - Initial creation of test fragment 'org.jboss.tools.hibernate.orm.runtime.v_6_4.test'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.v_6_4.test/src/org/jboss/tools/hibernate/orm/runtime/v_6_4/DummyTest.java
@@ -0,0 +1,14 @@
+package org.jboss.tools.hibernate.orm.runtime.v_6_4;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+	
+	@Test
+	public void testIt() {
+		assertFalse(false);
+	}
+
+}
