55308cf561098e0268100cec356df4ca81244d7f
Koen Aers <koen.aers@gmail.com>
2023-10-31 08:32:38 +0100

    JBIDE-29093: Create a Hibernate runtime plugin for the 6.4 releases   - Add new test class 'org.jboss.tools.hibernate.orm.runtime.v_6_4.IForeignKeyTest'   - Add new test class 'org.jboss.tools.hibernate.orm.runtime.v_6_4.IGenericExporterTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.v_6_4.test/src/org/jboss/tools/hibernate/orm/runtime/v_6_4/IGenericExporterTest.java
@@ -0,0 +1,72 @@
+package org.jboss.tools.hibernate.orm.runtime.v_6_4;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.hibernate.tool.api.export.ExporterConstants;
+import org.hibernate.tool.internal.export.common.GenericExporter;
+import org.hibernate.tool.orm.jbt.wrp.GenericExporterWrapperFactory;
+import org.hibernate.tool.orm.jbt.wrp.Wrapper;
+import org.jboss.tools.hibernate.orm.runtime.common.GenericFacadeFactory;
+import org.jboss.tools.hibernate.orm.runtime.common.IFacade;
+import org.jboss.tools.hibernate.runtime.spi.IGenericExporter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class IGenericExporterTest {
+	
+	private GenericExporter genericExporterTarget = null;
+	private IGenericExporter genericExporterFacade = null;
+	
+	@BeforeEach
+	public void beforeEach() {
+		genericExporterFacade = (IGenericExporter)GenericFacadeFactory.createFacade(
+				IGenericExporter.class, 
+				GenericExporterWrapperFactory.create(new GenericExporter()));
+		Object genericExporterWrapper = ((IFacade)genericExporterFacade).getTarget();
+		genericExporterTarget = (GenericExporter)((Wrapper)genericExporterWrapper).getWrappedObject();
+	}
+	
+	@Test
+	public void testConstruction() {
+		assertNotNull(genericExporterTarget);
+		assertNotNull(genericExporterFacade);
+	}
+
+	@Test
+	public void testSetFilePattern() {
+		assertNull(genericExporterTarget.getProperties().get(ExporterConstants.FILE_PATTERN));
+		genericExporterFacade.setFilePattern("foobar");
+		assertEquals("foobar", genericExporterTarget.getProperties().get(ExporterConstants.FILE_PATTERN));
+	}
+	
+	@Test
+	public void testSetTemplate() {
+		assertNull(genericExporterTarget.getProperties().get(ExporterConstants.TEMPLATE_NAME));
+		genericExporterFacade.setTemplateName("barfoo");
+		assertEquals("barfoo", genericExporterTarget.getProperties().get(ExporterConstants.TEMPLATE_NAME));
+	}
+	
+	@Test
+	public void testSetForEach() {
+		assertNull(genericExporterTarget.getProperties().get(ExporterConstants.FOR_EACH));
+		genericExporterFacade.setForEach("foobar");
+		assertEquals("foobar", genericExporterTarget.getProperties().get(ExporterConstants.FOR_EACH));
+	}
+	
+	@Test
+	public void testGetFilePattern() {
+		assertNull(genericExporterFacade.getFilePattern());
+		genericExporterTarget.getProperties().put(ExporterConstants.FILE_PATTERN, "foobar");
+		assertEquals("foobar", genericExporterFacade.getFilePattern());
+	}
+	
+	@Test
+	public void testGetTemplateName() {
+		assertNull(genericExporterFacade.getTemplateName());
+		genericExporterTarget.getProperties().put(ExporterConstants.TEMPLATE_NAME, "foobar");
+		assertEquals("foobar", genericExporterFacade.getTemplateName());
+	}
+	
+}
