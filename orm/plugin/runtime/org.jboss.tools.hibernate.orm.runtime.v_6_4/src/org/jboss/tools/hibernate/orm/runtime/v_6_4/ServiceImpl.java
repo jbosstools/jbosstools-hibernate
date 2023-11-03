907ee39b140b549a2e7ea35b58baac31ecc86945
Koen Aers <koen.aers@gmail.com>
2023-11-03 11:57:00 +0100

    JBIDE-29093: Create a Hibernate runtime plugin for the 6.4 releases   - Create new test case 'ServiceImplTest#testNewDefaultConfiguration()'   - Add implementation method 'ServiceImpl#newDefaultConfiguration()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/plugin/runtime/org.jboss.tools.hibernate.orm.runtime.v_6_4/src/org/jboss/tools/hibernate/orm/runtime/v_6_4/ServiceImpl.java
+++ orm/plugin/runtime/org.jboss.tools.hibernate.orm.runtime.v_6_4/src/org/jboss/tools/hibernate/orm/runtime/v_6_4/ServiceImpl.java
@@ -1,5 +1,15 @@
 package org.jboss.tools.hibernate.orm.runtime.v_6_4;
 
+import org.hibernate.tool.orm.jbt.wrp.WrapperFactory;
+import org.jboss.tools.hibernate.orm.runtime.common.GenericFacadeFactory;
+import org.jboss.tools.hibernate.runtime.spi.IConfiguration;
+
 public class ServiceImpl {
+
+	public IConfiguration newDefaultConfiguration() {
+		return (IConfiguration)GenericFacadeFactory.createFacade(
+				IConfiguration.class, 
+				WrapperFactory.createNativeConfigurationWrapper());
+	}
 
 }
