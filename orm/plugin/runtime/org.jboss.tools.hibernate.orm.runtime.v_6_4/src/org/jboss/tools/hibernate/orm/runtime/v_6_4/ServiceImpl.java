ca4b32462e6877a82586beb255ed7461f816155a
Koen Aers <koen.aers@gmail.com>
2023-11-03 12:23:36 +0100

    JBIDE-29093: Create a Hibernate runtime plugin for the 6.4 releases   - Create new test case 'ServiceImplTest#testNewJpaConfiguration()'   - Add implementation method 'ServiceImpl#newJpaConfiguration(String,String,Map)'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/plugin/runtime/org.jboss.tools.hibernate.orm.runtime.v_6_4/src/org/jboss/tools/hibernate/orm/runtime/v_6_4/ServiceImpl.java
+++ orm/plugin/runtime/org.jboss.tools.hibernate.orm.runtime.v_6_4/src/org/jboss/tools/hibernate/orm/runtime/v_6_4/ServiceImpl.java
@@ -1,5 +1,7 @@
 package org.jboss.tools.hibernate.orm.runtime.v_6_4;
 
+import java.util.Map;
+
 import org.hibernate.tool.orm.jbt.wrp.WrapperFactory;
 import org.jboss.tools.hibernate.orm.runtime.common.GenericFacadeFactory;
 import org.jboss.tools.hibernate.runtime.spi.IConfiguration;
@@ -14,6 +16,15 @@ public class ServiceImpl {
 
 	public IConfiguration newAnnotationConfiguration() {
 		return newDefaultConfiguration();
+	}
+
+	public IConfiguration newJpaConfiguration(
+			String entityResolver, 
+			String persistenceUnit,
+			Map<Object, Object> overrides) {
+		return (IConfiguration)GenericFacadeFactory.createFacade(
+				IConfiguration.class, 
+				WrapperFactory.createJpaConfigurationWrapper(persistenceUnit, overrides));
 	}
 
 }
