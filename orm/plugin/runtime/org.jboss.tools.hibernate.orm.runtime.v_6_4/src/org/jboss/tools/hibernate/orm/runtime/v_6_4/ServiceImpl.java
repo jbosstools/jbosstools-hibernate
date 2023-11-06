4b86278c94e61e97165ab89f0f60984ea4e34c03
Koen Aers <koen.aers@gmail.com>
2023-11-06 22:44:27 +0100

    JBIDE-29093: Create a Hibernate runtime plugin for the 6.4 releases   - Create new test case 'ServiceImplTest#testNewOneToOne()'   - Add implementation method 'ServiceImpl#newOneToOne(IPersistentClass)'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/plugin/runtime/org.jboss.tools.hibernate.orm.runtime.v_6_4/src/org/jboss/tools/hibernate/orm/runtime/v_6_4/ServiceImpl.java
+++ orm/plugin/runtime/org.jboss.tools.hibernate.orm.runtime.v_6_4/src/org/jboss/tools/hibernate/orm/runtime/v_6_4/ServiceImpl.java
@@ -288,6 +288,12 @@ public class ServiceImpl {
 				WrapperFactory.createOneToManyWrapper(((IFacade)persistentClass).getTarget()));
 	}
 
+	public IValue newOneToOne(IPersistentClass persistentClass) {
+		return (IValue)GenericFacadeFactory.createFacade(
+				IValue.class, 
+				WrapperFactory.createOneToOneWrapper(((IFacade)persistentClass).getTarget()));
+	}
+
 	private ServiceRegistry buildServiceRegistry(Properties properties) {
 		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder();
 		builder.applySettings(properties);
