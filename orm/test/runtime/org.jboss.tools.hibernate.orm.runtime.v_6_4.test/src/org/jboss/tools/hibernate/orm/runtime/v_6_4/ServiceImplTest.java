bf205bbe3c3406bd7fb1997e2fbad95807cd0234
Koen Aers <koen.aers@gmail.com>
2023-11-15 09:58:39 +0100

    JBIDE-29093: Create a Hibernate runtime plugin for the 6.4 releases   - Add method 'ServiceImplTest#beforeAll()' to make sure the tests run also in Eclipse
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.v_6_4.test/src/org/jboss/tools/hibernate/orm/runtime/v_6_4/ServiceImplTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.v_6_4.test/src/org/jboss/tools/hibernate/orm/runtime/v_6_4/ServiceImplTest.java
@@ -14,6 +14,7 @@ import java.sql.Statement;
 import java.util.List;
 import java.util.Properties;
 
+import org.h2.Driver;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.DefaultNamingStrategy;
@@ -79,6 +80,7 @@ import org.jboss.tools.hibernate.runtime.spi.ITable;
 import org.jboss.tools.hibernate.runtime.spi.ITableFilter;
 import org.jboss.tools.hibernate.runtime.spi.ITypeFactory;
 import org.jboss.tools.hibernate.runtime.spi.IValue;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -86,6 +88,11 @@ public class ServiceImplTest {
 
 	private ServiceImpl service = null;
 	
+	@BeforeAll
+	public static void beforeAll() throws Exception {
+		DriverManager.registerDriver(new Driver());		
+	}
+
 	@BeforeEach
 	public void beforeEach() {
 		service = new ServiceImpl();
