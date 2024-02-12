f965132bf4e24c22e43a3cb77b60abe9a4db41f4
Koen Aers <koen.aers@gmail.com>
2024-02-12 17:18:46 +0200

    JBIDE-29162: Create a Hibernate runtime plugin for the 6.5 releases   - Add new test class 'org.jboss.tools.hibernate.orm.runtime.v_6_5.IJoinTest'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.v_6_5.test/src/org/jboss/tools/hibernate/orm/runtime/v_6_5/IJoinTest.java
@@ -0,0 +1,59 @@
+package org.jboss.tools.hibernate.orm.runtime.v_6_5;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Iterator;
+
+import org.hibernate.mapping.Join;
+import org.hibernate.mapping.PersistentClass;
+import org.hibernate.mapping.Property;
+import org.hibernate.tool.orm.jbt.wrp.Wrapper;
+import org.hibernate.tool.orm.jbt.wrp.WrapperFactory;
+import org.jboss.tools.hibernate.orm.runtime.common.GenericFacadeFactory;
+import org.jboss.tools.hibernate.orm.runtime.common.IFacade;
+import org.jboss.tools.hibernate.runtime.spi.IJoin;
+import org.jboss.tools.hibernate.runtime.spi.IPersistentClass;
+import org.jboss.tools.hibernate.runtime.spi.IProperty;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class IJoinTest {
+	
+	private IJoin joinFacade = null;
+	private Join joinTarget = null;
+	
+	@BeforeEach
+	public void beforeEach() {
+		IPersistentClass persistentClassFacade = (IPersistentClass)GenericFacadeFactory.createFacade(
+				IPersistentClass.class, 
+				WrapperFactory.createRootClassWrapper());
+		PersistentClass persistentClassTarget = 
+				(PersistentClass)((Wrapper)((IFacade)persistentClassFacade).getTarget()).getWrappedObject();
+		joinTarget = new Join();
+		persistentClassTarget.addJoin(joinTarget);
+		joinFacade = persistentClassFacade.getJoinIterator().next();
+	}
+	
+	@Test
+	public void testConstruction() {
+		assertNotNull(joinFacade);
+		assertNotNull(joinTarget);
+		assertSame(((IFacade)joinFacade).getTarget(), joinTarget);
+	}
+	
+	@Test
+	public void testGetPropertyIterator() {
+		Iterator<IProperty> propertyFacadeIterator = joinFacade.getPropertyIterator();
+		assertFalse(propertyFacadeIterator.hasNext());
+		Property propertyTarget = new Property();
+		joinTarget.addProperty(propertyTarget);
+		propertyFacadeIterator = joinFacade.getPropertyIterator();
+		assertTrue(propertyFacadeIterator.hasNext());
+		IProperty propertyFacade = propertyFacadeIterator.next();
+		assertSame(((IFacade)propertyFacade).getTarget(), propertyTarget);
+	}
+
+}
