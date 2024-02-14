61d74e5214d3bbabe3445175f471f41be870a5dd
Koen Aers <koen.aers@gmail.com>
2024-02-14 17:52:39 +0200

    JBIDE-29162: Create a Hibernate runtime plugin for the 6.5 releases   - Add new test case 'org.jboss.tools.hibernate.orm.runtime.v_6_5.ServiceImplTest#testNewHibernateMappingExporter()'   - Complete implementation method 'org.jboss.tools.hibernate.orm.runtime.v_6_5.ServiceImpl#newHibernateMappingExporter(IConfiguration,File,File)'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.v_6_5.test/src/org/jboss/tools/hibernate/orm/runtime/v_6_5/ServiceImplTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.v_6_5.test/src/org/jboss/tools/hibernate/orm/runtime/v_6_5/ServiceImplTest.java
@@ -2,12 +2,22 @@ package org.jboss.tools.hibernate.orm.runtime.v_6_5;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.File;
+
+import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.tool.api.export.ExporterConstants;
+import org.hibernate.tool.internal.export.hbm.HbmExporter;
 import org.hibernate.tool.orm.jbt.util.JpaConfiguration;
+import org.hibernate.tool.orm.jbt.util.MetadataHelper;
+import org.hibernate.tool.orm.jbt.util.MockConnectionProvider;
+import org.hibernate.tool.orm.jbt.util.MockDialect;
 import org.jboss.tools.hibernate.orm.runtime.common.IFacade;
 import org.jboss.tools.hibernate.runtime.spi.IConfiguration;
+import org.jboss.tools.hibernate.runtime.spi.IHibernateMappingExporter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -48,6 +58,22 @@ public class ServiceImplTest {
 		assertTrue(target instanceof JpaConfiguration);
 		assertEquals("test", ((JpaConfiguration)target).getPersistenceUnit());
 		
+	}
+	
+	@Test
+	public void testNewHibernateMappingExporter() {
+		IConfiguration configuration = service.newDefaultConfiguration();
+		configuration.setProperty(AvailableSettings.DIALECT, MockDialect.class.getName());
+		configuration.setProperty(AvailableSettings.CONNECTION_PROVIDER, MockConnectionProvider.class.getName());
+		File file = new File("");
+		IHibernateMappingExporter hibernateMappingExporter = 
+				service.newHibernateMappingExporter(configuration, file);
+		HbmExporter hmee = 
+				(HbmExporter)((IFacade)hibernateMappingExporter).getTarget();
+		assertSame(file, hmee.getProperties().get(ExporterConstants.OUTPUT_FILE_NAME));
+		assertSame(
+				MetadataHelper.getMetadata((Configuration)((IFacade)configuration).getTarget()),
+				hmee.getMetadata());
 	}
 	
 }
