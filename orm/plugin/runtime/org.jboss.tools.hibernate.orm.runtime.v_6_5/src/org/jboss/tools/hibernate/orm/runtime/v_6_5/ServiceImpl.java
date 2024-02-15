c52ab0f2ab7b65adb6482783d4d9aabd2105bcc3
Koen Aers <koen.aers@gmail.com>
2024-02-15 13:46:22 +0200

    JBIDE-29162: Create a Hibernate runtime plugin for the 6.5 releases   - Add new test case 'org.jboss.tools.hibernate.orm.runtime.v_6_5.ServiceImplTest#testCreateCfgExporter()'   - Complete implementation method 'org.jboss.tools.hibernate.orm.runtime.v_6_5.ServiceImpl#createCfgExporter()'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/plugin/runtime/org.jboss.tools.hibernate.orm.runtime.v_6_5/src/org/jboss/tools/hibernate/orm/runtime/v_6_5/ServiceImpl.java
+++ orm/plugin/runtime/org.jboss.tools.hibernate.orm.runtime.v_6_5/src/org/jboss/tools/hibernate/orm/runtime/v_6_5/ServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import org.hibernate.tool.internal.export.cfg.CfgExporter;
 import org.hibernate.tool.orm.jbt.wrp.WrapperFactory;
 import org.jboss.tools.hibernate.orm.runtime.common.GenericFacadeFactory;
 import org.jboss.tools.hibernate.orm.runtime.common.IFacade;
@@ -100,11 +101,10 @@ public class ServiceImpl implements IService {
 
 	@Override
 	public IExporter createCfgExporter() {
-		// TODO Auto-generated method stub
-		return null;
+		return createExporter(CfgExporter.class.getName());
 	}
-
-	@Override
+	
+ 	@Override
 	public IArtifactCollector newArtifactCollector() {
 		// TODO Auto-generated method stub
 		return null;
