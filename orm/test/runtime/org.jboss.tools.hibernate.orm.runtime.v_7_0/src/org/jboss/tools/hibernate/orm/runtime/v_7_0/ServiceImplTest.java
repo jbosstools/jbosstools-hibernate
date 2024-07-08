90cb7d330d64cf00853a958724c513d4b110d986
Koen Aers <koen.aers@gmail.com>
2024-07-08 07:11:48 +0300

    JBIDE-29201: Create a runtime plugin for Hibernate 7.0   - Add new test method 'org.jboss.tools.hibernate.orm.runtime.v_7_0.ServiceImplTest#testGetClassWithoutInitializingProxy()'   - Implement method 'org.jboss.tools.hibernate.orm.runtime.v_7_0.ServiceImpl#getClassWithoutInitializingProxy(Object)'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.v_7_0/src/org/jboss/tools/hibernate/orm/runtime/v_7_0/ServiceImplTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.v_7_0/src/org/jboss/tools/hibernate/orm/runtime/v_7_0/ServiceImplTest.java
@@ -606,4 +606,11 @@ public class ServiceImplTest {
 		assertEquals(0, result.size());
 	}
 	
+	@Test
+	public void testGetClassWithoutInitializingProxy() {
+		assertSame(
+				Object.class, 
+				service.getClassWithoutInitializingProxy(new Object()));
+	}
+	
 }
