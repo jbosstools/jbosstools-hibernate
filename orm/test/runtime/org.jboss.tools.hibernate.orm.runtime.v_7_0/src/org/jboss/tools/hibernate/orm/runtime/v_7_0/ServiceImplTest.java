771b768b01b527dce1a5d0247e1e674bb8f8957e
Koen Aers <koen.aers@gmail.com>
2024-07-08 07:01:11 +0300

    JBIDE-29201: Create a runtime plugin for Hibernate 7.0   - Add new test method 'org.jboss.tools.hibernate.orm.runtime.v_7_0.ServiceImplTest#testIsInitialized()'   - Implement method 'org.jboss.tools.hibernate.orm.runtime.v_7_0.ServiceImpl#isInitialized(Object)'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.v_7_0/src/org/jboss/tools/hibernate/orm/runtime/v_7_0/ServiceImplTest.java
+++ orm/test/runtime/org.jboss.tools.hibernate.orm.runtime.v_7_0/src/org/jboss/tools/hibernate/orm/runtime/v_7_0/ServiceImplTest.java
@@ -595,4 +595,9 @@ public class ServiceImplTest {
 				((Wrapper)((IFacade)specialRootClass.getProperty()).getTarget()).getWrappedObject());
 	}
 	
+	@Test
+	public void testIsInitialized() {
+		assertTrue(service.isInitialized(new Object()));
+	}
+	
 }
