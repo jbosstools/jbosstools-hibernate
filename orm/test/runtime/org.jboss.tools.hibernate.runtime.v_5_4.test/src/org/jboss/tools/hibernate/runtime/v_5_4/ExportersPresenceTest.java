32dd97fac481b16033ae78395ffa051f9c8a126c
Koen Aers <koen.aers@gmail.com>
2024-08-14 17:53:29 +0300

    JBIDE-29221: Make sure that the Exporter classes needed by plugin 'org.hibernate.eclipse.console' are visible and available   - Handle the case of 'org.hibernate.tool.hbm2x.Hbm2DDLExporter'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/test/runtime/org.jboss.tools.hibernate.runtime.v_5_4.test/src/org/jboss/tools/hibernate/runtime/v_5_4/ExportersPresenceTest.java
@@ -0,0 +1,21 @@
+package org.jboss.tools.hibernate.runtime.v_5_4;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class ExportersPresenceTest {
+	
+	@Test
+	public void testDdlExporter() {
+		try {
+			ClassLoader cl = getClass().getClassLoader();
+			Class<?> ddlExporterClass = cl.loadClass("org.hibernate.tool.hbm2x.Hbm2DDLExporter");
+			assertNotNull(ddlExporterClass);
+		} catch (Throwable t) {
+			fail(t);
+		}
+	}
+
+}
